cf8eaa86850285547a0c44a8ac6bf98dcf6ce96e
Michele Clamp <michele@sanger.ac.uk>
1999-05-24 17:06:26 +0000

    Changed naming in tables.sql. Added conversion scripts

--- sql/queries.sql
+++ sql/queries.sql
@@ -22,40 +22,40 @@ where    exon_bridge.exon = exon.id                     AND
          transcript.geneid = 'dJ40E16.02584.GENSCAN.0';
          
 # how many genscan predictions are there?
-select   homolset,count(*) 
-from     homol
-where    homolset != '' 
-group by homolset;
+select   set,count(*) 
+from     feature
+where    featureset != '' 
+group by featureset;
 
 # how many times does a blastp match hit a contig
 select    id,count(*) 
-from      homol 
+from      feature 
 where     analysis = 'blastp_swir' 
 group by  id; 
 
 # which database hits hit different contigs - this prints ALL links
 # this should have a group by contig here somewhere
 select    p1.contig,p2.contig,p2.id 
-from      homol as p1, homol as p2 
+from      feature as p1, feature as p2 
 where     p1.contig != p2.contig AND 
           p1.id = p2.id;
 
 # this might be better
 # just prints out different contigs
-select    count(*),homol.id,homol.contig 
-from      homol 
+select    count(*),feature.id,feature.contig 
+from      feature 
 where     id = 'WP:F15G9.4A' 
-group by  homol.contig;
+group by  feature.contig;
 
-# groups by contigs for blastp hits for all homols
-select    count(*),homol.id,homol.contig 
-from      homol 
+# groups by contigs for blastp hits for all features
+select    count(*),feature.id,feature.contig 
+from      feature 
 where     analysis = 'blastp_swir'
-group by  homol.contig;
+group by  feature.contig;
 
 # similar to above but counting the number of contig-contig links
 select    p1.id,count(*) 
-from      homol as p1, homol as p2 
+from      feature as p1, feature as p2 
 where     p1.contig != p2.contig AND 
           p1.id = p2.id 
 group by  p1.id;
@@ -63,7 +63,7 @@ group by  p1.id;
 # show all hits between different contigs for id XL28SR
 # n.b. brings up hits twice - don't know how to get around this.
 select    p1.* 
-from      homol as p1, homol as p2 
+from      feature as p1, feature as p2 
 where     p1.contig != p2.contig AND 
           p1.id = p2.id          AND 
           p1.id = 'XL28SR';
@@ -82,43 +82,43 @@ where     exon_bridge.transcript = transcript.id
 group by  transcript.geneid;
 
 
-# find homol overlaps with exon dJ437I16.00294.GENSCAN.0.1
-select    homol.* 
-from      homol,exon 
-where     homol.analysis != ''                   AND
+# find feature overlaps with exon dJ437I16.00294.GENSCAN.0.1
+select    feature.* 
+from      feature,exon 
+where     feature.analysis != ''                   AND
           exon.id = 'dJ437I16.00294.GENSCAN.0.1' AND 
-          homol.strand = exon.strand             AND 
-          homol.start >= exon.start              AND 
-          homol.end   <= exon.end                AND 
-          homol.contig = exon.contig;
+          feature.strand = exon.strand             AND 
+          feature.start >= exon.start              AND 
+          feature.end   <= exon.end                AND 
+          feature.contig = exon.contig;
 
-# or to just report the different homol ids and number of hits
-select    homol.id,count(*)
-from      homol,exon 
-where     homol.analysis != ''                   AND
+# or to just report the different feature ids and number of hits
+select    feature.id,count(*)
+from      feature,exon 
+where     feature.analysis != ''                   AND
           exon.id = 'dJ437I16.00294.GENSCAN.0.1' AND 
-          homol.strand = exon.strand             AND 
-          homol.start >= exon.start              AND 
-          homol.end   <= exon.end                AND 
-          homol.contig = exon.contig
-group by  homol.id;
+          feature.strand = exon.strand             AND 
+          feature.start >= exon.start              AND 
+          feature.end   <= exon.end                AND 
+          feature.contig = exon.contig
+group by  feature.id;
 
 
-# find all homol hits with all exons
+# find all feature hits with all exons
 # note - can't find how to order these by exon id
-select    exon.id,homol.id,count(*)
-from      homol,exon 
-where     homol.analysis != ''                   AND
-          homol.strand = exon.strand             AND 
-          homol.start >= exon.start              AND 
-          homol.end   <= exon.end                AND 
-          homol.contig = exon.contig
-group by  homol.id;
+select    exon.id,feature.id,count(*)
+from      feature,exon 
+where     feature.analysis != ''                   AND
+          feature.strand = exon.strand             AND 
+          feature.start >= exon.start              AND 
+          feature.end   <= exon.end                AND 
+          feature.contig = exon.contig
+group by  feature.id;
 
 
-# find all homol hits that overlap with genscan predictions
+# find all feature hits that overlap with genscan predictions
 select    p1.id,p2.id,count(*) 
-from      homol as p1, homol as p2 
+from      feature as p1, feature as p2 
 where     p1.contig = p2.contig      AND
           p1.analysis != ''          AND
 	  p2.analysis  = ''          AND
--- sql/tables.sql
+++ sql/tables.sql
@@ -1,25 +1,81 @@
 use pog;
 
-create table exon(id VARCHAR(40) not null,primary key(id), contig VARCHAR(40) not null, version VARCHAR(10) not null, created DATE not null, modified DATE not null, start INT(10) not null, end INT(10) not null, strand VARCHAR(1) not null, key id_contig (id,contig));
+create table exon(
+    id         VARCHAR(40) not null,  primary key(id), 
+    contig     VARCHAR(40) not null, 
+    version    VARCHAR(10) not null, 
+    created    DATE        not null, 
+    modified   DATE        not null, 
+    start      INT(10)     not null, 
+    end        INT(10)     not null, 
+    strand     VARCHAR(1)  not null, 
+    key id_contig (id,contig));
 
-create table transcript(id VARCHAR(40) not null,primary key(id), geneid VARCHAR(40) not null, key id_geneid (id,geneid));
+create table transcript(
+    id         VARCHAR(40) not null, primary key(id), 
+    geneid     VARCHAR(40) not null, 
+    key id_geneid (id,geneid));
 
-create table homolhit(id VARCHAR(40) not null,primary key(id), name VARCHAR(40) not null, start INT(10) not null, end INT(10) not null,strand VARCHAR(1) not null, type VARCHAR(40) not null);
+create table featurehit(
+    id         VARCHAR(40) not null, primary key(id), 
+    name       VARCHAR(40) not null, 
+    start      INT(10)     not null, 
+    end        INT(10)     not null,
+    strand     VARCHAR(1)  not null, 
+    type       VARCHAR(40) not null);
 
-create table homolset (homolid VARCHAR(40) not null, setid VARCHAR(40) not null, key homol_set(homolid,setid));
+create table feature_bridge(
+    feature    VARCHAR(40) not null,
+    transcript VARCHAR(40) not null,
+    rank       INT,
+    type       VARCHAR(40));
 
-create table homol(id VARCHAR(40) not null, contig VARCHAR(40) not null, start INT(10) not null, end INT(10) not null, hstart INT(10) not null, hend INT(10) not null,score INT(10) not null, strand VARCHAR(1),analysis VARCHAR(40) not null, homolset VARCHAR(40), key id_contig (id,contig),key(start));
+create table featureset(
+    id         VARCHAR(40) not null,
+    feature    VARCHAR(40) not null, 
+    rank       INT,
+    key feature_id (feature,id));
 
-create table gene(id VARCHAR(40) not null, primary key(id), version VARCHAR(40) not null, created DATE not null, modified DATE not null);
+create table feature(
+    id         VARCHAR(40) not null, 
+    contig     VARCHAR(40) not null, 
+    start      INT(10)     not null, 
+    end        INT(10)     not null, 
+    hstart     INT(10), 
+    hend       INT(10),
+    score      INT(10)     not null, 
+    strand     VARCHAR(1), 
+    analysis   VARCHAR(40) not null, 
+    featureset   VARCHAR(40), 
+    key id_contig (id,contig),key(start));
 
-create table contig(id VARCHAR(40) not null, clone VARCHAR(40) not null,primary key(id), map VARCHAR(40) not null, start INT(10), end INT(10));
+create table gene(
+    id         VARCHAR(40) not null, primary key(id), 
+    version    VARCHAR(40) not null, 
+    created    DATE        not null, 
+    modified   DATE        not null);
 
-create table mapbin(id VARCHAR(40) not null,primary key(id), chromosome VARCHAR(2) not null);
+create table contig(
+    id         VARCHAR(40) not null, primary key(id),
+    clone      VARCHAR(40) not null,
+    mapbin     VARCHAR(40) not null, 
+    start      INT(10), 
+    end INT(10));
 
-create table analysis(id VARCHAR(40) not null, primary key(id),db VARCHAR(40),db_version VARCHAR(5), program VARCHAR(40) not null, program_version VARCHAR(5), method VARCHAR(40) not null);
+create table mapbin(
+    id         VARCHAR(40) not null, primary key(id), 
+    chromosome VARCHAR(2)  not null);
 
-create table exon_bridge(exon VARCHAR(40) not null, transcript VARCHAR(40) not null, rank INT(10) not null, key exon_transcript (exon,transcript));
+create table analysis(
+    id         VARCHAR(40) not null, primary key(id),
+    db         VARCHAR(40),
+    db_version VARCHAR(5), 
+    program    VARCHAR(40) not null, 
+    program_version VARCHAR(5), 
+    method     VARCHAR(40) not null);
 
-create table homol_bridge(homol VARCHAR(40) not null, homolhit VARCHAR(40) not null, rank INT(10) not null);
-
-create table test(id int  not null auto_increment, primary key(id), text VARCHAR(20));
+create table exon_bridge(
+    exon       VARCHAR(40) not null, 
+    transcript VARCHAR(40) not null, 
+    rank       INT(10)     not null, 
+    key exon_transcript (exon,transcript));
