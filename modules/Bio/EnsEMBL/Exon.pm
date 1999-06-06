75cfe5eea79e8e0326502dfc42f1304a6478fdf2
Ewan Birney <birney@sanger.ac.uk>
1999-06-06 16:52:47 +0000

    added in Exon/Gene/Transcripts

---
+++ modules/Bio/EnsEMBL/Exon.pm
@@ -0,0 +1,225 @@
+
+#
+# BioPerl module for Exon
+#
+# Cared for by Ewan Birney <birney@sanger.ac.uk>
+#
+# Copyright Ewan Birney
+#
+# You may distribute this module under the same terms as perl itself
+
+# POD documentation - main docs before the code
+
+=head1 NAME
+
+Bio::EnsEMBL::Exon - Confirmed Exon 
+
+=head1 SYNOPSIS
+
+    $ex = new Bio::EnsEMBL::Exon;
+
+    $ex->start(10);
+    $ex->end(100);
+
+
+=head1 DESCRIPTION
+
+Confirmed exon object. Not clear at the moment precisely what we are going
+to store in this...
+
+=head1 CONTACT
+
+Describe contact details here
+
+=head1 APPENDIX
+
+The rest of the documentation details each of the object methods. Internal methods are usually preceded with a _
+
+=cut
+
+
+# Let the code begin...
+
+
+package Bio::EnsEMBL::Exon;
+use vars qw(@ISA);
+use strict;
+
+# Object preamble - inheriets from Bio::SeqFeature::Generic
+
+use Bio::SeqFeature::Generic;
+
+
+@ISA = qw(Bio::SeqFeature::Generic);
+# new() is inherited from Bio::Root::Object
+
+# _initialize is where the heavy stuff will happen when new is called
+
+sub _initialize {
+  my($self,@args) = @_;
+
+  my $make = $self->SUPER::_initialize;
+
+  # add in EnsEMBL tag as 1.
+
+  $self->primary_tag('exon');
+  $self->source_tag('EnsEMBL');
+
+  # set stuff in self from @args
+  return $make; # success - we hope!
+}
+
+# rest is left until we know what is going on...
+# but you do have all the SeqFeature documentation: reproduced here
+# for convience...
+
+=head1 Methods inherieted from SeqFeature
+
+=head2 start
+
+ Title   : start
+ Usage   : $start = $feat->start
+ Function: Returns the start coordinate of the feature
+ Returns : integer
+ Args    : none
+
+
+=head2 end
+
+ Title   : end
+ Usage   : $end = $feat->end
+ Function: Returns the end coordinate of the feature
+ Returns : integer
+ Args    : none
+
+
+=head2 strand
+
+ Title   : strand
+ Usage   : $strand = $feat->strand()
+ Function: Returns strand information, being 1,-1 or 0
+ Returns : -1,1 or 0
+ Args    : none
+
+
+=head2 sub_SeqFeature
+
+ Title   : sub_SeqFeature
+ Usage   : @feats = $feat->sub_SeqFeature();
+ Function: Returns an array of sub Sequence Features
+ Returns : An array
+ Args    : none
+
+
+=head2 primary_tag
+
+ Title   : primary_tag
+ Usage   : $tag = $feat->primary_tag()
+ Function: Returns the primary tag for a feature,
+           eg 'exon'
+ Returns : a string 
+ Args    : none
+
+
+=head2 source_tag
+
+ Title   : source_tag
+ Usage   : $tag = $feat->source_tag()
+ Function: Returns the source tag for a feature,
+           eg, 'genscan' 
+ Returns : a string 
+ Args    : none
+
+
+=head2 has_tag
+
+ Title   : has_tag
+ Usage   : $value = $self->has_tag('some_tag')
+ Function: Returns the value of the tag (undef if 
+           none)
+ Returns : 
+ Args    :
+
+
+=head2 all_tags
+
+ Title   : all_tags
+ Usage   : @tags = $feat->all_tags()
+ Function: gives all tags for this feature
+ Returns : an array of strings
+ Args    : none
+
+
+
+=head2 gff_string
+
+ Title   : gff_string
+ Usage   : $str = $feat->gff_string
+ Function: provides the feature information in GFF
+           version 2 format.
+ Returns : A string
+ Args    : None
+
+
+
+=head1 RangeI methods
+
+These methods are inherited from RangeI and can be used
+directly from a SeqFeatureI interface. Remember that a 
+SeqFeature is-a RangeI, and so wherever you see RangeI you
+can use a feature ($r in the below documentation).
+
+=head2 overlaps
+
+  Title   : overlaps
+  Usage   : if($feat->overlaps($r)) { do stuff }
+            if($feat->overlaps(200)) { do stuff }
+  Function: tests if $feat overlaps $r
+  Args    : a RangeI to test for overlap with, or a point
+  Returns : true if the Range overlaps with the feature, false otherwise
+
+
+=head2 contains
+
+  Title   : contains
+  Usage   : if($feat->contains($r) { do stuff }
+  Function: tests whether $feat totally contains $r
+  Args    : a RangeI to test for being contained
+  Returns : true if the argument is totaly contained within this range
+
+
+=head2 equals
+
+  Title   : equals
+  Usage   : if($feat->equals($r))
+  Function: test whether $feat has the same start, end, strand as $r
+  Args    : a RangeI to test for equality
+  Returns : true if they are describing the same range
+
+
+=head1 Geometrical methods
+
+These methods do things to the geometry of ranges, and return
+triplets (start, stop, strand) from which new ranges could be built.
+
+=head2
+
+  Title   : intersection
+  Usage   : ($start, $stop, $strand) = $feat->intersection($r)
+  Function: gives the range that is contained by both ranges
+  Args    : a RangeI to compare this one to
+  Returns : nothing if they don't overlap, or the range that they do overlap
+
+
+=head2 union
+
+  Title   : union
+  Usage   : ($start, $stop, $strand) = $feat->union($r);
+          : ($start, $stop, $strand) = Bio::RangeI->union(@ranges);
+  Function: finds the minimal range that contains all of the ranges
+  Args    : a range or list of ranges to find the union of
+  Returns : the range containing all of the ranges
+
+=cut
+
+1;
---
+++ modules/Bio/EnsEMBL/Gene.pm
@@ -0,0 +1,265 @@
+
+#
+# BioPerl module for Gene
+#
+# Cared for by Ewan Birney <birney@sanger.ac.uk>
+#
+# Copyright Ewan Birney
+#
+# You may distribute this module under the same terms as perl itself
+
+# POD documentation - main docs before the code
+
+=head1 NAME
+
+Bio::EnsEMBL::Gene - Object for confirmed Genes
+
+=head1 SYNOPSIS
+
+Confirmed genes. Basically has a set of transcripts
+
+=head1 DESCRIPTION
+
+Needs more description.
+
+=head1 CONTACT
+
+Describe contact details here
+
+=head1 APPENDIX
+
+The rest of the documentation details each of the object methods. Internal methods are usually preceded with a _
+
+=cut
+
+
+# Let the code begin...
+
+
+package Bio::EnsEMBL::Gene;
+use vars qw(@ISA);
+use strict;
+
+# Object preamble - inheriets from Bio::SeqFeature::Generic
+
+use Bio::SeqFeature::Generic;
+
+
+@ISA = qw(Bio::SeqFeature::Generic);
+# new() is inherited from Bio::Root::Object
+
+# _initialize is where the heavy stuff will happen when new is called
+
+sub _initialize {
+  my($self,@args) = @_;
+
+  my $make = $self->SUPER::_initialize;
+
+# set stuff in self from @args
+  return $make; # success - we hope!
+}
+
+=head2 add_Transcript
+
+ Title   : add_Transcript
+ Usage   : $gene->add_Transcript($tr)
+ Function:
+ Example :
+ Returns : 
+ Args    :
+
+
+=cut
+
+sub add_Transcript{
+   my ($self,$trans) = @_;
+
+   if( ! $trans->isa("Bio::EnsEMBL::Transcript") ) {
+       $self->throw("$trans is not a Bio::EnsEMBL::Exon!");
+   }
+
+   # at the moment, use the SeqFeature sub hash. But in the future,
+   # possibly do something better?
+   
+   $self->add_sub_SeqFeature($trans,'EXPAND');
+}
+
+=head2 each_Transcript
+
+ Title   : each_Transcript
+ Usage   : foreach $trans ( $gene->each_Transcript)
+ Function:
+ Example :
+ Returns : An array of Transcript objects
+ Args    :
+
+
+=cut
+
+sub each_Transcript {
+   my ($self) = @_;
+   my @sub;
+   my @ret;
+
+   @sub = $self->sub_SeqFeature();
+
+   foreach my $f ( @sub ) {
+       if( $f->isa("Bio::EnsEMBL::Transcript") ) {
+	   push(@ret,$f);
+       }
+   }
+
+   return @ret;   
+
+}
+
+=head1 Methods inherieted from SeqFeature
+
+=head2 start
+
+ Title   : start
+ Usage   : $start = $feat->start
+ Function: Returns the start coordinate of the feature
+ Returns : integer
+ Args    : none
+
+
+=head2 end
+
+ Title   : end
+ Usage   : $end = $feat->end
+ Function: Returns the end coordinate of the feature
+ Returns : integer
+ Args    : none
+
+
+=head2 strand
+
+ Title   : strand
+ Usage   : $strand = $feat->strand()
+ Function: Returns strand information, being 1,-1 or 0
+ Returns : -1,1 or 0
+ Args    : none
+
+
+=head2 sub_SeqFeature
+
+ Title   : sub_SeqFeature
+ Usage   : @feats = $feat->sub_SeqFeature();
+ Function: Returns an array of sub Sequence Features
+ Returns : An array
+ Args    : none
+
+
+=head2 primary_tag
+
+ Title   : primary_tag
+ Usage   : $tag = $feat->primary_tag()
+ Function: Returns the primary tag for a feature,
+           eg 'exon'
+ Returns : a string 
+ Args    : none
+
+
+=head2 source_tag
+
+ Title   : source_tag
+ Usage   : $tag = $feat->source_tag()
+ Function: Returns the source tag for a feature,
+           eg, 'genscan' 
+ Returns : a string 
+ Args    : none
+
+
+=head2 has_tag
+
+ Title   : has_tag
+ Usage   : $value = $self->has_tag('some_tag')
+ Function: Returns the value of the tag (undef if 
+           none)
+ Returns : 
+ Args    :
+
+
+=head2 all_tags
+
+ Title   : all_tags
+ Usage   : @tags = $feat->all_tags()
+ Function: gives all tags for this feature
+ Returns : an array of strings
+ Args    : none
+
+
+
+=head2 gff_string
+
+ Title   : gff_string
+ Usage   : $str = $feat->gff_string
+ Function: provides the feature information in GFF
+           version 2 format.
+ Returns : A string
+ Args    : None
+
+
+
+=head1 RangeI methods
+
+These methods are inherited from RangeI and can be used
+directly from a SeqFeatureI interface. Remember that a 
+SeqFeature is-a RangeI, and so wherever you see RangeI you
+can use a feature ($r in the below documentation).
+
+=head2 overlaps
+
+  Title   : overlaps
+  Usage   : if($feat->overlaps($r)) { do stuff }
+            if($feat->overlaps(200)) { do stuff }
+  Function: tests if $feat overlaps $r
+  Args    : a RangeI to test for overlap with, or a point
+  Returns : true if the Range overlaps with the feature, false otherwise
+
+
+=head2 contains
+
+  Title   : contains
+  Usage   : if($feat->contains($r) { do stuff }
+  Function: tests whether $feat totally contains $r
+  Args    : a RangeI to test for being contained
+  Returns : true if the argument is totaly contained within this range
+
+
+=head2 equals
+
+  Title   : equals
+  Usage   : if($feat->equals($r))
+  Function: test whether $feat has the same start, end, strand as $r
+  Args    : a RangeI to test for equality
+  Returns : true if they are describing the same range
+
+
+=head1 Geometrical methods
+
+These methods do things to the geometry of ranges, and return
+triplets (start, stop, strand) from which new ranges could be built.
+
+=head2
+
+  Title   : intersection
+  Usage   : ($start, $stop, $strand) = $feat->intersection($r)
+  Function: gives the range that is contained by both ranges
+  Args    : a RangeI to compare this one to
+  Returns : nothing if they don't overlap, or the range that they do overlap
+
+
+=head2 union
+
+  Title   : union
+  Usage   : ($start, $stop, $strand) = $feat->union($r);
+          : ($start, $stop, $strand) = Bio::RangeI->union(@ranges);
+  Function: finds the minimal range that contains all of the ranges
+  Args    : a range or list of ranges to find the union of
+  Returns : the range containing all of the ranges
+
+=cut
+
+1;
