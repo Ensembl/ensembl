3d5d59005d05bc3fcbc9d3be909f781a5be2476c
Ewan Birney <birney@sanger.ac.uk>
1999-08-16 17:18:45 +0000

    ObjI interface of object

---
+++ modules/Bio/EnsEMBL/DB/ObjI.pm
@@ -0,0 +1,137 @@
+
+#
+# BioPerl module for Bio::EnsEMBL::DB::ObjI
+#
+# Cared for by Ewan Birney <birney@sanger.ac.uk>
+#
+# Copyright Ewan Birney
+#
+# You may distribute this module under the same terms as perl itself
+
+# POD documentation - main docs before the code
+
+=head1 NAME
+
+Bio::EnsEMBL::DB::ObjI - Abstract Interface of Database objects generically
+
+=head1 SYNOPSIS
+
+Give standard usage here
+
+=head1 DESCRIPTION
+
+Describe the object here
+
+=head1 CONTACT
+
+Describe contact details here
+
+=head1 APPENDIX
+
+The rest of the documentation details each of the object methods. Internal methods are usually preceded with a _
+
+=cut
+
+
+# Let the code begin...
+
+package Bio::EnsEMBL::DB::ObjI;
+use vars qw($AUTOLOAD @ISA);
+use strict;
+
+=head2 get_Gene
+
+ Title   : get_Gene
+ Usage   :
+ Function:
+ Example :
+ Returns : 
+ Args    :
+
+
+=cut
+
+sub get_Gene{
+   my ($self) = @_;
+
+   $self->throw("Not implemented in the object!");
+}
+
+=head2 get_Clone
+
+ Title   : get_Clone
+ Usage   :
+ Function:
+ Example :
+ Returns : 
+ Args    :
+
+
+=cut
+
+sub get_Clone{
+   my ($self) = @_;
+
+   $self->throw("Not implemented in the object!");
+}
+
+
+=head2 get_Contig
+
+ Title   : get_Contig
+ Usage   :
+ Function:
+ Example :
+ Returns : 
+ Args    :
+
+
+=cut
+
+sub get_Contig{
+
+   $self->throw("Not implemented in the object!");
+}
+
+
+=head2 write_Gene
+
+ Title   : write_Gene
+ Usage   : $obj->write_Gene($gene)
+ Function: writes a particular gene into the database
+           
+ Example :
+ Returns : 
+ Args    :
+
+
+=cut
+
+sub write_Gene{
+   my ($self) = @_;
+
+   $self->throw("Not implemented in the object!");
+}
+
+=head2 write_Contig
+
+ Title   : write_Contig
+ Usage   : $obj->write_Contig($contigid,$dna)
+ Function: writes a contig and its dna into the database
+ Example :
+ Returns : 
+ Args    :
+
+
+=cut
+
+sub write_Contig {
+   my ($self) = @_;
+
+   $self->throw("Not implemented in the object!");
+}
+
+
+
+
+
