759ab523ef97972e10f7655a311bd3f36b6da9c3
cvs2git <cvs2git>
1999-11-10 14:29:54 +0000

    This commit was manufactured by cvs2svn to create tag 'release-1'.
    Sprout from master 1999-11-10 14:29:53 UTC Ewan Birney <birney@sanger.ac.uk> 'added popt..'
    Delete:
        Apache/EnsEMBL/Footer.pm
        Apache/EnsEMBL/Header.pm
        Apache/EnsEMBL/NavBar.pm
        Apache/EnsEMBL/NavTrail.pm
        Apache/EnsEMBL/NotFound.pm
        Apache/EnsEMBL/README
        Apache/EnsEMBL/SendDecPage.pm
        Apache/EnsEMBL/ServerError.pm
        Apache/EnsEMBL/contig2gff.pm
        LICENSE
        README
        cgi-bin/clone2embl.pl
        cgi-bin/contig2gff.pl
        cgi-bin/contigview.pl
        corba/artemis/INSTALL
        corba/artemis/README
        corba/artemis/artemis-db-mysql.c
        corba/artemis/artemis-db-mysql.h
        corba/artemis/artemis-exon-impl.c
        corba/artemis/artemis-exon-impl.h
        corba/artemis/artemis-mysql-impl.c
        corba/artemis/artemis-mysql-impl.h
        corba/artemis/artemis-qual-helper.c
        corba/artemis/artemis-qual-helper.h
        corba/artemis/artemis.idl
        corba/artemis/ensembl-artemis-client.c
        corba/artemis/ensembl-artemis-server.c
        corba/artemis/makefile
        corba/artemis/test-art-seq-client.c
        corba/artemis/test-artentry-client.c
        corba/artemis/test-artentry-server.c
        corba/artemis/test-artseq-server.c
        corba/seq/bioseq.idl
        corba/seq/ensembl-seq-server.c
        corba/seq/ensembl-seq-server.h
        docs/README
        docs/map.txt
        minutes/02071999
        minutes/09071999
        minutes/16081999
        minutes/24061999
        modules/Bio/EnsEMBL/AceDB/Clone.pm
        modules/Bio/EnsEMBL/AceDB/Contig.pm
        modules/Bio/EnsEMBL/AceDB/Obj.pm
        modules/Bio/EnsEMBL/Analysis/CdnaResolver.pm
        modules/Bio/EnsEMBL/Analysis/ContigOrder.pm
        modules/Bio/EnsEMBL/Analysis/DNAparse.pm
        modules/Bio/EnsEMBL/Analysis/Exon2Gene.pm
        modules/Bio/EnsEMBL/Analysis/Genscan.pm
        modules/Bio/EnsEMBL/Analysis/LegacyParser.pm
        modules/Bio/EnsEMBL/Analysis/Log.pm
        modules/Bio/EnsEMBL/Analysis/LogMessage.pm
        modules/Bio/EnsEMBL/Analysis/ensConf.pm
        modules/Bio/EnsEMBL/AnnSeq.pm
        modules/Bio/EnsEMBL/DB/CloneI.pm
        modules/Bio/EnsEMBL/DB/ContigI.pm
        modules/Bio/EnsEMBL/DB/ObjI.pm
        modules/Bio/EnsEMBL/DBSQL/Clone.pm
        modules/Bio/EnsEMBL/DBSQL/Contig.pm
        modules/Bio/EnsEMBL/DBSQL/DummyStatement.pm
        modules/Bio/EnsEMBL/DBSQL/Obj.pm
        modules/Bio/EnsEMBL/EMBL_Dump.pm
        modules/Bio/EnsEMBL/Exon.pm
        modules/Bio/EnsEMBL/Gene.pm
        modules/Bio/EnsEMBL/GeneHandler.pm
        modules/Bio/EnsEMBL/PerlDB/Clone.pm
        modules/Bio/EnsEMBL/PerlDB/Contig.pm
        modules/Bio/EnsEMBL/PerlDB/Obj.pm
        modules/Bio/EnsEMBL/TimDB/Clone.pm
        modules/Bio/EnsEMBL/TimDB/Contig.pm
        modules/Bio/EnsEMBL/TimDB/Obj.pm
        modules/Bio/EnsEMBL/Transcript.pm
        modules/Bio/EnsEMBL/Translation.pm
        modules/Bio/SeqFeature2GFF.pm
        modules/Bio/Tracking/README
        modules/EnsEMBL/README
        modules/Makefile.PL
        modules/README
        modules/ensembl.lisp
        modules/t/AnalysisLog.t
        modules/t/DB.t
        modules/t/EmblDump.t
        modules/t/Gene.t
        modules/t/Genscan.t
        modules/t/PerlDB.t
        modules/t/README
        modules/t/cdnaresolver.t
        modules/t/emblloader.t
        modules/t/human.genomic
        scripts/README
        scripts/cdna2genome.pl
        scripts/cdnapipe.pl
        scripts/clone2contig.pl
        scripts/clone2embl.pl
        scripts/contig2gff.pl
        scripts/delete.pl
        scripts/gene2flat.pl
        scripts/genscan2sql
        scripts/get_all_clone_id.pl
        scripts/install_ensembl.pl
        scripts/loadstatus.pl
        scripts/msp2sql
        scripts/testDBI
        scripts/test_write_Contig
        scripts/test_write_features
        scripts/testdb
        scripts/transfer.pl
        sql/README
        sql/queries.sql
        sql/table.sql

--- corba/artemis/artemis-db-mysql.h
+++
@@ -1,9 +0,0 @@
-
-#ifndef ARTEMIS_DB_HEADER
-#define ARTEMIS_DB_HEADER
-
-#include "artemis-mysql-impl.h"
-
-Ensembl_artemis_DB new_EA_Database(PortableServer_POA poa,MYSQL * connection,int verbose,CORBA_Environment * ev);
-
-#endif
