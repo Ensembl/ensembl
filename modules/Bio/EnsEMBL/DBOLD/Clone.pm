d60078f3c73b2c2f6263c22702ec432396420696
Ewan Birney <birney@sanger.ac.uk>
1999-11-25 10:53:26 +0000

    added DBOLD and DBLoader

---
+++ modules/Bio/EnsEMBL/DBOLD/Clone.pm
@@ -0,0 +1,338 @@
+
+#
+# BioPerl module for DB::Clone
+#
+# Cared for by Ewan Birney <birney@sanger.ac.uk>
+#
+# Copyright Ewan Birney
+#
+# You may distribute this module under the same terms as perl itself
+
+# POD documentation - main docs before the code
+
+=head1 NAME
+
+Bio::EnsEMBL::DB::Clone - Object representing one clone
+
+=head1 SYNOPSIS
+
+    # $db is Bio::EnsEMBL::DB::Obj 
+
+    @contig = $db->get_Contigs();
+
+    $clone = $db->get_Clone();
+
+    @genes    = $clone->get_all_Genes();
+
+=head1 DESCRIPTION
+
+Represents information on one Clone
+
+=head1 CONTACT
+
+Describe contact details here
+
+=head1 APPENDIX
+
+The rest of the documentation details each of the object methods. Internal methods are usually preceded with a _
+
+=cut
+
+
+# Let the code begin...
+
+
+package Bio::EnsEMBL::DBOLD::Clone;
+use vars qw(@ISA);
+use strict;
+
+# Object preamble - inheriets from Bio::Root::Object
+
+use Bio::Root::Object;
+use Bio::EnsEMBL::DBOLD::Contig;
+use Bio::EnsEMBL::DB::CloneI;
+
+@ISA = qw(Bio::Root::Object Bio::EnsEMBL::DB::CloneI);
+
+# new() is inherited from Bio::Root::Object
+
+# _initialize is where the heavy stuff will happen when new is called
+
+sub _initialize {
+  my($self,@args) = @_;
+
+  my $make = $self->SUPER::_initialize;
+
+  my ($dbobj,$id) = $self->_rearrange([qw(DBOBJ
+					  ID
+					  )],@args);
+
+  $id || $self->throw("Cannot make contig db object without id");
+  $dbobj || $self->throw("Cannot make contig db object without db object");
+  $dbobj->isa('Bio::EnsEMBL::DBSQL::Obj') || $self->throw("Cannot make contig db object with a $dbobj object");
+
+  $self->id($id);
+  $self->_dbobj($dbobj);
+
+# set stuff in self from @args
+  return $make; # success - we hope!
+}
+
+
+=head2 get_all_Genes
+
+ Title   : get_all_Genes
+ Usage   :
+ Function:
+ Example :
+ Returns : 
+ Args    :
+
+
+=cut
+
+sub get_all_Genes{
+   my ($self,@args) = @_;
+   my @out;
+   my $id = $self->id();
+   my %got;
+
+   my $sth = $self->_dbobj->prepare("select p3.gene from contig as p4, transcript as p3, exon_transcript as p1, exon as p2 where p4.clone = '$id' and p2.contig = p4.id and p1.exon = p2.id and p3.id = p1.transcript");
+
+   my $res = $sth->execute();
+   while( my $rowhash = $sth->fetchrow_hashref) {
+
+       if( $got{$rowhash->{'gene'}} != 1 ) {
+          my $gene = $self->_dbobj->get_Gene($rowhash->{'gene'});
+	  push(@out,$gene);
+	  $got{$rowhash->{'gene'}} = 1;
+       }
+
+   }
+   
+
+   return @out;
+
+
+}
+
+# Alternative get_all_Genes. Does not use large joins, but does it in Perl.
+#     # prepare the SQL statement
+#     # try this differently:
+#     my %exon;
+#     my %trans;
+#     my %gene;
+#     my @contigs = $self->get_all_Contigs();
+#     foreach my $contig ( @contigs ) {
+#         my $sth = $self->_dbobj->prepare("select id from exon where contig = '". $contig->id ."'");
+#         my $res = $sth->execute();
+#         while( my $rowhash = $sth->fetchrow_hashref) {
+#  	   $exon{$rowhash->{'id'}} = 1;
+#         }
+#     }
+#     # if we have no exons - bomb out!
+#     if( scalar keys %exon == 0 ) {
+#         return;
+#     }
+#     # yank out genes from exons
+#     foreach my $exon_id ( keys %exon ) { 
+#         my $sth = $self->_dbobj->prepare("select transcript from exon_transcript where exon = '$exon_id'");
+#         my $res = $sth->execute();
+#         while( my $rowhash = $sth->fetchrow_hashref) {
+#  	   $trans{$rowhash->{'transcript'}} = 1;
+#         }
+#     }
+#     foreach my $trans_id ( keys %trans ) {
+#         my $sth = $self->_dbobj->prepare("select gene from transcript where id = '$trans_id'");
+#         my $res = $sth->execute();
+#         while( my $rowhash = $sth->fetchrow_hashref) {
+#  	   $gene{$rowhash->{'gene'}} = 1;
+#         }
+#     }
+#     foreach my $gene_id ( keys %gene ) {
+#         my $gene = $self->_dbobj->get_Gene($gene_id);
+#         push(@out,$gene);
+#     }
+#     return @out;
+
+=head2 get_Contig
+
+ Title   : get_Contig
+ Usage   :
+ Function:
+ Example :
+ Returns : 
+ Args    :
+
+
+=cut
+
+sub get_Contig{
+   my ($self,$contigid) = @_;
+
+   # should check this contig is in this clone?
+   return $self->_dbobj->get_Contig($contigid);
+}
+
+=head2 get_all_Contigs
+
+ Title   : get_Contigs
+ Usage   : foreach $contig ( $clone->get_all_Contigs ) 
+ Function:
+ Example :
+ Returns : 
+ Args    :
+
+
+=cut
+
+sub get_all_Contigs{
+   my ($self) = @_;
+   my $sth;
+   my @res;
+   my $name = $self->id();
+
+   my $sql = "select id from contig where clone = \"$name\" ";
+#   print STDERR "Looking at $name. [$sql]\n";
+   $sth= $self->_dbobj->prepare($sql);
+   my $res = $sth->execute();
+   my $seen = 0;
+
+   my $count = 0;
+   my $total = 0;
+
+   while( my $rowhash = $sth->fetchrow_hashref) {
+       my $contig = new Bio::EnsEMBL::DBSQL::Contig ( -dbobj => $self->_dbobj,
+						   -id => $rowhash->{'id'} );
+       $contig->order($count++);
+       #$contig->offset($total);
+       
+       #$total += $contig->length();
+       #$total += 400;
+
+       push(@res,$contig);
+       $seen = 1;
+   }
+   if( $seen == 0  ) {
+       $self->throw("Clone $name has no contigs in the database. Should be impossible, but clearly isnt...");
+   }
+
+   return @res;   
+}
+
+=head2 htg_phase
+
+ Title   : htg_phase
+ Usage   :
+ Function:
+ Example :
+ Returns : 
+ Args    :
+
+
+=cut
+
+sub htg_phase{
+   my ($self) = @_;
+
+   my $self = shift;
+   my $id = $self->id();
+
+   my $sth = $self->_dbobj->prepare("select htg_phase from clone where id = \"$id\" ");
+   $sth->execute();
+   my $rowhash = $sth->fetchrow_hashref();
+   return $rowhash->{'htg_phase'};
+}
+
+=head2 sv
+
+ Title   : sv
+ Usage   :
+ Function:
+ Example :
+ Returns : 
+ Args    :
+
+
+=cut
+
+sub sv{
+   my ($self) = @_;
+
+   my $self = shift;
+   my $id = $self->id();
+
+   my $sth = $self->_dbobj->prepare("select sv from clone where id = \"$id\" ");
+   $sth->execute();
+   my $rowhash = $sth->fetchrow_hashref();
+   return $rowhash->{'sv'};
+}
+
+=head2 embl_id
+
+ Title   : embl_id
+ Usage   :
+ Function:
+ Example :
+ Returns : 
+ Args    :
+
+
+=cut
+
+sub embl_id{
+   my ($self) = @_;
+
+   my $self = shift;
+   my $id = $self->id();
+
+   my $sth = $self->_dbobj->prepare("select embl_id from clone where id = \"$id\" ");
+   $sth->execute();
+   my $rowhash = $sth->fetchrow_hashref();
+   return $rowhash->{'embl_id'};
+}
+
+=head2 id
+
+ Title   : id
+ Usage   : $obj->id($newval)
+ Function: 
+ Example : 
+ Returns : value of id
+ Args    : newvalue (optional)
+
+
+=cut
+
+sub id {
+   my ($obj,$value) = @_;
+   if( defined $value) {
+      $obj->{'_clone_id'} = $value;
+    }
+    return $obj->{'_clone_id'};
+
+}
+
+
+=head2 _dbobj
+
+ Title   : _dbobj
+ Usage   : $obj->_dbobj($newval)
+ Function: 
+ Example : 
+ Returns : value of _dbobj
+ Args    : newvalue (optional)
+
+
+=cut
+
+sub _dbobj{
+   my ($obj,$value) = @_;
+   if( defined $value) {
+      $obj->{'_dbobj'} = $value;
+    }
+    return $obj->{'_dbobj'};
+
+}
+
+1;
---
+++ modules/Bio/EnsEMBL/DBOLD/Contig.pm
@@ -0,0 +1,347 @@
+
+#
+# BioPerl module for Contig
+#
+# Cared for by Ewan Birney <birney@sanger.ac.uk>
+#
+# Copyright Ewan Birney
+#
+# You may distribute this module under the same terms as perl itself
+
+# POD documentation - main docs before the code
+
+=head1 NAME
+
+Bio::EnsEMBL::DB::Contig - Handle onto a database stored contig
+
+=head1 SYNOPSIS
+
+Give standard usage here
+
+=head1 DESCRIPTION
+
+Describe the object here
+
+=head1 CONTACT
+
+Describe contact details here
+
+=head1 APPENDIX
+
+The rest of the documentation details each of the object methods. Internal methods are usually preceded with a _
+
+=cut
+
+
+# Let the code begin...
+
+
+package Bio::EnsEMBL::DBOLD::Contig;
+use vars qw(@ISA);
+use strict;
+
+# Object preamble - inheriets from Bio::Root::Object
+
+use Bio::Root::Object;
+use Bio::SeqFeature::Generic;
+use Bio::EnsEMBL::DBOLD::Obj;
+use Bio::EnsEMBL::DB::ContigI;
+
+
+@ISA = qw(Bio::Root::Object Bio::EnsEMBL::DB::ContigI);
+# new() is inherited from Bio::Root::Object
+
+# _initialize is where the heavy stuff will happen when new is called
+
+sub _initialize {
+  my($self,@args) = @_;
+
+  my $make = $self->SUPER::_initialize;
+
+  my ($dbobj,$id) = $self->_rearrange([qw(DBOBJ
+					  ID
+					  )],@args);
+
+  $id || $self->throw("Cannot make contig db object without id");
+  $dbobj || $self->throw("Cannot make contig db object without db object");
+  $dbobj->isa('Bio::EnsEMBL::DBSQL::Obj') || $self->throw("Cannot make contig db object with a $dbobj object");
+
+  $self->id($id);
+  $self->_dbobj($dbobj);
+
+# set stuff in self from @args
+  return $make; # success - we hope!
+}
+
+=head2 get_all_Genes
+
+ Title   : get_all_Genes
+ Usage   :
+ Function:
+ Example :
+ Returns : 
+ Args    :
+
+
+=cut
+
+sub get_all_Genes{
+   my ($self,@args) = @_;
+   my @out;
+   my $contig_id = $self->id();
+   # prepare the SQL statement
+   my %got;
+
+   my $sth = $self->_dbobj->prepare("select p3.gene from transcript as p3, exon_transcript as p1, exon as p2 where p2.contig = '$contig_id' and p1.exon = p2.id and p3.id = p1.transcript");
+
+   my $res = $sth->execute();
+   while( my $rowhash = $sth->fetchrow_hashref) {
+       if( $got{$rowhash->{'gene'}} != 1 ) {
+          my $gene = $self->_dbobj->get_Gene($rowhash->{'gene'});
+	  push(@out,$gene);
+	  $got{$rowhash->{'gene'}} = 1;
+       }
+       
+   }
+   
+
+   return @out;
+
+}
+
+
+=head2 seq
+
+ Title   : seq
+ Usage   : $seq = $contig->seq();
+ Function: Gets a Bio::Seq object out from the contig
+ Example :
+ Returns : Bio::Seq object
+ Args    :
+
+
+=cut
+
+sub seq{
+   my ($self) = @_;
+   my $id = $self->id();
+
+   if( $self->_seq_cache() ) {
+       return $self->_seq_cache();
+   }
+
+   my $sth = $self->_dbobj->prepare("select sequence from dna where contig = \"$id\"");
+   my $res = $sth->execute();
+
+   # should be a better way of doing this
+   while(my $rowhash = $sth->fetchrow_hashref) {
+     my $str = $rowhash->{sequence};
+
+     if( ! $str) {
+       $self->throw("No DNA sequence in contig $id");
+     } 
+     $str =~ /[^ATGCNRY]/ && $self->warn("Got some non standard DNA characters here! Yuk!");
+     $str =~ s/\s//g;
+     $str =~ s/[^ATGCNRY]/N/g;
+
+     my $ret =Bio::Seq->new ( -seq => $str, -id => $id, -type => 'Dna' );
+     $self->_seq_cache($ret);
+     
+     return $ret;
+   }
+
+   $self->throw("No dna sequence associated with $id!");
+   
+}
+
+=head2 _seq_cache
+
+ Title   : _seq_cache
+ Usage   : $obj->_seq_cache($newval)
+ Function: 
+ Returns : value of _seq_cache
+ Args    : newvalue (optional)
+
+
+=cut
+
+sub _seq_cache{
+   my $obj = shift;
+   if( @_ ) {
+       my $value = shift;
+       $obj->{'_seq_cache'} = $value;
+   }
+   return $obj->{'_seq_cache'};
+
+}
+
+=head2 get_all_SeqFeatures
+
+ Title   : get_all_SeqFeatures
+ Usage   : foreach my $sf ( $contig->get_all_SeqFeatures ) 
+ Function:
+ Example :
+ Returns : 
+ Args    :
+
+
+=cut
+
+sub get_all_SeqFeatures{
+   my ($self,@args) = @_;
+   my @array;
+
+   my $id = $self->id();
+
+   # make the SQL query
+
+   my $sth = $self->_dbobj->prepare("select start,end,strand,score,analysis from feature where contig = \"$id\"");
+   my $res = $sth->execute();
+
+   while( my $rowhash = $sth->fetchrow_hashref) {
+      my $out = new Bio::SeqFeature::Generic;
+      $out->start($rowhash->{start});
+      $out->end($rowhash->{end});
+      $out->strand($rowhash->{strand});
+      if( defined $rowhash->{score} ) {
+	  $out->score($rowhash->{score});
+      }
+      $out->primary_tag($rowhash->{analysis});
+      $out->source_tag('EnsEMBL');
+      push(@array,$out);
+  }
+ 
+   return @array;
+}
+
+=head2 length
+
+ Title   : length
+ Usage   :
+ Function:
+ Example :
+ Returns : 
+ Args    :
+
+
+=cut
+
+sub length{
+   my ($self,@args) = @_;
+   my $id= $self->id();
+
+   my $sth = $self->_dbobj->prepare("select length from contig where id = \"$id\" ");
+   $sth->execute();
+   my $rowhash = $sth->fetchrow_hashref();
+   return $rowhash->{'length'};
+}
+
+
+=head2 order
+
+ Title   : order
+ Usage   : $obj->order($newval)
+ Function: 
+ Returns : value of order
+ Args    : newvalue (optional)
+
+
+=cut
+
+sub order{
+   my $self = shift;
+   my $id = $self->id();
+   my $sth = $self->_dbobj->prepare("select corder from contig where id = \"$id\" ");
+   $sth->execute();
+   my $rowhash = $sth->fetchrow_hashref();
+   return $rowhash->{'corder'};
+   
+}
+
+=head2 offset
+
+ Title   : offset
+ Usage   : 
+ Returns : 
+ Args    :
+
+
+=cut
+
+sub offset{
+   my $self = shift;
+   my $id = $self->id();
+
+   my $sth = $self->_dbobj->prepare("select offset from contig where id = \"$id\" ");
+   $sth->execute();
+   my $rowhash = $sth->fetchrow_hashref();
+   return $rowhash->{'offset'};
+
+}
+
+
+=head2 orientation
+
+ Title   : orientation
+ Usage   : 
+ Function:
+ Example :
+ Returns : 
+ Args    :
+
+
+=cut
+
+sub orientation{
+   my ($self) = @_;
+   my $id = $self->id();
+
+   my $sth = $self->_dbobj->prepare("select orientation from contig where id = \"$id\" ");
+   $sth->execute();
+   my $rowhash = $sth->fetchrow_hashref();
+   return $rowhash->{'orientation'};
+}
+
+
+=head2 id
+
+ Title   : id
+ Usage   : $obj->id($newval)
+ Function: 
+ Example : 
+ Returns : value of id
+ Args    : newvalue (optional)
+
+
+=cut
+
+sub id{
+   my ($self,$value) = @_;
+   if( defined $value) {
+      $self->{'id'} = $value;
+    }
+    return $self->{'id'};
+
+}
+
+=head2 _dbobj
+
+ Title   : _dbobj
+ Usage   : $obj->_dbobj($newval)
+ Function: 
+ Example : 
+ Returns : value of _dbobj
+ Args    : newvalue (optional)
+
+
+=cut
+
+sub _dbobj{
+   my ($self,$value) = @_;
+   if( defined $value) {
+      $self->{'_dbobj'} = $value;
+    }
+    return $self->{'_dbobj'};
+
+}
+
