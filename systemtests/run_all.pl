c5a6fd9fbef710925d6ca38334162584ac91b6be
Elia Stupka <elia@sanger.ac.uk>
1999-11-26 16:25:53 +0000

    Added this script which runs all tests

---
+++ systemtests/run_all.pl
@@ -0,0 +1,9 @@
+#!/usr/local/bin/perl
+
+use strict;
+
+system ("perl dna.pl");
+system ("perl every_atleast.pl");
+system ("perl stop_codons.pl");
+system ("perl transcript_strand.pl");
+system ("perl exon_duplicates.pl");
