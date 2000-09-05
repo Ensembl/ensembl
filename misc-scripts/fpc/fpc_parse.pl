9b7a601d8f790b91ce3b28ce9bcd33810aa0362a
Arne Stabenau <stabenau@sanger.ac.uk>
2000-09-05 10:36:41 +0000

    Updated fpc_parse for eye candy distribution of fpc_contigs on the chromosomes.

--- misc-scripts/fpc/fpc_parse.pl
+++ misc-scripts/fpc/fpc_parse.pl
@@ -4,6 +4,9 @@
 # Fpc_Contig table entries
 
 $allfile = shift;
+
+$mingap = 150000;
+
 open( FH, $allfile ) or die( "Couldnt open input file" );
 open( CO, ">fpc_contig.txt" ) or 
   die( "Couldnt open fpc_contig.txt output file." );
@@ -12,7 +15,7 @@ open( CL, ">fpc_clone.txt" ) or
 
 $cloneHash = {};
 $contigHash = {};
-$currentContigNum;
+$contigNum = 0;
 
 %chrlength = ( "1", 263000058, "10", 143999990, "11", 144000020, "12", 142999999, "13", 113999999, "14", 108999990, "15", 106000020, "16", 98000000, "17", 92000000, "18", 85000029, "19", 66999999, "2", 254999998, "20", 72000009, "21", 50000000, "22", 50000000, "3", 214000030, "4", 203000002, "5", 194000011, "6", 183000009, "7", 171000011, "8", 155000010, "9", 144999990, "X", 164000011, "Y", 50810739 );
 
@@ -87,9 +90,10 @@ for (keys %$contigHash ) {
 }
 
 for $chrom (keys %chrom) {
-  my @newlist = sort {$a->{start} <=> $b->{start}} @{$chrom{$chrom}};
-  $chrom{$chrom} = \@newlist;
-  recalc_starts_standard( $chrom, $chrom{$chrom} );
+#  my @newlist = sort {$a->{start} <=> $b->{start}} @{$chrom{$chrom}};
+#  $chrom{$chrom} = \@newlist;
+#  recalc_starts_standard( $chrom, $chrom{$chrom} );
+  recalc_eye_candy( $chrom, $chrom{$chrom} );
 }
 
 
@@ -99,9 +103,11 @@ for (keys %$contigHash ) {
     print STDERR ("Key $_ empty contig.\n" );
     next;
   }
+  print_Clones_in_Contig( $contigRef );
+
   print CO ( $contigRef->{num},"\t" );
   print CO ( $contigRef->{name},"\t" );
-  if( ! defined $contigRef->{start} ) {
+  if( ! defined $contigRef->{start_bp} ) {
     print CO ( "-1\t" );
   } else {
     print CO ( $contigRef->{start_bp},"\t" );
@@ -110,42 +116,28 @@ for (keys %$contigHash ) {
   print CO ( $contigRef->{chrom},"\n" );
 }
 
-sub contig_done {
-  my $chrom = shift;
-  my $name = shift;
-  my $ordered = shift;
-  my $moreName = shift;
-  my $global = shift;
-  
-  if( defined $moreName &&
-      defined $global ) {
-    $contigHash->{$moreName}->{start} = $global;
-  }
-  $contigHash->{$name}->{chrom} = $chrom;
-  if( $ordered && ! defined $contigHash->{$name}->{start} ) {
-    print( "$name has no start pos.\n" );
-  }
-
-  # print( $name, " ", $contigHash->{$name}->{length}," ",
-  #  $contigHash->{$name}->{start},"\n" );
-    
-}
-
-
-
 sub submit_clone {
   my $chrom = shift;
   my $ordered = shift;
   my $clone = shift;
   
+  # filter for double clones
+  if( defined $cloneHash{$clone->{embl}."-".$clone->{name}} ) {
+    return;
+  } else {
+    $cloneHash{$clone->{embl}."-".$clone->{name}} = 1;
+  }
+
   if( ! defined $contigHash->{$clone->{contig}} ) {
-    $contigNum = $currentContigNum++;
+    $contigNum++;
     $contigHash->{$clone->{contig}}->{num} = $contigNum;
     $contigHash->{$clone->{contig}}->{name} = $clone->{contig};
     
     $contigHash->{$clone->{contig}}->{chrom} = $chrom;
     $contigHash->{$clone->{contig}}->{ordered} = $ordered;
   }
+
+  push( @{$contigHash->{$clone->{contig}}->{clones}}, $clone );
 
   my $contiglen = $clone->{start} + $clone->{length};
   if( !defined $contigHash->{$clone->{contig}}->{length} ) {
@@ -156,14 +148,23 @@ sub submit_clone {
     }
   }
 
-  print CL ( $clone->{embl},"\t" );
-  print CL ( $clone->{name},"\t" );
-  print CL ( $clone->{institute},"\t" );
-  print CL ( $contigHash->{$clone->{contig}}->{num},"\t" );
-  print CL ( $clone->{length},"\t" );
-  print CL ( $clone->{start},"\n" );
- 
+  return;
 }
+
+sub print_Clones_in_Contig {
+  my $contig = shift;
+
+  for $clone ( @{$contig->{clones}} ) {
+    print CL ( $clone->{embl},"\t" );
+    print CL ( $clone->{name},"\t" );
+    print CL ( $clone->{institute},"\t" );
+    print CL ( $contigHash->{$clone->{contig}}->{num},"\t" );
+    print CL ( $clone->{length},"\t" );
+    print CL ( $clone->{start},"\n" );
+  }
+}
+
+
 
 sub recalc_starts_standard {
   my $chrom = shift;
@@ -172,6 +173,8 @@ sub recalc_starts_standard {
   my $maxlen = $chrlength{"$chrom"};
   print "Chromosome $chrom $maxlen\n";
   my $lastContig = $contigList[ $#contigList ];
+
+  
   my $factor =( $maxlen - $lastContig->{length} ) / $lastContig->{start};
   
   for my $contigRef ( @contigList ) {
@@ -181,7 +184,90 @@ sub recalc_starts_standard {
   }
 }
 
-  
+sub recalc_eye_candy {
+  my $chrom  =shift;
+  my $contigListRef = shift;
+
+  my @contigList = @$contigListRef;
+  my $maxlen = $chrlength{"$chrom"};
+
+  print "Chromosome $chrom $maxlen\n";
+
+  # length we have
+  my $sumLength = 0 ;
+  for my $contigRef ( @contigList ) {
+    $sumLength += $contigRef->{length};
+  }
+
+  my $gap = $maxlen - $sumLength;
+  if( $gap < 0 ) {
+    # die( "Chromosome length estimate for $chrom is tooo low, please update.\n" );
+    print( "Chromosome $chrom is ",-$gap, " too short, no gaps\n" );
+    $gap = 0;
+  }
+
+  if( $#contigList > 0 ) {
+    $allgap = $maxlen - $sumLength;
+    $gap = int( $allgap / $#contigList );
+    if( $gap < $mingap ) {
+      $needgap = $mingap * $#contigList - $allgap;
+      
+      fake_clonestarts( $contigListRef, $needgap );
+      $gap = $mingap;
+
+    }
+  }
+
+  my $start = 0;
+  for my $contigRef ( @contigList ) {
+    $contigRef->{start_bp} = $start;
+    $start += ( $gap + $contigRef->{length} );
+  }
+}
+ 
+# The contigs are too long and cover too much chromosome
+# so we make wrong clonestarts within the contigs to make
+# everything shorter
+
+
+sub fake_clonestarts {
+  my $contigListRef = shift;
+  my $neededGap = shift;
+
+  my @contigList = @$contigListRef;
+
+  my ( $maxStartPositions, $clone, $end );
+
+  for my $contig ( @contigList ) {
+    my $contigLength = 0;
+    my $endCloneStart = 0;
+    for my $clone ( @{$contig->{clones}} ) {
+      $end = $clone->{start} + $clone->{length};
+      if( $end > $contigLength ) {
+	$contigLength = $end;
+	$endCloneStart = $clone->{start};
+      }
+    }
+    $maxStartPositions += $endCloneStart;
+  }
+
+  my $factor = ($maxStartPositions - $neededGap) / $maxStartPositions;
+  print "Set Factor to $factor.\n";
+
+  for my $contig ( @contigList ) {
+    my $contigLength = 0;
+    for my $clone ( @{$contig->{clones}} ) {
+      $clone->{start} = int( $factor * $clone->{start});
+      $end = $clone->{start} + $clone->{length};
+      if( $end > $contigLength ) {
+	$contigLength = $end;
+      }
+    }
+    
+    $contig->{length} = $contigLength;
+  }
+}
+
 
 
 
