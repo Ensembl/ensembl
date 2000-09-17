0c846ae375ee6cb30397a6be4157726a52c91049
Ewan Birney <birney@sanger.ac.uk>
2000-09-17 15:15:32 +0000

    added makeunique...

---
+++ misc-scripts/golden_path/makeunique.pl
@@ -0,0 +1,15 @@
+#!/usr/local/bin/perl
+
+open(ERROR,">nonunique.txt");
+
+while(<>) {
+    chomp;
+    ($f,$c,$rid,$cstart,$cend,$fstart,$fend,$rstart,$rend,$ori,$type) = split;
+
+    if( defined $h{$rid} ) {
+	print ERROR "$_\n";
+	next;
+    }
+
+    $h{$rid} = 1;
+}
