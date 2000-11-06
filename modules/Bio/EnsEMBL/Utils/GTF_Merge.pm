46c7f43b9d462b91a28a00fda9c450b8dd60aa75
Ewan Birney <birney@sanger.ac.uk>
2000-11-06 13:30:55 +0000

    updated GTF_Merge

--- modules/Bio/EnsEMBL/Utils/GTF_Merge.pm
+++ modules/Bio/EnsEMBL/Utils/GTF_Merge.pm
@@ -59,117 +59,156 @@ use Carp;
 
 @EXPORT_OK = ( 'gtf_merge');
 
+=head2 gtf_merge
+
+ Title   : gtf_merge
+ Usage   : &gtf_merge(\*INPUT,\*OUTPUT,'IGI_M3_');
+ Function: Merges transcripts on the basis of overlapping
+           exons and writes out the merged file to output
+           The genes are given the new numbering scheme based on
+           the prefix written above
+ Example :
+ Returns : nothing
+ Args    : read stream, MUST BE SORTED by contig, strand, start
+           (sort -k1,1 -k7,7 -k4,4n in UNIX commands)
+           output stream
+           prefix
+
+
+=cut
 
 sub gtf_merge {
-    my ($infile1,$infile2,$outfile,$new_prefix) = @_;
+    my ($sort,$out,$new_prefix) = @_;
 
 
     if( !defined $new_prefix ) {
-	&confess("Did not pass in new_prefix - must be infile1,infile2,outfile,new_prefix");
+	&confess("Did not pass in new_prefix - must be sort,out,new_prefix");
     }
 
-    if( !-e $infile1 ) { &confess("$infile1 does not exist!") }
-    if( !-e $infile2 ) { &confess("$infile2 does not exist!") }
-    open(OUT,">$outfile") || &confess("Unable to open outfile $!");
-    
-    # this sorts all the exon lines first by ctg, then strand, then start position
-    open(SORT,"cat $infile1 $infile2 | grep 'exon' | sort +0 +6 +4n |") || &confess("Unable to spawn pipe!");
-
-    my ($prevctg,$prevstrand,$prevtrans,$prevend);
+    my ($prevctg,$prevstrand,$prevtrans,$prevend,$prevstart);
     my %thash;
+    my %seenctg;
+    my %ghash;
     my $unique_number = 1;
     
 
     # main loop. Only process things with transcript_id lines
     # see whether previous line is contig or strand different, in which case,
     # simply move to the next line.
-    
-    # otherwise see if there is an overlap. %thash is a hash of transcript_ids
-    # pointing to new gene ids. The gene_ids start being called as 'single'.
-    # and get reassigned when overlaps occur
 
-    while( <SORT> ) {
+    # save each line in @lines
+    
+    # each transcript creates an igi when first seen
+    # when an overlap occurs, all the igis assigned to the later
+    # transcript (may well be more than one, though one is likely to
+    # to be the most often case) get transfered to previous overlapped igi
+    
+    my @lines;
+
+    while( <$sort> ) {
+	push(@lines,$_);
 	/transcript_id\s+"(\S+)"/ || next;
 	my $trans = $1;
+	
 	if( !defined $thash{$trans} ) {
-	    $thash{$trans} = 'single';
+	    my $new_igi = "temp_" . $unique_number;
+	    $unique_number++;
+	    $thash{$trans} = $new_igi;
+	    $ghash{$new_igi} = [];
+	    push(@{$ghash{$new_igi}},$trans);
 	}
 
 	my ($ctg,$source,$tag,$start,$end,$score,$strand) = split;
 	if( !defined $prevctg || $ctg ne $prevctg || $strand ne $prevstrand) {
 	    # new contig/strand/file
+	    if( $strand eq $prevstrand && $seenctg{$ctg} ) {
+		&confess("Not sorted GTF file - Seen $ctg already but in a between contig move!");
+	    }
+	    $seenctg{$ctg} = 1;
 	    $prevctg   = $ctg;
 	    $prevend = $end;
 	    $prevtrans   = $trans;
 	    $prevstrand = $strand;
+	    $prevstart  = $start;
 	    next;
 	} 
 	# else potential merge
 	  
+	# non sorted catcher
+	if( $start < $prevstart ) {
+	    &confess("Not sorted GTF file - start $ctg:$start is smaller than previous line of $ctg:$prevstart");
+	}
+
+
 	if( $start <= $prevend ) {
-	    # merge candidate
-	    if( $thash{$prevtrans} eq 'single' ) {
-		# new cluster between prevtrans and this one!
-		my $new_gene = $new_prefix . "_" . $unique_number;
-		$unique_number++;
-		$thash{$prevtrans} = $new_gene;
-		$thash{$trans}     = $new_gene;
-	    } else {
-		# existing cluster
-		$thash{$trans} = $thash{$prevtrans};
-	    }
+	    # merge $trans into $prevtrans
+
+
+	    my $combined_igi  =  $thash{$prevtrans};
+	    my $dead_igi      = $thash{$trans};
+	    foreach my $t ( @{$ghash{$dead_igi}} ) {
+		# move it across to the prevtrans igi
+		push(@{$ghash{$combined_igi}},$t);
+		# reset this guys thash
+		$thash{$t} = $combined_igi;
+	    } 
+            # delete igi from $trans
+	    delete $ghash{$dead_igi};
 	}
 
-	$prevctg    = $ctg;
-	$prevend    = $end;
-	$prevtrans  = $trans;
-	$prevstrand = $strand;    
+	if( $prevend <= $end ) {
+	    $prevend    = $end;
+	    $prevtrans  = $trans;
+	}
+	$prevstart = $start;
 
     }
 
 
-    close(SORT) || &confess("Pipe did not open successfully, $!");
+    close($sort) || &confess("Unable to close sorted GTF file stream $!");
 
-    # assign all 'single' transcripts their own separate gene
-
-    foreach my $t ( keys %thash ) {
-	if( $thash{$t} eq 'single' ) {
-	    my $new_gene = $new_prefix . "_" . $unique_number;
-	    $unique_number++;
-	    $thash{$t} = $new_gene;
-	}
-    }
 
     # dump results for the moment
+    #
+    #foreach my $t ( keys %thash ) {
+    #	print STDERR "$thash{$t}\t$t\n";
+    #}
 
-    foreach my $t ( keys %thash ) {
-	print STDERR "$thash{$t}\t$t\n";
+    # reassign unique numbers
+    $unique_number = 1;
+    my %finaligi;
+    
+    foreach my $igi ( keys %ghash ) {
+	my $new_igi = $new_prefix . "_" . $unique_number;
+	$unique_number++;
+	# point final to old array
+	$finaligi{$new_igi} = $ghash{$igi};
+	# loop over old array, switching thash to new
+	foreach my $t ( @{$finaligi{$new_igi}} ) {
+	    $thash{$t} = $new_igi;
+	}
     }
 
-    # process final output
 
-    open(CAT,"cat $infile1 $infile2 |") || &confess("Could not spawn pipe");
-
-    while( <CAT> ) {
-	/^#/ && do { print OUT $_; next; };
-
+    
+    foreach ( @lines ) {
+	/^#/ && do { print $out $_; next; };
+	
 	my ($trans,$gene);
-	/gene_id\s+"(\S+)"/ && do { $gene = $1; };
 	/transcript_id\s+"(\S+)"/ && do { $trans = $1; };
 
-	if( !defined $trans || !defined $gene ) {
-	    print STDERR "Line has no transcript or gene id. Cannot merge";
+	if( !defined $trans ) {
+	    print STDERR "Line has no transcript id. Cannot provide IGI ";
+	    print $out $_;
 	    next;
 	}
 
-	my $new_gene = $thash{$trans};
+	my $igi = $thash{$trans};
+	s/transcript_id/igi_id "$igi" transcript_id/;
 
-	s/gene_id\s+"$gene"/gene_id "$new_gene"/;
-
-	print OUT $_;
+	print $out $_;
     }
-	
-    close(CAT) || &confess("cat pipe did not open successfully $!");
+
 
 
 }
