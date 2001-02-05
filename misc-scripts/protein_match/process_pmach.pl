7530dd534680a8c3c8dff60e6911603a64c1bec1
Emmanuel Mongin <mongin@sanger.ac.uk>
2001-02-05 15:08:03 +0000

    Some small important changes...

--- misc-scripts/protein_match/process_pmach.pl
+++ misc-scripts/protein_match/process_pmach.pl
@@ -1,4 +1,20 @@
-#use strict;
+use strict;
+
+=head1 Process pmatch
+
+=head1 Description
+
+=head2 Aims
+This script aims to run pmatch and postprocess pmatch to map Ensembl peptides to external databases (currently Swissprot and Refseq but may be extented). The first part of the script runs pmatch, the second part gets the percentage of a match of a unique Ensembl peptide which match to an unique external protein. The third part classify each ensembl match as PRIMARY match (the longest one and the one which will be used for the mapping, PSEUDO, DUPLICATE and REPEAT (pretty arbitrary criterias but may be useful for quality control).
+NB: All of the intermediary files are written.
+
+=head2 Options
+-ens : Ensembl peptide fasta file
+-sp : SP, SPTREMBL fasta file
+-refseq: Refseq peptide fasta file
+
+=cut  
+
 use Getopt::Long;
 
 my ($ens,$sp,$refseq);
@@ -23,8 +39,8 @@ sub runpmatch {
     my $pmatch1 = "/nfs/griffin2/rd/bin.ALPHA/pmatch $sp $ens > ens_sp_rawpmatch";
     my $pmatch2 = "/nfs/griffin2/rd/bin.ALPHA/pmatch $refseq $ens > ens_refseq_rawpmatch";
     
-    system($pmatch1);# == 0 or die "$0\Error running '$pmatch1' : $!";
-    system($pmatch2);# == 0 or die "$0\Error running '$pmatch2' : $!";
+    system($pmatch1); # == 0 or die "$0\Error running '$pmatch1' : $!";
+    system($pmatch2); #== 0 or die "$0\Error running '$pmatch2' : $!";
 }
 
 
@@ -33,7 +49,7 @@ sub postprocesspmatch {
     my ($db) = @_;
     my %hash1;
     
-       #open (OUT, ">../data/$db.processed");
+      
 #Post process the raw data from pmatch
     if ($db eq $sp) {
 	print STDERR "Postprocessing pmatch for SP mapping\n";
@@ -48,12 +64,17 @@ sub postprocesspmatch {
     }
     
     while (<PROC>) {
-	## 20      Q9UN99  36      55      8.1     ENSP00000051351 63      82      7.8
+#538     COBP00000033978 1       538     35.3    Q14146  1       538     35.3 
 	my ($len,$id,$start,$end,$perc,$query,$qst,$qend,$qperc) = split;
-	#print STDERR "$id:$query";
+
+	if ($db eq $refseq) {
+	    #Get the refseq ac (NP_\d+) 
+	    ($query) = $query =~ /\w+\|\d+\|\w+\|(\w+)/;
+	}
+
 	my $uniq = "$id:$query";
 	
-	$hash1{$uniq} += $qperc;
+	$hash1{$uniq} += $perc;
     }
 
 
@@ -62,7 +83,8 @@ sub postprocesspmatch {
 	($a,$b) = split(/:/,$key);
 	print OUT "$a\t$b\t$hash1{$key}\n";
     }
-   close (OUT);                               
+    close (PROC);
+    close (OUT);                               
 }
 
 sub finalprocess {
@@ -90,7 +112,7 @@ sub finalprocess {
 	    $hash2{$known} = [];
 	}
 	
-	$p= NamePerc->new;
+	my $p= NamePerc->new;
 	$p->name($ens);
 	$p->perc($perc);
 	
@@ -99,10 +121,10 @@ sub finalprocess {
 
 
     foreach my $know ( keys %hash2 ) {
-	@array = @{$hash2{$know}};
+	my @array = @{$hash2{$know}};
 	@array = sort { $b->perc <=> $a->perc } @array;
 	
-	$top = shift @array;
+	my $top = shift @array;
 	print  OUT "$know\t",$top->name,"\t",$top->perc,"\tPRIMARY\n";
 	
 	foreach $ens ( @array ) {
@@ -128,6 +150,7 @@ sub finalprocess {
 	    } 
 	}              
     } 
+    close (PROC);
     close (OUT);
 }
 
