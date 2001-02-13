8aa3b52da083ef7d8fe8d020b3cd960f62a33b91
Elia Stupka <elia@sanger.ac.uk>
2001-02-13 16:26:44 +0000

    Archive schema

--- sql/archive.sql
+++ sql/archive.sql
@@ -20,10 +20,48 @@ CREATE TABLE sequence (
   PRIMARY KEY (id,version,seq_type)
 );
 
+CREATE TABLE dead_genes (
+  id varchar(40) DEFAULT '' NOT NULL,
+  PRIMARY KEY (id)
+);
+
 CREATE TABLE deleted_id (
   old_id varchar(40) DEFAULT '' NOT NULL,
   old_version int(10) DEFAULT '' NOT NULL,
   new_id varchar(40), 
   id_type varchar(40) DEFAULT '' NOT NULL, 
   PRIMARY KEY (old_id,id_type)
+);
+
+
+create TABLE gene_stable (
+       internal_id            int(10) unsigned NOT NULL auto_increment,
+       external_id	     varchar(40) NOT NULL,
+       created   datetime NOT NULL,
+       PRIMARY KEY(internal_id),
+       UNIQUE (external_id)
+);
+
+create TABLE exon_stable (
+       internal_id            int(10) unsigned NOT NULL auto_increment,
+       external_id	     varchar(40) NOT NULL,
+       created   datetime NOT NULL,
+       PRIMARY KEY(internal_id),
+       UNIQUE (external_id)
+);
+
+create TABLE transcript_stable (
+       internal_id            int(10) unsigned NOT NULL auto_increment,
+       external_id	     varchar(40) NOT NULL,
+       created   datetime NOT NULL,
+       PRIMARY KEY(internal_id),
+       UNIQUE (external_id)
+);
+
+create TABLE translation_stable (
+       internal_id            int(10) unsigned NOT NULL auto_increment,
+       external_id	     varchar(40) NOT NULL,
+       created   datetime NOT NULL,
+       PRIMARY KEY(internal_id),
+       UNIQUE (external_id)
 );
