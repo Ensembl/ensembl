cc7007ea593ccdcd38ad77eebd71e20d577d59af
Philip Lijnzaad <lijnzaad@sanger.ac.uk>
2001-03-16 14:55:53 +0000

    *** empty log message ***

--- scripts/igi/fgenesh.collate.sh
+++ scripts/igi/fgenesh.collate.sh
@@ -6,12 +6,11 @@
 convert=fgenesh-chromo2fpc.pl # in your src/ensembl/scripts dir
 
 infiles=*.sgp.gff
-outfile=./all.gtf
-rm $outfile
+
 
 # $sort="sort -k1,1 -k7,7 -k4,4n "
 
-for f in $infiles; do
-    nawk -F\t '$3=="exon" || $3 ~ "_codon" ' $f |  $convert >> $outfile
+for f in "$@" ; do
+    nawk -F\t '$3=="exon" || $3 ~ "_codon" ' $f |  $convert
 done
-#c'est tout
+
