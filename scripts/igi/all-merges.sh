388331aa0ac60e0b4412b0c58e2789f68218dfab
Philip Lijnzaad <lijnzaad@sanger.ac.uk>
2001-03-27 08:30:16 +0000

    *** empty log message ***

--- scripts/igi/all-merges.sh
+++ scripts/igi/all-merges.sh
@@ -13,12 +13,18 @@
 ens=ensembl/all.gtf
 affy=affymetrix/all.gtf
 fgenesh=fgenesh/chr_gff/all.gtf 
+outdir=merges
 
 prefix=igi3
 
-gtf_merge.pl -p $prefix $ens $affy > ens_affy.merge  2> ens_affy.log
-gtf_merge.pl -p $prefix $ens $fgenesh > ens_fgenesh.merge 2> ens_fgenesh.log
-gtf_merge.pl -p $prefix $affy $fgenesh > affy_fgenesh.merge 2> affy_fgenesh.log
-gtf_merge.pl -p $prefix $ens $affy $fgenesh > ens_affy_fgenesh.merge 2>ens_affy_fgenesh.log
+gtf_merge.pl -p $prefix $ens $affy > $outdir/ens_affy.merge  2> ens_affy.log
+gtf_merge.pl -p $prefix $ens $fgenesh > $outdir/ens_fgenesh.merge 2> ens_fgenesh.log
+gtf_merge.pl -p $prefix $affy $fgenesh > $outdir/affy_fgenesh.merge 2> affy_fgenesh.log
+gtf_merge.pl -p $prefix $ens $affy $fgenesh > $outdir/ens_affy_fgenesh.merge 2>ens_affy_fgenesh.log
+
+cd $outdir
+for i in *.merge; do
+    gzip < $i > $i.gz
+done
 
 ### after this, run the statistics on these files using all-stats.sh
