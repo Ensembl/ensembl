0acf50b0d6cf9271673c74eba69859495354d62b
Philip Lijnzaad <lijnzaad@sanger.ac.uk>
2001-03-30 16:32:16 +0000

    *** empty log message ***

--- scripts/igi/fgenesh.missing-stats.sh
+++ scripts/igi/fgenesh.missing-stats.sh
@@ -19,13 +19,12 @@ fi
 
 logfile=$1
 
-
 awk -F';' '/initial/{print $1}' $logfile | sort -u > $init
-awk -F';' '/ignor/{print $1}' $logfile | sort -u > $missing
+awk -F';' '/cannot/{print $1}' $logfile | sort -u > $missing
 
 chroms="1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 X Y"
 
-
+rm $tmp
 echo "# features assumed to lie on first (missing) bit of fpc_contig" > $tmp
 for c in $chroms; do
     echo -n "chr$c:" >> $tmp
@@ -38,10 +37,10 @@ echo "# missing features" >> $tmp
 chroms="1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 X Y"
 for c in $chroms; do
     echo -n "chr$c:" >> $tmp
-    awk -F';' '/ignor/{print $1}' $logfile | grep chr$c | sort -u | wc -l  >> $tmp
+    awk -F';' '/cannot/{print $1}' $logfile | grep chr$c | sort -u | wc -l  >> $tmp
 done
 echo -n "# total" >> $tmp
-awk -F';' '/ignor/{print $1}' $logfile | sort -u | wc -l >> $tmp
+awk -F';' '/cannot/{print $1}' $logfile | sort -u | wc -l >> $tmp
 
 awk -F: '/#/ || $2 > 0' < $tmp > $stats
 exit 0    
