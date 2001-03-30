ba9ab6533a4961e0c73fea6a0416123e35fe863a
Philip Lijnzaad <lijnzaad@sanger.ac.uk>
2001-03-30 16:27:51 +0000

    fixed status checking; also will now ignore the inferred 'initial' coodinates

--- scripts/igi/fgenesh-chromo2fpc.pl
+++ scripts/igi/fgenesh-chromo2fpc.pl
@@ -73,6 +73,7 @@ sub read_mapping {
     return;
 }
 
+# return fpc_ctg_name, start and end, status, given the chromosome+start/end
 sub map_coords {
     my ($chr, $start, $end) = @_;
 
@@ -81,7 +82,7 @@ sub map_coords {
 
     if (! defined $contigs) {
         warn "don't know $chr";
-        return ('uknown', -999, -999);
+        return ('ctg_on_unknown_chromosome', -999, -999, 'bug');
     }
 
     foreach my $l ( @{$contigs} ) {
@@ -91,7 +92,7 @@ sub map_coords {
               = @$l;
             return ($fpcctg_name,
                     $fpcctg_start-1+ $start - $chr_start,
-                    $fpcctg_start-1+ $end - $chr_start);
+                    $fpcctg_start-1+ $end - $chr_start, undef);
         }
     }
     # this happens (only?) if we have an fpccontig with start != 1; 
@@ -107,20 +108,19 @@ sub map_coords {
                 # this means we can't pretend that it's on the missing
                 # piece of this contig; so this contig is simply missing
                 # from our static_golden_path
-                return ('missing', -999, -999, 'missing');
+                return ('missing_ctg', -999, -999, 'missing');
             }
             #else:
             # fpccontig_start is not 1; now just pretend this feature
             # is on the 'unknown' first bit of the current fpc contig,
             # and pretend our fpccontig actually did start at 1:
+            my $status = 'init';
             $chr_start -= ($fpcctg_start -1);
             $fpcctg_start =1;
-            my ($newstart)  = $fpcctg_start-1+ $start - $chr_start;
-            my ($status) = 'init';
+            my $newstart  = $fpcctg_start-1+ $start - $chr_start;
             $status = 'missing' if ($newstart < 1); 
-
-            # i.e., it is too far away from our fpcctg_start to still be
-            # able to pretend it's still on this contig, so report as missing
+            # (i.e., it is too far away from our fpcctg_start to still be
+            # able to pretend it's still on this contig, so report as missing)
 
             return ($fpcctg_name,
                     $newstart, 
@@ -128,33 +128,36 @@ sub map_coords {
         }
         $prev = $curr;
     }
-    warn "really couldn find coords of $chr $start $end";
-    return ('bug2', -999, -999, 'bug?');
-}
+    warn "really couldn find coords of $chr $start $end\n";
+    return ('bug_ctg', -999, -999, 'bug');
+}                                       # map_coords
 
 
 while(<>)  {
-
-    my ($chr, $source, $tag, $start, $end, $score, $strand, $frame, @rest)  = split("\t");
+    chomp($_);
+    my ($chr, $source, $tag, $start, $end, $score, $strand, $frame, @rest) 
+      = split("\t");
 ### depracated, much too slow (2 s per query)
 ###    ($chr, $start, $end) = $stadaptor->convert_chromosome_to_fpc($chr,$start, $end);
 
-    my ($contig, $ctg_start, $ctg_end, $status) = map_coords($chr,$start, $end);
-
-
-    #format is a bit different:
+    #reformat a bit as well:
     grep( s/ ([^ ]+)/ "$1";/ , @rest);
     my $rest = join(' ', @rest);
     chomp $rest;
 
+    my ($contig, $ctg_start, $ctg_end, $status) = map_coords($chr,$start, $end);
 
-    if ($status ne 'missing') { 
-        print join("\t", ($contig, $source, $tag, $ctg_start, $ctg_end, $score, $strand, $frame, $rest)), "\n";;
-
-        if ($status eq 'init' ) {
-            warn "$chr: assumed that $rest lies on missing initial part of $contig";
-        }
-    } else  {# ($status eq 'missing')
-        warn "$chr: cannot find contig for $rest; ignoring it";
+    if (! $status ) {
+        print join("\t", ($contig, $source, $tag, 
+                          $ctg_start, $ctg_end, $score, 
+                          $strand, $frame, $rest)), "\n";
+    } elsif ($status eq 'init' ) {
+        warn "$chr: ignoring it, but it might lie on missing initial part of $contig: $_\n";
+    } elsif ($status eq 'missing') { 
+        warn "$chr: cannot find contig for $_; ignoring it\n";
+    } elsif ($status eq 'bug') {
+        warn "$chr: bug with $_; ignoring it\n";
+    } else {
+        die "Bugger";               # even bigger bug
     }
-}
+}                                       # while
