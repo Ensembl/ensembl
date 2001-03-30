86a832c71d0301f22ce621273504c508034723c7
Philip Lijnzaad <lijnzaad@sanger.ac.uk>
2001-03-30 10:22:25 +0000

    docu

--- scripts/igi/fgenesh.collate.sh
+++ scripts/igi/fgenesh.collate.sh
@@ -1,12 +1,11 @@
 #!/bin/sh -x
 # -*- mode: sh; -*-
 
-# mangle all files into one big happy file
-
-convert=fgenesh-chromo2fpc.pl # in your src/ensembl/scripts dir
+# mangle all files into one big happy file; have to convert chromosome to fpc
+# coordinates first:
+convert=fgenesh-chromo2fpc.pl # in ensembl/scripts/igi
 
 infiles=*.sgp.gff
-
 
 # $sort="sort -k1,1 -k7,7 -k4,4n "
 
