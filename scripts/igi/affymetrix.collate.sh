49e1afd4fa9ebac118b83ee446323c02ea71fb2e
Philip Lijnzaad <lijnzaad@sanger.ac.uk>
2001-03-30 11:04:37 +0000

    chromosome names now come from command line, and there are no intermediate files anymore

--- scripts/igi/affymetrix.collate.sh
+++ scripts/igi/affymetrix.collate.sh
@@ -1,20 +1,8 @@
 #!/bin/sh -x
 # -*- mode: sh; -*-
 
-# concatenate everything into one big happy sorted file
-chroms="1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 X Y UL NA"
-echo "make sure that these are the right chromosomes:" | tee /dev/tty >&2
-echo $chroms >&2
-echo "" > /dev/tty
-
-for c in $chroms; do
-# untested, nawk bit used to be done at end
-    find $c -name '*.affymetrix.gtf' -exec nawk -F\t '$3=="exon" || $3 ~ "_codon" ' {} \; > $c/all.collated
-done
-
-# for c in $chroms; do
-#     sort -k1,1 -k7,7 -k4,4n  $c/all > $c/all.sorted
-# done
-
-sort -m -k1,1 -k7,7 -k4,4n `find . -name 'all.collated' -print` 
+for chr in "$@"; do
+    find $chr -name '*.affymetrix.gtf' -exec \
+      nawk -F\t '$3=="exon" || $3 ~ "_codon" ' {} \;
+done | sort -m -k1,1 -k7,7 -k4,4n 
 
