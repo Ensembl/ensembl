f74e048c218914605ea037024aeed316de5f0ad8
Philip Lijnzaad <lijnzaad@sanger.ac.uk>
2001-03-30 16:32:48 +0000

    testing for existence of dirs; catching stderr in appropriate way

--- scripts/igi/all-collates.sh
+++ scripts/igi/all-collates.sh
@@ -15,19 +15,21 @@ igihome=$HOME/proj/igi                  # change to needs
 
 all=all.gtf                             # where local collated data goes to
 log=collate.log                         # where local log goes to
+rename=remap-sources.sed                # to translate the source field
+
 ### EnsEMBL:
 dir=$igihome/ensembl                    # can be symlink
 [ ! -d $dir ] && echo "$dir: not found" >&2 && exit 1
 cd $dir
 [ -f $all  ] && echo "found $all, not overwriting it ">&2 && exit 1
-ensembl.collate.sh < ensembl.gtf > $all 2> $log
+ensembl.collate.sh < ensembl.gtf 2> $log | $rename > $all
 ### end Ensembl
 
 
 ### Affymetrix
 ## It has everything in chromosome directories that don't seem to
 ## change across releases, but warn about this:
-chroms="1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 X Y UL NA"
+chroms="1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 X Y"  #UL NA"
 echo "make sure that these are the right chromosomes:" | tee /dev/tty >&2
 echo $chroms >&2
 echo "" > /dev/tty
@@ -35,7 +37,7 @@ dir=$igihome/affymetrix                    # can be symlink
 [ ! -d $dir ] && echo "$dir: not found" >&2 && exit 1
 cd $dir
 [ -f $all  ] && echo "found $all, not overwriting it ">&2 && exit 1
-affymetrix.collate.sh  $chroms > $all 2> $log 
+affymetrix.collate.sh  $chroms 2> $log   | $rename > $all 
 ### end Affymetrix
 
 ### Softberry/Fgenesh
@@ -43,7 +45,18 @@ dir=$igihome/fgenesh                    # can be symlink
 [ !  -d $dir ] && echo "$dir: not found" >&2 && exit 1
 cd $dir
 [ -f $all  ] && echo "found $all, not overwriting it ">&2 && exit 1
-fgenesh.collate.sh chr_gff/*.sgp.gff chr_r/*.sgp.gff  > $all 2>$log
+
+c2f_log=chromo2fpc.log
+# fgenesh.collate.sh chr_gff/*.sgp.gff chr_r/*.sgp.gff  > $all 2>$log
+fgenesh.collate.sh chr_gff/*.sgp.gff 2>$log | $rename > $all
+
+#  fgenesh chromo+chromo_coords -> fpcctg+fpc_coords mapping may be funny, run
+#  statistics on this:
+mkdir missing-stats
+cd missing-stats
+fgenesh.missing-stats.sh ../collate.log  # puts stuff into missing-stats/*
+
+
 ### end Softberry/Fgenesh
 
 
