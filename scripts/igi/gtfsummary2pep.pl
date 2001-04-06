ce81c2788bb79fa2ed1ec01359f697e5ea573975
Philip Lijnzaad <lijnzaad@sanger.ac.uk>
2001-04-06 16:53:33 +0000

    works finally ... one small bug still there, if a peptide of one source is missing, the whole ipi disappears (happens for ENSG00000105155 and ENSG00000110755, which for some reason were not in the peptide dump). Ignored for now.

--- scripts/igi/gtfsummary2pep.pl
+++ scripts/igi/gtfsummary2pep.pl
@@ -25,49 +25,123 @@ die $usage if $help;
 
 open(SUMMARY, "< $summary") || die "Summary file $summary: $!";
 
-my $all_igis = Bio::EnsEMBL::Utils::igi_utils::read_igis_from_summary(\*SUMMARY); # all igi's in one big hash
-# (a hash ref of igi to native_ids)
+# put all igi's in one big hash(ref) (a table of of igi to lists of
+# native_ids):
+my $all_igis = 
+  Bio::EnsEMBL::Utils::igi_utils::read_igis_from_summary(\*SUMMARY); 
 
-my $blurp = blurp();
-print $blurp;
-print <<MOREBLURP
-### IPI file {based on summary/containing peptides} from @ARGV.
-MOREBLURP
-  ;
+my $igis_of_natives = igi_of_native($all_igis);
+my $all_peptides={};
 
-foreach $peptides ( @ARGV) {
-    open(SOURCE, "<$peptides") || die "$peptides:$!";
-    read_and_print(\*SOURCE, $all_igis);
-    close(SOURCE);
+foreach my $file ( @ARGV) {
+    warn "reading $file ...\n";
+    read_peptide_file($all_peptides, $file, $igis_of_natives);
+    warn " done\n";
 }
 
-### simple log message
-sub blurp {
-    my (@stuff)  = ("on", `date`, "by", `whoami`, "@",  `hostname`);
-    foreach (@stuff) {chomp};
-    my $s =  '### Produced by $Id$  ' . "\n" 
-      . "### run " . join(' ',@stuff) .  "\n"
-      . "### for EnsEMBL (http://www.ensembl.org)\n"
-      . "### argument(s): ". join(' ', @argv_copy) . "\n";
-    foreach (@argv_copy) {   $s .= "### ", `ls -l $_`; }
-    $s .= "###\n";
-    $s;
-}                                       # blurp
+# my $blurp = blurp();
+# print $blurp;
+# print <<MOREBLURP
+# ### IPI file {based on: see arguments line)
+# MOREBLURP
+#   ;
 
-sub read_and_print {
-    my ($IN, $all_igis) = @_;
+print_peptides($all_igis, $all_peptides);
+
+# ### simple log message
+# sub blurp {
+#     my (@stuff)  = ("on", `date`, "by", `whoami`, "@",  `hostname`);
+#     foreach (@stuff) {chomp};
+#     my $s =  '### Produced by $Id$  ' . "\n" 
+#       . "### run " . join(' ',@stuff) .  "\n"
+#       . "### for EnsEMBL (http://www.ensembl.org)\n"
+#       . "### argument(s): ". join(' ', @argv_copy) . "\n";
+#     foreach (@argv_copy) {   $s .= "### ", `ls -l $_`; }
+#     $s .= "###\n";
+#     $s;
+# }                                       # blurp
+
+# invert the igis hash so we can lookup the igi given a native id:
+sub igi_of_native  {
+    my ($all_igis) = @_;
+    my %igis_of_natives = undef;
+    foreach my $igi (keys %$all_igis) {
+        my @natives = @{$all_igis->{$igi}};
+        foreach my $nat ( @natives ) {  # $nat includes the source prefix,
+                                        # e.g. FGENH:S.C16000125
+            $igis_of_natives{$nat} = $igi;
+        }
+    }
+    return \%igis_of_natives;
+}
+
+# add peptides all into one big ugly hash
+sub read_peptide_file {
+    my ($peptides, $file, $igis_of_natives) = @_;
+    my ($kept, $ignored) = (0,0);
+
+    open(IN, $file) || die "$file: $!";
 
     local( $/ ) = "\n>";
-    while( $_=<> ) {
+  PEPTIDE:
+    while( <IN> ) {
         s/>//g;
         my $n =(index $_, "\n");
         my $firstline =  substr($_, 0, $n);
-        my $rest = substr($_, $n+1);
+        my $sequence = substr($_, $n+1);
+        my $seqlen = ($sequence =~ tr /a-zA-Z//);
 
-        my ($native_id, $rest) = ( />([^ \t])+(.*)$/);
-        die "no native_id for $." unless $native_id;
+        my ($native_id, $rest) = ( $firstline =~ /(\S+)(.*)$/);
+        unless ($native_id) {
+            die "no native_id for $." 
+        }
 
-        print "first line: '$firstline'\n";
-        print "rest: '$rest'\n";
+        my $igi = $igis_of_natives->{$native_id};
+        
+        if (! defined($igi)) {; # didn't make it into the IGI set
+            $ignored++;
+            next PEPTIDE ;
+        }
+        $kept++;
+        push @{$peptides->{$igi}}, [$seqlen, $native_id, $rest, $sequence ];
     }
-}                                       # read_and_print
+    warn "From $file, kept $kept , ignored $ignored peptides\n";
+    close(IN);
+    return;
+}                                       # read_peptide_file
+
+
+sub print_peptides { 
+    my ($igis,$peptides) = @_; 
+    my ($ignored, $kept) = (0,0);
+
+  IGI:
+    foreach my $igi (sort { substr($a,5) <=> substr($b,5) } keys %$igis) {
+        # (this sorts on the numerical part of the "igi3_" identifier)
+        my $peps= $peptides->{$igi};
+        if (!defined $peps) {
+            warn "no  peptides for $igi\n";
+            next IGI;
+        }
+        my (@peps) = @$peps;
+        
+        my $max = -1;
+        my $maxpep = undef;
+        # get the longest (or ENS if equal;-) peptide:
+        my @all_natives; # 
+        foreach my $pep (@peps) {
+            my $len = $ {$pep}[0];
+            my $native = $ {$pep}[1];
+            push(@all_natives, $native);
+            if ($len > $max  || ( $len == $max && $native =~ /^ENS/ )  ) {
+                $max=$len;
+                $maxpep = $pep;
+            }
+        }
+        my ($len, $native_id, $rest, $sequence) = @$maxpep;
+        my @others = grep ( $_ ne $native_id, @all_natives);
+        my $others = join(',', @others);
+        my $newheader = ">$igi $native_id; length: $len; others: $others; original: $rest";
+#        print "$newheader\n$sequence";
+    }
+}                                       # print_peptides
