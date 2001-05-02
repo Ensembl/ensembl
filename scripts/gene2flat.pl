fc66f19a7dc1a7fbf72b793c820de0e94ee5c604
Michele Clamp <michele@sanger.ac.uk>
2001-05-02 16:25:18 +0000

    Added chromosome and chromosome coords to webdump Removed TimDB lines

--- scripts/gene2flat.pl
+++ scripts/gene2flat.pl
@@ -15,7 +15,6 @@ in particular the protein translation
 
 =head1 OPTIONS
 
-    -dbtype    Database type (only used for TimDB)
 
     -dbhost    host name for database (gets put as host= in locator)
 
@@ -25,7 +24,7 @@ in particular the protein translation
 
     -dbpass    For RDBs, what password to use (dbpass= in locator)
 
-    -module    Module name to load (Defaults to Bio::EnsEMBL::DBSQL::Obj)
+    -module    Module name to load (Defaults to Bio::EnsEMBL::DBSQL::DBAdaptor)
 
     -format    [pep/dump/transcript] dump in peptides/info/dna format
 
@@ -50,9 +49,8 @@ in particular the protein translation
 
 use strict;
 
-use Bio::EnsEMBL::DBSQL::Obj;
+use Bio::EnsEMBL::DBSQL::DBAdaptor;
 use Bio::EnsEMBL::DBLoader;
-use Bio::EnsEMBL::TimDB::Obj;
 use Bio::SeqIO;
 
 use Bio::EnsEMBL::Utils::Eprof('eprof_start','eprof_end','eprof_dump');
@@ -60,12 +58,12 @@ use Bio::EnsEMBL::Utils::Eprof('eprof_start','eprof_end','eprof_dump');
 use Getopt::Long;
 
 my $dbtype = 'rdb';
-my $host   = 'localhost';
-my $port   = '410000';
-my $dbname = 'matloob_freeze17';
+my $host   = 'ecs1c';
+my $port   = '3360';
+my $dbname = 'ensembl080';
 my $dbuser = 'root';
 my $dbpass = undef;
-my $module = 'Bio::EnsEMBL::DBSQL::Obj';
+my $module = 'Bio::EnsEMBL::DBSQL::DBAdaptor';
 
 my $format  = 'transcript';
 my $usefile = 0;
@@ -105,12 +103,9 @@ if ($help) {
 
 &eprof_start('init');
 
-if ( $dbtype =~ 'timdb' ) {
-    $db = Bio::EnsEMBL::TimDB::Obj->new('',$noacc,$test);
-} else {
-    my $locator = "$module/host=$host;port=$port;dbname=$dbname;user=$dbuser;pass=$dbpass";
-    $db =  Bio::EnsEMBL::DBLoader->new($locator);
-}
+
+my $locator = "$module/host=$host;port=;dbname=$dbname;user=$dbuser;pass=$dbpass";
+$db =  Bio::EnsEMBL::DBLoader->new($locator);
 
 my @gene_id;
 
@@ -139,7 +134,9 @@ if( $format eq 'id' ) {
 }
 
 if ($format eq 'webdump') {
+  if (! -e $webdir) {
     mkdir($webdir, 0777) or die "Can't create '$webdir' : $!";
+  }
 }
 
 $db->DESTROY;
@@ -157,12 +154,10 @@ while ( @gene_id > 0 ) {
     }
 
     eval {
-	if ( $dbtype =~ 'timdb' ) {
-	    $db = Bio::EnsEMBL::TimDB::Obj->new('',$noacc,$test);
-	} else {
-	    my $locator = "$module/host=$host;port=$port;dbname=$dbname;user=$dbuser;pass=$dbpass";
+	    my $locator = "$module/host=$host;port=;dbname=$dbname;user=$dbuser;pass=$dbpass";
 	    $db =  Bio::EnsEMBL::DBLoader->new($locator);
-	}
+            $db->static_golden_path_type('UCSC');
+            my $sa = $db->get_StaticGoldenPathAdaptor();
 
 	&eprof_start('gene_get');
 	my @genes = $db->gene_Obj->get_array_supporting('none',@chunk_list);
@@ -175,12 +170,17 @@ while ( @gene_id > 0 ) {
 		    # get out first exon. Tag it to clone and gene on this basis
 		    my @exon = $trans->each_Exon;
 		    my $fe = $exon[0];
+
+                    my ($chr,$gene_start,$cdna_start) = find_trans_start($trans);
+
 		    my $tseq = $trans->translate();
 		    if ( $tseq->seq =~ /\*/ ) {
 			print STDERR "translation has stop codons. Skipping! (in clone". $fe->clone_id .")\n";
 			#next;
 		    }
-		    $tseq->desc("Gene:$gene_id Clone:".$fe->clone_id . " Contig:" . $fe->contig_id);
+                    my $gene_version = $gene->version;
+
+		    $tseq->desc("Gene:$gene_id.$gene_version Clone:".$fe->clone_id . " Contig:" . $fe->contig_id . " Chr: " . $chr . " Pos: " . $cdna_start);
 		    $seqio->write_seq($tseq);
 		}
 	    } elsif ( $format eq 'dump' ) {
@@ -197,17 +197,27 @@ while ( @gene_id > 0 ) {
 	    } 
 	    elsif ($format eq 'transcript') {
 		foreach my $trans ( $gene->each_Transcript ) {
+                    my ($chr,$gene_start,$cdna_start) = find_trans_start($trans);
 		    my $seq = $trans->dna_seq();
 		    $seq->id($trans->id);
 		    my @exon = $trans->each_Exon;
 		    print STDERR "Translation start".$trans->translation->start."\n";
 		    my $fe = $exon[0];
-		    $seq->desc("Gene:$gene_id Clone:".$fe->clone_id . " Contig:" . $fe->contig_id);
+                    my $gene_version = $gene->version;
+		    $seq->desc("Gene:$gene_id.$gene_version Clone:".$fe->clone_id . " Contig:" . $fe->contig_id . " Chr: " . $chr . " Pos: " . $cdna_start);
 		    $seqio->write_seq($seq);
 		}
 	    }
 	    elsif ($format eq 'webdump') {
-		foreach my $trans ( $gene->each_Transcript ) {
+	      TRAN: foreach my $trans ( $gene->each_Transcript ) {
+
+		  my $pep_file =  $webdir.$trans->id.".pep";
+
+		  if (-e $pep_file) {
+		    print STDERR "Peptide file exists - skipping\n";
+		    next TRAN;
+		  }
+
 		    my $trans_file = $webdir.$trans->id.".trans";
 		    open (TRANS,">$trans_file");
 		    my $seqiot = Bio::SeqIO->new('-format' => 'Fasta' , -fh => \*TRANS );
@@ -216,12 +226,15 @@ while ( @gene_id > 0 ) {
 		    $seq->display_id($trans->id);
 		    my @exon = $trans->each_Exon;
 		    my $fe = $exon[0];
-		    $seq->desc("Gene:$gene_id Clone:".$fe->clone_id . " Contig:" . $fe->contig_id);
+
+                    my ($chr,$gene_start,$cdna_start) = find_trans_start($trans);
+                    my $gene_version = $gene->version;
+		    $seq->desc("Gene:$gene_id.$gene_version Clone:".$fe->clone_id . " Contig:" . $fe->contig_id . " Chr: " . $chr . " Pos: " . $cdna_start);
 		    $seqiot->write_seq($seq);
 		    $seqiot=undef;
 		    close (TRANS);
 		    
-		    my $pep_file =  $webdir.$trans->id.".pep";
+
 		    open (PEP,">$pep_file");
 		    my $seqiop = Bio::SeqIO->new('-format' => 'Fasta' , -fh => \*PEP) ;
 		    my $tseq = $trans->translate();
@@ -229,7 +242,7 @@ while ( @gene_id > 0 ) {
 			print STDERR "Skipping peptide dumping of ".$gene->id.", translation has stop codons. (in clone ". $fe->clone_id .")\n\n";
 			next;
 		    }
-		    $tseq->desc("Gene:$gene_id Clone:".$fe->clone_id . " Contig: " . $fe->contig_id);
+		    $tseq->desc("Gene:$gene_id.$gene_version Clone:".$fe->clone_id . " Contig:" . $fe->contig_id . " Chr: " . $chr . " Pos: " . $gene_start);
 		    $seqiop->write_seq($tseq);
 		    $seqiop=undef;
 		    close(PEP);
@@ -258,4 +271,54 @@ while ( @gene_id > 0 ) {
 
 
 
+
+sub  find_trans_start {
+ my ($trans) = @_;
+
+ my $start_pos;
+ my $trans_pos;
+
+ my $contig; 
+ foreach my $exon ($trans->each_Exon) {
+    if ($exon->id eq $trans->translation->start_exon_id) {
+       $contig = $exon->contig_id;
+       if ($exon->strand == 1) {
+         $start_pos = $exon->start;
+         $trans_pos = $exon->start + $trans->translation->start - 1;
+       } else {
+         $start_pos = $exon->end;
+         $trans_pos = $exon->end - $trans->translation->start + 1;
+       }
+    }
+ }
+ if (!defined($start_pos)) {
+    print STDERR "Couldn't find start exon for " . $trans->id . "\n";
+    die;
+ }
+ my $query = "select chr_name,chr_start,chr_end,raw_start,raw_end,raw_ori from static_golden_path,contig where raw_id = internal_id and contig.id = \'" . $contig . "\'";
+ my $sth  = $db->prepare($query);
+ my $res  = $sth->execute;
+
+ my $row = $sth->fetchrow_hashref;
+
+ my $chr = $row->{chr_name};
+ my $chr_start = $row->{chr_start};
+ my $chr_end   = $row->{chr_end};
+ my $raw_start = $row->{raw_start};
+ my $raw_end   = $row->{raw_end}; 
+ my $raw_ori   = $row->{raw_ori};
+
+ my $gene_start; 
+ my $cdna_start;
+
+ if ($raw_ori == 1) {
+    $gene_start = $chr_start + ($trans_pos - $raw_start);
+    $cdna_start = $chr_start + ($start_pos - $raw_start);
+ } else {
+    $cdna_start = $chr_end   - ($start_pos - $raw_start);
+    $gene_start = $chr_end   - ($trans_pos - $raw_start);
+ } 
+
+ return ($chr,$gene_start,$cdna_start);
+}
 
