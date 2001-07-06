bfbd1b840e6ed16b54697b314eb3ae04e28a2830
Elia Stupka <elia@sanger.ac.uk>
2001-07-06 12:27:11 +0000

    Creates translation table surgical sql from the grep result of a surgical duplicate eliminating script mapped via the idmapping files... it's important to add useful comments to cvs commits...

---
+++ misc-scripts/surgery/create_fixsql.pl
@@ -0,0 +1,22 @@
+my $old;
+my $new;
+while (<>) {
+    
+    if (/grep (\S+)/) {
+	$old=$1;
+	next;
+    }
+
+    if (/(ENSE\S+)\s(ENSE\S+)/) {
+	if ($2 eq $old) {
+	    $new=$1;
+	    print "update translation set start_exon\=\"$1\" where start_exon\=\"$old\"\;\n";
+	    print "update translation set end_exon\=\"$1\" where end_exon\=\"$old\"\;\n";
+	}
+	elsif ($1 eq $old) {
+	    print "update translation set start_exon\=\"$new\" where start_exon\=\"$2\"\;\n";
+	    print "update translation set end_exon\=\"$new\" where end_exon\=\"$2\"\;\n";
+	    $old = $2;
+	}
+    }
+}
