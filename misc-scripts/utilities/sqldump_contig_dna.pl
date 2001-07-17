15fc5a48b987543cda2ace0ae560a3bb1b8187fc
Elia Stupka <elia@sanger.ac.uk>
2001-07-17 13:58:34 +0000

    Added sql fasta contig dumping script

--- misc-scripts/utilities/sqldump_contig_dna.pl
+++ misc-scripts/utilities/sqldump_contig_dna.pl
@@ -4,17 +4,19 @@ use Bio::EnsEMBL::DBSQL::DBAdaptor;
 
 my $db=Bio::EnsEMBL::DBSQL::DBAdaptor->new(-dbname=>'mouse',-host=>'ecs1a',-user=>'ensro');
 
-my $sth = $db->prepare("select id from contig");
+my $sth = $db->prepare("select internal_id from contig c, static_golden_path sgp where raw_id = c.internal_id");
 $sth->execute;
 my (@cids) = $sth->fetchrow_array;
 foreach my $contig (@cids) {
-    $sth = $db->prepare("select concat('>',c.id,'\tlength: ',c.length,'\n',d.sequence) from dna d, contig c, static_golden_path sgp where sgp.raw_id = c.internal_id and c.dna = d.id and c.internal_id = $contig");
+    $sth = $db->prepare("select concat('>',c.id,'\tlength: ',c.length,'\n',d.sequence) from dna d, contig c where c.dna = d.id and c.internal_id = $contig");
+    #print STDERR "select concat('>',c.id,'\tlength: ',c.length,'\n',d.sequence) from dna d, contig c where c.dna = d.id and c.internal_id = $contig\n";
     $sth->execute;
     my ($string) = $sth->fetchrow_array();
-    my $width = 80; 
-    $string =~ s/(.{1,60})/$1\n/g;
-    print "$string\n";
+    my $width = 60; 
+    $string =~ s/(.{1,$width})/$1\n/g;
+    $string =~ s/\n//;
+    print "$string";
 }
-
+$db->DESTROY;
     
 	
