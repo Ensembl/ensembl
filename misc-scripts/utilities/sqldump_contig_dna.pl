bdfb0dd38270bdc0eea2da4b11ea2198f0f41adc
Elia Stupka <elia@sanger.ac.uk>
2001-07-17 13:50:06 +0000

    Added sql fasta contig dumping script

---
+++ misc-scripts/utilities/sqldump_contig_dna.pl
@@ -0,0 +1,20 @@
+#!/usr/local/bin/perl
+use strict;
+use Bio::EnsEMBL::DBSQL::DBAdaptor;
+
+my $db=Bio::EnsEMBL::DBSQL::DBAdaptor->new(-dbname=>'mouse',-host=>'ecs1a',-user=>'ensro');
+
+my $sth = $db->prepare("select id from contig limit 1");
+$sth->execute;
+my (@cids) = $sth->fetchrow_array;
+foreach my $contig (@cids) {
+    $sth = $db->prepare("select concat('>',c.id,'\tlength: ',c.length,'\n',d.sequence) from dna d, contig c, static_golden_path sgp where sgp.raw_id = c.internal_id and c.dna = d.id");
+    $sth->execute;
+    my ($string) = $sth->fetchrow_array();
+    my $width = 80; 
+    $string =~ s/(.{1,60})/$1\n/g;
+    print "$string\n";
+}
+
+    
+	
