39cb84ba0a281a4e6a51cc75e243599044b80074
Web Admin <w3adm@sanger.ac.uk>
2001-08-07 13:37:21 +0000

    Added routine to perform a request by chr name as well as ID in Chromosome Stats adaptor

--- modules/Bio/EnsEMBL/DBSQL/ChromosomeStatsAdaptor.pm
+++ modules/Bio/EnsEMBL/DBSQL/ChromosomeStatsAdaptor.pm
@@ -7,7 +7,6 @@ use Bio::EnsEMBL::ChromosomeStats;
 
 @ISA = qw(Bio::EnsEMBL::DBSQL::BaseAdaptor);
 
-
 sub get_stats_per_chromosome
 {
 
@@ -43,5 +42,42 @@ sub get_stats_per_chromosome
 
     return @density_array;
 }
+
+sub get_stats_per_chromosome_name
+{
+
+    my ($self,$chromosome)=@_;
+
+    $self->throw("I need a chromosome") unless defined $chromosome;
+
+    my $query="select chromosome_id,id,name,species_id,known_genes,unknown_genes,snps,length 
+               from chromosome where name='$chromosome'";
+
+    my $sth = $self->db->prepare($query);
+    my $res = $sth->execute();
+
+    my ($chr_id,$id,$name,$species,$known_genes,$unknown_genes,$snps,$length);
+
+    $sth->bind_columns(undef,\$chr_id,\$id,\$name,\$species,\$known_genes,\$unknown_genes,\$snps,\$length);
+
+    my @density_array;
+
+    while( $sth->fetch() ) {
+	my $cs=Bio::EnsEMBL::ChromosomeStats->new();
+	$cs->chromosome_id($chr_id);
+	$cs->id($id);
+	$cs->name($name);
+	$cs->species($species);
+	$cs->known_genes($known_genes);
+	$cs->unknown_genes($unknown_genes);
+	$cs->snps($snps);
+	$cs->length($length);
+
+	push @density_array,$cs;
+    }
+
+    return @density_array;
+}
+
 
 
