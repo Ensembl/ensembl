fc0a8f728720d9b789b603f780c0e64ed6c7c55d
Web Admin <w3adm@sanger.ac.uk>
2001-08-08 10:40:39 +0000

    Added in get_stats_per_chromosome_name function - so we don't have to use the artificial chromosome number

--- modules/Bio/EnsEMBL/DBSQL/ChromosomeStatsAdaptor.pm
+++ modules/Bio/EnsEMBL/DBSQL/ChromosomeStatsAdaptor.pm
@@ -45,3 +45,40 @@ sub get_stats_per_chromosome
 }
 
 
+sub get_stats_per_chromosome_name
+{
+
+    my ($self,$chromosome)=@_;
+
+    $self->throw("I need a chromosome") unless defined $chromosome;
+
+    my $query="select chromosome_id,id,name,species_id,known_genes,unknown_genes,snps,length 
+               from chromosome where name='$chromosome'";
+
+    my $sth = $self->db->prepare($query);
+    my $res = $sth->execute();
+
+    my ($chr_id,$id,$name,$species,$known_genes,$unknown_genes,$snps,$length);
+
+    $sth->bind_columns(undef,\$chr_id,\$id,\$name,\$species,\$known_genes,\$unknown_genes,\$snps,\$length);
+
+    my @density_array;
+
+    while( $sth->fetch() ) {
+	my $cs=Bio::EnsEMBL::ChromosomeStats->new();
+	$cs->chromosome_id($chr_id);
+	$cs->id($id);
+	$cs->name($name);
+	$cs->species($species);
+	$cs->known_genes($known_genes);
+	$cs->unknown_genes($unknown_genes);
+	$cs->snps($snps);
+	$cs->length($length);
+
+	push @density_array,$cs;
+    }
+
+    return @density_array;
+}
+
+
