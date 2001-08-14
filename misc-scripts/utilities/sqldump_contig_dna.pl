a9a49920e6636f57946e2c44ae274ed78d6b0d05
Elia Stupka <elia@sanger.ac.uk>
2001-08-14 10:49:46 +0000

    Still tidying up

--- misc-scripts/utilities/sqldump_contig_dna.pl
+++ misc-scripts/utilities/sqldump_contig_dna.pl
@@ -6,7 +6,10 @@ my $db=Bio::EnsEMBL::DBSQL::DBAdaptor->new(-dbname=>'mouse',-host=>'ecs1a',-user
 
 my $sth = $db->prepare("select internal_id from contig c, static_golden_path sgp where raw_id = c.internal_id");
 $sth->execute;
-my (@cids) = $sth->fetchrow_array;
+my @cids; 
+while (my ($contig) = $sth->fetchrow_array ) {
+    push (@cids,$contig);
+}
 foreach my $contig (@cids) {
     $sth = $db->prepare("select concat('>',c.id,'\tlength: ',c.length,'\n',d.sequence) from dna d, contig c where c.dna = d.id and c.internal_id = $contig");
     #print STDERR "select concat('>',c.id,'\tlength: ',c.length,'\n',d.sequence) from dna d, contig c where c.dna = d.id and c.internal_id = $contig\n";
