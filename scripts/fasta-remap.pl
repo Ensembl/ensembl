f299303d61de55f91a36d20567585f9b2b0b3886
Philip Lijnzaad <lijnzaad@sanger.ac.uk>
2001-09-18 15:20:27 +0000

    now spits out the ones not mapped

--- scripts/fasta-remap.pl
+++ scripts/fasta-remap.pl
@@ -1,8 +1,8 @@
 #!/usr/bin/env perl
 # $Id$
-# quick script to remap fasta files with COBP id's. Use as 
+# quick script to remap fasta files with COBP id's. Use as
 #
-#   megamap.pl peptide.map gene.map  < input > output
+#   fasta-remap.pl peptide.map gene.map  < input > output
 
 use strict; 
 
@@ -21,16 +21,25 @@ my $word_delim = '[-> \t:.\n;,_]';
 my $ensid_regexp = '^([A-Z]{3})([PG])(0\d{10})$'; # '; # fool emacs
 
 my $remapped=0;
+my @notmapped=();
 while (<STDIN>) {
     if (/^>/) {
         my @words = split /($word_delim)/;
         foreach my $w ( @words ) {
-            if (length ($w) > 1 && $map{$w} ) {
-                s/$w/$map{$w}/g;
-                $remapped++;
+            if (length ($w) > 1 && $w =~ /$ensid_regexp/  ) {
+                if (defined $map{$w}) {
+                    s/$w/$map{$w}/g;
+                    $remapped++;
+                } else {
+                    push(@notmapped, $w);
+                }
             }
         }
     }
     print;
 }
-warn "mapped $remapped identifiers\n";
+warn "Mapped $remapped identifiers\n";
+if (@notmapped) {
+    warn "Not mapped:\n" , join("\n", @notmapped) ,
+    "\n" , int(@notmapped), " were not mapped\n";
+}
