b6ea6dcf113625c8001756b372fdacc71959f696
Emmanuel Mongin <mongin@sanger.ac.uk>
2001-10-10 12:47:40 +0000

    Went back to the previous version

--- modules/Bio/EnsEMBL/DBSQL/ExonAdaptor.pm
+++ modules/Bio/EnsEMBL/DBSQL/ExonAdaptor.pm
@@ -379,9 +379,9 @@ sub store {
 
   my $sth  = $self->prepare("
      INSERT INTO supporting_feature( 
-        exon_id,seq_start,seq_end,score,
+        exon_id,contig_id,seq_start,seq_end,score,
         strand,analysis,name,hstart,hend,hid,hstrand) 
-     VALUES(?,?,?,?,?,?,?,?,?,?,?) 
+     VALUES(?,?,?,?,?,?,?,?,?,?,?,?) 
    ");
 
 
@@ -409,6 +409,7 @@ sub store {
 	
 	if ($f->isa("Bio::EnsEMBL::FeaturePair")) {
 	    $sth->execute($exon->dbID(),
+			  $f->seqname,
 			  $f->start,
 			  $f->end,
 			  $f->score,
