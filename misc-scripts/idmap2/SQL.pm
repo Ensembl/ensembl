caa7f539a57c35aef972201e146a1decee02e8d7
Arne Stabenau <stabenau@sanger.ac.uk>
2001-10-24 10:37:55 +0000

    Added MapGenes.pm. It builds envelopes around exons, which contain all Exons of the genes they are from. Maps genes inside this envelopes.

--- misc-scripts/idmap2/SQL.pm
+++ misc-scripts/idmap2/SQL.pm
@@ -17,8 +17,51 @@ sub orig_exon_information {
   my %res;
 
   my $sth = $dbh->prepare( "
+    SELECT exon_stable, transcript_stable, 
+           gene_stable, exon_id, gene_id, transcript_id, 
+           clone_id, clone_version, 
+           contig_offset, contig_length,
+           exon_start, exon_end, exon_strand,
+           chr_name, chr_start, chr_end, 
+           raw_start, raw_end, raw_ori
+     FROM  exon_temp 
+    ORDER by clone_id
+" );
+#    LIMIT 10000" );
+  $sth->execute();
+  return _store_in_arrayref( $sth );
+}
+
+sub target_exon_information {
+  my $dbh = shift;
+  
+  my $sth = $dbh->prepare( "
+    SELECT exon_id, transcript_id, gene_id,
+           clone_id, clone_version, 
+           contig_offset, contig_length,
+           exon_start, exon_end, exon_strand,
+           chr_name, chr_start, chr_end, 
+           raw_start, raw_end, raw_ori
+     FROM exon_temp
+    ORDER by clone_id
+ " );
+#   LIMIT 10000");
+  $sth->execute();
+  return _store_in_arrayref( $sth );
+}
+
+
+
+sub old_orig_exon_information {
+  my $dbh = shift;
+  my %res;
+  $dbh->do( "drop table exon_temp" );
+
+  my $sth = $dbh->prepare( "
+    CREATE TABLE exon_temp
     SELECT esi.stable_id as exon_stable, tsi.stable_id as transcript_stable, 
-           gsi.stable_id as gene_stable, e.exon_id as exon_id, 
+           gsi.stable_id as gene_stable, e.exon_id as exon_id, t.gene_id as gene_id,
+           t.transcript_id as transcript_id,
            cl.embl_id as clone_id, cl.embl_version as clone_version, 
            c.offset as contig_offset, c.length as contig_length,
            e.seq_start as exon_start, e.seq_end as exon_end, e.strand as exon_strand,
@@ -35,20 +78,22 @@ sub orig_exon_information {
        AND c.internal_id = e.contig_id
        AND cl.internal_id = c.clone
        AND sgp.raw_id = c.internal_id
-     ORDER by gene_stable, transcript_stable, exon_stable, e.sticky_rank
+     ORDER by clone_id, clone_version,contig_offset, exon_strand
 " );
   $sth->execute();
+#     ORDER by gene_stable, transcript_stable, exon_stable, e.sticky_rank
 
-  return _store_in_arrayref( $sth );
+#  return _store_in_arrayref( $sth );
 }
 
-sub target_exon_information {
+sub old_target_exon_information {
   my $dbh = shift;
-  
+  $dbh->do( "drop table exon_temp" );
   my $sth = $dbh->prepare( "
+    CREATE TABLE exon_temp
     SELECT e.exon_id, t.transcript_id, t.gene_id,
            cl.embl_id as clone_id, cl.embl_version as clone_version, 
-           c.offset as contig_offset, c.length as contig_length, e.exon_id as exon_id,
+           c.offset as contig_offset, c.length as contig_length,
            e.seq_start as exon_start, e.seq_end as exon_end, e.strand as exon_strand,
            sgp.chr_name as chr_name, sgp.chr_start as chr_start, sgp.chr_end as chr_end, 
            sgp.raw_start as raw_start, sgp.raw_end as raw_end, sgp.raw_ori as raw_ori
@@ -59,10 +104,12 @@ sub target_exon_information {
        AND c.internal_id = e.contig_id
        AND cl.internal_id = c.clone
        AND sgp.raw_id = c.internal_id
+     ORDER by clone_id, clone_version,contig_offset, exon_strand
 " );
+  
+  $sth->execute();
 
-  $sth->execute ();
-  return _store_in_arrayref( $sth );
+#  return _store_in_arrayref( $sth );
 }
 
 
@@ -78,5 +125,35 @@ sub _store_in_arrayref {
   }
   return $res;
 }
+
+sub exon_sequence {
+  my $dbh = shift;
+  my $exon_id = shift;
+  my $exonSeq;
+
+  my $sth = $dbh->prepare( "
+    SELECT e.exon_id, substring( d.sequence, e.seq_start, e.seq_end-e.seq_start+1 ), 
+           e.seq_start as start, e.seq_end as end,
+           e.strand as strand, e.sticky_rank as rank
+      FROM exon e, contig c, dna d
+     WHERE e.contig_id = c.internal_id
+       AND c.dna = d.id
+       AND e.exon_id = ?
+     ORDER by rank
+  " );
+  $sth->execute( $exon_id );
+  
+  while( my $arrRef = $sth->fetchrow_arrayref ) {
+    my $part  = $arrRef->[1];
+    if( $arrRef->[4] != 1 ) {
+      $part = reverse( $part );
+      $part =~ tr/ACGTacgt/TGCATGCA/;
+    }
+    $exonSeq .= $part;
+  }
+
+  return $exonSeq;
+}
+
 
 1;
--- misc-scripts/idmap2/mapping.pl
+++ misc-scripts/idmap2/mapping.pl
@@ -8,24 +8,85 @@
 
 use DBI;
 use SQL;
+use Bio::PrimarySeq;
+
+use strict;
+use Bio::EnsEMBL::Pipeline::Runnable::CrossMatch;
+use MapGenes;
+
+my $direct_maps = 0;
+my $crossmatches = 0;
+my $cloneGroups = 0;
+my $seqMatches = 0;
+my $cmmaps = 0;
 
 
-my $sdbh = DBI->connect( "DBI:mysql:host=geordy;port=3310;database=arne_ens080", "ensro" );
+my $sdbh = DBI->connect( "DBI:mysql:host=ecs1f;database=arne_ens080", "ensadmin", "ensembl");
+my $tdbh = DBI->connect( "DBI:mysql:host=ecs1f;database=arne_ens100", "ensadmin", "ensembl" );
 
-# my $tdbh = 
+my $starttime = scalar( localtime() );
 
 print STDERR "Start: ",scalar(localtime()),"\n";
 my $oExonInfo = &SQL::orig_exon_information( $sdbh );
-# my $nExonInfo = &SQL::target_exon_information( $tdbh );
+my $nExonInfo = &SQL::target_exon_information( $tdbh );
 print STDERR "Finish: ",scalar(localtime()),"\n";
 
-print "Count: ",scalar( @$oExonInfo ),"\n";
+print STDERR "Count: ",scalar( @$oExonInfo ),"\n";
 
-direct_mapping( $oExonInfo, undef );
 
+direct_mapping( $oExonInfo, $nExonInfo );
+print "# finished direct mapping\n";
+
+contig_version_update( $oExonInfo, $nExonInfo );
+MapGenes::map_genes( $oExonInfo, $nExonInfo );
 
 
 $sdbh->disconnect();
+
+$tdbh->disconnect();
+
+print STDERR "Start: $starttime\n";
+print STDERR "End : ",scalar( localtime()),"\n";
+
+print STDERR "Old Exon Count: ", scalar( @$nExonInfo ), "\n";
+print STDERR "New Exon Count: ", scalar( @$nExonInfo ), "\n";
+print STDERR "Direct Mappings: ", $direct_maps,"\n";
+print STDERR "Crossmatches: $crossmatches\n";
+print STDERR "CloneGroups: $cloneGroups\n";
+print STDERR "SeqMatches: $seqMatches\n";
+print STDERR "Mappings on Crossmatch: $cmmaps\n";
+exit;
+
+
+open( D1, ">dump1.txt" );
+open( D2, ">dump2.txt" );
+
+
+my ( @dumpOld, @dumpNew );
+
+print STDERR "Dumping ...\n";
+
+@dumpOld = sort { ( $a->{'clone_id'} cmp $b->{'clone_id'} ) ||
+		( $a->{'exon_start'} <=> $b->{'exon_start'}) } @$oExonInfo;
+@dumpNew = sort { ( $a->{'clone_id'} cmp $b->{'clone_id'} ) ||
+		( $a->{'exon_start'} <=> $b->{'exon_start'}) } @$nExonInfo;
+
+
+*STDOUT = *D1;
+for my $ex ( @dumpOld ) {
+  print_exon( $ex );
+}
+
+close D1;
+
+*STDOUT  = *D2;
+for my $ex ( @dumpNew ) {
+  print_exon( $ex );
+}
+
+close D2;
+
+
 exit;
 
 
@@ -40,11 +101,174 @@ exit;
 # updated version
 
 sub contig_version_update {
+  my ( $old, $new ) = @_;
   
+  my ( $pruneOld, $pruneNew, $sortOld, $sortNew  );
+  my ( $old_iter, $new_iter );
+
+  @{$pruneOld} = grep { ! exists $_->{'mapped'} } @{$old};
+  @{$pruneNew} = grep { ! exists $_->{'mapped'} } @{$new};
+
+  # prune short exons (<10) would be appropriat
+  print "Old Exons left to map: ",scalar( @$pruneOld ),"\n";
+  print "New Exons left to map: ",scalar( @$pruneNew ),"\n";
+ 
+  @{$sortOld} = sort { $a->{'clone_id'} cmp $b->{'clone_id'} } @$pruneOld;
+  @{$sortNew} = sort { $a->{'clone_id'} cmp $b->{'clone_id'} } @$pruneNew;
+
+
+  $new_iter = 0;
+  $old_iter = 0;
+
+  while( 1 ) {
+
+    my $old_exon = $sortOld->[$old_iter];
+    my $new_exon = $sortNew->[$new_iter];
+
+    my $cmp = ( $old_exon->{'clone_id'} cmp $new_exon->{'clone_id'} );
+    if( $cmp == 0 ) {
+      my $clone_id = $old_exon->{'clone_id'};
+      my ( $newCloneExons, $oldCloneExons );
+      while( defined $old_exon && 
+	     $old_exon->{'clone_id'} eq $clone_id ) {
+	push( @{$oldCloneExons}, $old_exon );
+	$old_iter++;
+	if( $old_iter % 10000 == 0 ) {
+	  print STDERR "$old_iter exons done.\n";
+	}
+	$old_exon = $sortOld->[$old_iter];
+      }
+	  
+      while( defined $new_exon && 
+	     $new_exon->{'clone_id'} eq $clone_id ) {
+	push( @{$newCloneExons}, $new_exon );
+	$new_iter++;
+	$new_exon = $sortNew->[$new_iter];
+      }
+      
+      clone_map( $oldCloneExons, $newCloneExons );
+      # build groups of exons and compare
+    } elsif ( $cmp < 0 ) {
+      $old_iter++;
+      if( $old_iter % 10000 == 0 ) {
+	print STDERR "$old_iter exons done.\n";
+      }
+    } else {
+      $new_iter++;
+    }
+    if( $new_iter >= scalar( @$sortNew ) || 
+	$old_iter >= scalar( @$sortOld )) {
+      last;
+    }
+    if( $old_iter % 10000 == 0 ) {
+      print STDERR "$old_iter exons done.\n";
+    }
+  }
 
 }
 
 
+
+# map two sets of exons in the same clone from old to new
+# by using crossmatch module ...
+sub clone_map {
+  my ( $oldCloneExons, $newCloneExons ) = @_;
+  my ( $oldExHash, $newExHash );
+  local *HERR, *HOUT;
+ 
+  open ( NULL, ">/dev/null" );
+  
+  $cloneGroups++;
+
+  for my $exon ( @$oldCloneExons ) {
+    $oldExHash->{$exon->{'exon_id'}} = SQL::exon_sequence( $sdbh, $exon->{'exon_id'} );
+    # $exon->{'seq'} = $oldExHash->{$exon->{'exon_id'}};
+  }
+
+  for my $exon ( @$newCloneExons ) {
+    $newExHash->{$exon->{'exon_id'}} = SQL::exon_sequence( $tdbh, $exon->{'exon_id'} );
+    # $exon->{'seq'} = $newExHash->{$exon->{'exon_id'}};
+  }
+
+  for my $oEx ( @$oldCloneExons ) {
+    for my $nEx ( @$newCloneExons ) {
+      if( $oldExHash->{$oEx->{'exon_id'}} eq 
+	  $newExHash->{$nEx->{'exon_id'}} ) {
+	# we have equal sequence exons here, map them
+	$oEx->{'mapped'} = 'same Clone Seq';
+	$nEx->{'mapped'} = 'same Clone Seq';
+	$nEx->{'exon_stable'} = $oEx->{'exon_stable'};
+	print ( "Exon mapped:\t", $oEx->{'exon_id'},"\t",
+		$nEx->{'exon_id'},"\t", $oEx->{'exon_stable'},"\n" );
+	$seqMatches++;
+#	print STDERR "---- DIRECT SEQUENCE MAP ----\n";
+      }
+    }
+  }
+
+  # no crossmatch mappings
+  return;
+
+  my ( @oldPrune, @newPrune, @scorelist );
+  @oldPrune = grep { ! exists $_->{'mapped'} } @$oldCloneExons;
+  @newPrune = grep { ! exists $_->{'mapped'} } @$newCloneExons;
+
+  for my $oEx ( @oldPrune ) {
+    for my $nEx ( @newPrune ) {
+      my $crossMatcher = new Bio::EnsEMBL::Pipeline::Runnable::CrossMatch
+	( -seq1 => new Bio::PrimarySeq( -seq => $oldExHash->{$oEx->{'exon_id'}}, 
+					-id => $oEx->{'exon_id'} ),
+	  -seq2 => new Bio::PrimarySeq( -seq => $newExHash->{$nEx->{'exon_id'}}, 
+					-id => $nEx->{'exon_id'} ),
+	  -score => 200
+	);
+
+      *HERR = *STDERR;
+      *HOUT = *STDOUT;
+
+      *STDOUT = *NULL;
+      *STDERR = *NULL;
+
+      $crossMatcher->run();
+
+      *STDOUT = *HOUT;
+      *STDERR = *HERR;
+
+      $crossmatches++;
+      my $score = 0;
+      my $matches = 0;
+      for my $fp ( $crossMatcher->output() ) {
+	$score += $fp->score();
+	$matches = 1;
+      }
+      if( $matches ) {
+	push( @scorelist, { score => $score, 
+			    oldEx => $oEx,
+			    newEx => $nEx } );
+      }
+									 
+    }
+  }
+
+  my @sortedScores;
+  @sortedScores = sort { $b->{'score'} <=> $a->{'score'} } @scorelist;
+  
+  for my $scoreRecord ( @sortedScores ) {
+    if( ! exists $scoreRecord->{'oldEx'}{'mapped'} &&
+	! exists $scoreRecord->{'newEx'}{'mapped'} ) {
+      $scoreRecord->{'oldEx'}{'mapped'} = 'crossmatch Clone Seq';
+      $scoreRecord->{'newEx'}{'mapped'} = 'crossmatch Clone Seq';;
+      $scoreRecord->{'newEx'}{'exon_stable'} = 
+	$scoreRecord->{'oldEx'}{'exon_stable'};
+      print ( "Exon mapped:\t", $scoreRecord->{'oldEx'}{'exon_id'},"\t",
+	      $scoreRecord->{'newEx'}{'exon_id'},"\t", 
+	      $scoreRecord->{'oldEx'}{'exon_stable'},"\n" ); 
+      
+      $cmmaps++;
+#      print STDERR "---- CROSSMATCH MAP ----",$scoreRecord->{'score'},"\n";
+    }
+  }
+}
 
 
 
@@ -71,10 +295,11 @@ sub direct_mapping {
   $old_iter = 0;
 
   while( 1 ) {
+
     $old_exon = $sold->[$old_iter];
     $new_exon = $snew->[$new_iter];
     my  $exon_compare_result;
-    if( $old_exon->{'clone_id'} == $new_exon->{'clone_id'} &&
+    if(( $old_exon->{'clone_id'} cmp $new_exon->{'clone_id'}) == 0 &&
 	$old_exon->{'clone_version'} == $new_exon->{'clone_version'} &&
 	$old_exon->{'exon_strand'} == $new_exon->{'exon_strand'} &&
 	$old_exon->{'contig_offset'} == $new_exon->{'contig_offset'} ) {
@@ -86,7 +311,7 @@ sub direct_mapping {
     if(  $exon_compare_result == 0 ) {
       # direct map possible
       
-      # find overlap between the two exons. Need 95% of old exon
+      # find overlap between the two exons. Need >50% of new exon
       # covered
 
       my ( $overlap ) = 
@@ -94,17 +319,34 @@ sub direct_mapping {
 			   $new_exon->{'exon_end'}-$new_exon->{'exon_start'}+1,
 			   $old_exon->{'exon_end'}-$new_exon->{'exon_start'}+1,
 			   $new_exon->{'exon_end'}-$old_exon->{'exon_start'}+1 );
+#      print "---- MAPPING ----\n";
+
+#      print_exon( $old_exon );
+#      print_exon( $new_exon );
+#      print "OVERLAP: $overlap\n";
+
       if(( $overlap / 
-	  ( $new_exon->{'exon_end'} - $nex_exon->{'exon_start'} )) > 0.95 ) {
+	  ( $new_exon->{'exon_end'} - $new_exon->{'exon_start'} + 1 )) > 0.5 ) {
 	
+#	print "MAPPED\n";
 	$old_exon->{'mapped'} = 'direct';
+	$new_exon->{'mapped'} = 'direct';
+
 	$new_exon->{'exon_stable'} = $old_exon->{'exon_stable'};
+	print ( "Exon mapped:\t", $old_exon->{'exon_id'},"\t",
+		$new_exon->{'exon_id'},"\t", $old_exon->{'exon_stable'},"\n" );
 	$old_iter++;
 	$new_iter++;
+
+	if( $old_iter >= scalar( @$old ) ||
+	    $new_iter >= scalar( @$new ) ) {
+	  last;
+	}
 	$direct_maps++;
 	next;
       }
     }
+#    print "NOT MAPPED\n";
 
     $exon_compare_result = exon_direct_compare( $old_exon, $new_exon );
 
@@ -133,16 +375,13 @@ sub direct_mapping {
     }
   }
 
-  print STDERR "Old Exons: ",scalar( @$old ),"\n";
-  print STDERR "New Exons: ",scalar( @$new ),"\n";
-  print STDERR "Mapped : ",$direct_maps,"\n";
   print STDERR "Direct Mapping finished: ",scalar( localtime() ),"\n";
   return;
 }
 
 sub exon_direct_compare {
   my ( $a, $b ) = @_;
-  return $a->{'clone_id'} <=> $b->{'clone_id'} ||
+  return $a->{'clone_id'} cmp $b->{'clone_id'} ||
     $a->{'clone_version'} <=> $b->{'clone_version'} ||
       $a->{'contig_offset'} <=> $b->{'contig_offset'} ||
 	$a->{'exon_start'} <=> $b->{'exon_start'} ||
@@ -153,13 +392,10 @@ sub exon_direct_compare {
   
 
 
-sub print_arrayref {
-  my $aref = shift;
-  
-  for( my $i=1; $i <= @$aref; $i++ ) {
-    print "------- new record -------\n";
-    for my $key ( keys %{$aref->[$i]} ) {
-      print $key,"\t",$aref->[$i]{$key},"\n";
-    }
+sub print_exon {
+  my $exon = shift;
+  for my $key ( sort keys %{$exon} ) {
+    print $key,"\t",$exon->{$key},"\n";
   }
+  print "\n";
 }
