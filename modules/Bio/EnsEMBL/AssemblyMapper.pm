8146f09e3fa8019a5f21417b8e8cd65f5f8b7d24
Ewan Birney <birney@sanger.ac.uk>
2001-11-25 20:10:35 +0000

    fixed up AssemblyMapper. Not elegant etc etc (see previous message!)

--- modules/Bio/EnsEMBL/AssemblyMapper.pm
+++ modules/Bio/EnsEMBL/AssemblyMapper.pm
@@ -57,25 +57,201 @@ use strict;
 # Object preamble - inheriets from Bio::Root::RootI
 
 use Bio::Root::RootI;
-
+use Bio::EnsEMBL::Mapper;
 
 @ISA = qw(Bio::Root::RootI);
 
 sub new {
-  my($class,@args) = @_;
+  my($class,$adaptor,$type) = @_;
 
-    my $self = {};
-    bless $self,$class;
+  my $self = {};
+  bless $self,$class;
 
-  $self->throw("Waiting for implementation by Ewan - placeholder object!!!");
-
-    return $self;
+  if( !defined $type ) {
+      $self->throw("Must have adaptor, type for the assembly mapper");
+  }
+  $self->{'_contig_register'} = {};
+  $self->adaptor($adaptor);
+  $self->_type($type);
+  
+  my $mapper = Bio::EnsEMBL::Mapper->new('rawcontig','assembly');
+  $self->_mapper($mapper);
+  return $self;
 }
 
 
-#
-#
-#
+
+=head2 map_coordinates
+
+ Title   : map_coordinates
+ Usage   :
+ Function:
+ Example :
+ Returns : 
+ Args    :
+
+
+=cut
+
+sub map_coordinates{
+   my ($self,$start,$end,$strand,$id,$maptype) = @_;
+
+   if( !defined $maptype ) {
+       $self->throw("map_coordinates start,end,strand,id,maptype");
+   }
+
+   if( $maptype ne "rawcontig" && $maptype ne "assembly" ) {
+       $self->throw("maptype must be either rawcontig or assembly, not $maptype");
+   }
+
+   if( $maptype eq "rawcontig" && $id =~ /^\w/ ) {
+       $self->throw("You have a rawcontig type, but the id is $id. AssemblyMappers expect internal_ids of the contigs in their mappers - probably you have given us a raw contig text id");
+   }
+
+   # ok map it 
+
+   return $self->_mapper->map_coordinates($start,$end,$strand,$id,$maptype);
+}
+
+
+=head2 register_region
+
+ Title   : register_region
+ Usage   :
+ Function:
+ Example :
+ Returns : 
+ Args    :
+
+
+=cut
+
+sub register_region{
+   my ($self,$id,$start,$end) = @_;
+
+   $self->adaptor->register_region($self,$self->_type,$id,$start,$end);
+}
+
+
+
+
+
+=head2 Internal functions
+
+Internal functions
+
+=cut
+
+
+=head2 _have_loaded_contig
+
+ Title   : _have_loaded_contig
+ Usage   :
+ Function:
+ Example :
+ Returns : 
+ Args    :
+
+
+=cut
+
+sub _have_loaded_contig{
+   my ($self,$id) = @_;
+
+   if( $self->{'_contig_register'}->{$id} ) {
+       return 1;
+   } else {
+       return 0;
+   }
+
+}
+
+=head2 _register_contig
+
+ Title   : _register_contig
+ Usage   :
+ Function:
+ Example :
+ Returns : 
+ Args    :
+
+
+=cut
+
+sub _register_contig{
+   my ($self,$id) = @_;
+
+   $self->{'_contig_register'}->{$id} = 1;
+
+}
+
+
+=head2 _type
+
+ Title   : _type
+ Usage   : $obj->_type($newval)
+ Function: 
+ Example : 
+ Returns : value of _type
+ Args    : newvalue (optional)
+
+
+=cut
+
+sub _type{
+   my ($self,$value) = @_;
+   if( defined $value) {
+      $self->{'_type'} = $value;
+    }
+    return $self->{'_type'};
+
+}
+
+
+=head2 _mapper
+
+ Title   : _mapper
+ Usage   : $obj->_mapper($newval)
+ Function: 
+ Example : 
+ Returns : value of _mapper
+ Args    : newvalue (optional)
+
+
+=cut
+
+sub _mapper{
+   my ($self,$value) = @_;
+   if( defined $value) {
+      $self->{'_mapper'} = $value;
+    }
+    return $self->{'_mapper'};
+
+}
+
+
+
+=head2 adaptor
+
+ Title   : adaptor
+ Usage   : $obj->adaptor($newval)
+ Function: 
+ Example : 
+ Returns : value of adaptor
+ Args    : newvalue (optional)
+
+
+=cut
+
+sub adaptor{
+   my ($self,$value) = @_;
+   if( defined $value) {
+      $self->{'adaptor'} = $value;
+    }
+    return $self->{'adaptor'};
+
+}
+
 
 
 1;
