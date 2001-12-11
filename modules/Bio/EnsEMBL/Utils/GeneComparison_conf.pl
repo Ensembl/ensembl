0f970668df1d4d66e055e320edeb56ce09ddef0a
Eduardo Eyras <eae@sanger.ac.uk>
2001-12-11 15:12:32 +0000

    version with specific stuff commented out

--- modules/Bio/EnsEMBL/Utils/GeneComparison_conf.pl
+++ modules/Bio/EnsEMBL/Utils/GeneComparison_conf.pl
@@ -6,43 +6,86 @@
 
 # Databases in 120 schema (schema as for the 120 release)
 #
-# ensembl110_new_schema@ensrv3, path_type='UCSC', genetype = '1'
+# prediction release 110: ensembl110_new_schema_2@ecs1c, path_type='UCSC', genetype = '1'
 #
-# homo_sapiens_core_120@ensrv3, path_type='UCSC', genetype='ensembl'
+# prediction release 120: homo_sapiens_core_120@ensrv3, path_type='UCSC', genetype='ensembl'
+#
+# prediction release 121: ens_NCBI_26_temp@ecs1f, path='NCBI_26', genetype = 'ensembl' 
 #
 # chr20_120@ecs1b, path_type='Sanger_02', genetypes='HUMACE-Novel_CDS','HUMACE-Known',etc...
 #
-
+# genetypes is an array_ref, to allow for multiple types
 
 BEGIN {
 package main;
 
+# configuration for the annotation (or benchmark) genes
+
 %db1_conf = (
-	     'host'      => "ecs1b",
-
-	     'dbname'    => "chr20_120",
-
-	     'path'      => "Sanger_02",
-
-	     'user'      => "ensro",
+	     ######################################################
+	     # chr20 annotation database (schema as release 120 ) #
+	     ######################################################
+	     #'host'      => "ecs1b",
+	     #'dbname'    => "chr20_120",
+	     #'path'      => "Sanger_02",   # to be used for predictions on UCSC
+	     #'path'      => "NCBI_26",     # for release 121
+	     #'user'      => "ensro",
+	     #'genetypes'  => ["HUMACE-Novel_CDS","HUMACE-Known"],
+	     #'genetypes' => ["HUMACE-Pseudogene"],
 	     
-	     # genetypes should be array_ref, to allow for multiple types
-	     'genetypes'  => ["HUMACE-Novel_CDS","HUMACE-Known"],
+	     'host'      =>  ,
+	     'dbname'    =>  ,
+	     'path'      =>  ,
+	     'user'      =>  ,
+	     'genetypes' =>  ,
 	     
 );
 
+# configuration for the prediction genes
 	      
 %db2_conf = (
-	     'host'      => "ensrv3",
+	     ##############################
+	     # parameters for release 110 #
+	     ##############################
+	     #'host'      => "ecs1c",                    
+	     #'dbname'    => "ensembl110_new_schema_2",
+	     #'path'      => "UCSC",                     
+	     #'user'      => "ensro",
+	     #'genetypes' => ["1"],                  
 
-	     'dbname'    => "homo_sapiens_core_120",
-	     	     
-	     'path'      => "UCSC",
-	    	    
-	     'user'      => "ensro",
-
-	     'genetypes'  => ["ensembl"], 
+	     ##############################
+	     # parameters for release 120 #
+	     ##############################
+	     #'host'      => "ensrv3",                   
+	     #'dbname'    => "homo_sapiens_core_120",    
+	     #'path'      => "UCSC",                     
+	     #'user'      => "ensro",
+	     #'genetypes' => ["ensembl"],               
 	     
+	     ##############################
+	     # parameters for release 121 #
+	     ##############################
+	     #'host'      => "ecs1e",                   
+	     #'dbname'    => "ens_NCBI_26",              
+	     #'path'      => "NCBI_26",                  
+	     #'user'      => "ensro",
+	     #'genetypes' => ["ensembl"],              
+	     
+	     ##################################
+	     # parameters for genes from ESTs #
+	     ##################################
+	     #'host'      => "ecs1e",                    
+	     #'dbname'    => "ens_UCSC_0801_est90",      
+	     #'path'      => "UCSC",                     
+	     #'user'      => "ensro",
+	     #'genetypes' => ["genomewise_clustering_twice"], 
+
+	     
+	     'host'      =>  ,
+	     'dbname'    =>  ,
+	     'path'      =>  ,
+	     'user'      =>  ,
+	     'genetypes' =>  ,
 );
 
 }
