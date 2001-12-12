0de0cd2066ee384e80aa1eea2125b2e22de6577a
Arne Stabenau <stabenau@sanger.ac.uk>
2001-12-12 17:09:27 +0000

    some comments on what has to be done in the new RawContig

--- modules/Bio/EnsEMBL/RawContig.pm
+++ modules/Bio/EnsEMBL/RawContig.pm
@@ -78,7 +78,57 @@ sub new {
     ( $international_name );
 }
 
+# things to take over from DBSQL/RawContig ?
+# fetch - done in DBSQL/RawContigAdaptor
+# get_all_Genes - should be in GeneAdaptor
+#    How to handle halve off genes?
+# get_old_Genes - dead
+# get_all_Exons - ExonAdaptor
+#   What happens with halve on/off ?
+# get_old_Exons - dead
+# get_Genes_by_Type - see get_all_Genes
+# has_Genes - Gene or ExonAdaptor ...
+# primary_seq - seq should do it now
+# db_primary_seq - Assume seq does it ?
+# perl_primary_seq - and again
+# get_all_SeqFeatures - Call the relevant Adaptors
+# get_all_SimilarityFeatures_above_score
+# get_all_SimilarityFeatures
+# get_all_RepeatFeatures
 
+# Are this used from Pipeline? Web uses StaticContig
+# get_MarkerFeatures
+# get_landmark_MarkerFeatures
+
+
+# get_all_PredictionFeatures - use PredictionTranscriptAdaptor
+# get_genscan_peptides - should work via above, so deprecated
+# get_all_ExternalFeatures - which adaptors provide them??
+# get_all_ExternalGenes - again, we need adaptors for them
+# cloneid - dead, use clone->dbID or clone->name,embl_name etc
+# old_chromosome - ?? I assume unused and dead
+# seq_version - Maybe clone->embl_version ?
+# embl_order - good name, corder to be renamed
+# embl_offset- good name, offset to be renamed
+# embl_accession - Is there one for our contigs ?
+# id - should be name
+# internal_id - dbID
+# dna_id - hidden in seq
+# seq_date - hidden in seq
+
+# static_golden_path functionality 
+# All things should lie on an Assembly object.
+#  get info out by supplying the contig as parameter
+
+
+
+
+
+############################
+#
+#  Attribute section       #
+#                          #
+############################
 
 
 sub adaptor {
