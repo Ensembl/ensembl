cfc6c4c5b9a50b6580f3336a9930d9fe632845d5
Michele Clamp <michele@sanger.ac.uk>
2002-01-15 10:18:55 +0000

    *** empty log message ***

--- modules/Bio/EnsEMBL/DnaDnaAlignFeature.pm
+++ modules/Bio/EnsEMBL/DnaDnaAlignFeature.pm
@@ -11,7 +11,7 @@ package Bio::EnsEMBL::DnaDnaAlignFeature;
 
   Bio::EnsEMBL::DnaDnaAlignFeature - Ensembl specific dna-dna pairwise alignment feature
 
-= head1 SYNOPSIS
+=head1 SYNOPSIS
 
   my $feat = new Bio::EnsEMBL::DnaDnaAlignFeature(-seqname => 'myseq',
 						  -start   => 100,
@@ -19,10 +19,20 @@ package Bio::EnsEMBL::DnaDnaAlignFeature;
 						  -strand  => 1,
 						  -hstart  => 200,
 						  -hend    => 220,
-						  -source_tag => 'blastn',
-						  -primary_tag => 'similarity',
 						  -analysis    => $analysis,
 						  -cigar_string => '100,200,3:110,210,11');
+
+  # Alternatively if you have an array of ungapped features
+
+      my $feat = new Bio::EnsEMBL::DnaDnaAlignFeature(-features => \@features);
+
+  # Where @features is an array of Bio::EnsEMBL::FeaturePair
+
+  # There is a method to manipulate the cigar_string into ungapped features
+
+      my @ungapped_features = $feat->ungapped_features;
+
+  # This converts the cigar string into an array of Bio::EnsEMBL::FeaturePair
 
   # $analysis is a Bio::EnsEMBL::Analysis object
   
@@ -40,11 +50,45 @@ package Bio::EnsEMBL::DnaDnaAlignFeature;
   #
   # The length parts of the cigar string are positive as the strand is +ve.
 
-  # There is a method to manipulate the cigar_string into ungapped features
 
-  my @ungapped_features = $feat->ungapped_features;
+  # To make things clearer this is how a blast HSP would be parsed
 
-  # This converts the cigar string into an array of Bio::EnsEMBL::FeaturePair
+  #>AK014066
+  #       Length = 146
+
+  #  Minus Strand HSPs:
+
+  #  Score = 76 (26.8 bits), Expect = 1.4, P = 0.74
+  #  Identities = 20/71 (28%), Positives = 29/71 (40%), Frame = -1
+
+  #Query:   479 GLQAPPPTPQGCRLIPPPPLGLQAPLPTLRAVGSSHHHP*GRQGSSLSSFRSSLASKASA 300
+  #             G  APPP PQG R   P P G + P   L             + + ++  R  +A   +
+  #Sbjct:     7 GALAPPPAPQG-RWAFPRPTG-KRPATPLHGTARQDRQVRRSEAAKVTGCRGRVAPHVAP 64
+
+  #Query:   299 SSPHNPSPLPS 267
+  #                H P+P P+
+  #Sbjct:    65 PLTHTPTPTPT 75
+
+  #The alignment goes from 267 to 479 in sequence 1 and 7 to 75 in sequence 2 and the
+  #strand is -1.
+
+  #The alignment is made up of the following ungapped pieces :
+
+  #sequence 1 start 447 , sequence 2 start 7  , match length 33 , strand -1
+  #sequence 1 start 417 , sequence 2 start 18 , match length 27 , strand -1
+  #sequence 1 start 267 , sequence 2 start 27 , match length 137 , strand -1
+
+  #These ungapped pieces are made up into the following string (called a cigar string)
+
+  #447,7,-33:417,18,-27:267,27,-137
+
+  #i.e. seqstart1,seqstart2,length: etc
+
+
+=cut 
+
+use Bio::EnsEMBL::FeaturePair;
+use Bio::EnsEMBL::SeqFeature;
 
 use vars qw(@ISA);
 use strict;
@@ -55,12 +99,18 @@ sub new {
     my ($class,@args) = @_;
 
     my $self = $class->SUPER::new(@args);
+    
+    my ($cigar_string,$features) = $self->_rearrange([qw(CIGAR_STRING FEATURES)],
+						     @args);
 
-    my ($cigar_string) = $self->_rearrange([qw(CIGAR_STRING)],
-					   @args);
-
-    if (!defined($cigar_string)) {
+    if (defined($cigar_string) && defined($features)) {
+      $self->throw("Can't input cigar_string and an array of features to DnaDnaAlignFeature");
+    } elsif (defined($features)) {
+      $self->_parse_features($features);
+    } elsif (!defined($cigar_string)) {
       $self->throw("A DnaDnaAlignFeature must have a cigar string defining the alignment");
+    } else {
+      $self->throw("DnaDnaAlignFeature has wrong arguments in constructor");
     }
 
     $self->cigar_string($cigar_string);
@@ -194,8 +244,6 @@ sub _parse_cigar {
     $feature1->end  ($start1 + $length - 1);
     $feature1->strand($strand);
     $feature1->score($self->score);
-    $feature1->source_tag($self->source_tag);
-    $feature1->primary_tag($self->primary_tag);
     $feature1->seqname($self->seqname);
     $feature1->phase($self->phase);
     $feature1->p_value($self->p_value);
@@ -203,12 +251,15 @@ sub _parse_cigar {
 
     my $feature2 = new Bio::EnsEMBL::SeqFeature();
 
-    $feature2->start($start2);
-    $feature2->end  ($start2 + $strand*($length - 1));
+    if ($strand == 1) {
+      $feature2->start($start2);
+      $feature2->end  ($start2 + $strand*($length - 1));
+    } else {
+      $feature2->end  ($start2);
+      $feature2->start($start2 + $strand*($length - 1));
+    }      
     $feature2->strand($strand);
     $feature2->score($self->score);
-    $feature2->source_tag($self->source_tag);
-    $feature2->primary_tag($self->primary_tag);
     $feature2->seqname($self->hseqname);
     $feature2->phase($self->phase);
     $feature2->p_value($self->p_value);
@@ -223,6 +274,180 @@ sub _parse_cigar {
     push(@features,$fp);
   }
   return @features;
+}
+
+      
+=head2 _parse_features
+
+    Arg      : Array of Bio::EnsEMBL::FeaturePair
+
+    Usage    : Internal method - not used.
+
+    Function : Converts an array of FeaturePairs into a gapped feature with
+               a cigar string describing the 
+
+               See sub cigar_string for an explanation of what that is.
+
+    Exception: If the argument passed is not an array reference
+
+               All the features must have arisen from the same source
+               i.e. a blast HSP or some other alignment.  Thus
+               exceptions are thrown when the scores,percent ids,p_values
+               seqnames , hseqnames and strands differ amongst the input 
+               features.
+
+               All the features must not overlap in order to provide a 
+               sane gapped alignment.  An exception is thrown if they do.
+
+               If any element of the array is not a Bio::EnsEMBL::FeaturePair
+
+               If there are no elements in the array
+
+    Caller   : Called internally to the module by the constructor
+
+=cut
+
+sub _parse_features {
+  my ($self,$features) = @_;
+
+  if (ref($features) ne "ARRAY") {
+    $self->throw("features must be an array reference not a [" . ref($features) . "]");
+  }
+
+  my @f = sort {$a->start <=> $b->start} @$features;
+
+  if (scalar(@f) == 0) {
+    $self->throw("No features in the array to parse");
+  }
+  my $hstrand     = $f[0]->hstrand;
+  my $name        = $f[0]->seqname;
+  my $hname       = $f[0]->hseqname;
+  my $score       = $f[0]->score;
+  my $percent     = $f[0]->percent_id;
+  my $pvalue      = $f[0]->p_value;
+  my $analysis    = $f[0]->analysis;
+  my $phase       = $f[0]->phase;
+
+  print STDERR "Analysis is $analysis\n";
+  print STDERR "Strand is $hstrand\n";
+
+  my $prev1;
+  my $prev2;
+
+  my $string;
+ 
+  my $f1start = $f[0]->start;
+  my $f1end   = $f[$#f]->end;
+
+  my $f2start;
+  my $f2end;
+
+  if ($hstrand == 1) {
+    $f2start = $f[0]->hstart;
+    $f2end   = $f[$#f]->hend;
+  } else {
+    $f2end   = $f[0]->hend;
+    $f2start = $f[$#f]->hstart;
+  }
+
+  foreach my $f (@f) {
+    if (!$f->isa("Bio::EnsEMBL::FeaturePair")) {
+      $self->throw("Array element [$f] is not a Bio::EnsEMBL::FeaturePair");
+    }
+
+    print STDERR "Processing " . $f->gffstring . "\n";
+
+    if ($f->strand != $hstrand) {
+      $self->throw("Inconsistent strands in feature array");
+    }
+
+    if ($name ne $f->seqname) {
+      $self->throw("Inconsistent names in feature array [$name - " . $f->seqname . "]");
+    }
+    if ($hname ne $f->hseqname) {
+      $self->throw("Inconsistent names in feature array [$hname - " . $f->hseqname . "]");
+    }
+    if ($score ne $f->score) {
+      $self->throw("Inconsisent scores in feature array [$score - " . $f->score . "]");
+    }
+    if ($percent ne $f->percent_id) {
+      $self->throw("Inconsistent pids in feature array [$percent - " . $f->percent_id . "]");
+    }
+
+    my $start1 = $f->start;
+    my $start2;
+
+    if ($hstrand == 1) {
+       $start2 = $f->hstart;
+    } else {
+       $start2 = $f->hend;
+    }
+
+    if (defined($prev1)) {
+      if ($start1 < $prev1) {
+        $self->throw("Inconsistent coordinates in feature array $start1 < $prev1");
+      }
+    }
+
+    if (defined($prev2)) {
+      if ($hstrand == 1) {
+        if ($f->hstart < $prev2) {
+          $self->throw("Inconsistent coordinates in feature array " . $f->hstart . " < $prev2");
+        }
+      } else {
+        if ($f->hstart > $prev2) {
+          $self->throw("Inconsistent coordinates in feature array " . $f->hstart . " > $prev2");
+        }
+      }
+
+    }
+    my $length = ($f->end - $f->start + 1)*$hstrand;
+
+    $string = $string . $start1 . "," . $start2 . "," . $length . ":";
+
+    $prev1 = $f->end;
+
+    if ($hstrand == 1) {
+      $prev2 = $f->hend;
+    } else {
+      $prev2 = $f->hstart;
+    }
+
+  }
+
+  print STDERR "Making feature pair\n";
+  $string =~ s/:$//;
+
+  my $feature1 = new Bio::EnsEMBL::SeqFeature();
+ 
+  $feature1->start($f1start);
+  $feature1->end  ($f1end);
+  $feature1->strand($hstrand);
+  $feature1->score($score);
+  $feature1->percent_id($percent);
+  $feature1->p_value($pvalue);
+  $feature1->seqname($name);
+  $feature1->percent_id($percent);
+  $feature1->p_value($pvalue);
+  $feature1->phase($phase);
+  $feature1->analysis($analysis);
+
+  my $feature2 = new Bio::EnsEMBL::SeqFeature();
+ 
+  $feature2->start($f2start);
+  $feature2->end  ($f2end);
+  $feature2->strand($hstrand);
+  $feature2->score($score);
+  $feature2->seqname($hname);
+  $feature2->percent_id($percent);
+  $feature2->p_value($pvalue);
+  $feature2->phase($phase);
+  $feature2->analysis($analysis);
+
+  $self->feature1($feature1);
+  $self->feature2($feature2);
+  $self->cigar_string($string);
+
 }
 
 =head2 ungapped_features
@@ -245,8 +470,8 @@ sub _parse_cigar {
 sub ungapped_features {
   my ($self) = @_;
 
-  if (defined($self->cigar)) {
-    my @features = $self->_parse_cigar_string;
+  if (defined($self->cigar_string)) {
+    my @features = $self->_parse_cigar;
     return @features;
   } else {
     $self->throw("No cigar_string defined.  Can't return ungapped features");
