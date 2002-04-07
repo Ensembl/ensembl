0eabefe43837c916d7c02483ad69fef8f9d7cdb7
Emmanuel Mongin <mongin@sanger.ac.uk>
2002-04-07 13:53:02 +0000

    Add a avalue to the method interpro_ac

--- modules/Bio/EnsEMBL/DBSQL/Protein_Feature_Adaptor.pm
+++ modules/Bio/EnsEMBL/DBSQL/Protein_Feature_Adaptor.pm
@@ -206,7 +206,7 @@ sub fetch_by_feature_and_dbID{
 #The call has to be specific for the Interpro components because there is one join to make on the interpro table and then with the Xref table
     if (($feature eq "PRINTS") || ($feature eq "Pfam") || ($feature eq "PROSITE") || ($feature eq "PROFILE")) {
 #      &eprof_start('interpro');
-	my $sth = $self->prepare ("select p.seq_start, p.seq_end, p.analysis, p.score, p.perc_id, p.evalue, p.hstart, p.hend, p.hid, x.display_id from protein_feature p,interpro i,analysisprocess a, Xref x  where p.translation = '$transl' and i.id = p.hid and i.interpro_ac = x.dbprimary_id and p.analysis = a.analysisId and a.gff_feature = 'domain' and a.gff_source = '$feature'");
+	my $sth = $self->prepare ("select p.seq_start, p.seq_end, p.analysis, p.score, p.perc_id, p.evalue, p.hstart, p.hend, p.hid, x.display_id, x.dbprimary_id from protein_feature p,interpro i,analysisprocess a, Xref x  where p.translation = '$transl' and i.id = p.hid and i.interpro_ac = x.dbprimary_id and p.analysis = a.analysisId and a.gff_feature = 'domain' and a.gff_source = '$feature'");
 	
 	$sth->execute();
 	
@@ -215,7 +215,7 @@ sub fetch_by_feature_and_dbID{
 	
 	    
     
-	    my ($start,$end,$analysisid,$score,$perc_id,$evalue,$hstart,$hend,$hid,$desc) = @{$arrayref};
+	    my ($start,$end,$analysisid,$score,$perc_id,$evalue,$hstart,$hend,$hid,$desc,$interproac) = @{$arrayref};
 
 
 	if( !defined $anahash{$analysisid} ) {
@@ -240,6 +240,7 @@ sub fetch_by_feature_and_dbID{
 								-feature2 => $feat2,);
 	    
 	    $feature->idesc($desc);
+	    $feature->interpro_ac($interproac);
 	    
 	    if ($feature) {
 		push(@features,$feature);
