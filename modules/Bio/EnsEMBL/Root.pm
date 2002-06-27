bb606ef44b2d299da79f3f7cc918c5dbf8c42966
cvs2git <cvs2git>
2002-06-27 16:30:42 +0000

    This commit was manufactured by cvs2svn to create branch 'branch-baseline'.
    Sprout from branch-ensembl-6 2002-06-27 16:30:41 UTC Web Admin <w3adm@sanger.ac.uk> 'We made a major error and committed new schema api on the old schema branch.  This is an older version re-committed to patch our mistake.'
    Cherrypick from master 2002-04-18 13:14:36 UTC Ewan Birney <birney@sanger.ac.uk> 'minor fixes to make sure 1.0 to 0.7 compatibility remains there':
        modules/Bio/EnsEMBL/Root.pm

---
+++ modules/Bio/EnsEMBL/Root.pm
@@ -0,0 +1,38 @@
+
+=head1 DESCRIPTION
+
+This package provides a bare-bones root class for Ensembl objects to
+inheriet from. In particular it allows us to put some magic in here
+to make sure we can manage 1.0 <-> 0.7 transitions for bioperl
+
+=cut
+
+
+
+package Bio::EnsEMBL::Root;
+
+use Bio::Root::RootI;
+
+@ISA = qw(Bio::Root::RootI);
+
+
+sub new{
+  my($caller,@args) = @_;
+  
+  my $self = {};
+  bless $self, $caller;
+
+  return $self;
+}
+
+
+
+sub DESTROY{
+
+  my ($self) = @_;
+
+
+  #print STDERR "destroying $self in Bio::EnsEMBL::Root\n";
+  
+
+}
