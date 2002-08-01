31d6a786355702340db99644ceab5393ec77f4f5
Eduardo Eyras <eae@sanger.ac.uk>
2002-08-01 21:26:38 +0000

    end_phase is not left anymore in undefined and is passed explicitly

--- modules/Bio/EnsEMBL/DBSQL/ExonAdaptor.pm
+++ modules/Bio/EnsEMBL/DBSQL/ExonAdaptor.pm
@@ -187,6 +187,7 @@ sub _exon_from_sth {
     $sticky_str    .= $component->seq->seq;
 
     $exon->phase($component->phase);
+    $exon->end_phase($component->end_phase);
     $exon->adaptor($self);
 
     # continue while loop until we hit sticky_rank 1
@@ -235,7 +236,7 @@ sub _new_Exon_from_hashRef {
    $exon->end( $hashRef->{'contig_end'} );
    $exon->strand( $hashRef->{'contig_strand'} );
    $exon->phase( $hashRef->{phase} );
-#    $exon->end_phase( $hashRef->{end_phase} );
+   $exon->end_phase( $hashRef->{end_phase} );
    $exon->dbID($hashRef->{'exon_id'});
    $exon->sticky_rank($hashRef->{'sticky_rank'});
    $exon->adaptor($self);
