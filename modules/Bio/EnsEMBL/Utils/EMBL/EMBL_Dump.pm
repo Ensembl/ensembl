bd9e09fdb65fc600ee9184247c2acbd533f500e5
Graham McVicker <mcvicker@sanger.ac.uk>
2002-09-30 10:59:41 +0000

    Moved EMBL_Dump and EMBL_Dump_Sanger modules, Added GeneWrapper and SliceWrappers so embl dump works with new main trunk

---
+++ modules/Bio/EnsEMBL/Utils/EMBL/EMBL_Dump.pm
@@ -0,0 +1,259 @@
+
+#
+# BioPerl module for Bio::EnsEMBL::EMBL_Dump.pm
+#
+# Cared for by Ewan Birney <birney@sanger.ac.uk>
+#
+# Copyright Ewan Birney
+#
+# You may distribute this module under the same terms as perl itself
+
+# POD documentation - main docs before the code
+
+=head1 NAME
+
+Bio::EnsEMBL::EMBL_Dump.pm - A collection of variables, subroutines and modifers for EMBL dumping
+
+=head1 SYNOPSIS
+
+    use Bio::EnsEMBL::EMBL_Dump;
+
+    # adds standard comments, feature table source lines etc for embl
+    &Bio::EnsEMBL::EMBL_Dump::add_ensembl_comments($aseq); 
+    
+    # adds standard dumping information to the aseqstream to drive off the
+    # the annseq objects
+    &Bio::EnsEMBL::EMBL_Dump::ensembl_annseq_output($aseqstream);
+
+    #
+    # Complete dumping from a clone object
+    #
+
+    my $as = $clone->get_AnnSeq();
+    &Bio::EnsEMBL::EMBL_Dump::add_ensembl_comments($as);
+    
+    my $emblout = Bio::AnnSeqIO->new( -format => 'EMBL', -fh => \*STDOUT);
+    &Bio::EnsEMBL::EMBL_Dump::ensembl_annseq_output($emblout);    
+    
+    $emblout->write_annseq($as);
+
+
+=head1 DESCRIPTION
+
+We need this information in a module because more than one script uses it. However,
+the information here is really just comments etc to add onto or manipulate embl
+dumping. There is no "object" here - it is just a placeholder for all this information.
+
+=head1 CONTACT
+
+Describe contact details here
+
+=head1 APPENDIX
+
+The rest of the documentation details each of the object methods. Internal methods are usually preceded with a _
+
+=cut
+
+
+# Let the code begin...
+
+
+package Bio::EnsEMBL::Utils::EMBL::EMBL_Dump;
+use vars qw( @ISA @EXPORT_OK );
+use Exporter;
+use strict;
+use Carp;
+use Bio::Species;
+
+@ISA = ('Exporter');
+
+# Some of these subroutines are used
+# by Bio::EnsEMBL::EMBL_Dump_Sanger
+@EXPORT_OK = qw(
+                add_ensembl_comments
+                ensembl_annseq_output
+                id_EnsEMBL
+                kw_EnsEMBL
+                sv_EnsEMBL
+                ac_EnsEMBL
+                sort_FTHelper_EnsEMBL
+                );
+
+
+
+=head2 add_ensembl_comments
+
+ Title   : add_ensembl_comments
+ Usage   : &Bio::EnsEMBL::EMBL_Dump::add_ensembl_comments($aseq);
+ Function: adds ensembl comments for
+             - de line - reannotated clone
+             - comment about ensembl
+             - comment interpreting ensembl flat files
+ Example :
+ Returns : 
+ Args    :
+
+
+=cut
+
+sub add_ensembl_comments {
+   my ($aseq) = @_;
+
+   if( !$aseq  ) {
+       confess "Ok. No aseq passed into EMBL_Dump and I can't even throw a nice exception!";
+   }
+
+   $aseq->desc("Reannotated sequence via Ensembl");
+   my $comment = Bio::Annotation::Comment->new();
+   
+   $comment->text("This sequence was reannotated via the Ensembl system. Please visit the Ensembl web site, http://www.ensembl.org/ for more information.");
+   $aseq->annotation->add_Comment($comment);
+
+   $comment = Bio::Annotation::Comment->new();
+   $comment->text("The reference, comment, description and feature table of the original entry can be found in the DDBJ/EMBL/GenBank database with the identical accession number.");
+   $aseq->annotation->add_Comment($comment);
+   
+   $comment = Bio::Annotation::Comment->new();
+   $comment->text("The /gene indicates a unique id for a gene, /cds a unique id for a translation and a /exon a unique id for an exon. These ids are maintained wherever possible between versions. For more information on how to interpret the feature table, please visit http://www.ensembl.org/Docs/embl.html.");
+
+   $aseq->annotation->add_Comment($comment);
+
+   $comment = Bio::Annotation::Comment->new();
+   $comment->text("All the exons and transcripts in Ensembl are confirmed by similarity to either protein or cDNA sequences.");
+   $aseq->annotation->add_Comment($comment); 
+
+   $comment = Bio::Annotation::Comment->new();
+   $comment->text("In unfinished, rough draft DNA sequence gene structures can cross fragments and, in these cases, the order and orientation of the fragments is likely to be different from the order in the the International Nucleotide Sequence Databases DDBJ/EMBL/GenBank.");
+
+   $aseq->annotation->add_Comment($comment); 
+  
+   # done!
+}
+
+=head2 ensembl_annseq_output
+
+ Title   : ensembl_annseq_output
+ Usage   :
+ Function:
+ Example :
+ Returns : 
+ Args    :
+
+
+=cut
+
+sub ensembl_annseq_output {
+   my ($aseqstream) = @_;
+
+   if( !$aseqstream  ) {
+       confess "Ok. No aseq passed into EMBL_Dump and I can't even throw a nice exception!";
+   }
+
+   if( !$aseqstream->isa('Bio::SeqIO') ) {
+       $aseqstream->throw("not got EMBL IO but a $aseqstream. Not going to add output functions");
+   }
+
+   if( $aseqstream->can('_post_sort') ) {
+       $aseqstream->_post_sort(\&sort_FTHelper_EnsEMBL);
+   }
+
+   # attach ensembl specific dumping functions
+   $aseqstream->_id_generation_func(\&id_EnsEMBL);
+   $aseqstream->_kw_generation_func(\&kw_EnsEMBL);
+   $aseqstream->_sv_generation_func(\&sv_EnsEMBL);
+   $aseqstream->_ac_generation_func(\&ac_EnsEMBL);
+   
+}
+
+#########################
+# sub routines
+#########################
+
+sub id_EnsEMBL {
+    my ($annseq,$type) = @_;
+
+    #return $annseq->id;
+
+    # JGRG - is this correct?  I thought phase 3 was HUM.
+    my $division = $annseq->htg_phase == 4 ? 'HUM' : 'HTG';
+    my $length = $annseq->length();
+    my $id = $annseq->id();
+
+    # yuk. Don't look
+    my @dates = $annseq->each_date();
+    my $date = shift @dates;
+    $date =~ s/\(.*\)//g;
+    $type ||='';		
+    if( $type eq 'genbank' ) {
+	return sprintf("%-9s  %d bp  DNA   %s   %s",$id,$length,$division,$date);
+    }
+
+
+    return sprintf("%-9s  ENSEMBL; DNA; %s; %d BP.", $id, $division, $length );
+}
+
+
+sub kw_EnsEMBL {
+   my ($annseq) = @_;
+
+   if( !$annseq->can('htg_phase') ) {
+       return "ENSEMBL";
+   }
+
+   if( $annseq->htg_phase == 4 ) {
+       return "HTG.";
+   }
+
+   return "HTG; HTGS_PHASE" . $annseq->htg_phase() . ".";
+}
+
+sub sv_EnsEMBL {
+   my ($annseq) = @_;
+
+   if( ! $annseq->can('sv') || ! $annseq->sv ) {
+       return "NO_SV_NUMBER";
+   }
+   if( $annseq->sv == -1 ) {
+       return undef;
+   }
+
+   return $annseq->id() . "." . $annseq->sv;
+}
+
+sub ac_EnsEMBL {
+   my ($annseq) = @_;
+
+   return $annseq->id();
+}
+
+
+BEGIN {
+    # A value of 0 is illegal in %sort_order
+    my %sort_order = (
+        source          => 1,
+        CDS             => 2,
+        exon            => 3,
+        repeat_region   => 4,
+    );
+
+    # $last is one more than the largest value in %sort_order
+    my $last = (sort {$b <=> $a} values %sort_order)[0] + 1;
+
+    sub sort_FTHelper_EnsEMBL {
+        my $a = shift;
+        my $b = shift;
+
+        my $a_ord = $sort_order{$a->key} || $last;
+        my $b_ord = $sort_order{$b->key} || $last;
+
+        my ($a_5prime) = $a->loc =~ /(\d+)/;
+        my ($b_5prime) = $b->loc =~ /(\d+)/;
+
+        # Features are sorted by location if they don't
+        # sort by their keys.
+        return $a_ord <=> $b_ord  || $a_5prime <=> $b_5prime;
+    }
+}
+
+
+1;
---
+++ modules/Bio/EnsEMBL/Utils/EMBL/GeneWrapper.pm
@@ -0,0 +1,298 @@
+#
+# Ensembl module for Bio::EnsEMBL::Utils::EMBL::GeneWrapper
+#
+# Cared for by Ewan Birney <ensembl-dev@ebi.ac.uk>
+#
+# Copyright EMBL / EBI
+#
+# You may distribute this module under the same terms as perl itself
+
+# POD documentation - main docs before the code
+
+=head1 NAME
+
+Bio::EnsEMBL::Utils::EMBL::GeneWrapper
+
+=head1 SYNOPSIS
+
+   foreach $gene ( $slice->get_all_Genes ) {
+     push @seq_feats, new Bio::EnsEMBL::Utils::EMBL::GeneWrapper($gene);
+   }
+       
+
+=head1 DESCRIPTION
+
+Allows genes to be EMBL dumped as though they were seq features (which in 
+EnsEMBL they are not).  Essentially a workaround to force ensembl to fit 
+the Bioperl embl dumping code. This wrapper is a replacement for some of the
+old VirtualGene functionality.
+
+=head1 AUTHOR - Graham McVicker
+
+This modules is part of the Ensembl project http://www.ensembl.org
+
+Email ensembl-dev@ebi.ac.uk
+
+
+=head1 APPENDIX
+
+The rest of the documentation details each of the object methods. Internal 
+methods are usually preceded with a _
+
+=cut
+
+
+use strict;
+
+package Bio::EnsEMBL::Utils::EMBL::GeneWrapper;
+
+use Bio::SeqIO::FTHelper;
+use Bio::EnsEMBL::Root;
+use vars qw(@ISA);
+
+@ISA = qw(Bio::EnsEMBL::Root);
+
+
+=head2 new
+
+  Arg [1]    : Bio::EnsEMBL::Gene
+  Example    : $gw = new Bio::EnsEMBL::Utils::EMBL::GeneWrapper($gene)
+  Description: Creates a new GeneWrapper 'wrapped' around a gene object to
+               allow it to be embl dumped. 
+  Returntype : Bio::EnsEMBL::Utils::EMBL::GeneWrapper
+  Exceptions : none
+  Caller     : Embl dumping scripts
+
+=cut
+
+sub new {
+  my ($class, $gene) = @_;
+
+  my $self = $class->SUPER::new();
+
+  unless($gene && $gene->isa('Bio::EnsEMBL::Gene')) {
+    $self->throw("Gene argument must be a Bio::EnsEMBL::Gene");
+  }
+
+  $self->gene($gene);
+  $self->{_strict_embl_dumping} = 0;
+
+  return $self;
+}
+
+
+=head2 gene
+
+  Arg [1]    : (optional) Bio::EnsEMBL::Gene $gene
+  Example    : $gene = $gene_wrapper->gene();
+  Description: getter/setter fot the gene contained inside this wrapper
+  Returntype : Bio::EnsEMBL::Gene
+  Exceptions : thrown if the $gene arg is not a Bio::EnsEMBL::Gene
+  Caller     : internal
+
+=cut
+
+sub gene {
+  my ($self, $gene) = @_;
+
+  if($gene) {
+    unless($gene->isa('Bio::EnsEMBL::Gene')) {
+      $self->throw("[$gene] is not a Bio::EnsEMBL::Gene");
+    }
+    
+    $self->{_gene} = $gene;
+  }
+
+  return $self->{_gene};
+}
+
+
+=head2 to_FTHelper
+
+  Arg [1]    : none
+  Example    : none
+  Description: Wrapper function - 
+               This is where all the embl dumping magic around genes takes 
+               place. Most of the embl output for ensembl genes is defined 
+               here.
+  Returntype : list of Bio::SeqIO::FTHelper
+  Exceptions : none
+  Caller     : Bio::SeqIO::embl
+
+=cut
+
+sub to_FTHelper {
+  my $self = shift;
+
+  my @out;
+
+  my @dblinks = $self->gene->each_DBLink();
+
+  foreach my $trans ($self->gene->get_all_Transcripts()) {
+    my $join = "";
+    
+    foreach my $exon ($trans->translateable_exons()) {
+      $join .= ',' if($join); #append a comma to the last coord set
+
+      if($exon->strand() == 1) {
+	$join .= $exon->start()."..".$exon->end();
+      } else {
+	$join .= "complement(".$exon->start()."..".$exon->end().")";
+      }
+    }
+
+    my $ft = Bio::SeqIO::FTHelper->new();
+    $ft->loc("join(".$join.")");
+    $ft->key('CDS');
+
+    $ft->add_field('translation', $trans->translate()->seq());
+    $ft->add_field('cds', $trans->translation->stable_id());
+    $ft->add_field('gene', $self->gene->stable_id());
+    $ft->add_field('transcript', $trans->stable_id());
+    foreach my $dbl (@dblinks) {
+      $ft->add_field('db_xref', $dbl->database().":".$dbl->primary_id());
+    }
+    push(@out, $ft);
+  }
+
+  foreach my $exon ($self->gene->get_all_Exons()) {
+    my $ft = Bio::SeqIO::FTHelper->new();
+
+    if( $exon->strand() == 1 ) {
+      $ft->loc($exon->start."..".$exon->end());
+    } else {
+      $ft->loc("complement(".$exon->start()."..".$exon->end().")");
+    }
+
+    $ft->key("exon");
+    
+    if( $self->strict_EMBL_dumping()) {
+      $ft->add_field('db_xref', 'ENSEMBL:HUMAN-Exon-'.$exon->stable_id());
+    } else {
+      $ft->add_field('exon_id', $exon->stable_id());
+      $ft->add_field('start_phase', $exon->phase());
+      $ft->add_field('end_phase', $exon->phase());
+    }
+    
+    push (@out, $ft);
+  }
+  
+  return @out;
+}
+
+  
+=head2 strict_EMBL_dumping
+
+  Arg [1]    : optional boolean $newval
+  Example    : none
+  Description: Getter/ setter for strict embl dumping attribute which 
+               determines whether embl dumping will be 'strict'. Default is 0.
+  Returntype : boolean
+  Exceptions : none
+  Caller     : to_FTHelper
+
+=cut
+
+sub strict_EMBL_dumping {
+  my ($self, $newval) = @_;
+  
+  if(defined $newval) {
+    $self->{_strict_embl_dumping} = $newval;
+  }
+
+  return $self->{_strict_embl_dumping};
+}
+    
+
+=head2 source_tag
+
+  Arg [1]    : none
+  Example    : none
+  Description: Wrapper function - returns a source tag for the gene
+  Returntype : none
+  Exceptions : none
+  Caller     : Bio::SeqIO::embl
+
+=cut
+
+sub source_tag {
+  my ($self, @args) = @_;
+
+  return "ensembl";
+}
+
+
+=head2 primary_tag
+
+  Arg [1]    : none
+  Example    : none
+  Description: Wrapper function returns a primary tag for thie gene
+  Returntype : string
+  Exceptions : none
+  Caller     : Bio::SeqIO::embl
+
+=cut
+
+sub primary_tag {
+  my ($self, @args) = @_;
+
+  return "genefragment";
+}
+
+
+=head2 has_tag
+
+  Arg [1]    : none
+  Example    : none
+  Description: Wrapper function - always returns 0
+  Returntype : none
+  Exceptions : none
+  Caller     : Bio::SeqIO::embl
+
+=cut
+
+sub has_tag {
+  my $self = shift;
+
+  return 0;
+}
+
+
+=head2 all_tags
+
+  Arg [1]    : none
+  Example    : none
+  Description: Wrapper function - does nothing
+  Returntype : none
+  Exceptions : none
+  Caller     : Bio::SeqIO::embl
+
+=cut
+
+sub all_tags {
+  my $self = shift;
+
+  return;
+}
+
+
+=head2 each_tag_value
+
+  Arg [1]    : none
+  Example    : none
+  Description: Wrapper function - returns empty list
+  Returntype : empty list
+  Exceptions : none
+  Caller     : Bio::SeqIO::embl
+
+=cut
+
+sub each_tag_value {
+  my $self = shift;
+
+  return ();
+}
+
+
+
+1;
