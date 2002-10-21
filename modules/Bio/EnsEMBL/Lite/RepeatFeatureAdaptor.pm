95f1871205b774dea49246b6f79a73c0d0c88d08
Graham McVicker <mcvicker@sanger.ac.uk>
2002-10-21 09:12:16 +0000

    Lite RepeatFeatureAdaptor now works for negative slices

--- modules/Bio/EnsEMBL/Lite/RepeatFeatureAdaptor.pm
+++ modules/Bio/EnsEMBL/Lite/RepeatFeatureAdaptor.pm
@@ -55,7 +55,7 @@ my $MAX_REPEAT_LENGTH = 100000;
 
 =cut
 
-sub fetch_by_all_Slice_and_type {
+sub fetch_all_by_Slice_and_type {
   my ($self, $slice, $type) = @_;
  
   my $sth = $self->prepare(
@@ -68,53 +68,20 @@ sub fetch_by_all_Slice_and_type {
 		 $slice->chr_start() - $MAX_REPEAT_LENGTH, 
 		 $slice->chr_start(), $type );
 
-  my @repeats = ();
-
-  my ($rc, $core, $repeat_adaptor, $rc_adaptor);
-  my @repeats;
-  my($rc, $core, $repeat_adaptor, $rc_adaptor);
-  my $slice_start = $slice->chr_start + 1;
-  my %rc_hash;
-
-  if($core = $self->db()->get_db_adaptor('core')) {
-    $repeat_adaptor = $core->get_RepeatFeatureAdaptor();
-    $rc_adaptor = $core->get_RepeatConsensusAdaptor();
-  } else {
-    $repeat_adaptor = $self;
-    $self->warn("Core Database not attached to lite database.  Not able to 
-                 Retrieve repeat consensus adaptor for repeat features\n");
-  }
-
-  my ($id, $hid, $chr_name, $start, $end, $strand);
-  $sth->bind_columns(\$id, \$hid, \$chr_name, \$start, \$end, \$strand);
-
-  while($sth->fetch) {
-    #cache repeat_consensi to reduce object construction overhead
-    unless($rc = $rc_hash{$hid}) {
-      $rc = new Bio::EnsEMBL::RepeatConsensus();
-      $rc->name($hid);
-      if($rc_adaptor) {
-	$rc->adaptor($rc_adaptor);
-      }
-      $rc_hash{$hid} = $rc;
-    }
-
-    #create partially filled repeat object using fast (hacky) constructor 
-    push @repeats, Bio::EnsEMBL::RepeatFeature->new_fast(
-			   { '_gsf_tag_hash'  => {},
-			     '_gsf_sub_array' => [],
-			     '_parse_h'       => {},
-                             '_gsf_start'         => $start - $slice_start,
-                             '_gsf_end'           => $end - $slice_start,
-			     '_gsf_strand'        => $strand,
-			     '_adaptor'       => $repeat_adaptor,
-			     '_repeat_consensus' => $rc,
-			     '_db_id'         => $id } );
-  }
-
-  return \@repeats;
+  return $self->_objs_from_sth($sth, $slice);
 }
 
+
+=head2 fetch_all_by_Slice
+
+  Arg [1]    : Bio::EnsEMBL::Slice $slice 
+  Example    : $repeats = $lite_repeat_apaptor->fetch_all_by_Slice($slice);
+  Description: Retrieves a list of RepeatFeatures from the lite database.
+  Returntype : Bio::EnsEMBL::RepeatFeature
+  Exceptions : none
+  Caller     : ProxyRepeatFeatureAdaptor (via Slice via contigview)
+
+=cut
 
 sub fetch_all_by_Slice {
     my ( $self, $slice ) = @_;
@@ -129,51 +96,91 @@ sub fetch_all_by_Slice {
 		   $slice->chr_start() - $MAX_REPEAT_LENGTH, 
 		   $slice->chr_start() );
     
-    my @repeats = ();
-    my($rc, $core, $repeat_adaptor, $rc_adaptor);
-    my $slice_start = $slice->chr_start + 1;
-    my %rc_hash;
+    return $self->_objs_from_sth($sth, $slice);
+}
 
-    if($core = $self->db()->get_db_adaptor('core')) {
-      $repeat_adaptor = $core->get_RepeatFeatureAdaptor();
-      $rc_adaptor = $core->get_RepeatConsensusAdaptor();
-    } else {
-      $repeat_adaptor = $self;
-      $self->warn("Core Database not attached to lite database.  Not able to 
+
+
+=head2 _objs_from_sth
+
+  Arg [1]    : DBI:st $sth 
+               An executed DBI statement handle
+  Arg [2]    : Bio::EnsEMBL::Slice $slice
+               The slice to map the coordinates to
+  Example    : $repeats = $self->_objs_from_sth($sth, $slice);
+  Description: Creates a list of repeat objects from an executed DBI statement
+               handle.
+  Returntype : list reference to Bio::EnsEMBL::RepeatFeatures
+  Exceptions : none
+  Caller     : internal
+
+=cut
+
+
+sub _objs_from_sth {
+  my ($self, $sth, $slice) = @_;
+
+  my @repeats = ();
+  my($rc, $core, $repeat_adaptor, $rc_adaptor);
+  my $slice_start = $slice->chr_start;
+  my $slice_end   = $slice->chr_end;
+  my $slice_strand = $slice->strand;
+  my %rc_hash;
+
+  if($core = $self->db->get_db_adaptor('core')) {
+    $repeat_adaptor = $core->get_RepeatFeatureAdaptor();
+    $rc_adaptor = $core->get_RepeatConsensusAdaptor();
+  } else {
+    $repeat_adaptor = $self;
+    $self->warn("Core Database not attached to lite database.  Not able to 
                    Retrieve repeat consensus adaptor for repeat features\n");
+  }
+
+  my ($id, $hid, $chr_name, $start, $end, $strand);
+  $sth->bind_columns(\$id, \$hid, \$chr_name, \$start, \$end, \$strand);
+
+  my ($feat_start, $feat_end, $feat_strand);
+  
+  while($sth->fetch) {
+    #skip features that are entirely outsied the slice area
+    next if ($start > $slice_end || $end < $slice_start);
+
+    #cache repeat_consensi to reduce object construction overhead
+    unless($rc = $rc_hash{$hid}) {
+      $rc = new Bio::EnsEMBL::RepeatConsensus();
+      $rc->name($hid);
+      if($rc_adaptor) {
+	$rc->adaptor($rc_adaptor);
+      }
+      $rc_hash{$hid} = $rc;
     }
 
-    my ($id, $hid, $chr_name, $start, $end, $strand);
-    $sth->bind_columns(\$id, \$hid, \$chr_name, \$start, \$end, \$strand);
+    #convert chromosomal coordinates to slice coordinates
+    if($slice_strand == 1) {
+      $feat_start  = $start - $slice_start + 1;
+      $feat_end    = $end   - $slice_start   + 1;
+      $feat_strand = $strand;
+    } else {
+      $feat_start  = $slice_end - $end   + 1;
+      $feat_end    = $slice_end - $start + 1;
+      $feat_strand = $strand * -1;
+    }
 
-    while($sth->fetch) {
-      #cache repeat_consensi to reduce object construction overhead
-      unless($rc = $rc_hash{$hid}) {
-	$rc = new Bio::EnsEMBL::RepeatConsensus();
-	$rc->name($hid);
-	if($rc_adaptor) {
-	  $rc->adaptor($rc_adaptor);
-	}
-	$rc_hash{$hid} = $rc;
-      }
-
-      #create partially filled repeat object using fast (hacky) constructor 
-      push @repeats, Bio::EnsEMBL::RepeatFeature->new_fast(
+    #create partially filled repeat object using fast (hacky) constructor 
+    push @repeats, Bio::EnsEMBL::RepeatFeature->new_fast(
 			   { '_gsf_tag_hash'  => {},
 			     '_gsf_sub_array' => [],
 			     '_parse_h'       => {},
-                             '_gsf_start'         => $start - $slice_start,
-                             '_gsf_end'           => $end - $slice_start,
-			     '_gsf_strand'        => $strand,
+                             '_gsf_start'         => $feat_start,
+                             '_gsf_end'           => $feat_end,
+			     '_gsf_strand'        => $feat_strand,
 			     '_adaptor'       => $repeat_adaptor,
 			     '_repeat_consensus' => $rc,
 			     '_db_id'         => $id } );
     }
 
-
-    return \@repeats; 
+  return \@repeats; 
 }
-
 
 
 =head2 fetch_by_Slice_and_type
