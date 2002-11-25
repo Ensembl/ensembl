8af84e631904646a0b7c971719141badc1249c47
Emmanuel Mongin <mongin@sanger.ac.uk>
2002-11-25 14:23:12 +0000

    Corectly retrieves synonyms

--- modules/Bio/EnsEMBL/DBSQL/DBEntryAdaptor.pm
+++ modules/Bio/EnsEMBL/DBSQL/DBEntryAdaptor.pm
@@ -195,8 +195,8 @@ sub store {
 	( $dbX ) = $sth->fetchrow_array();
 	
 	# synonyms
-	my @synonyms = $exObj->get_all_synonyms();
-	foreach my $syn ( @synonyms ) {
+	my $synonyms = $exObj->get_all_synonyms();
+	foreach my $syn ( @{$synonyms} ) {
 	    
 #Check if this synonym is already in the database for the given primary id
 	    my $sth = $self->prepare( "
