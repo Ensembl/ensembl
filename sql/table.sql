5457e0c10357ec8a21c30183bb0c04164e6c6ef1
Graham McVicker <mcvicker@sanger.ac.uk>
2003-03-14 11:16:57 +0000

    added qtl tables

--- sql/table.sql
+++ sql/table.sql
@@ -747,3 +747,30 @@ CREATE TABLE mapfrag_mapset (
 ) TYPE=MyISAM;
 
 
+#
+# Tables for qtls
+#
+
+CREATE TABLE qtl (
+  qtl_id int unsigned auto_increment not null,
+  source_database enum("rat genome database") not null,
+  source_primary_id varchar(255) not null,
+  trait varchar(255) not null,
+  lod_score float,
+  flank_marker_id_1 int,
+  flank_marker_id_2 int,
+  peak_marker_id int,
+  primary key ( qtl_id ),
+  key trait_idx( trait )
+);
+
+CREATE TABLE qtl_feature (
+  chromosome_id int not null,
+  start int not null,
+  end int not null,
+  qtl_id int not null,
+  analysis_id int not null,
+  key( qtl_id ),
+  key loc_idx( chromosome_id, start )
+);
+
