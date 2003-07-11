4e7b9ad19ae4101d301fb9ff1ec44248b64aab24
Andreas Kusalananda Kähäri <ak4@sanger.ac.uk>
2003-07-11 12:41:18 +0000

    build.pl:  Program for building binary delta between two database revisions. apply.pl:  Program for applying above delta.
    ... plus README files.

---
+++ misc-scripts/binary_delta/apply.pl
@@ -0,0 +1,134 @@
+#!/usr/bin/perl -w
+
+# $Id$
+#
+# apply.pl
+#
+# A program that uses a previously created set of binary delta
+# files to produce a new revision of an ensembl database out of
+# an older revision of the same database.  The delta files must
+# have been built with the build.pl Perl program.
+#
+# See also apply.README
+#
+# Author: Andreas Kahari, <andreas.kahari@ebi.ac.uk>
+#
+
+use strict;
+use warnings;
+
+use Cwd;
+
+use File::Basename;
+use File::Copy;
+
+use Getopt::Std;
+
+use aux qw(:default :apply);
+
+my %opts;
+my $xdelta_cmd	= $opts{'c'} = 'xdelta';
+my $src_prefix	= $opts{'s'} = '.';
+my $dst_prefix	= $opts{'d'} = '.';
+
+if (!getopts('c:s:d:', \%opts)) {
+    usage_apply(\%opts);
+    die;
+}
+
+$xdelta_cmd = $opts{'c'};
+$src_prefix = $opts{'s'};
+$dst_prefix = $opts{'d'};
+
+if ($#ARGV != 2) {
+    usage_apply(\%opts);
+    die;
+}
+
+my $db = $ARGV[0];
+my $v1 = $ARGV[1]; my $v1_dir = sprintf "%s/%s_%s", $src_prefix, $db, $v1;
+my $v2 = $ARGV[2]; my $v2_dir = sprintf "%s/%s_%s", $src_prefix, $db, $v2;
+
+my $delta_dir = sprintf "%s/%s_%s_delta_%s", $dst_prefix, $db, $v1, $v2;
+
+die $! if (! -d $v1_dir);
+die $! if (! -d $delta_dir);
+
+if (-d $v2_dir) {
+    printf STDERR "Whoa, the directory '%s' already exists\n", $v2_dir;
+    die $!;
+}
+
+printf STDERR "Creating the directory '%s'\n", $v2_dir;
+mkdir($v2_dir) or dir $!;
+
+my $v1_all_size = 0;
+my $v2_all_size = 0;
+my $delta_all_size = 0;
+
+foreach my $info_file (glob($delta_dir . '/*.info')) {
+    my $base_name  = basename($info_file);
+
+    $base_name     =~ s/\.info$//;
+
+    my $v1_file    = sprintf "%s/%s", $v1_dir, $base_name;
+    my $v2_file    = sprintf "%s/%s", $v2_dir, $base_name;
+    my $delta_file = sprintf "%s/%s", $delta_dir, $base_name;
+
+    printf "Processing '%s'\n", $base_name;
+
+    open(INFO, $info_file) or die $!;
+
+    my $patch_command = <INFO>; chomp $patch_command;
+
+    my $v1_line = <INFO>; chomp $v1_line;
+    my ($v1_sum, $v1_size) = split /\s+/, $v1_line;
+
+    my $v2_line = <INFO>; chomp $v2_line;
+    my ($v2_sum, $v2_size) = split /\s+/, $v2_line;
+
+    my $delta_line = <INFO>; chomp $delta_line;
+    my ($delta_sum, $delta_size) = split /\s+/, $delta_line;
+
+    close INFO;
+
+    if ($v1_sum ne '(none)' && $v1_sum ne make_checksum($v1_file)) {
+	print "\tChecksum mismatch for old file\n";
+	print "\tCan not continue\n";
+	die;
+    } else {
+	print "\tChecksum ok for old file\n";
+    }
+
+    if ($delta_sum ne '(none)' && $delta_sum ne make_checksum($delta_file)) {
+	print "\tChecksum mismatch for delta file\n";
+	print "\tCan not continue\n";
+	die;
+    } else {
+	print "\tChecksum ok for delta file\n";
+    }
+
+    if ($patch_command eq 'PATCH') {
+	print "\tPatching file\n";
+	system($xdelta_cmd, 'patch', $delta_file, $v1_file, $v2_file);
+    } elsif ($patch_command eq 'ADD') {
+	print "\tAdding new file\n";
+	copy($delta_file, $v2_file);
+    } elsif ($patch_command eq 'COPY') {
+	print "\tCopying old file\n";
+	copy($v1_file, $v2_file);
+    } elsif ($patch_command eq 'ZIP') {
+	print "\tDecompressing compressed file\n";
+	do_decompress($delta_file, $v2_file);
+    } else {
+	warn "\tStrange patch command: $patch_command\n";
+    }
+
+    if ($v2_sum ne '(none)' && $v2_sum ne make_checksum($v2_file)) {
+	print "\tChecksum mismatch for new file\n";
+	print "\tCan not continue\n";
+	die;
+    } else {
+	print "\tChecksum ok for new file\n";
+    }
+}
---
+++ misc-scripts/binary_delta/build.pl
@@ -0,0 +1,143 @@
+#!/usr/bin/perl -w
+
+# $Id$
+#
+# build.pl
+#
+# A program that creates binary delta files containing the
+# differences between two revisions of an ensembl database.  The
+# delta files must be applied with the apply.pl Perl program.
+#
+# See also build.README
+#
+# Author: Andreas Kahari, <andreas.kahari@ebi.ac.uk>
+#
+
+use strict;
+use warnings;
+
+use Cwd;
+
+use File::Basename;
+use File::Copy;
+
+use Getopt::Std;
+
+use aux qw(:default :build);
+
+my %opts;
+my $xdelta_cmd	= $opts{'c'} = 'xdelta';
+my $src_prefix	= $opts{'s'} = '.';
+my $dst_prefix	= $opts{'d'} = '.';
+
+my $too_big	= 2_147_483_648;    # 2 Gb
+#my $too_big	=       307_200;    # 300 Kb (for debugging gzipping)
+
+if (!getopts('c:s:d:', \%opts)) {
+    usage_build(\%opts);
+    die;
+}
+
+$xdelta_cmd = $opts{'c'};
+$src_prefix = $opts{'s'};
+$dst_prefix = $opts{'d'};
+
+if ($#ARGV != 2) {
+    usage_usage(\%opts);
+    die;
+}
+
+my $db = $ARGV[0];
+my $v1 = $ARGV[1]; my $v1_dir = sprintf "%s/%s_%s", $src_prefix, $db, $v1;
+my $v2 = $ARGV[2]; my $v2_dir = sprintf "%s/%s_%s", $src_prefix, $db, $v2;
+
+my $delta_dir = sprintf "%s/%s_%s_delta_%s", $dst_prefix, $db, $v1, $v2;
+
+die $! if (! -d $v1_dir);
+die $! if (! -d $v2_dir);
+
+if (! -d $delta_dir) {
+    printf STDERR "Creating delta directory '%s'\n", $delta_dir;
+    mkdir($delta_dir) or die $!;
+}
+
+my $v1_all_size = 0;
+my $v2_all_size = 0;
+my $delta_all_size = 0;
+
+foreach my $v2_file (glob($v2_dir . '/*')) {
+    my $base_name  = basename($v2_file);
+    my $v1_file    = sprintf "%s/%s", $v1_dir, $base_name;
+    my $delta_file = sprintf "%s/%s", $delta_dir, $base_name;
+
+    printf "Processing '%s'\n", $base_name;
+    my $v1_sum = '(none)';
+    my $v2_sum;
+    my $delta_sum = '(none)';
+
+    print "\tCalculating checksum of new file\n";
+    $v2_sum = make_checksum($v2_file);
+
+    my $v1_size = 0;
+    my $v2_size = (stat $v2_file)[7];
+    my $delta_size = 0;
+
+    my $patch_command;
+
+    if (-f $v1_file) {
+	print "\tCalculating checksum of old file\n";
+	$v1_sum = make_checksum($v1_file);
+
+	$v1_size = (stat $v1_file)[7];
+
+	if ($v1_sum eq $v2_sum && $v1_size == $v2_size) {
+	    $patch_command = 'COPY';
+	    print "\tThe files are identical\n";
+	} elsif ($v1_size >= $too_big || $v2_size >= $too_big) {
+	    $patch_command = 'ZIP';
+	    print "\tFiles are huge, compressing new file\n";
+	    do_compress($v2_file, $delta_file);
+	} else {
+	    $patch_command = 'PATCH';
+	    print "\tCreating delta file\n";
+	    system($xdelta_cmd, 'delta', '-9',
+		$v1_file, $v2_file, $delta_file);
+	}
+    } else {
+	$patch_command = 'ADD';	    # use 'ZIP' here as well?
+	print "\tCopying file\n";
+	copy($v2_file, $delta_file);
+    }
+
+    if ($patch_command ne 'COPY') {
+	print "\tCalculating checksum of delta file\n";
+	$delta_sum = make_checksum($delta_file);
+	$delta_size = (stat $delta_file)[7];
+    }
+
+    print "\tWriting info file\n";
+    open(INFO, '>' . $delta_file . '.info') or die $!;
+    printf INFO "%s\n%s\t%d\n%s\t%d\n%s\t%d\n",
+	$patch_command,
+	$v1_sum, $v1_size,
+	$v2_sum, $v2_size,
+	$delta_sum, $delta_size;
+    close INFO;
+
+    $v1_all_size += $v1_size;
+    $v2_all_size += $v2_size;
+    $delta_all_size += $delta_size;
+
+    printf "This file:\nOld %s, New %s, Delta %s, Saved %s (%.2f%%)\n",
+	make_human_readable($v1_size),
+	make_human_readable($v2_size),
+	make_human_readable($delta_size),
+	make_human_readable($v2_size - $delta_size),
+	($v2_size == 0 ? 0 : 100 * (1.0 - $delta_size / $v2_size));
+    printf "Overall:\nOld %s, New %s, Delta %s, Saved %s (%.2f%%)\n\n",
+	make_human_readable($v1_all_size),
+	make_human_readable($v2_all_size),
+	make_human_readable($delta_all_size),
+	make_human_readable($v2_all_size - $delta_all_size),
+	($v2_all_size == 0 ? 0 : 100 * (1.0 - $delta_all_size / $v2_all_size));
+}
