c33424e2f725314582460a31d02d00f63e88f542
Graham McVicker <mcvicker@sanger.ac.uk>
2003-07-16 14:34:51 +0000

    modified go_xref table def

--- sql/table.sql
+++ sql/table.sql
@@ -548,8 +548,9 @@ CREATE TABLE identity_xref(
 CREATE TABLE go_xref (
   object_xref_id int(10) unsigned DEFAULT '0' NOT NULL,
   linkage_type enum('IC', 'IDA', 'IEA', 'IEP', 'IGI', 'IMP', 
-		    'IPI', 'ISS', 'NAS', 'ND', 'NR' , 'TAS' ) NOT NULL,
-  PRIMARY KEY (object_xref_id)
+		    'IPI', 'ISS', 'NAS', 'ND', 'NR' , 'TAS', 'NR') NOT NULL,
+  KEY (object_xref_id),
+  UNIQUE(object_xref_id, linkage_type)
 );
 
 
