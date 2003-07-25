ce8cd267bb0d3a8783b75f1d4213ccbc96f64ab2
Graham McVicker <mcvicker@sanger.ac.uk>
2003-07-25 22:15:14 +0000

    cleaned up documentation, converted object methods to static class methods & other minor changes

--- modules/Bio/EnsEMBL/Utils/Argument.pm
+++ modules/Bio/EnsEMBL/Utils/Argument.pm
@@ -1,16 +1,69 @@
-package Bio::EnsEMBL::Utils::Exception;
+# EnsEMBL module for Bio::EnsEMBL::Utils::Exception
+#
+#
 
-use vars qw(@ISA);
+=head1 NAME
+
+Bio::EnsEMBL::Utils::Argument - Utility functions for argument handling
+
+=head1 SYNOPSIS
+
+    use Bio::EnsEMBL::Utils::Argument qw(rearrange)
+
+    package Feature;
+
+    sub new {
+      my $class = shift;
+      my ($start, $end, $strand) = rearrange(['START', 'END', 'STRAND'],@_);
+      return bless({'start'=>$start, 'end'=>$end, 'strand'=>$strand}, $class);
+    }
+
+=head1 DESCRIPTION
+
+This is derived from the Bio::Root module in BioPerl.  The _rearrange
+object method taken from BioPerl has been renamed rearrange and is now a 
+static class method.  This method was originally written by Lincoln Stein, 
+and has since been refactored several times by various people (as described 
+below).
+
+It is recommended that this package be used instead of inheriting 
+unnecessarily from the Bio::EnsEMBL::Root or Bio::Root object.
+
+=head1 CONTACT
+
+Post questions to the EnsEMBL development list: ensembl-dev@ebi.ac.uk
+
+=head1 APPENDIX
+
+The rest of the documentation details exported static class methods. 
+
+=cut
+
+
 use strict;
+use warnings;
 
-=head2 _rearrange
+package Bio::EnsEMBL::Utils::Argument;
 
- Usage     : $object->_rearrange( array_ref, list_of_arguments)
+use Exporter;
+
+use vars qw(@ISA @EXPORT_OK);
+
+@ISA = qw(Exporter);
+
+@EXPORT_OK = qw(&rearrange);
+
+
+
+=head2 rearrange
+
+ Usage     : rearrange( array_ref, list_of_arguments)
  Purpose   : Rearranges named parameters to requested order.
- Example   : $self->_rearrange([qw(SEQUENCE ID DESC)],@param);
+ Example   : use Bio::EnsEMBL::Utils::Argument qw(rearrange);
+           : rearrange([qw(SEQUENCE ID DESC)],@param);
            : Where @param = (-sequence => $s, 
-	   :                 -id       => $i, 
-	   :	             -desc     => $d);
+	         :                 -id       => $i, 
+	         :	               -desc     => $d);
  Returns   : @params - an array of parameters in the requested order.
            : The above example would return ($s, $i, $d)
  Argument  : $order : a reference to an array which describes the desired
@@ -26,6 +79,8 @@ use strict;
  Source    : This function was taken from CGI.pm, written by Dr. Lincoln
            : Stein, and adapted for use in Bio::Seq by Richard Resnick and
            : then adapted for use in Bio::Root::Object.pm by Steve A. Chervitz.
+           : This has since been adapted as an exported static method in this 
+             class Bio::EnsEMBL::Utils::Argument 
  Comments  : (SAC)
            : This method may not be appropriate for method calls that are
            : within in an inner loop if efficiency is a concern.
@@ -39,31 +94,29 @@ use strict;
            : indicate that named parameters are being used.
            : Therefore, the ('me', 'blue') list will be returned as-is.
            :
-	   : Note that Perl will confuse unquoted, hyphenated tags as 
+           : Note that Perl will confuse unquoted, hyphenated tags as 
            : function calls if there is a function of the same name 
            : in the current namespace:
            :    -name => 'foo' is interpreted as -&name => 'foo'
-	   :
+           :
            : For ultimate safety, put single quotes around the tag:
-	   :    ('-name'=>'me', '-color' =>'blue');
+	         :    ('-name'=>'me', '-color' =>'blue');
            : This can be a bit cumbersome and I find not as readable
            : as using all uppercase, which is also fairly safe:
-	   :    (-NAME=>'me', -COLOR =>'blue');
-	   :
+           :    (-NAME=>'me', -COLOR =>'blue');
+	         :
            : Personal note (SAC): I have found all uppercase tags to
            : be more managable: it involves less single-quoting,
-           : the code is more readable, and there are no method naming conlicts.
+           : the code is more readable, and there are no method naming 
+           : conlicts.
            : Regardless of the style, it greatly helps to line
-	   : the parameters up vertically for long/complex lists.
-
-See Also   : L<_initialize>() 
+	         : the parameters up vertically for long/complex lists.
 
 =cut
 
 
 sub rearrange {
-
-  my($self,$order,@param) = @_;
+  my($order,@param) = @_;
 
   return unless @param;
 
--- modules/Bio/EnsEMBL/Utils/Exception.pm
+++ modules/Bio/EnsEMBL/Utils/Exception.pm
@@ -1,192 +1,206 @@
-package Bio::EnsEMBL::Utils::Exception;
+# EnsEMBL module for Bio::EnsEMBL::Utils::Exception
+#
+#
+
+=head1 NAME
+
+Bio::EnsEMBL::Utils::Exception - Utility functions for error handling
+
+=head1 SYNOPSIS
+
+    use Bio::EnsEMBL::Utils::Exception qw(throw warning deprecate verbose)
+
+    eval {
+      throw("this is an exception with a stack trace");
+    );
+    
+    if($@) {
+      print "Caught exception:\n$@";
+    }
+
+    #silence warnings
+    verbose(-1);
+
+    warning('this is a silent warning');
+
+    verbose(0);
+   
+    warning('this is a warning');
+
+    sub my_sub {
+      deprecate('use other_sub() instead');
+    }
+
+
+=head1 DESCRIPTION
+
+This is derived from the Bio::Root module in BioPerl.  Some formatting has
+been changed and the deprecate function has been added.  Most notably the
+object methods are now static class methods that can be called without 
+inheriting from Bio::Root or Bio::EnsEMBL::Root.  This is especially useful
+for throwing exceptions with stack traces outside of a blessed context.
+
+The originaly implementations of these methods were by Steve Chervitz and
+refactored by Ewan Birney.
+
+It is recommended that these functions be used instead of inheriting 
+unnecessarily from the Bio::EnsEMBL::Root or Bio::Root object.
+The functions exported by this package provide a set of useful error handling
+methods.
+
+=head1 CONTACT
+
+Post questions to the EnsEMBL development list: ensembl-dev@ebi.ac.uk
+
+=head1 APPENDIX
+
+The rest of the documentation details exported static class methods. 
+
+=cut
 
 use strict;
-use vars qw($VERBOSITY);
+use warnings;
 
-$VERBOSITY = 0;
+package Bio::EnsEMBL::Utils::Exception;
+
+use Exporter;
+
+use vars qw(@ISA @EXPORT_OK);
+
+@ISA = qw(Exporter);
+
+@EXPORT_OK = qw(&throw &warning &stack_trace_dump 
+                &stack_trace &verbose &deprecate);
+
 
 =head2 throw
 
-  Title   : throw
-  Usage   : $obj->throw("throwing exception message")
-  Function: Throws an exception, which, if not caught with an eval brace
-  will provide a nice stack trace to STDERR with the message
-  Returns : nothing
-  Args    : A string giving a descriptive error message
-
+  Arg [1]    : string $msg
+  Example    : use Bio::EnsEMBL::Utils::Exception qw(throw);
+               throw('We have a problem');
+  Description: Throws an exception which if not caught be an eval will
+               provide a stack trace to STDERR and die.
+  Returntype : none
+  Exceptions : thrown every time
+  Caller     : generally on error
 
 =cut
 
 sub throw {
+  my $string = shift;
 
-  my ($self,$string) = @_;
+  my $std = stack_trace_dump(3);
 
-  my $std = $self->stack_trace_dump();
-
-  my $out = "-------------------- EXCEPTION --------------------\n".
-    "MSG: ".$string."\n".$std."-------------------------------------------\n";
+  my $out = "\n-------------------- EXCEPTION --------------------\n" .
+            "MSG: $string\n" .
+            "$std" .
+            "---------------------------------------------------\n";
   die $out;
-
 }
+
+
 
 =head2 warning
 
-  Title   : warning
-  Usage   : $object->warn("Warning message");
-  Function: Places a warning. What happens now is down to the
-            verbosity of the object  (value of $obj->verbose) 
-            verbosity 0 or not set => small warning
-            verbosity -1 => no warning
-            verbosity 1 => warning with stack trace
-            verbosity 2 => converts warnings into throw
-  Example :
-  Returns : 
-  Args    :
+  Arg [1]    : string warning(message);
+  Example    : use Bio::EnsEMBL::Utils::Exception qw(warning)
+               warning('This is a warning');
+  Description: Places a warning. What happens now is down to the
+               verbosity level which may be set through a call to verbose()
+               verbosity 0 or not set => small warning
+               verbosity -1 => no warning
+               verbosity 1 => warning with stack trace
+               verbosity 2 => converts warnings into throw
+  Returntype : none
+  Exceptions : warning every time
+  Caller     : general
 
 =cut
 
 sub warning {
+  my $string = shift;
 
-  my ($self,$string) = @_;
-
-  my $verbose = $self->verbose;
-  $verbose = 0 unless defined $verbose;
+  my $verbose = verbose() || 0;
 
   if ( $verbose == 2 ) {
-    $self->throw($string);
+    throw($string);
   } elsif ( $verbose == -1 ) {
     return;
   } elsif ( $verbose == 1 ) {
-    my $out = "-------------------- WARNING ---------------------\n".
-      "MSG: ".$string."\n";
-    $out .= $self->stack_trace_dump;
+    my $out = "\n-------------------- WARNING ---------------------\n".
+              "MSG: $string\n" .
+              stack_trace_dump(3).
+              "--------------------------------------------------\n";
 	
     print STDERR $out;
     return;
   }
 
-  my $out = "-------------------- WARNING ---------------------\n".
-    "MSG: ".$string."\n".
-      "---------------------------------------------------\n";
+  my $out = "\n-------------------- WARNING ----------------------\n".
+            "MSG: $string".
+            "---------------------------------------------------\n";
   print STDERR $out;
 }
 
 
-=head2 throw
 
-  Title   : throw
-  Usage   : $obj->throw("throwing exception message")
-  Function: Throws an exception, which, if not caught with an eval brace
-            will provide a nice stack trace to STDERR with the message
-  Returns : nothing
-  Args    : A string giving a descriptive error message
-
-
-=cut
-
-sub throw {
-
-  my ($self,$string) = @_;
-
-  my $std = $self->stack_trace_dump();
-
-  my $out = "-------------------- EXCEPTION --------------------\n".
-  "MSG: ".$string."\n".$std."-------------------------------------------\n";
-
-  die $out;
-
-}
-
-=head2 warning
-
-  Title   : warning
-  Usage   : $object->warning("Warning message");
-  Function: Places a warning. What happens now is down to the
-  verbosity of the object  (value of $obj->verbose) 
-  verbosity 0 or not set => small warning
-  verbosity -1 => no warning
-  verbosity 1 => warning with stack trace
-  verbosity 2 => converts warnings into throw
-  Example :
-  Returns : 
-  Args    :
-
-=cut
-
-sub warning {
-
-  my ($self,$string) = @_;
-
-  my $verbose = $self->verbose;
-  $verbose = 0 unless defined $verbose;
-
-
-  if ( $verbose == 2 ) {
-    $self->throw($string);
-  } elsif ( $verbose == -1 ) {
-    return;
-  } elsif ( $verbose == 1 ) {
-    my $out = "-------------------- WARNING ---------------------\n".
-      "MSG: ".$string."\n";
-    $out .= $self->stack_trace_dump;
-	
-    print STDERR $out;
-    return;
-  }    
-
-  my $out = "-------------------- WARNING ---------------------\n".
-    "MSG: ".$string."\n".
-      "---------------------------------------------------\n";
-  print STDERR $out;
-}
 
 =head2 verbose
 
-  Title   : verbose
-  Usage   : $self->verbose(1)
-  Function: Sets verbose level for how ->warn behaves
-            -1 = no warning
-             0 = standard, small warning
-             1 = warning with stack trace
-             2 = warning becomes throw
-  Returns : nothing
-  Args    : -1,0,1 or 2
+  Arg [1]    : (optional) int 
+  Example    : use Bio::EnsEMBL::Utils::Exception qw(verbose warning);
+               verbose(-1);
+               warning("No warning displayed");
+               verbose(0);
+               warning("Regular warning displayed");
+  Description: Gets/Sets verbose level for how warning() behaves
+               -1 = no warning
+                0 = standard, small warning
+                1 = warning with stack trace
+                2 = warning becomes throw
+  Returntype : int 
+  Exceptions : none
+  Caller     : general
 
+=cut
 
-  =cut
+my $VERBOSITY = 0;
 
 sub verbose {
+  $VERBOSITY = shift if(@_);
 
-  my ($self,$value) = @_;
-
-  if (ref($self) && (defined $value || ! defined $self->{'verbose'}) ) {
-    $value = 0 unless defined $value;
-    $self->{'verbose'} = $value;
-  }
-  return (ref($self) ? $self->{'_rootI_verbose'} : $VERBOSITY);
+  return $VERBOSITY;
 }
+
+
 
 =head2 stack_trace_dump
 
- Title   : stack_trace_dump
- Usage   :
- Function:
- Example :
- Returns : 
- Args    :
-
+  Arg [1]    : (optional) int $levels
+               The number of levels to ignore from the top of the stack when
+               creating the dump. This is useful when this is called internally
+               from a warning or throw function when the immediate caller and 
+               stack_trace_dump function calls are themselves uninteresting.
+  Example    : use Bio::EnsEMBL::Utils::Exception qw(stack_trace_dump);
+               print STDERR stack_trace_dump();
+  Description: Returns a stack trace formatted as a string
+  Returntype : string
+  Exceptions : none
+  Caller     : general, throw, warning
 
 =cut
 
 sub stack_trace_dump{
+  my @stack = stack_trace();
 
-  my ($self) = @_;
-
-  my @stack = $self->stack_trace();
-
-  shift @stack;
-  shift @stack;
-  shift @stack;
+  my $levels = 2; #default is 2 levels so stack_trace_dump call is not present
+  $levels = shift if(@_);
+  $levels = 1 if($levels < 1);
+  
+  while($levels) {
+    $levels--;
+    shift @stack;
+  }
 
   my $out;
   my ($module,$function,$file,$position);
@@ -201,22 +215,20 @@ sub stack_trace_dump{
 }
 
 
+
 =head2 stack_trace
 
- Title   : stack_trace
- Usage   : @stack_array_ref= $self->stack_trace
- Function: gives an array to a reference of arrays with stack trace info
-           each coming from the caller(stack_number) call
- Returns : array containing a reference of arrays
- Args    : none
-
+  Arg [1]    : none
+  Example    : use Bio::EnsEMBL::Utils::Exception qw(stack_trace)
+  Description: Gives an array to a reference of arrays with stack trace info
+               each coming from the caller(stack_number) call
+  Returntype : array of listrefs of strings
+  Exceptions : none
+  Caller     : general, stack_trace_dump()
 
 =cut
 
 sub stack_trace {
-
-  my ($self) = @_;
-
   my $i = 0;
   my @out;
   my $prev;
@@ -231,25 +243,58 @@ sub stack_trace {
   $prev->[3] = 'toplevel';
   push(@out,$prev);
   return @out;
-
 }
 
-my %deprecate_cache;
+
+=head2 deprecate
+
+  Arg [1]    : string $mesg
+               A message describing why a method is deprecated
+  Example    : use Bio::EnsEMBL::Utils::Exception qw(deprecate)
+               sub old_sub {
+                 deprecate('Please use new_sub() instead');
+               }
+  Description: Prints a warning to STDERR that the method which called 
+               deprecate() is deprecated.  Also prints the line number and 
+               file from which the deprecated method was called.  Deprecated
+               warnings only appear once for each location the method was 
+               called from
+  Returntype : none
+  Exceptions : warning every time
+  Caller     : deprecated methods
+
+=cut
+
+my %DEPRECATED;
 
 sub deprecate {
   my $mesg = shift;
 
-  my $subname = (caller( 1 ))[3] ;
-  my $line_number = (caller( 1 ))[2];
-  my ($file) = join( "/", (split(/\//, (caller( 1 ))[1]) )[-3,-2,-1] );
-  
-  return if $deprecate_cache{"$line_number:$file"};
+  my @caller = caller(1);
+  my $subname = $caller[3] ;
+  my $line = $caller[2];
 
-  print STDERR "Deprecated method call in file $file line $line_number\n";
-  print STDERR "Method $subname is deprecated.\n";
-  print STDERR $mesg;
+  #use only 2 subdirs for brevity when reporting the filename
+  my $file;
+  my @path = $caller[1];
+  $file = pop(@path);
+  my $i = 0;
+  while(@path && $i < 2) {
+    $i++;
+    $file .= pop(@path);
+  }
 
-  $deprecate_cache{"$line_number:$file"} = 1;
+  #keep track of who called this method so that the warning is only displayed
+  #once per deprecated call
+  return if $DEPRECATED{"$line:$file"};
+
+  if($VERBOSITY > -1) {
+    print STDERR "Deprecated method call in file $file line $line.\n" .
+                 "Method $subname is deprecated.\n" .
+                 "$mesg\n";
+  }
+
+  $DEPRECATED{"$line:$file"} = 1;
 }
 
 1;
