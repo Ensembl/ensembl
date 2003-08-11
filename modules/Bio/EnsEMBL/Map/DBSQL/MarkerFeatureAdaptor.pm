63f4dd83b6f1fcede0add686b1331b8a842b2b6c
Graham McVicker <mcvicker@sanger.ac.uk>
2003-08-11 16:14:42 +0000

    updates for new system

--- modules/Bio/EnsEMBL/Map/DBSQL/MarkerFeatureAdaptor.pm
+++ modules/Bio/EnsEMBL/Map/DBSQL/MarkerFeatureAdaptor.pm
@@ -34,6 +34,8 @@ use Bio::EnsEMBL::Map::Marker;
 use Bio::EnsEMBL::Map::MarkerSynonym;
 use Bio::EnsEMBL::DBSQL::BaseFeatureAdaptor;
 
+use Bio::EnsEMBL::Utils::Exception qw(throw);
+
 use vars qw(@ISA);
 
 @ISA = qw(Bio::EnsEMBL::DBSQL::BaseFeatureAdaptor);
@@ -54,9 +56,9 @@ use vars qw(@ISA);
 sub fetch_all_by_Marker {
   my $self = shift;
   my $marker = shift;
-  
+
   my $constraint = 'm.marker_id = ' . $marker->dbID;
-  
+
   return $self->generic_fetch($constraint, @_);
 }
 
@@ -100,60 +102,39 @@ sub fetch_all_by_Slice_and_priority {
 
 =head2 fetch_all_by_RawContig_and_priority
 
-  Arg [1]    : Bio::EnsEMBL::RawContig $contig
-  Arg [2]    : (optional) int $priority
-  Arg [3]    : (optional) string $logic_name
-  Example    : @feats = @{$mfa->fetch_all_by_RawContig_and_priority($ctg, 80)};
-  Description: Retrieves all marker features above a specified threshold
-               priority which overlap the provided slice.
-  Returntype : listref of Bio::EnsEMBL::Map::MarkerFeatures in contig coords
-  Exceptions : none
-  Caller     : general
+  Description: DEPRECATED use fetch_all_by_Slice_and_priority instead
 
 =cut
 
 sub fetch_all_by_RawContig_and_priority {
-  my ($self, $contig, $priority, @args) = @_;
-
-  my $constraint;
-  if(defined $constraint) {
-    $constraint = "m.priority > $priority";
-  }
-  
-  return $self->fetch_all_by_RawContig_constraint($constraint, @args);
+  deprecate('Use fetch_all_by_Slice_and_priority() instead.');
+  fetch_all_by_Slice_and_priority(@_);
 }
 
-
-
-sub fetch_all_by_RawContig_and_score {
-  my $self = shift;
-  $self->throw('fetch_all_by_RawContig_and_score should not be used to fetch' .
-           ' marker features');
-}
 
 sub fetch_all_by_Slice_and_score {
   my $self = shift;
-  $self->throw('fetch_all_by_Slice_and_score should not be used to fetch' .
-	       ' marker_features');
+  throw('fetch_all_by_Slice_and_score should not be' .
+        'used to fetch marker_features');
 }
 
 sub _columns {
   my $self = shift;
 
-  return ('mf.marker_feature_id', 'mf.marker_id', 
-	  'mf.contig_id', 'mf.contig_start', 'mf.contig_end', 
-	  'mf.analysis_id', 'mf.map_weight',
-	  'm.left_primer', 'm.right_primer', 'm.min_primer_dist', 
-	  'm.max_primer_dist', 'm.priority', 'm.type', 'ms.marker_synonym_id',
-	  'ms.name', 'ms.source');
+  return ('mf.marker_feature_id', 'mf.marker_id',
+          'mf.seq_region_id', 'mf.seq_region_start', 'mf.seq_region_end',
+          'mf.analysis_id', 'mf.map_weight',
+          'm.left_primer', 'm.right_primer', 'm.min_primer_dist',
+          'm.max_primer_dist', 'm.priority', 'm.type', 'ms.marker_synonym_id',
+          'ms.name', 'ms.source');
 }
 
 sub _tables {
   my $self = shift;
 
   return (['marker_feature', 'mf'], #primary table
-	  ['marker', 'm'],
-	  ['marker_synonym', 'ms']);
+	        ['marker', 'm'],
+	        ['marker_synonym', 'ms']);
 }
 
 sub _left_join {
@@ -162,7 +143,7 @@ sub _left_join {
   return ('marker_synonym', 
 	  'ON m.display_marker_synonym_id = ms.marker_synonym_id');
 }
-          
+
 sub _default_where_clause {
   my $self = shift;
 
@@ -174,14 +155,14 @@ sub _objs_from_sth {
   my $sth  = shift;
 
   my ($marker_feature_id, $marker_id, 
-      $contig_id, $contig_start, $contig_end,
+      $seq_region_id, $seq_region_start, $seq_region_end,
       $analysis_id, $map_weight,
-      $left_primer, $right_primer, $min_primer_dist, $max_primer_dist, 
+      $left_primer, $right_primer, $min_primer_dist, $max_primer_dist,
       $priority, $type, $ms_id, $ms_name, $ms_source);
 
   #warning: ordering depends on _columns function implementation
-  $sth->bind_columns(\$marker_feature_id, \$marker_id, 
-      \$contig_id, \$contig_start, \$contig_end,
+  $sth->bind_columns(\$marker_feature_id, \$marker_id,
+      \$seq_region_id, \$seq_region_start, \$seq_region_end,
       \$analysis_id, \$map_weight,
       \$left_primer, \$right_primer, \$min_primer_dist, \$max_primer_dist,
       \$priority, \$type, \$ms_id, \$ms_name, \$ms_source);
@@ -189,47 +170,49 @@ sub _objs_from_sth {
   my @out = ();
 
   my %marker_cache;
-  my %contig_cache;
+  my %slice_cache;
   my %analysis_cache;
   my $marker_adp = $self->db->get_MarkerAdaptor;
-  while($sth->fetch) {
+  my $slice_adp  = $self->db->get_SliceAdaptor;
+  my $analysis_adp = $self->db->get_AnalysisAdaptor;
 
+  while($sth->fetch) {
     #create a new marker unless this one has been seen already
     my $marker;
-    unless($marker = $marker_cache{$marker_id}) {
+    if(!($marker = $marker_cache{$marker_id})) {
       #create a new marker synonym for the display synonym (if defined)
       my $ms;
       if($ms_id) {
-	$ms = Bio::EnsEMBL::Map::MarkerSynonym->new
-	  ($ms_id, $ms_source, $ms_name);
+        $ms = Bio::EnsEMBL::Map::MarkerSynonym->new
+          ($ms_id, $ms_source, $ms_name);
       }
 
       #create a new marker
       $marker = Bio::EnsEMBL::Map::Marker->new
-	($marker_id, $marker_adp, 
-	 $left_primer, $right_primer, $min_primer_dist, $max_primer_dist, 
-	 $priority, $type, $ms);
+        ($marker_id, $marker_adp, 
+         $left_primer, $right_primer, $min_primer_dist, $max_primer_dist, 
+         $priority, $type, $ms);
       $marker_cache{$marker_id} = $marker;
     }
 
-    #retrieve the contig from the database
-    my $contig;
-    unless($contig = $contig_cache{$contig_id}) {
-      $contig = $self->db->get_RawContigAdaptor->fetch_by_dbID($contig_id);
-      $contig_cache{$contig_id} = $contig;
+    #retrieve the slice from the database
+    my $slice;
+    if(!($slice = $slice_cache{$seq_region_id})) {
+      $slice = $slice_adp->fetch_by_seq_region_id($seq_region_id);
+      $slice_cache{$seq_region_id} = $slice;
     }
 
     #retrieve analysis
     my $analysis;
     unless($analysis = $analysis_cache{$analysis_id}) {
-      $analysis = $self->db->get_AnalysisAdaptor->fetch_by_dbID($analysis_id);
+      $analysis = $analysis_adp->fetch_by_dbID($analysis_id);
       $analysis_cache{$analysis_id} = $analysis;
     }
 
     #now create a new marker_feature using the marker
     push @out, Bio::EnsEMBL::Map::MarkerFeature->new
-      ($marker_feature_id, $self, 
-       $contig_start, $contig_end, $contig, 
+      ($marker_feature_id, $self,
+       $seq_region_start, $seq_region_end, $slice,
        $analysis, $marker_id, $map_weight, $marker);
   }
 
@@ -265,63 +248,62 @@ sub store {
       $self->throw('Incorrect argument [$mf] to store.  Expected ' .
 		   'Bio::EnsEMBL::Map::MarkerFeature');
     }
-    
+
     #don't store this feature if it has already been stored
-    next if($mf->adaptor && ($mf->adaptor == $self));
-    
+    next if($mf->is_stored($self->db()));
+
     my $marker = $mf->marker;
-    
-    unless($marker && ref $marker && 
-	   $marker->isa('Bio::EnsEMBL::Map::Marker')) {
-      $self->throw('Cannot store MarkerFeature without an associated Marker');
+
+    if(!$marker || !ref($marker) ||
+       !$marker->isa('Bio::EnsEMBL::Map::Marker')) {
+      throw('Cannot store MarkerFeature without an associated Marker');
     }
 
     my $marker_id = $marker->dbID;
-    unless($marker_id) {
-      $self->throw('Associated marker must have dbID to store MarkerFeature');
+    if(!$marker_id) {
+      throw('Associated marker must have dbID to store MarkerFeature');
     }
 
-
     my $analysis = $mf->analysis;
-    
-    unless($analysis && ref $analysis && 
-	   $analysis->isa('Bio::EnsEMBL::Analysis')) {
-      $self->throw('Cannot store MarkerFeature without associated Analysis');
+
+    if(!$analysis || !ref($analysis) ||
+       $analysis->isa('Bio::EnsEMBL::Analysis')) {
+      throw('Cannot store MarkerFeature without associated Analysis');
     }
 
     my $analysis_id = $analysis->dbID;
 
-    unless($analysis_id) {
-     $self->throw('Associated analysis must have dbID to store MarkerFeature');
+    if(!$analysis_id) {
+      throw('Associated analysis must have dbID to store MarkerFeature');
     }
 
-    my $contig = $mf->contig;
+    my $slice = $mf->slice;
 
-    unless($contig && ref $contig && $contig->isa('Bio::EnsEMBL::RawContig')) {
-      $self->throw('Cannot store MarkerFeature that is not in contig coords');
+    if(!$slice || !ref($slice) || !$slice->isa('Bio::EnsEMBL::Slice')) {
+      throw('Cannot store MarkerFeature that is not in contig coords');
     }
-    
-    my $contig_id = $contig->dbID;
 
-    unless($contig_id) {
-      $self->throw('Attached contig must have dbID to store MarkerFeature');
+    my $seq_region_id = $self->get_SliceAdaptor->get_seq_region_id($slice);
+
+    if(!$seq_region_id) {
+      throw('Attached slice\'s seq_region is not in database');
     }
 
     #
     # Everything looks ok so store
     #
 
-    my $sth = 
+    my $sth =
       $self->prepare("INSERT INTO marker_feature (marker_id,
-                           contig_id, contig_start, contig_end,
+                           seq_region_id, seq_region_start, seq_region_end,
                            analysis_id, map_weight)
-                    VALUES (?, ?, ?, ?, ?, ?)");
-    $sth->execute($marker_id, 
-		  $contig_id, $mf->start, $mf->end,
-		  $analysis_id, 0);
+                      VALUES (?, ?, ?, ?, ?, ?)");
+    $sth->execute($marker_id,
+                  $seq_region_id, $mf->start, $mf->end,
+                  $analysis_id, 0);
 
     my $dbID = $sth->{'mysql_insertid'};
-    
+
     $mf->dbID($dbID);
     $mf->adaptor($self);
   }
