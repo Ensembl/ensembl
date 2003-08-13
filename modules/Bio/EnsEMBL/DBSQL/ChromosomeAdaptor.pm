53503c4a0de076ee504d1c5b674175ded5f1ab40
Graham McVicker <mcvicker@sanger.ac.uk>
2003-08-13 13:29:51 +0000

    Deprecated and chained method calls to SliceAdaptor

--- modules/Bio/EnsEMBL/DBSQL/ChromosomeAdaptor.pm
+++ modules/Bio/EnsEMBL/DBSQL/ChromosomeAdaptor.pm
@@ -11,16 +11,12 @@
 
 =head1 NAME
 
-Bio::EnsEMBL::DBSQL::ChromosomeAdaptor - DB Connectivity for Chromosome Object
-
-=head1 SYNOPSIS
-
-$chromosome_adaptor = $db_adaptor->get_ChromosomeAdaptor();
-$chromosome = $chromosome_adaptor->fetch_by_chr_name('12');
+Bio::EnsEMBL::DBSQL::ChromosomeAdaptor - DEPRECATED
+use Bio::EnsEMBL::SliceAdator instead
 
 =head1 DESCRIPTION
 
-This is a database adaptor used to retrieve chromosome objects from a database.
+This class is deprecated.  SliceAdaptor should be used instead.
 
 =head1 AUTHOR - Ewan Birney
 
@@ -34,148 +30,91 @@ package Bio::EnsEMBL::DBSQL::ChromosomeAdaptor;
 use vars qw(@ISA);
 use strict;
 
-use Bio::EnsEMBL::DBSQL::BaseAdaptor;
+use Bio::EnsEMBL::Utils::Exception qw(deprecate);
+use Bio::EnsEMBL::DBSQL::SliceAdaptor;
 use Bio::EnsEMBL::Chromosome;
 
-@ISA = qw(Bio::EnsEMBL::DBSQL::BaseAdaptor);
+@ISA = qw(Bio::EnsEMBL::DBSQL::SliceAdaptor);
+
 
 =head2 fetch_by_dbID
 
-  Arg [1]    : int $id 
-               unique database identifier for chromosome to retrieve  
-  Example    : my $chromosome = $chromosome_adaptor->fetch_by_dbID(1);
-  Description: Retrieves a Chromosome object from the database using its
-               unique identifier.  Note the the identifier is the dbID and
-               does NOT correspond to the chromosome name.  dbID 1 does NOT
-               necessarily correspond to chromosome '1'
-  Returntype : Bio::EnsEMBL::Chromosome
-  Exceptions : thrown if $id not defined
-  Caller     : general
+  Description: DEPRECATED Use SliceAdaptor::fetch_by_seq_region_id instead
 
 =cut
 
 sub fetch_by_dbID {
   my ($self,$id) = @_;
 
-  $self->throw("Chromosome dbID argument required\n") unless defined $id;
+  deprecate('Use SliceAdaptor::fetch_by_seq_region_id instead.');
 
-  unless(defined $self->{'_chr_cache'} ) {
-    $self->{'_chr_cache'} = {};
-    $self->{'_chr_name_cache'} = {};
-  }
+  my $chr = $self->SUPER::fetch_by_seq_region_id($id);
 
-  # If there is not already a cached version of this chromosome pull it
-  #     from the database and add it to the cache.
-  unless( $self->{'_chr_cache'}->{$id} ) {
-    my $sth;
-
-    $sth = $self->prepare( "SELECT chromosome_id, name, length 
-                              FROM chromosome 
-                              WHERE chromosome_id = ?" );
-    $sth->execute( $id );
-
-    my $a = $sth->fetchrow_arrayref();
-    $self->throw("Could determine chromosome name from dbID $id\n") unless $a;
-    $self->_create_object_from_arrayref( $a );
-  }
-
-  return $self->{'_chr_cache'}->{$id};
+  #change blessing to chromosome and reset adaptor
+  bless $chr, 'Bio::EnsEMBL::Chromosome';
+  $chr->adaptor($self);
+  return $chr;
 }
 
 
 =head2 fetch_by_chr_name
 
-  Arg [1]    : string $chr_name
-               the name of the chromosome to retrieve
-  Example    : $chromosome = $chromosome_adaptor->fetch_by_chr_name('X');
-  Description: Retrieves a chromosome object from the database using its name.
-  Returntype : Bio::EnsEMBL::Chromosome
-  Exceptions : none
-  Caller     : general
+  Description: DEPRECATED Use SliceAdaptor::fetch_by_region instead
 
 =cut
 
 sub fetch_by_chr_name{
   my ($self,$chr_name) = @_;
 
-  unless(defined $self->{'_chr_cache'} ) {
-    $self->{'_chr_cache'} = {};
-    $self->{'_chr_name_cache'} = {};
-  }
+  deprecate('Use SliceAdaptor::fetch_by_region instead.');
 
-  unless($self->{'_chr_name_cache'}->{lc($chr_name)}) {
-    my $sth;
+  my $top_level = $self->db->get_CoordSystemAdaptor->fetch_top_level();
 
-    $sth = $self->prepare( "SELECT chromosome_id, name, length  
-                              FROM chromosome WHERE name = ?" );
-    $sth->execute( "$chr_name" );
-
-    my $a = $sth->fetchrow_arrayref();
-
-    $self->throw("Do not recognise chromosome $chr_name\n") unless $a;
-
-    $self->_create_object_from_arrayref( $a );
-  }
-  return $self->{'_chr_name_cache'}->{lc($chr_name)};
+  my $chr = $self->SUPER::fetch_by_region($top_level->name(),
+                                          $chr_name,
+                                          undef,
+                                          undef,
+                                          undef,
+                                          $top_level->version);
+  #change blessing and adaptor
+  bless $chr, 'Bio::EnsEMBL::Chromosome';
+  $chr->adaptor($self);
+  return $chr;
 }
 
 
 =head2 fetch_all
 
-  Args       : none
-  Example    : @chromosomes = $chromosome_adaptor->fetch_all(); 
-  Description: Retrieves every chromosome object from the database.
-  Returntype : listref of Bio::EnsEMBL::Chromosome
-  Exceptions : none
-  Caller     : general
+  Description: DEPRECATED Use SliceAdaptor::fetch_all instead
 
 =cut
 
 sub fetch_all {
   my($self) = @_;
-  my @chrs = (); 
 
-  my $sth = $self->prepare( "SELECT chromosome_id, name, length 
-                             FROM chromosome");
-  $sth->execute();
-  while( my $a = $sth->fetchrow_arrayref() ) {
-    push @chrs, $self->_create_object_from_arrayref( $a );
-  } 
-  return \@chrs;
+  deprecate('Use SliceAdaptor::fetch_all instead.');
+
+  my $top_level = $self->db->get_CoordSystemAdaptor->fetch_top_level();
+  my $chrs = $self->SUPER::fetch_all($top_level->name, $top_level->version);
+
+  foreach my $chr (@$chrs) {
+    bless $chr, 'Bio::EnsEMBL::Chromosome';
+    $chr->adaptor($self);
+  }
+
+  return $chrs;
 }
 
-=head2 _create_object_from_arrayref
 
-  Args       : array ref containing a row of the chromosome table
-  Example    : $self->_create_object_from_arrayref
-  Description: Creates object from array reference
-  Returntype : Bio::Chromosome object
-  Exceptions : none
-  Caller     : general
+=head2 store
+
+  Description: DEPRECATED no replacement exists
 
 =cut
 
-sub _create_object_from_arrayref {
-  my( $self,$a ) =@_;
-  my $chr = new Bio::EnsEMBL::Chromosome(
-    -adaptor  => $self,   -dbID   => $a->[0],
-    -chr_name => $a->[1], -length => $a->[2]
-  );
-  return $self->{'_chr_cache'     }->{$a->[0]} = 
-         $self->{'_chr_name_cache'}->{lc($a->[1])} = $chr ;
-}
-
 sub store{
-  my ($self, $chromosome) = @_;
-
-  $self->throw("can't store a chromosome without a name") 
-    unless my $chr_name = $chromosome->chr_name;
-  $self->throw("can't store a chromosome without a length") 
-    unless my $length = $chromosome->length;
-  my $sth = $self->db->prepare("insert into chromosome set name=?, length=?");
-  $sth->execute("$chr_name", $length);
-  $chromosome->dbID($sth->{'mysql_insertid'});
-  $chromosome->adaptor($self);
+  ### XXX Should there be a replacement for this?
+  deprecate('No replacement has been implemented.');
 }
 
 1;
