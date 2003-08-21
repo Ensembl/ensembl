48d695d70eaf213ed7bdbe5a7d28f93752cb114c
Web Admin <w3adm@sanger.ac.uk>
2003-08-21 11:03:08 +0000

    Fixed insert hit/hsp code

--- modules/Bio/EnsEMBL/External/BlastAdaptor.pm
+++ modules/Bio/EnsEMBL/External/BlastAdaptor.pm
@@ -303,6 +303,8 @@ sub store_result{
     $sth->finish;
   }
   if( $rv < 1 ){# Insert
+    my $use_date = $res->use_date() || 
+      $res->use_date($self->use_date('RESULT'));
     my $sth = $dbh->prepare( sprintf $SQL_RESULT_STORE, $use_date );
     $sth->execute( $frozen, $ticket ) || $self->throw( $sth->errstr );
     my $id = $dbh->{mysql_insertid};
@@ -377,14 +379,14 @@ sub store_hit{
   my ( $id, $use_date ) = split( '!!', $hit->token);
   $use_date ||= '';
 
-  my( $rv );
+  my $rv = 0;
   if( $id ){
     my $sth = $dbh->prepare( sprintf $SQL_HIT_RETRIEVE, $use_date );
     $rv = $sth->execute( $id ) ||  $self->throw( $sth->errstr );
     $sth->finish;
   }
   if( $rv < 1 ){ # Insert
-    my $use_date = $hit->use_date() || $hit->( $self->use_date('HIT') );
+    my $use_date = $hit->use_date() || $hit->use_date($self->use_date('HIT'));
     my $sth = $dbh->prepare( sprintf $SQL_HIT_STORE, $use_date );
     $sth->execute( $frozen, $ticket ) || $self->throw( $sth->errstr );
     my $id = $dbh->{mysql_insertid};
@@ -474,7 +476,7 @@ sub store_hsp{
     $sth->finish;
   }
   if( $rv < 1 ){ # Insert
-    my $use_date = $hsp->use_date() || $hsp->( $self->use_date('HSP') );
+    my $use_date = $hsp->use_date() || $hsp->use_date($self->use_date('HSP'));
     my $sth = $dbh->prepare( sprintf $SQL_HSP_STORE, $use_date );
     my @bound = ( $frozen, $ticket, $chr_name,  $chr_start, $chr_end );
     $sth->execute( @bound ) || $self->throw( $sth->errstr );
