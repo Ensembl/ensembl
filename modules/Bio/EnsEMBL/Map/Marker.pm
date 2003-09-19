8077bf2a58e536f6b041b65ed71e065fa0a23348
Graham McVicker <mcvicker@sanger.ac.uk>
2003-09-19 12:12:28 +0000

    fix to get_MapLocation bug (submitted by Karl Forner)

--- modules/Bio/EnsEMBL/Map/Marker.pm
+++ modules/Bio/EnsEMBL/Map/Marker.pm
@@ -455,7 +455,7 @@ sub get_MapLocation {
   #lazy-load the map locations if they have not been fetched yet
   if(!exists $self->{'map_locations'} && 
      $self->{'adaptor'} && $self->{'dbID'}) {
-    $self->fetch_attributes;
+    $self->adaptor->fetch_attributes;
   }
 
   unless($map_name) {
