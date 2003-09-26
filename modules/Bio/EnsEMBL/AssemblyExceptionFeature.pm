74b9bd46a329f9136feeae3d2a20ae3c48b60378
Glenn Proctor <gp1@sanger.ac.uk>
2003-09-26 10:01:54 +0000

    Created. Won't work yet.

---
+++ modules/Bio/EnsEMBL/AssemblyExceptionFeature.pm
@@ -0,0 +1,122 @@
+#
+# Ensembl module for Bio::EnsEMBL::AssemblyExceptionFeature
+#
+# Cared for by Ewan Birney <birney@ebi.ac.uk>
+#
+# Copyright Ewan Birney
+#
+# You may distribute this module under the same terms as perl itself
+
+# POD documentation - main docs before the code
+
+=head1 NAME
+
+Bio::EnsEMBL::AssemblyExceptionFeature - A feature that represents an assembly exception
+
+=head1 SYNOPSIS
+
+use Bio::EnsEMBL::AssemblyExceptionFeature;
+
+$feature = Bio::EnsEMBL::AssemblyExceptionFeature->new(-start    => 100,
+                                                       -end      => 220,
+                                                       -type     => 'HAP',
+                                                       -slice    => $slice,
+                                                       -adaptor  => $adaptor);
+
+=head1 DESCRIPTION
+
+Certain features, e.g. Haplotypes and PARs, are represented as "exceptions" to the 
+normal assembly. This class represents such features.
+
+=head1 AUTHOR - Glenn Proctor
+
+This modules is part of the Ensembl project http://www.ensembl.org
+
+Email birney@ebi.ac.uk
+
+Describe contact details here
+
+=head1 APPENDIX
+
+The rest of the documentation details each of the object methods. Internal
+methods are usually preceded with a _
+
+=cut
+
+use strict;
+
+package Bio::EnsEMBL::AssemblyExceptionFeature;
+
+use vars qw(@ISA);
+
+use Bio::EnsEMBL::Feature;
+use Bio::EnsEMBL::Utils::Argument qw(rearrange);
+
+@ISA = qw(Bio::EnsEMBL::Feature);
+
+
+=head2 new
+
+  Arg [TYPE] : The type (e.g. HAP for haplotype, PAR for PAR)
+  Arg [...]  : Named arguments passed to superclass
+  Example    : $feature = Bio::EnsEMBL::AssemblyExceptionFeature->new
+                        (-start    => 1,
+                         -end      => 100,
+                         -slice    => $slice,
+                         -analysis => $analysis,
+                         -adaptor  => $adaptor,
+                         -dbID     => 10,
+                         -type     => 'HAP')
+  Description: Constructs a new Bio::EnsEMBL::Feature.  Generally subclasses
+               of this method are instantiated, rather than this class itself.
+  Returntype : Bio::EnsEMBL::Feature
+  Exceptions : Thrown on invalid -SLICE, -ANALYSIS, -STRAND arguments
+  Caller     : general, subclass constructors
+
+=cut
+
+sub new {
+  my $caller = shift;
+
+  #allow this to be called as class or object method
+  my $class = ref($caller) || $caller;
+  my $self = $class->SUPER::new(@_);
+
+  my ($type) = rearrange(['TYPE'],@_);
+
+  $self->{'type'} = $type;
+
+  return $self;
+}
+
+
+sub new_fast {
+  my $class = shift;
+  my $hashref = shift;
+  return bless $hashref, $class;
+}
+
+
+=head2 type
+
+  Arg [1]    : (optional) string $value
+  Example    : $type = $assembly_exception_feature->type();
+  Description: Getter/Setter for the type associated with this
+               feature.
+  Returntype : string
+  Exceptions : none
+  Caller     : general
+
+=cut
+
+sub type {
+  my $self = shift;
+
+  # TODO check for legal type
+
+  $self->{'type'} = shift if(@_);
+
+  return $self->{'type'};
+}
+
+1;
