449f86dae352daa53aa01d241b73bd82bc22e59a
Graham McVicker <mcvicker@sanger.ac.uk>
2003-09-30 14:36:08 +0000

    -renamed & fixed fetch_all_by_DBEntry (now called fetch_all_by_external_name) -fixed fetch_by_dbID -added fetch_by_stable_id for consistancy with Gene, Exon, Transcript adaptors

--- modules/Bio/EnsEMBL/DBSQL/TranslationAdaptor.pm
+++ modules/Bio/EnsEMBL/DBSQL/TranslationAdaptor.pm
@@ -9,20 +9,29 @@
 
 =head1 NAME
 
-Bio::EnsEMBL::DBSQL::TranslationAdaptor - MySQL Database queries to generate 
-and store translations.
+Bio::EnsEMBL::DBSQL::TranslationAdaptor - Provides a means to fetch and store
+Translation objects from a database.
 
 =head1 SYNOPSIS
 
-Translations are stored and fetched with this
-object. 
+  my $db = Bio::EnsEMBL::DBSQL::DBAdaptor->new(...);
+
+  my $transcript_adaptor = $db->get_TranscriptAdaptor();
+  my $translation_adaptor = $db->get_TranslationAdaptor();
+  my $transcript = $transcript_adaptor->fetch_by_dbID(131243);
+  my $translation = $translation_adaptor->fetch_by_Transcript($transcript);
+
+  print "Translation Start Site: " .
+        $translation->start_Exon()->stable_id(), " ", $translation->start();
+  print "Translation Stop: " . 
+        $translation->end_Exon()->stable_id(), " ", $translation->end();
+
 
 =head1 CONTACT
 
   ensembl-dev@ebi.ac.uk
 
-
-=head1 APPENDIX
+=head1 METHODS
 
 =cut
 
@@ -55,6 +64,10 @@ use Bio::EnsEMBL::Utils::Exception qw( throw warning deprecate );
 
 sub fetch_by_Transcript {
   my ( $self, $transcript ) = @_;
+
+  if(!ref($transcript) || !$transcript->isa('Bio::EnsEMBL::Transcript')) {
+    throw("Bio::EnsEMBL::Transcript argument is required.");
+  }
 
   my $sql = "
     SELECT tl.translation_id, tl.start_exon_id,
@@ -111,35 +124,44 @@ sub fetch_by_Transcript {
   return $translation;
 }
 
-=head2 fetch_all_by_DBEntry
 
-  Arg [1]    : in $external_id
-                the external identifier for the translation to be obtained
-  Example    : @trans = @{$trans_adaptor->fetch_all_by_DBEntry($ext_id)}
-  Description: retrieves a list of translation with an external database
-                idenitifier $external_id
-  Returntype : listref of Bio::EnsEMBL::DBSQL::Translation in contig coordinates
+
+
+=head2 fetch_all_by_external_name
+
+  Arg [1]    : string $external_id
+               The external identifier for the tranlsation(s) to be obtained.
+  Example    : @tls = @{$trl_adaptor->fetch_all_by_external_name('BRCA2')};
+  Description: Retrieves a list of translations fetched via an external
+               identifier.  Note that this may not be a particularly useful
+               method, because translations do not make much sense out of the 
+               context of their transcript.  It may be better to use the
+               TranscriptAdaptor::fetch_all_by_external_name instead.
+  Returntype : reference to a list of Translations
   Exceptions : none
-  Caller        : ?
+  Caller     : general
 
 =cut
 
-sub fetch_all_by_DBEntry {
+sub fetch_all_by_external_name {
   my $self = shift;
   my $external_id = shift;
-  my @trans = ();
-  warning( "Is this really usefull ???" );
 
-  my $entryAdaptor = $self->db->get_DBEntryAdaptor();
-  my @ids = $entryAdaptor->translationids_by_extids($external_id);
-  foreach my $trans_id ( @ids ) {
-    my $trans = $self->fetch_by_dbID( $trans_id );
-    warn $trans_id;
-    if( $trans ) {
-        push( @trans, $trans );
+  my $entry_adaptor = $self->db->get_DBEntryAdaptor();
+  my @ids = $entry_adaptor->list_translation_ids_by_extids($external_id);
+
+  my $transcript_adaptor = $self->db()->get_TranscriptAdaptor();
+
+  my @out;
+
+  foreach my $id (@ids) {
+    my $transcript = $transcript_adaptor->fetch_by_translation_id($id);
+    if($transcript) {
+      push @out, $self->fetch_by_Transcript($transcript);
     }
-  }
-  return \@trans;
+  } 
+
+  return \@out;
 }
 
 
@@ -150,40 +172,49 @@ sub fetch_all_by_DBEntry {
                The translation object to be stored in the database 
   Example    : $transl_id = $translation_adaptor->store($translation);
   Description: Stores a translation object in the database
-  Returntype : 
-  Exceptions : 
-  Caller     : 
+  Returntype : int - the new dbID of the stored translation
+  Exceptions : thrown if the dbID of the start_Exon or end_Exon is not 
+               defined.
+               thrown if only partial stable id information is present (e.g.
+               identifier but not version number)
+  Caller     : Transcript::store
 
 =cut
 
 sub store {
   my ( $self, $translation, $transcript_id )  = @_;
 
-  unless( defined $translation->start_Exon->dbID && 
-	  defined $translation->end_Exon->dbID ) {
-    throw("Attempting to write a translation where the dbIDs of the " .
-		 "start and exons are not set. This is most likely to be " .
-		 "because you assigned the exons for translation start_exon " .
-		 "and translation end_exon to be different in memory " .
-		 "objects from your transcript exons - although it could " .
-		 "also be an internal error in the adaptors. For your " .
-		 "info the exon memory locations are " . 
-		 $translation->start_Exon." and ".$translation->end_Exon());
+  my $start_exon = $translation->start_Exon();
+  my $end_exon   = $translation->end_Exon();
+
+  if(!$start_exon) {
+    throw("Translation must define a start_Exon to be stored.");
   }
 
+  if(!$end_exon) {
+    throw("Translation must define an end_Exon to be stored.");
+  }
+
+  if(!$start_exon->dbID) {
+    throw("start_Exon must have a dbID for Translation to be stored.");
+  }
+
+  if(!$end_exon->dbID) {
+    throw("end_Exon must have a dbID for Translation to be stored.");
+  }
+                   
   my $sth = $self->prepare( "
          INSERT INTO translation( seq_start, start_exon_id,
                                   seq_end, end_exon_id, transcript_id) 
          VALUES ( ?,?,?,?,? )");
 
   $sth->execute( $translation->start(),
-		 $translation->start_Exon()->dbID(),
-		 $translation->end(),
-		 $translation->end_Exon()->dbID(),
-	         $transcript_id );
-
+                 $translation->start_Exon()->dbID(),
+                 $translation->end(),
+                 $translation->end_Exon()->dbID(),
+                 $transcript_id );
+  
   my $transl_dbID = $sth->{'mysql_insertid'};
-
 
   #store object xref mappings to translations
 
@@ -199,16 +230,15 @@ sub store {
      throw("Trying to store incomplete stable id information for translation");
     }
     
-    my $statement = "INSERT INTO translation_stable_id(translation_id," .
-                                   "stable_id,version)".
-				     " VALUES(" . $transl_dbID . "," .
-				       "'" . $translation->stable_id . "'," .
-					 $translation->version . 
-					   ")";
-        
-    my $sth = $self->prepare($statement);
-    $sth->execute();
-   }
+    my $sth = $self->prepare
+      ("INSERT INTO translation_stable_id(translation_id, stable_id, version)".
+       "     VALUES (?, ?, ?)");
+   
+    $sth->execute($transl_dbID, $translation->stable_id(), 
+                  $translation->version());
+    
+    $sth->finish();
+  }
 
   $translation->dbID( $transl_dbID );
   $translation->adaptor( $self );
@@ -217,14 +247,151 @@ sub store {
 }
 
 
+
+
+sub remove {
+  my $self = shift;
+  my $translation = shift;
+
+  my $sth = $self->prepare("DELETE FROM translation 
+                            WHERE translation_id = ?" );
+  $sth->execute( $translation->dbID );
+  $sth = $self->prepare("DELETE FROM translation_stable_id 
+                         WHERE translation_id = ?" );
+  $sth->execute( $translation->dbID );
+  $translation->dbID( undef );
+  $translation->adaptor(undef);
+}
+
+
+=head2 list_dbIDs
+
+  Arg [1]    : none
+  Example    : @translation_ids = @{$translation_adaptor->list_dbIDs()};
+  Description: Gets an array of internal ids for all translations in the current db
+  Returntype : list of ints
+  Exceptions : none
+  Caller     : ?
+
+=cut
+
+sub list_dbIDs {
+   my ($self) = @_;
+
+   return $self->_list_dbIDs("translation");
+}
+
+
+=head2 list_stable_dbIDs
+
+  Arg [1]    : none
+  Example    : @transl_stable_ids = @{$transl_adaptor->list_stable_dbIDs()};
+  Description: Gets an array of stable ids for all translations in the current 
+               db
+  Returntype : reference to a list of strings
+  Exceptions : none
+  Caller     : general
+
+=cut
+
+sub list_stable_ids {
+   my ($self) = @_;
+
+   return $self->_list_dbIDs("translation_stable_id", "stable_id");
+}
+
+
+
+=head2 fetch_by_dbID
+
+  Arg [1]    : int $dbID
+               The internal identifier of the Translation to obtain
+  Example    : $translation = $translation_adaptor->fetch_by_dbID(1234);
+  Description: This fetches a Translation object via its internal id.  This
+               is only debatably useful since translations do not make much
+               sense outside of the context of their Translation.  Consider
+               using fetch_by_Transcript instead.
+  Returntype : Bio::EnsEMBL::Translation or undef if the translation is not
+               found.
+  Exceptions : warning if an additional (old style) Transcript argument is
+               provided
+  Caller     : ?
+
+=cut
+
+sub fetch_by_dbID {
+   my ($self,$dbID, $transcript) = @_;
+
+   if($transcript) {
+     deprecate("Use of fetch_by_dbID with a Transcript argument is deprecated."
+               . "Use fetch_by_Transcript instead." );
+   }
+
+   if(!$dbID) {
+     throw("dbID argument is required");
+   }
+
+   my $transcript_adaptor = $self->db()->get_TranscriptAdaptor();
+   my $transcript = $transcript_adaptor->fetch_by_translation_id($dbID);
+
+   return undef if(!$transcript);
+
+   return $self->fetch_by_Transcript($transcript);
+}
+
+
+=head2 fetch_by_dbID
+
+  Arg [1]    : int $dbID
+               The internal identifier of the Translation to obtain
+  Example    : $translation = $translation_adaptor->fetch_by_dbID(1234);
+  Description: This fetches a Translation object via its internal id.  This
+               is only debatably useful since translations do not make much
+               sense outside of the context of their Translation.  Consider
+               using fetch_by_Transcript instead.
+  Returntype : Bio::EnsEMBL::Translation or undef if the translation is not
+               found.
+  Exceptions : warning if an additional (old style) Transcript argument is
+               provided
+  Caller     : ?
+
+=cut
+
+sub fetch_by_stable_id {
+   my ($self,$stable_id) = @_;
+
+   if(!$stable_id) {
+     throw("stable id argument is required");
+   }
+
+   my $transcript_adaptor = $self->db()->get_TranscriptAdaptor();
+   my $transcript = 
+     $transcript_adaptor->fetch_by_translation_stable_id($stable_id);
+
+   return undef if(!$transcript);
+
+   return $self->fetch_by_Transcript($transcript);
+}
+
+
+
+
+=head2 fetch_all_by_DBEntry
+
+  Description: DEPRECATED, this has been renames fetch_all_by_external_name
+
+=cut
+
+sub fetch_all_by_DBEntry {
+  my $self = shift;
+  deprecate("Use fetch_all_by_external_name instead.");
+  return $self->fetch_all_by_external_name(@_);
+}
+
 =head2 get_stable_entry_info
 
- Title   : get_stable_entry_info
- Usage   : $translationAdaptor->get_stable_entry_info($translation)
- Function: gets stable info for translation and places it into the hash
- Returns : 
- Args    : 
-
+ Description: DEPRECATED - This method should no longer be needed. Stable
+              id info is fetched when the transcript is.
 
 =cut
 
@@ -248,120 +415,6 @@ sub get_stable_entry_info {
   $translation->{'_version'}   = $array[1];
   
   return 1;
-}
-
-
-sub remove {
-  my $self = shift;
-  my $translation = shift;
-
-  my $sth = $self->prepare("DELETE FROM translation 
-                            WHERE translation_id = ?" );
-  $sth->execute( $translation->dbID );
-  $sth = $self->prepare("DELETE FROM translation_stable_id 
-                         WHERE translation_id = ?" );
-  $sth->execute( $translation->dbID );
-  $translation->dbID( undef );
-  $translation->adaptor(undef);
-}
-
-=head2 list_dbIDs
-
-  Arg [1]    : none
-  Example    : @translation_ids = @{$translation_adaptor->list_dbIDs()};
-  Description: Gets an array of internal ids for all translations in the current db
-  Returntype : list of ints
-  Exceptions : none
-  Caller     : ?
-
-=cut
-
-sub list_dbIDs {
-   my ($self) = @_;
-
-   return $self->_list_dbIDs("translation");
-}
-
-=head2 list_stable_dbIDs
-
-  Arg [1]    : none
-  Example    : @stable_translation_ids = @{$translation_adaptor->list_stable_dbIDs()};
-  Description: Gets an array of stable ids for all translations in the current db
-  Returntype : list of ints
-  Exceptions : none
-  Caller     : ?
-
-=cut
-
-sub list_stable_ids {
-   my ($self) = @_;
-
-   return $self->_list_dbIDs("translation_stable_id", "stable_id");
-}
-
-
-=head2 fetch_by_dbID
-
- Title   : fetch_by_dbID
- Usage   :
- Function:
- Example :
- Returns : 
- Args    :
-
-
-=cut
-
-sub fetch_by_dbID {
-   my ($self,$dbID,$transcript) = @_;
-
-   deprecate( "This call shouldnt be necessary" );
-
-   if( !defined $transcript ) {
-     throw("Translations make no sense outside of their " .
-		  "parent Transcript objects. You must retrieve " .
-		  "with Transcript parent");
-   }
-
-   my $sth = $self->prepare("SELECT translation_id tlid, seq_start, 
-                                    start_exon_id, seq_end, end_exon_id 
-                             FROM   translation 
-                             WHERE  translation_id = ?");
-   $sth->execute($dbID);
-   my $rowhash = $sth->fetchrow_hashref;
-
-   if( !defined $rowhash ) {
-     # assumme this is a translationless transcript deliberately
-     return undef;
-   }
-
-   my $out = Bio::EnsEMBL::Translation->new();
-
-   $out->start        ($rowhash->{'seq_start'});
-   $out->end          ($rowhash->{'seq_end'});
-
-
-   #search through the transcript's exons for the start and end exons
-   my ($start_exon, $end_exon);
-   foreach my $exon (@{$transcript->get_all_Exons()}) {
-     if($exon->dbID() == $rowhash->{'start_exon_id'}) {
-       $start_exon = $exon;
-     }
-
-     if($exon->dbID() == $rowhash->{'end_exon_id'}) {
-       $end_exon = $exon;
-     }
-   }
-   unless($start_exon && $end_exon) {
-     throw("Could not find start or end exon in transcript\n");
-   }
-
-   $out->start_Exon($start_exon);
-   $out->end_Exon($end_exon);
-   $out->dbID($rowhash->{'tlid'});
-   $out->adaptor( $self );
-   
-   return $out;
 }
 
 1;
