1323e26badb0d290670aa354a16d673363403384
Graham McVicker <mcvicker@sanger.ac.uk>
2004-02-05 18:26:08 +0000

    script for running other script so i don't have to type all the command line args

---
+++ misc-scripts/chimp/run_hum2chimp.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+/usr/local/ensembl/bin/perl human2chimp.pl -hdbname homo_sapiens_core_20_34b -hhost ecs4 -huser ensro -hport 3351 -cdbname mcvicker_chimp_human_merge -chost ecs4 -cport 3350 -cuser ensro -verbose -hchromosome 9 -hassembly NCBI34 -cassembly BROAD1
