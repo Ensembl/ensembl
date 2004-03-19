cdad44d09adadae2bbee06c1551c8dda6fe92888
Graham McVicker <mcvicker@sanger.ac.uk>
2004-03-19 09:17:43 +0000

    fixed auto-vivification bug in get_all_ExternalFeatures and get_all_ExternalLiteFeatures, (spotted by pm2)

--- modules/Bio/EnsEMBL/Slice.pm
+++ modules/Bio/EnsEMBL/Slice.pm
@@ -1474,7 +1474,9 @@ sub get_all_ExternalFeatures {
 
    if($track_name) {
      #use a specific adaptor
-     push @xf_adaptors, $xfa_hash->{$track_name};
+     if(exists $xfa_hash->{$track_name}) {
+       push @xf_adaptors, $xfa_hash->{$track_name};
+     }
    } else {
      #use all of the adaptors
      push @xf_adaptors, values %$xfa_hash;
@@ -1516,7 +1518,9 @@ sub get_all_ExternalLiteFeatures {
 
    if($track_name) {
      #use a specific adaptor
-     push @xf_adaptors, $xfa_hash->{$track_name};
+     if(exists $xfa_hash->{$track_name}) {
+       push @xf_adaptors, $xfa_hash->{$track_name};
+     }
    } else {
      #use all of the adaptors
      push @xf_adaptors, values %$xfa_hash;
