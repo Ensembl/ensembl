a338b749f5d163cae796a47e752e491613545392
cvs2git <cvs2git>
2004-03-29 15:13:11 +0000

    This commit was manufactured by cvs2svn to create branch 'branch- ensembl-19'.
    Cherrypick from master 2004-03-29 15:13:10 UTC Martin Hammond <mh4@sanger.ac.uk> 'finds ENSANGP# proteins that have moved between releases':
        misc-scripts/anopheles_scripts/location_check_for_dump.pl
        modules/Bio/EnsEMBL/DBSQL/StatementHandle.pm
        modules/Bio/EnsEMBL/Intron.pm

---
+++ modules/Bio/EnsEMBL/DBSQL/StatementHandle.pm
@@ -0,0 +1,66 @@
+=head1 NAME - Bio::EnsEMBL::DBSQL::StatementHandle
+
+=head1 SYNOPSIS
+
+  Do not use this class directly.  It will automatically be used by
+  the Bio::EnsEMBL::DBSQL::DBConnection class.
+
+=head1 DESCRIPTION
+
+  This class extends DBD::mysql::st so that the DESTROY method may be
+  overridden.  If the DBConnection::disconnect_when_inactive flag is set
+  this statement handle will cause the database connection to be closed
+  when it goes out of scope and there are no other open statement handles.
+
+=head1 CONTACT
+
+  This module is part of the Ensembl project: www.ensembl.org
+
+  Ensembl development mailing list: <ensembl-dev@ebi.ac.uk>
+
+=head1 METHODS
+
+=cut
+
+package Bio::EnsEMBL::DBSQL::StatementHandle;
+
+use vars qw(@ISA);
+use strict;
+
+use Bio::EnsEMBL::Utils::Exception qw(warning);
+
+use DBD::mysql;
+
+@ISA = qw(DBI::st);
+
+# As DBD::mysql::st is a tied hash can't store things in it,
+# so have to have parallel hash
+my %dbchash;
+sub dbc {
+  my $self = shift;
+
+  if (@_) {
+    $dbchash{$self} = shift;
+  }
+
+  return $dbchash{$self};
+}
+
+
+sub DESTROY {
+  my ($obj) = @_;
+
+  my $dbc = $obj->dbc;
+  $obj->dbc(undef);
+
+  DBI::st::DESTROY($obj);
+
+  # The count for the number of kids is decremented only after this
+  # function is complete. Disconnect if there is 1 kid (this one) remaining.
+  if($dbc  && $dbc->disconnect_when_inactive() &&
+     $dbc->db_handle->{Kids} == 1) {
+    $dbc->db_handle->disconnect();
+  }
+}
+
+1;
