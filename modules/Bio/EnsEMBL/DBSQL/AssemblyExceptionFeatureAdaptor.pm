2d699811b180f51af8da61c9223b8205ea526454
Graham McVicker <mcvicker@sanger.ac.uk>
2004-04-23 11:03:31 +0000

    Reimplemented completely.  Old implementation was hacky, and had subtle problems with the way PAR exception features were retrieved

--- modules/Bio/EnsEMBL/DBSQL/AssemblyExceptionFeatureAdaptor.pm
+++ modules/Bio/EnsEMBL/DBSQL/AssemblyExceptionFeatureAdaptor.pm
@@ -2,7 +2,7 @@
 # Ensembl module for Bio::EnsEMBL::DBSQL::AssemblyExceptionFeatureAdaptor
 #
 #
-# Copyright EMBL/EBI
+# Copyright Ensembl
 #
 # You may distribute this module under the same terms as perl itself
 
@@ -32,158 +32,186 @@ package Bio::EnsEMBL::DBSQL::AssemblyExceptionFeatureAdaptor;
 use vars qw(@ISA);
 use strict;
 
+use Bio::EnsEMBL::DBSQL::BaseAdaptor;
 use Bio::EnsEMBL::DBSQL::BaseFeatureAdaptor;
 use Bio::EnsEMBL::AssemblyExceptionFeature;
 use Bio::EnsEMBL::Utils::Exception qw(throw warning);
 
-@ISA = qw(Bio::EnsEMBL::DBSQL::BaseFeatureAdaptor);
+@ISA = qw(Bio::EnsEMBL::DBSQL::BaseAdaptor);
 
-=head2 generic_fetch
 
-  Arg [1]    : (optional) string $constraint
-               An SQL query constraint (i.e. part of the WHERE clause)
-  Arg [2]    : (optional) Bio::EnsEMBL::AssemblyMapper $mapper
-               A mapper object used to remap features
-               as they are retrieved from the database
-  Arg [3]    : (optional) Bio::EnsEMBL::Slice $slice
-               A slice that features should be remapped to
-  Arg [4]    : (optional) hashref $options
-               Hashref of options flags passed on to the _objs_from_sth method
-               implementation.
-  Example    : $fts = $a->generic_fetch('contig_id in (1234, 1235)', 'Swall');
-  Description: Overrides the default generic fetch for this object. Will
-               ignore slice constraints.
-  Returntype : listref of Bio::EnsEMBL::SeqFeature in contig coordinates
+
+=head2 fetch_all
+
+  Arg [1]    : none
+  Example    : my @axfs = @{$axfa->fetch_all()};
+  Description: Retrieves all assembly exception features which are in the
+               database and builds internal caches of the features.
+  Returntype : reference to list of Bio::EnsEMBL::AssemblyExceptionFeatures
   Exceptions : none
-  Caller     : BaseFeatureAdaptor
+  Caller     : fetch_by_dbID, fetch_by_Slice
 
 =cut
 
-sub generic_fetch {
-  my ($self, $constraint, $mapper, $slice) = @_;
+sub fetch_all {
+  my $self = shift;
 
-  my $columns = join(', ', $self->_columns());
-
-  my $db = $self->db();
-
-  my $sql = "SELECT $columns FROM assembly_exception ae";
-
-  # if there is a slice constraint we remove the constraints ...
-  unless( $constraint =~ /seq_region_id/ || $constraint eq "" ) {
-    $sql .= " WHERE $constraint";
+  if(defined($self->{'aexc_cache'})) {
+    return $self->{'aexc_cache'};
   }
 
-  my $sth = $db->prepare($sql);
+  my $sth = $self->prepare
+    ("SELECT assembly_exception_id, seq_region_id, seq_region_start,
+             seq_region_end, exc_type, exc_seq_region_id, exc_seq_region_start,
+             exc_seq_region_end, ori
+      FROM assembly_exception");
 
-  $sth->execute;
+  $sth->execute();
 
-  my $res = $self->_objs_from_sth($sth, $mapper, $slice);
+  my ($ax_id, $sr_id, $sr_start, $sr_end,
+      $x_type, $x_sr_id, $x_sr_start, $x_sr_end, $ori);
 
-  return $res;
-}
-
-=head2 _tablename
-
-  Arg [1]    : none
-  Example    : none
-  Description: PROTECTED implementation of superclass abstract method
-               returns the names, aliases of the tables to use for queries
-  Returntype : list of listrefs of strings
-  Exceptions : none
-  Caller     : internal
-
-=cut
-
-sub _tables {
-  my $self = shift;
-
-  return ['assembly_exception', 'ae'];
-}
-
-
-=head2 _columns
-
-  Arg [1]    : none
-  Example    : none
-  Description: PROTECTED implementation of superclass abstract method
-               returns a list of columns to use for queries
-  Returntype : list of strings
-  Exceptions : none
-  Caller     : internal
-
-=cut
-
-sub _columns {
-  my $self = shift;
-
-  return qw( ae.assembly_exception_id
-             ae.seq_region_id ae.seq_region_start ae.seq_region_end 
-             ae.exc_type ae.exc_seq_region_id ae.exc_seq_region_start 
-             ae.exc_seq_region_end );
-}
-
-
-=head2 _objs_from_sth
-
-  Arg [1]    : hash reference $hashref
-  Example    : none
-  Description: PROTECTED implementation of superclass abstract method.
-               creates AssemblyExceptionFeatures from an executed DBI statement handle.
-  Returntype : list reference to Bio::EnsEMBL::AssemblyExceptionFeature objects
-  Exceptions : none
-  Caller     : internal
-
-=cut
-
-sub _objs_from_sth {
-
-  my ($self, $sth, $mapper, $dest_slice) = @_;
+  $sth->bind_columns(\$ax_id, \$sr_id, \$sr_start, \$sr_end,
+                     \$x_type, \$x_sr_id, \$x_sr_start, \$x_sr_end, \$ori);
 
   my @features;
+  my $sa = $self->db()->get_SliceAdaptor();
 
-  my $slice_adaptor = $self->db()->get_SliceAdaptor();
+  $self->{'aexc_dbID_cache'} = {};
 
-  my ($dbID, $seq_region_id, $seq_region_start, $seq_region_end, $type,
-      $exc_seq_region_id, $exc_seq_region_start, $exc_seq_region_end);
+  while($sth->fetch()) {
+    my $slice   = $sa->fetch_by_seq_region_id($sr_id);
+    my $x_slice = $sa->fetch_by_seq_region_id($x_sr_id);
 
-  $sth->bind_columns(\$dbID,
-                     \$seq_region_id, \$seq_region_start, \$seq_region_end,
-                     \$type, \$exc_seq_region_id, \$exc_seq_region_start, 
-                     \$exc_seq_region_end);
-
-  while ($sth->fetch()) {
-
-    my $sr_slice  = $slice_adaptor->fetch_by_seq_region_id($seq_region_id);
-    my $exc_slice = $slice_adaptor->fetch_by_seq_region_id($exc_seq_region_id);
-
-    # each row creates TWO features, each of which has alternate_slice 
+    # each row creates TWO features, each of which has alternate_slice
     # pointing to the "other" one
 
-    push @features,
-      Bio::EnsEMBL::AssemblyExceptionFeature->new
-          ('-dbID'            => $dbID,
-           '-start'           => $seq_region_start,
-           '-end'             => $seq_region_end,
+    my $a = Bio::EnsEMBL::AssemblyExceptionFeature->new
+          ('-dbID'            => $ax_id,
+           '-start'           => $sr_start,
+           '-end'             => $sr_end,
            '-strand'          => 1,
            '-adaptor'         => $self,
-           '-slice'           => $sr_slice,
-           '-alternate_slice' => $exc_slice,
-           '-type'            => $type);
+           '-slice'           => $slice,
+           '-alternate_slice' => $x_slice->sub_Slice($x_sr_start, $x_sr_end),
+           '-type'            => $x_type);
 
-    push @features, 
-      Bio::EnsEMBL::AssemblyExceptionFeature->new
-          ('-dbID'            => $dbID,
-           '-start'           => $exc_seq_region_start,
-           '-end'             => $exc_seq_region_end,
+    push @features, $a;
+    $self->{'aexc_dbID_cache'}->{$ax_id} = $a;
+
+    push @features, Bio::EnsEMBL::AssemblyExceptionFeature->new
+          ('-dbID'            => $ax_id,
+           '-start'           => $x_sr_start,
+           '-end'             => $x_sr_end,
            '-strand'          => 1,
            '-adaptor'         => $self,
-           '-slice'           => $exc_slice,
-           '-alternate_slice' => $sr_slice,
-           '-type'            => $type);
+           '-slice'           => $x_slice,
+           '-alternate_slice' => $slice->sub_Slice($sr_start, $sr_end),
+           '-type'            => $x_type);
   }
+
+  $sth->finish();
+
+  $self->{'aexc_cache'} = \@features;
 
   return \@features;
 }
+
+#
+# cleans up internal caches during garbage collection
+#
+sub deleteObj {
+  my $self = shift;
+
+  $self->SUPER::deleteObj(@_);
+
+  delete $self->{'aexc_cache'};
+  delete $self->{'aexc_slice_cache'};
+  delete $self->{'aexc_dbID_cache'};
+
+  return;
+}
+
+
+
+=head2 fetch_by_dbID
+
+  Arg [1]    : int $dbID
+  Example    : my $axf = $axfa->fetch_by_dbID(3);
+  Description: Retrieves a single assembly exception feature via its internal
+               identifier.  Note that this only retrieves one of the two
+               assembly exception features which are represented by a single
+               row in the assembly_exception table.
+  Returntype : Bio::EnsEMBL::AssemblyExceptionFeature
+  Exceptions : none
+  Caller     : general
+
+=cut
+
+sub fetch_by_dbID {
+  my $self = shift;
+  my $dbID = shift;
+
+  if(!exists($self->{'aexc_dbID_cache'})) {
+    # force loading of cache
+    $self->fetch_all();
+  }
+
+  return $self->{'aexc_dbID_cache'}->{$dbID};
+}
+
+
+=head2 fetch_all_by_Slice
+
+  Arg [1]    : Bio::EnsEMBL::Slice $slice
+  Example    : my @axfs = @{$axfa->fetch_all_by_Slice($slice)};
+  Description: Retrieves all assembly exception features which overlap the
+               provided slice.  The returned features will be in coordinate
+               system of the slice.
+  Returntype : reference to list of Bio::EnsEMBL::AssemblyException features
+  Exceptions : none
+  Caller     : Feature::get_all_alt_locations, general
+
+=cut
+
+sub fetch_all_by_Slice {
+  my $self = shift;
+  my $slice = shift;
+
+  my $key= uc($slice->name());
+
+  if(exists($self->{'aexc_slice_cache'}->{$key})) {
+    return $self->{'aexc_slice_cache'}->{$key};
+  }
+
+  my $all_features = $self->fetch_all();
+
+  my $mcc = $self->db()->get_MetaCoordContainer();
+  my $css = $mcc->fetch_all_CoordSystems_by_feature_type('assembly_exception');
+
+  my @features;
+
+  my $remap = \&Bio::EnsEMBL::DBSQL::BaseFeatureAdaptor::_remap;
+  my $ma = $self->db()->get_AssemblyMapperAdaptor();
+
+  foreach my $cs (@$css) {
+    my $mapper;
+    if($cs->equals($slice->coord_system)) {
+      $mapper = undef;
+    } else {
+      $mapper = $ma->fetch_by_CoordSystems($cs,$slice->coord_system());
+    }
+
+    push @features, @{&$remap($all_features, $mapper, $slice)};
+  }
+
+  $self->{'aexc_slice_cache'}->{$key} = \@features;
+
+  return \@features;
+}
+
+
+
 
 
 1;
