356cc3b1ee12bd7eb00882c54685405faacf446c
Arne Stabenau <stabenau@sanger.ac.uk>
2004-05-11 13:29:35 +0000

    tables for translation and transcript attributes

--- sql/table.sql
+++ sql/table.sql
@@ -806,6 +806,34 @@ CREATE TABLE misc_attrib (
   KEY misc_feature_idx( misc_feature_id )
 ) TYPE=MyISAM;
 
+################################################################################
+#
+# Table structure for table 'translation_attrib'
+#
+
+CREATE TABLE translation_attrib (
+  translation_id              int(10) unsigned NOT NULL default '0',
+  attrib_type_id              smallint(5) unsigned NOT NULL default '0',
+  value                       varchar(255) NOT NULL default '',
+
+  KEY type_val_idx( attrib_type_id, value ),
+  KEY translation_idx( translation_id )
+) TYPE=MyISAM;
+
+################################################################################
+#
+# Table structure for table 'transcript_attrib'
+#
+
+CREATE TABLE transcript_attrib (
+  transcript_id               int(10) unsigned NOT NULL default '0',
+  attrib_type_id              smallint(5) unsigned NOT NULL default '0',
+  value                       varchar(255) NOT NULL default '',
+
+  KEY type_val_idx( attrib_type_id, value ),
+  KEY transcript_idx( transcript_id )
+) TYPE=MyISAM;
+
 
 
 
