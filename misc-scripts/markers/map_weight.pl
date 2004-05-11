d87db3bfa879a9963d8d3ccfcb0923d90ffc9e64
Arne Stabenau <stabenau@sanger.ac.uk>
2004-05-11 12:57:12 +0000

    script creates map_weights inside the database now

--- misc-scripts/markers/map_weight.pl
+++ misc-scripts/markers/map_weight.pl
@@ -1,24 +1,67 @@
+# script to calculate map_weights in a database that has markers
+# and marker_features. Recreates the marker_feature table with weights set
+
 use strict;
+use DBI;
 
-my $marker_feature_in = shift;
-my $marker_hits_in = shift;
+use Getopt::Long;
 
-open MARKER_FEATURES, $marker_feature_in or die;
-open MARKER_HITS, $marker_hits_in or die;
+my ( $host, $user, $pass, $port, $dbname );
+my $verbose = 0;
 
-my %marker_hit_hash;
-while(<MARKER_HITS>) {
-	my ($id, $count) = split;
-	$marker_hit_hash{$id} = $count;
+GetOptions( "host=s", \$host,
+	    "user=s", \$user,
+	    "pass=s", \$pass,
+	    "port=i", \$port,
+	    "dbname=s", \$dbname,
+	    "verbose", \$verbose
+	  );
+
+if( !$host ) {
+  usage();
 }
 
-close MARKER_HITS;
 
-while(<MARKER_FEATURES>) {
-	chomp;
-	my @row = split;
-	$row[-1] = $marker_hit_hash{$row[1]};
-	print join("\t", @row) . "\n";
+
+my $dsn = "DBI:mysql:host=$host;dbname=$dbname";
+if( $port ) {
+  $dsn .= ";port=$port";
 }
 
-close MARKER_FEATURES;
+my $db = DBI->connect( $dsn, $user, $pass );
+
+
+$db->do( "
+  CREATE TABLE tmp_m_weight
+  SELECT marker_id, count(*) as count 
+  FROM marker_feature
+  GROUP BY marker_id
+" );
+
+$db->do( "
+  CREATE TABLE new_marker_feature
+  SELECT mf.marker_feature_id, mf.marker_id, mf.seq_region_id, mf.seq_region_start,
+         mf.seq_region_end, mf.analysis_id, tmw.count
+  FROM   marker_feature mf, tmp_m_weight tmw
+  WHERE  mf.marker_id = tmw.marker_id
+" );
+
+$db->do( "delete from marker_feature" );
+$db->do( "insert into marker_feature select * from new_marker_feature" );
+$db->do( "drop table tmp_m_weight" );
+$db->do( "drop table new_marker_feature" );
+
+sub usage {
+  print <<EOF;
+    
+Usage: perl map_weight.pl [options]
+   -user username for a write enabled user
+   -host hostname
+   -port portnumber
+   -pass password
+   -dbname database name where the markers and the features are
+
+EOF
+
+  exit;
+}
