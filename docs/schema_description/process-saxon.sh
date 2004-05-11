3defc6deb6c2c84b4b725644c2264f9cb599b3cb
Glenn Proctor <gp1@sanger.ac.uk>
2004-05-11 13:58:45 +0000

    Added automatic copy of schema_description.html to private-web/htdocs/Docs to save doing it manually

--- docs/schema_description/process-saxon.sh
+++ docs/schema_description/process-saxon.sh
@@ -3,3 +3,5 @@
 java -jar "C:/Program Files/saxon/saxon7.jar" tables.xml xml2html.xsl > schema_description.html 
 
 java -jar "C:/Program Files/saxon/saxon7.jar" tables.xml xml2wiki.xsl > schema_description.txt
+
+cp schema_description.html ../../../private-web/htdocs/Docs/
