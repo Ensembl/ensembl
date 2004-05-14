20bd5b08f4f8e842aa76237e39a8e0435ef5103b
Graham McVicker <mcvicker@sanger.ac.uk>
2004-05-14 15:57:22 +0000

    moved snp typing methods into utility class in an attempt to make very large transcript module more manageable

--- modules/Bio/EnsEMBL/Transcript.pm
+++ modules/Bio/EnsEMBL/Transcript.pm
@@ -12,11 +12,6 @@ Transcript - gene transcript object
 
 =head1 SYNOPSIS
 
-=head1 DESCRIPTION
-
-Contains details of coordinates of all exons that make
-up a gene transcript.
-
 Creation:
 
      my $tran = new Bio::EnsEMBL::Transcript();
@@ -25,10 +20,15 @@ Creation:
 Manipulation:
 
      # Returns an array of Exon objects
-     my @exons = @{$tran->get_all_Exons};
+     my @exons = @{$tran->get_all_Exons()};
      # Returns the peptide translation of the exons as a Bio::Seq
      my $pep   = $tran->translate();
 
+=head1 DESCRIPTION
+
+A representation of a transcript within the ensembl system.  A transcript
+consists of a set of Exons and (possibly) a Translation which defines the
+coding and non-coding regions of the exons.
 
 =head1 CONTACT
 
@@ -39,15 +39,14 @@ Email questions to the ensembl developer mailing list <ensembl-dev@ebi.ac.uk>
 =cut
 
 package Bio::EnsEMBL::Transcript;
+
 use vars qw(@ISA);
 use strict;
 
 use Bio::EnsEMBL::Feature;
-use Bio::EnsEMBL::Exon;
 use Bio::EnsEMBL::Intron;
-use Bio::EnsEMBL::Translation;
-use Bio::Tools::CodonTable;
 use Bio::EnsEMBL::TranscriptMapper;
+use Bio::EnsEMBL::Utils::TranscriptSNPs;
 
 use Bio::EnsEMBL::Utils::Argument qw( rearrange );
 use Bio::EnsEMBL::Utils::Exception qw( deprecate warning throw );
@@ -903,318 +902,6 @@ sub length {
 }
 
 
-=head2 get_all_peptide_variations
-
-  Arg [1]    : (optional) $snps listref of coding snps in cdna coordinates
-  Example    : $pep_hash = $trans->get_all_peptide_variations;
-  Description: Takes an optional list of coding snps on this transcript in 
-               which are in cdna coordinates and returns a hash with peptide 
-               coordinate keys and listrefs of alternative amino acids as 
-               values.  If no argument is provided all of the coding snps on 
-               this transcript are used by default. Note that the peptide 
-               encoded by the reference sequence is also present in the results
-               and that duplicate peptides (e.g. resulting from synonomous 
-               mutations) are discarded.  It is possible to have greated than
-               two peptides variations at a given location given
-               adjacent or overlapping snps. Insertion/deletion variations
-               are ignored by this method. 
-               Example of a data structure that could be returned:
-               {  1  => ['I', 'M'], 
-                 10  => ['I', 'T'], 
-                 37  => ['N', 'D'], 
-                 56  => ['G', 'E'], 
-                 118 => ['R', 'K'], 
-                 159 => ['D', 'E'], 
-                 167 => ['Q', 'R'], 
-                 173 => ['H', 'Q'] } 
-  Returntype : hashref
-  Exceptions : none
-  Caller     : general
-
-=cut
-
-sub get_all_peptide_variations {
-  my $self = shift;
-  my $snps = shift;
-
-  my $codon_table = Bio::Tools::CodonTable->new;
-  my $codon_length = 3;
-  my $cdna = $self->spliced_seq;
-
-  unless(defined $snps) {
-    $snps = $self->get_all_cdna_SNPs->{'coding'};
-  }
-
-  my $variant_alleles;
-  my $translation_start = $self->cdna_coding_start;
-  foreach my $snp (@$snps) {
-    #skip variations not on a single base
-    next if ($snp->start != $snp->end);
-
-    my $start = $snp->start;
-    my $strand = $snp->strand;    
-
-    #calculate offset of the nucleotide from codon start (0|1|2)
-    my $codon_pos = ($start - $translation_start) % $codon_length;
-
-    #calculate the peptide coordinate of the snp
-    my $peptide = ($start - $translation_start + 
-		   ($codon_length - $codon_pos)) / $codon_length;
-
-    #retrieve the codon
-    my $codon = substr($cdna, $start - $codon_pos-1, $codon_length);
-
-    #store each alternative allele by its location in the peptide
-    my @alleles = split(/\/|\|/, lc($snp->alleles));
-    foreach my $allele (@alleles) {
-      next if $allele eq '-';       #skip deletions
-      next if CORE::length($allele) != 1; #skip insertions
-
-      #get_all_cdna_SNPs always gives strand of 1 now
-      #if($strand == -1) {
-      #  #complement the allele if the snp is on the reverse strand
-      #  $allele =~ 
-      #  tr/acgtrymkswhbvdnxACGTRYMKSWHBVDNX/tgcayrkmswdvbhnxTGCAYRKMSWDVBHNX/;
-      #}
-
-      #create a data structure of variant alleles sorted by both their
-      #peptide position and their position within the peptides codon
-      $variant_alleles ||= {};
-      if(exists $variant_alleles->{$peptide}) {
-        my $alleles_arr = $variant_alleles->{$peptide}->[1];
-        push @{$alleles_arr->[$codon_pos]}, $allele;
-      } else {
-        #create a list of 3 lists (one list for each codon position)
-        my $alleles_arr = [[],[],[]];
-        push @{$alleles_arr->[$codon_pos]}, $allele;
-        $variant_alleles->{$peptide} = [$codon, $alleles_arr];
-      }
-    }
-  }
-
-  my %out;
-  #now generate all possible codons for each peptide and translate them
-  foreach my $peptide (keys %$variant_alleles) {
-    my ($codon, $alleles) = @{$variant_alleles->{$peptide}};
-
-    #need to push original nucleotides onto each position
-    #so that all possible combinations can be generated
-    push @{$alleles->[0]}, substr($codon,0,1);
-    push @{$alleles->[1]}, substr($codon,1,1);
-    push @{$alleles->[2]}, substr($codon,2,1);
-
-    my %alt_amino_acids;
-    foreach my $a1 (@{$alleles->[0]}) {
-      substr($codon, 0, 1) = $a1;
-      foreach my $a2 (@{$alleles->[1]}) {
-        substr($codon, 1, 1) = $a2;
-        foreach my $a3 (@{$alleles->[2]}) {
-          substr($codon, 2, 1) = $a3;
-          my $aa = $codon_table->translate($codon);
-          #print "$codon translation is $aa\n";
-          $alt_amino_acids{$aa} = 1;
-        }
-      }
-    }
-
-    my @aas = keys %alt_amino_acids;
-    $out{$peptide} = \@aas;
-  }
-
-  return \%out;
-}
-
-
-=head2 get_all_SNPs
-
-  Arg [1]    : (optional) int $flanking
-               The number of basepairs of transcript flanking sequence to 
-               retrieve snps from (default 0) 
-  Example    : $snp_hashref = $transcript->get_all_SNPs;
-  Description: Retrieves all snps found within the region of this transcript. 
-               The snps are returned in a hash with keys corresponding
-               to the region the snp was found in.  Possible keys are:
-               'three prime UTR', 'five prime UTR', 'coding', 'intronic',
-               'three prime flanking', 'five prime flanking'
-               If no flanking argument is provided no flanking snps will be
-               obtained.
-               The listrefs which are the values of the returned hash
-               contain snps in coordinates of the transcript region 
-               (i.e. first base = first base of the first exon on the
-               postive strand - flanking bases + 1) 
-  Returntype : hasref with string keys and listrefs of Bio::EnsEMBL::SNPs for 
-               values
-  Exceptions : none
-  Caller     : general
-
-=cut
-
-sub get_all_SNPs {
-  my $self = shift;
-  my $flanking = shift;
-
-  my %snp_hash;
-  my $sa = $self->adaptor->db->get_SliceAdaptor;
-
-  #retrieve a slice in the region of the transcript
-  my $slice = $sa->fetch_by_transcript_id($self->dbID, $flanking );
-
-  #copy this transcript, so we can work in coord system we are interested in
-  my $transcript = $self->transfer( $slice );
-
-  #get all of the snps in the transcript region
-  my $snps = $slice->get_all_SNPs;
-
-  my $trans_start  = $flanking + 1;
-  my $trans_end    = $slice->length - $flanking;
-  my $trans_strand = $transcript->get_all_Exons->[0]->strand;
-
-  #classify each snp
-  foreach my $snp (@$snps) {
-    my $key;
-
-    if(($trans_strand == 1 && $snp->end < $trans_start) ||
-       ($trans_strand == -1 && $snp->start > $trans_end)) {
-      #this snp is upstream from the transcript
-      $key = 'five prime flanking';
-    }
-
-    elsif(($trans_strand == 1 && $snp->start > $trans_end) ||
-	  ($trans_strand == -1 && $snp->start < $trans_start)) {
-      #this snp is downstream from the transcript
-      $key = 'three prime flanking';
-    }
-
-    else {
-      #snp is inside transcript region check if it overlaps an exon
-      foreach my $e (@{$transcript->get_all_Exons}) {
-        if($snp->end >= $e->start && $snp->start <= $e->end) {
-          #this snp is in an exon
-
-          if(($trans_strand == 1 && 
-              $snp->end < $transcript->coding_region_start) ||
-             ($trans_strand == -1 && 
-              $snp->start > $transcript->coding_region_end)) {
-            #this snp is in the 5' UTR
-            $key = 'five prime UTR';
-          }
-
-          elsif(($trans_strand == 1 && 
-                 $snp->start > $transcript->coding_region_end)||
-                ($trans_strand == -1 && 
-                 $snp->end < $transcript->coding_region_start)) {
-            #this snp is in the 3' UTR
-            $key = 'three prime UTR';
-          }
-
-          else {
-            #snp is coding
-            $key = 'coding';
-          }
-          last;
-        }
-      }
-      unless($key) {
-        #snp was not in an exon and is therefore intronic
-        $key = 'intronic';
-      }
-    }
-
-    unless($key) {
-      #warning('SNP could not be mapped. In/Dels not supported yet...');
-      next;
-    }
-
-    if(exists $snp_hash{$key}) {
-      push @{$snp_hash{$key}}, $snp;
-    } else {
-      $snp_hash{$key} = [$snp];
-    }
-  }
-
-  return \%snp_hash;
-}
-
-
-
-=head2 get_all_cdna_SNPs
-
-  Arg [1]    : none 
-  Example    : $cdna_snp_hasref = $transcript->get_all_cdna_SNPs;
-  Description: Retrieves all snps found within exons of this transcript. 
-               The snps are returned in a hash with three keys corresponding
-               to the region the snp was found in.  Valid keys are:
-               'three prime UTR', 'five prime UTR', 'coding'
-               The listrefs which are the values of the returned hash
-               contain snps in CDNA coordinates.
-  Returntype : hasref with string keys and listrefs of Bio::EnsEMBL::SNPs for 
-               values
-  Exceptions : none
-  Caller     : general
-
-=cut
-
-sub get_all_cdna_SNPs {
-  my ($self) = shift;
-
-  #retrieve all of the snps from this transcript
-  my $all_snps = $self->get_all_SNPs;
-  my %snp_hash;
-
-  my @cdna_types = ('three prime UTR', 'five prime UTR','coding');
-
-  my $sa = $self->adaptor->db->get_SliceAdaptor;
-  my $slice = $sa->fetch_by_transcript_id($self->dbID);
-
-  #copy this transcript, so we can work in coord system we are interested in
-  my $transcript = $self->transfer($slice);
-
-  foreach my $type (@cdna_types) {
-    $snp_hash{$type} = [];
-    foreach my $snp (@{$all_snps->{$type}}) {
-      my @coords = $transcript->genomic2cdna($snp->start, $snp->end,
-                                             $snp->strand);
-
-      #skip snps that don't map cleanly (possibly an indel...)
-      if(scalar(@coords) != 1) {
-        #warning("snp of type $type does not map cleanly\n");
-        next;
-      }
-
-      my ($coord) = @coords;
-
-      unless($coord->isa('Bio::EnsEMBL::Mapper::Coordinate')) {
-        #warning("snp of type $type maps to gap\n");
-        next;
-      }
-
-      my $alleles = $snp->{'alleles'};
-      my $ambicode = $snp->{'_ambiguity_code'};
-
-      #we arbitrarily put the SNP on the +ve strand because it is easier to
-      #work with in the webcode.
-      if($coord->strand == -1) {
-        $alleles =~
-         tr/acgthvmrdbkynwsACGTDBKYHVMRNWS\//tgcadbkyhvmrnwsTGCAHVMRDBKYNWS\//;
-        $ambicode =~
-         tr/acgthvmrdbkynwsACGTDBKYHVMRNWS\//tgcadbkyhvmrnwsTGCAHVMRDBKYNWS\//;
-      }
-
-      #copy the snp and convert to cdna coords...
-      my $new_snp;
-      %$new_snp = %$snp;
-      bless $new_snp, ref $snp;
-      $new_snp->start($coord->start);
-      $new_snp->end($coord->end);
-      $new_snp->strand(1);
-      $new_snp->{'alleles'} = $alleles;
-      $new_snp->{'_ambiguity_code'} = $ambicode;
-      push @{$snp_hash{$type}}, $new_snp;
-    }
-  }
-
-  return \%snp_hash;
-}
 
 
 =head2 flush_Exons
@@ -1357,11 +1044,6 @@ sub get_all_translateable_Exons {
 }
 
 
-
-# needs selenocystein - U
-# use attributes from Translation
-# maybe API call ?
-
 =head2 translate
 
   Args       : none
@@ -1389,22 +1071,23 @@ sub translate {
     $display_id = scalar($self->translation());
   }
 
+  # remove final stop codon from the mrna if it is present
+  # produced peptides will not have '*' at end
+  # if terminal stop codon is desired call translatable_seq directly 
+  # and produce a translation from it
+
   if( CORE::length( $mrna ) % 3 == 0 ) {
     $mrna =~ s/TAG$|TGA$|TAA$//i;
   }
-  # the above line will remove the final stop codon from the mrna
-  # sequence produced if it is present, this is so any peptide produced
-  # won't have a terminal stop codon
-  # if you want to have a terminal stop codon either comment this line out
-  # or call translatable seq directly and produce a translation from it
 
-  my $peptide = Bio::Seq->new( -seq => $mrna,
-                               -moltype => "dna",
+  my $peptide = Bio::Seq->new( -seq      => $mrna,
+                               -moltype  => "dna",
                                -alphabet => 'dna',
-                               -id => $display_id );
+                               -id       => $display_id );
 
   return $self->translation->modify_translation( $peptide->translate() );
 }
+
 
 =head2 seq
 
@@ -1813,8 +1496,6 @@ sub recalculate_coordinates {
 }
 
 
-
-
 =head2 display_id
 
   Arg [1]    : none
@@ -1834,14 +1515,48 @@ sub display_id {
 }
 
 
+=head2 get_all_peptide_variations
+
+Description: See Bio::EnsEMBL::Utils::TranscriptSNPs::get_all_peptide_variations
+
+=cut
+
+sub get_all_peptide_variations {
+  my $self = shift;
+  my $snps = shift;
+
+  if(!$snps) {
+    my $shash = Bio::EnsEMBL::Utils::TranscriptSNPs::get_all_cdna_SNPs($self);
+    $snps = $shash->{'coding'};
+  }
+
+  return Bio::EnsEMBL::Utils::TranscriptSNPs::get_all_peptide_variations($self,
+                                                                        $snps);
+}
+
+=head2 get_all_SNPs
+
+Description: See Bio::EnsEMBL::Utils::TranscriptSNPs::get_all_SNPs
+
+=cut
+
+sub get_all_SNPs {
+  return Bio::EnsEMBL::Utils::TranscriptSNPs::get_all_SNPs(@_);
+}
+
+=head2 get_all_peptide_variations
+
+Description: See Bio::EnsEMBL::Utils::TranscriptSNPs::get_all_cdna_SNPs
+
+=cut
+
+sub get_all_cdna_SNPs {
+  return Bio::EnsEMBL::Utils::TranscriptSNPs::get_all_cdna_SNPs(@_);
+}
+
 ###########################
 # DEPRECATED METHODS FOLLOW
 ###########################
-
-# _translation_id
-# Usage   : DEPRECATED - not needed anymore
-
-
 
 =head2 sort
 
@@ -1874,6 +1589,9 @@ sub sort {
   }
 }
 
+
+# _translation_id
+# Usage   : DEPRECATED - not needed anymore
 
 sub _translation_id {
    my $self = shift;
