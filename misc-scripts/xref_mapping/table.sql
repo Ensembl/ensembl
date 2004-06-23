e5829a6c0458fd54f24146782108dde44375519e
Glenn Proctor <gp1@sanger.ac.uk>
2004-06-23 14:59:35 +0000

    Removed checksum from source table

--- misc-scripts/xref_mapping/table.sql
+++ misc-scripts/xref_mapping/table.sql
@@ -64,7 +64,6 @@ CREATE TABLE source (
   source_id                   int unsigned not null auto_increment,
   name                        varchar(255) not null,
   url                         varchar(255),
-  checksum                    varchar(255),
   file_modified_date          timestamp(14) not null,
   upload_date                 timestamp(14) not null,
   release                     varchar(255),
