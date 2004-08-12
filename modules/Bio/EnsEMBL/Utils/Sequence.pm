147268a05929e8d61f5ea1bb4340e0d8425149f8
Daniel Rios <dr2@sanger.ac.uk>
2004-08-12 13:04:37 +0000

    Added expand function to expand sequences in the (AC)5 format

--- modules/Bio/EnsEMBL/Utils/Sequence.pm
+++ modules/Bio/EnsEMBL/Utils/Sequence.pm
@@ -8,7 +8,7 @@ Bio::EnsEMBL::Utils::Sequence - Utility functions for sequences
 
 =head1 SYNOPSIS
 
-  use Bio::EnsEMBL::Utils::Sequence qw(reverse_comp);
+  use Bio::EnsEMBL::Utils::Sequence qw(reverse_comp expand);
 
   my $seq = 'ACTTTAAAGGCTATCCCAATATG';
 
@@ -17,6 +17,10 @@ Bio::EnsEMBL::Utils::Sequence - Utility functions for sequences
   reverse_comp(\$seq);
 
   print "my reverse comp = $seq\n";
+
+  my $compressed_seq = '(AC)3';
+
+  print "my expanded seq is = expand($compressed_seq)";
 
 
 =head1 METHODS
@@ -35,7 +39,7 @@ use vars qw(@ISA @EXPORT_OK);
 
 @ISA = qw(Exporter);
 
-@EXPORT_OK = qw(&reverse_comp);
+@EXPORT_OK = qw(&reverse_comp &expand);
 
 
 =head2 reverse_comp
@@ -63,6 +67,30 @@ sub reverse_comp {
   $$seqref =~
     tr/acgtrymkswhbvdnxACGTRYMKSWHBVDNX/tgcayrkmswdvbhnxTGCAYRKMSWDVBHNX/;
 
+  return;
+}
+
+=head2 expand
+
+  Arg [1]    : reference to a string $seqref
+  Example    : use Bio::EnsEMBL::Utils::Sequence qw(expand);
+
+               $seq = '(AC)3';
+               expand(\$seq);
+               print $seq;
+              
+
+  Description: Expands a genomic sequence. The string is passed by reference
+               rather than by value for memory efficiency.
+  Returntype : none
+  Exceptions : none
+  Caller     : SequenceAdaptor, SliceAdaptor
+
+=cut
+
+sub expand {
+  my $seq_ref = shift;
+     $$seq_ref =~ s/(\w*)\((\w+)\)(\d+)/$1.$2 x $3/eg  if ($$seq_ref =~ /\(/);#expressions with parenthesis, expand the alleles			    
   return;
 }
 
