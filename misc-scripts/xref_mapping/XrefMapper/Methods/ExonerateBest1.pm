b07132cfb0fe47f6623c08efb3f120388199c0af
Glenn Proctor <gp1@sanger.ac.uk>
2004-10-06 14:58:51 +0000

    Simple example of how to write a method class

---
+++ misc-scripts/xref_mapping/XrefMapper/Methods/ExonerateBest1.pm
@@ -0,0 +1,19 @@
+package XrefMapper::Methods::ExonerateBest1;
+
+use XrefMapper::Methods::ExonerateBasic;
+
+use vars '@ISA';
+
+@ISA = qw{XrefMapper::Methods::ExonerateBasic};
+
+
+
+sub options {
+
+  return ('--bestn', '1');
+
+}
+
+
+
+1;
