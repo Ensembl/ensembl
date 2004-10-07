a2d9fe77de4dc8f342a0f5c67a742a24ce40630b
Glenn Proctor <gp1@sanger.ac.uk>
2004-10-07 15:44:07 +0000

    Dump a few more xrefs.

--- misc-scripts/xref_mapping/xref_dumper.pl
+++ misc-scripts/xref_mapping/xref_dumper.pl
@@ -18,7 +18,7 @@ my $dbi = DBI->connect("dbi:mysql:host=$host;port=$port;database=$database",
 
 open(FILE, ">" . $file);
 
-my $sth = $dbi->prepare("SELECT x.xref_id, px.sequence FROM primary_xref px, source so, xref x, species sp WHERE sp.name='homo_sapiens' AND so.name='RefSeq' AND x.species_id=sp.species_id AND px.source_id=so.source_id AND x.xref_id=px.xref_id AND px.sequence_type='dna' LIMIT 100");
+my $sth = $dbi->prepare("SELECT x.xref_id, px.sequence FROM primary_xref px, source so, xref x, species sp WHERE sp.name='homo_sapiens' AND so.name='RefSeq' AND x.species_id=sp.species_id AND px.source_id=so.source_id AND x.xref_id=px.xref_id AND px.sequence_type='dna' LIMIT 500");
 $sth->execute();
 
 my ($xref_id, $sequence);
