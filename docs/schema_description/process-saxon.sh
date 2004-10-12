838ab37f92e5a60ab7ae930e6b37eb2645ddf35c
Glenn Proctor <gp1@sanger.ac.uk>
2004-10-12 12:49:23 +0000

    Updated to work with latest version of Saxon.

--- docs/schema_description/process-saxon.sh
+++ docs/schema_description/process-saxon.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-java -jar "C:/Program Files/saxon/saxon7.jar" tables.xml xml2html.xsl > schema_description.html 
+java -jar "/nfs/acari/gp1/saxon/saxon8.jar" tables.xml xml2html.xsl > schema_description.html 
 
-java -jar "C:/Program Files/saxon/saxon7.jar" tables.xml xml2wiki.xsl > schema_description.txt
+java -jar "/nfs/acari/gp1/saxon/saxon8.jar" tables.xml xml2wiki.xsl > schema_description.txt
 
 cp schema_description.html ../../../private-web/htdocs/Docs/
