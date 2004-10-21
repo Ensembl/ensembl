3f04a01096bccb8d1e2c77ec74c73a16dc8956cc
Fiona Cunningham <fiona@ebi.ac.uk>
2004-10-21 12:35:55 +0000

    *** empty log message ***

--- modules/Bio/EnsEMBL/DBSQL/DBAdaptor.pm
+++ modules/Bio/EnsEMBL/DBSQL/DBAdaptor.pm
@@ -262,47 +262,46 @@ sub get_db_adaptor {
 
 sub get_available_adaptors{
 
-    my %pairs =  ( 
-	'AffyFeature'          => 'Bio::EnsEMBL::DBSQL::AffyFeatureAdaptor',
-	'AffyProbe'            => 'Bio::EnsEMBL::DBSQL::AffyProbeAdaptor',
-	'AffyArray'            => 'Bio::EnsEMBL::DBSQL::AffyArrayAdaptor',
-	'Analysis'             => 'Bio::EnsEMBL::DBSQL::AnalysisAdaptor',
-	'ArchiveStableId'      => 'Bio::EnsEMBL::DBSQL::ArchiveStableIdAdaptor',
-	'Attribute'            => 'Bio::EnsEMBL::DBSQL::AttributeAdaptor',
-	'AssemblyExceptionFeature' => 'Bio::EnsEMBL::DBSQL::AssemblyExceptionFeatureAdaptor',
-	'AssemblyMapper'       => 'Bio::EnsEMBL::DBSQL::AssemblyMapperAdaptor',
-	'Blast'                => 'Bio::EnsEMBL::External::BlastAdaptor',
-	'MetaContainer'        => 'Bio::EnsEMBL::DBSQL::MetaContainer',
-	'CoordSystem'          => 'Bio::EnsEMBL::DBSQL::CoordSystemAdaptor',
-	'CompressedSequence'   => 'Bio::EnsEMBL::DBSQL::CompressedSequenceAdaptor',
-	'DBEntry'              => 'Bio::EnsEMBL::DBSQL::DBEntryAdaptor',
-	'DnaAlignFeature'      => 'Bio::EnsEMBL::DBSQL::DnaAlignFeatureAdaptor',
-	'DensityFeature'       => 'Bio::EnsEMBL::DBSQL::DensityFeatureAdaptor',
-	'DensityType'          => 'Bio::EnsEMBL::DBSQL::DensityTypeAdaptor',
-	'Exon'                 => 'Bio::EnsEMBL::DBSQL::ExonAdaptor',
-	'Gene'                 => 'Bio::EnsEMBL::DBSQL::GeneAdaptor',
-	'KaryotypeBand'        => 'Bio::EnsEMBL::DBSQL::KaryotypeBandAdaptor',
-	'Marker'               => 'Bio::EnsEMBL::Map::DBSQL::MarkerAdaptor',
-	'MarkerFeature'        =>
-	'Bio::EnsEMBL::Map::DBSQL::MarkerFeatureAdaptor',
-	'MetaCoordContainer'   => 'Bio::EnsEMBL::DBSQL::MetaCoordContainer',
-	'MiscSet'              => 'Bio::EnsEMBL::DBSQL::MiscSetAdaptor',
-	'MiscFeature'          => 'Bio::EnsEMBL::DBSQL::MiscFeatureAdaptor',
-	'PredictionTranscript' => 'Bio::EnsEMBL::DBSQL::PredictionTranscriptAdaptor',
-	'PredictionExon'       => 'Bio::EnsEMBL::DBSQL::PredictionExonAdaptor',
-	'ProteinFeature'       => 'Bio::EnsEMBL::DBSQL::ProteinFeatureAdaptor',
-	'ProteinAlignFeature'  => 'Bio::EnsEMBL::DBSQL::ProteinAlignFeatureAdaptor',
-	'SNP'                  => 'Bio::EnsEMBL::DBSQL::ProxySNPAdaptor',
-	'QtlFeature'           => 'Bio::EnsEMBL::Map::DBSQL::QtlFeatureAdaptor',
-	'Qtl'                  => 'Bio::EnsEMBL::Map::DBSQL::QtlAdaptor',
-	'RepeatConsensus'      => 'Bio::EnsEMBL::DBSQL::RepeatConsensusAdaptor',
-	'RepeatFeature'        => 'Bio::EnsEMBL::DBSQL::RepeatFeatureAdaptor',
-	'Sequence'             => 'Bio::EnsEMBL::DBSQL::SequenceAdaptor',
-	'SimpleFeature'        => 'Bio::EnsEMBL::DBSQL::SimpleFeatureAdaptor',
-	'Slice'                => 'Bio::EnsEMBL::DBSQL::SliceAdaptor',
-	'SupportingFeature'    => 'Bio::EnsEMBL::DBSQL::SupportingFeatureAdaptor',
-	'Transcript'           => 'Bio::EnsEMBL::DBSQL::TranscriptAdaptor',
-	'Translation'          => 'Bio::EnsEMBL::DBSQL::TranslationAdaptor');
+  my %pairs =  ( 'Analysis'             => 'Bio::EnsEMBL::DBSQL::AnalysisAdaptor',
+		 'ArchiveStableId'      => 'Bio::EnsEMBL::DBSQL::ArchiveStableIdAdaptor',
+		 'Attribute'            => 'Bio::EnsEMBL::DBSQL::AttributeAdaptor',
+		 'AssemblyExceptionFeature' => 'Bio::EnsEMBL::DBSQL::AssemblyExceptionFeatureAdaptor',
+		 'AssemblyMapper'       => 'Bio::EnsEMBL::DBSQL::AssemblyMapperAdaptor',
+		 'Blast'                => 'Bio::EnsEMBL::External::BlastAdaptor',
+		 'MetaContainer'        => 'Bio::EnsEMBL::DBSQL::MetaContainer',
+		 'CoordSystem'   => 'Bio::EnsEMBL::DBSQL::CoordSystemAdaptor',
+		 'CompressedSequence' => 'Bio::EnsEMBL::DBSQL::CompressedSequenceAdaptor',
+		 'DBEntry'              => 'Bio::EnsEMBL::DBSQL::DBEntryAdaptor',
+		 'DnaAlignFeature'      => 'Bio::EnsEMBL::DBSQL::DnaAlignFeatureAdaptor',
+		 'DensityFeature'       => 'Bio::EnsEMBL::DBSQL::DensityFeatureAdaptor',
+		 'DensityType'          => 'Bio::EnsEMBL::DBSQL::DensityTypeAdaptor',
+		 'Exon'                 => 'Bio::EnsEMBL::DBSQL::ExonAdaptor',
+		 'Gene'                 => 'Bio::EnsEMBL::DBSQL::GeneAdaptor',
+		 'KaryotypeBand'        => 'Bio::EnsEMBL::DBSQL::KaryotypeBandAdaptor',
+		 'Marker'               => 'Bio::EnsEMBL::Map::DBSQL::MarkerAdaptor',
+		 'MarkerFeature'        =>
+		 'Bio::EnsEMBL::Map::DBSQL::MarkerFeatureAdaptor',
+		 'MetaCoordContainer'   => 'Bio::EnsEMBL::DBSQL::MetaCoordContainer',
+		 'MiscSet'              => 'Bio::EnsEMBL::DBSQL::MiscSetAdaptor',
+		 'MiscFeature'          => 'Bio::EnsEMBL::DBSQL::MiscFeatureAdaptor',
+		 'PredictionTranscript' => 'Bio::EnsEMBL::DBSQL::PredictionTranscriptAdaptor',
+		 'PredictionExon'       => 'Bio::EnsEMBL::DBSQL::PredictionExonAdaptor',
+		 'ProteinFeature'       => 'Bio::EnsEMBL::DBSQL::ProteinFeatureAdaptor',
+		 'ProteinAlignFeature'  =>
+		 'Bio::EnsEMBL::DBSQL::ProteinAlignFeatureAdaptor',
+		 'SNP'                  => 'Bio::EnsEMBL::DBSQL::ProxySNPAdaptor',
+		 'QtlFeature'           => 'Bio::EnsEMBL::Map::DBSQL::QtlFeatureAdaptor',
+		 'Qtl'                  => 'Bio::EnsEMBL::Map::DBSQL::QtlAdaptor',
+		 'RepeatConsensus'      => 'Bio::EnsEMBL::DBSQL::RepeatConsensusAdaptor',
+		 'RepeatFeature'        => 'Bio::EnsEMBL::DBSQL::RepeatFeatureAdaptor',
+		 'Sequence'             => 'Bio::EnsEMBL::DBSQL::SequenceAdaptor',
+		 'SimpleFeature'        => 'Bio::EnsEMBL::DBSQL::SimpleFeatureAdaptor',
+		 'Slice'                => 'Bio::EnsEMBL::DBSQL::SliceAdaptor',
+		 'SupportingFeature'    => 'Bio::EnsEMBL::DBSQL::SupportingFeatureAdaptor',
+		 'Transcript'           => 'Bio::EnsEMBL::DBSQL::TranscriptAdaptor',
+		 'Translation'          => 'Bio::EnsEMBL::DBSQL::TranslationAdaptor',
+);
+
 
   return (\%pairs);
 }
@@ -718,6 +717,7 @@ sub AUTOLOAD {
     return $ret;
   }
   else{
+    throw( "Couldnt get adaptor $type for ".$self->species." ".$self->group."\n");
     warning("Could not find $type adaptor in the registry for ".$self->species." ".$self->group."\n");
     return $ret;
   }
@@ -787,7 +787,7 @@ sub dbname{
 
  deprecate("dbname Should no longer be called from the DBAdaptor. DBConnection should now be used OR preferably the object adaptor itself\n");
   $self->dbc->dbname(@args);
-}
+} 
 
 sub disconnect_when_inactive{
   my ($self, @args) = @_;
