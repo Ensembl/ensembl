eb80bcbc93d197cebf14ec94b08162ed570992ca
Glenn Proctor <gp1@sanger.ac.uk>
2004-11-03 11:01:17 +0000

    Control script for running parsers; contains stuff that used to be in BaseParser.pm

---
+++ misc-scripts/xref_mapping/xref_parser.pl
@@ -0,0 +1,42 @@
+use strict;
+
+use Getopt::Long;
+use BaseParser;
+
+my ($host, $port, $dbname, $user, $pass, @species, @sources);
+
+GetOptions('user=s'    => \$user,
+	   'pass=s'    => \$pass,
+	   'host=s'    => \$host,
+	   'port=i'    => \$port,
+	   'dbname=s'  => \$dbname,
+	   'species=s' => \@species,
+	   'source=s'  => \@sources,
+	   'help'     => sub { usage(); exit(0); });
+
+@species = split(/,/,join(',',@species));
+@sources  = split(/,/,join(',',@sources));
+
+if (!$user || !$host || !$dbname) {
+
+  usage();
+  exit(1);
+
+
+}
+
+BaseParser::run($host, $port, $dbname, $user, $pass, \@species, \@sources);
+
+# --------------------------------------------------------------------------------
+
+sub usage {
+
+  print << "EOF";
+
+  BaseParser.pm -user {user} -pass {password} -host {host} -port {port} -dbname {database} -species {species1,species2} -source {source1,source2}
+
+EOF
+
+}
+
+# --------------------------------------------------------------------------------
