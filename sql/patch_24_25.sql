b8f23535014cc81c50dac678a89150c4ff621b87
Arne Stabenau <stabenau@sanger.ac.uk>
2004-11-25 10:14:34 +0000

    mor eunified better date creation for the created/modified dates of the stable ids

--- sql/patch_24_25.sql
+++ sql/patch_24_25.sql
@@ -1,22 +1,24 @@
 # Add and populate created_date and modified_date to stable_id tables
 # Schema 24-25
 
+set @today = concat( curdate(), " 12:00:00" );
+
 ALTER TABLE gene_stable_id ADD COLUMN created_date DATETIME NOT NULL;
 ALTER TABLE gene_stable_id ADD COLUMN modified_date DATETIME NOT NULL;
-UPDATE gene_stable_id SET created_date='2004-09-20 00:00:00';
-UPDATE gene_stable_id SET modified_date='2004-09-20 00:00:00';
+UPDATE gene_stable_id SET created_date=@today;
+UPDATE gene_stable_id SET modified_date=@today;
 
 ALTER TABLE exon_stable_id ADD COLUMN created_date DATETIME NOT NULL;
 ALTER TABLE exon_stable_id ADD COLUMN modified_date DATETIME NOT NULL;
-UPDATE exon_stable_id SET created_date='2004-09-20 00:00:00';
-UPDATE exon_stable_id SET modified_date='2004-09-20 00:00:00';
+UPDATE exon_stable_id SET created_date=@today;
+UPDATE exon_stable_id SET modified_date=@today;
 
 ALTER TABLE transcript_stable_id ADD COLUMN created_date DATETIME NOT NULL;
 ALTER TABLE transcript_stable_id ADD COLUMN modified_date DATETIME NOT NULL;
-UPDATE transcript_stable_id SET created_date='2004-09-20 00:00:00';
-UPDATE transcript_stable_id SET modified_date='2004-09-20 00:00:00';
+UPDATE transcript_stable_id SET created_date=@today;
+UPDATE transcript_stable_id SET modified_date=@today;
 
 ALTER TABLE translation_stable_id ADD COLUMN created_date DATETIME NOT NULL;
 ALTER TABLE translation_stable_id ADD COLUMN modified_date DATETIME NOT NULL;
-UPDATE translation_stable_id SET created_date='2004-09-20 00:00:00';
-UPDATE translation_stable_id SET modified_date='2004-09-20 00:00:00';
+UPDATE translation_stable_id SET created_date=@today;
+UPDATE translation_stable_id SET modified_date=@today;
