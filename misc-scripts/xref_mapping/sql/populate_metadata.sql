e952912d0f2bc5cd4e4df64c9db9fd0c141952b4
Glenn Proctor <gp1@sanger.ac.uk>
2004-12-06 09:13:44 +0000

    Added monodelphis domestica and xenopus tropicalis.

--- misc-scripts/xref_mapping/sql/populate_metadata.sql
+++ misc-scripts/xref_mapping/sql/populate_metadata.sql
@@ -18,6 +18,8 @@ INSERT INTO species (species_id, taxonomy_id, name, aliases) VALUES (9031, 9031,
 INSERT INTO species (species_id, taxonomy_id, name, aliases) VALUES (99883, 99883,'tetraodon_nigroviridis',  'tetraodon,tnigroviridis,tetraodonnigroviridis');
 INSERT INTO species (species_id, taxonomy_id, name, aliases) VALUES (9913, 9913,  'bos_taurus',             'cow,btaurus,bostaurus');
 INSERT INTO species (species_id, taxonomy_id, name, aliases) VALUES (9615, 9615,  'canis_familaris',        'dog,doggy,cfamiliaris,canisfamiliaris');
+INSERT INTO species (species_id, taxonomy_id, name, aliases) VALUES (8364, 8364,  'xenopus_tropicalis',        'pipid,pipidfrog,xenopus,xtropicalis,xenopustropicalis');
+INSERT INTO species (species_id, taxonomy_id, name, aliases) VALUES (13616, 13616,  'monodelphis_domestica',        'opossum,monodelphis,mdomestica,monodelphisdomestica');
 
 ################################################################################
 # SOURCES - types of data we can read
