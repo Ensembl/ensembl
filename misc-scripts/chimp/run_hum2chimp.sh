19497b97d95c579c8206e4deb054ad26934b6e19
Arne Stabenau <stabenau@sanger.ac.uk>
2004-12-08 10:29:54 +0000

    new run parameters. shouldnt really be in cvs I guess

--- misc-scripts/chimp/run_hum2chimp.sh
+++ misc-scripts/chimp/run_hum2chimp.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 /usr/local/ensembl/bin/perl human2chimp.pl  \
--hdbname mcvicker_human_genes -hhost ecs4 -huser ensro -hport 3350 \
--cdbname mcvicker_chimp_agp -chost ecs4 -cport 3350 -cuser ensro \
--hassembly NCBI34 -cassembly BROAD1 -logfile chimp.test.log \
--store -duser ensadmin -dpass ensembl -dhost ecs4 -dport 3350 -ddbname mcvicker_chimp_test
+-hdbname homo_sapiens_core_26_35 -hchromosome 1 -hhost ecs2 -huser ensro -hport 3364 \
+-cdbname pan_troglodytes_core_26_1 -chost ecs2 -cport 3364 -cuser ensro \
+-hassembly NCBI35 -cassembly CHIMP1 -logfile chimp.test.log \
+-store -duser ensadmin -dpass ensembl -dhost ecs2 -dport 3364 -ddbname pan_troglodytes_core_26_1
 
