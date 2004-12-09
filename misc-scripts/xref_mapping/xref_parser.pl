137de37c65556ff5040b5448900995795b436fc1
Glenn Proctor <gp1@sanger.ac.uk>
2004-12-09 15:49:35 +0000

    Added -setrelease parameter.
    Improved usage docs.

--- misc-scripts/xref_mapping/xref_parser.pl
+++ misc-scripts/xref_mapping/xref_parser.pl
@@ -3,7 +3,7 @@ use strict;
 use Getopt::Long;
 use XrefParser::BaseParser;
 
-my ($host, $port, $dbname, $user, $pass, @species, @sources, $skipdownload, $create);
+my ($host, $port, $dbname, $user, $pass, @species, @sources, $skipdownload, $create, $release);
 
 GetOptions('user=s'       => \$user,
 	   'pass=s'       => \$pass,
@@ -14,6 +14,7 @@ GetOptions('user=s'       => \$user,
 	   'source=s'     => \@sources,
 	   'skipdownload' => \$skipdownload,
 	   'create'       => \$create,
+	   'setrelease=s' => \$release,
 	   'help'         => sub { usage(); exit(0); });
 
 @species = split(/,/,join(',',@species));
@@ -27,16 +28,43 @@ if (!$user || !$host || !$dbname) {
 
 }
 
-XrefParser::BaseParser::run($host, $port, $dbname, $user, $pass, \@species, \@sources, $skipdownload, $create);
+XrefParser::BaseParser::run($host, $port, $dbname, $user, $pass, \@species, \@sources, $skipdownload, $create, $release);
 
 # --------------------------------------------------------------------------------
 
-# TODO - better usage instructions
 sub usage {
 
   print << "EOF";
 
-  xref_parser.pl -user {user} -pass {password} -host {host} -port {port} -dbname {database} -species {species1,species2} -source {source1,source2} -skipdownload -create
+  xref_parser.pl -user {user} -pass {password} -host {host} -port {port} -dbname {database} -species {species1,species2} -source {source1,source2} -skipdownload -create -setrelease
+
+  -user         User name to access database. Must allow writing.
+
+  -pass         Password for user.
+
+  -host         Database host.
+
+  -port         Database port.
+
+  -dbname       Name of xref database to use/create.
+
+  -species      Which species to import. Multiple -species arguments and/or comma,
+                separated lists of species are allowed. Species may be referred to
+                by genus/species (e.g. homo_sapiens) or common aliases (e.g. human).
+                Specifying an unknown species will cause a list of valid species to
+                be printed.
+
+  -source       Which sources to import. Multiple -source arguments and/or comma,
+                separated lists of sources are allowed.
+                Specifying an unknown source will cause a list of valid sources to
+                be printed.
+
+  -create       If specified, cause dbname to be deleted and re-created if it
+                already exists.
+
+  -skipdownload Don't download new data, parse existing.
+
+  -setrelease   Set the release version for ALL the sources specified.
 
 EOF
 
