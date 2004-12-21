e9a447c3d14da9da66b51da046446625c8280f83
Glenn Proctor <gp1@sanger.ac.uk>
2004-12-21 13:40:44 +0000

    Added species-specific get_set_lists

--- misc-scripts/xref_mapping/XrefMapper/homo_sapiens.pm
+++ misc-scripts/xref_mapping/XrefMapper/homo_sapiens.pm
@@ -1,11 +1,15 @@
 package XrefMapper::homo_sapiens;
- 
+
 use  XrefMapper::BasicMapper;
 
 use vars '@ISA';
- 
-@ISA = qw{ XrefMapper::BasicMapper };
- 
-1;
 
- 
+@ISA = qw{ XrefMapper::BasicMapper };
+
+sub get_set_lists{
+
+  return [["ExonerateGappedBest1", ["homo_sapiens","*"]]];
+
+}
+
+1;
