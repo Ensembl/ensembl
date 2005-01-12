acaf4d33015b9b6bb6442116c04a3ec738a8aa82
Ian Longden <ianl@sanger.ac.uk>
2005-01-12 11:55:41 +0000

    fix to make sure species is checked

--- misc-scripts/xref_mapping/XrefParser/RefSeqGPFFParser.pm
+++ misc-scripts/xref_mapping/XrefParser/RefSeqGPFFParser.pm
@@ -101,7 +101,7 @@ sub create_xrefs {
     my $species_id_check = $name2species_id{$species};
 
     # skip xrefs for species that aren't in the species table
-    if (defined ($species_id) and $species_id = $species_id_check) {
+    if (defined ($species_id) and $species_id == $species_id_check) {
 
       my ($acc) = $entry =~ /ACCESSION\s+(\S+)/;
       my ($ver) = $entry =~ /VERSION\s+(\S+)/;
