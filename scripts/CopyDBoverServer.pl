d577d8eacb0458af1f71c2ff4b69a7bacfef5595
Glenn Proctor <gp1@sanger.ac.uk>
2005-01-17 11:49:13 +0000

    Allow trailing whitespace in input file.

--- scripts/CopyDBoverServer.pl
+++ scripts/CopyDBoverServer.pl
@@ -93,7 +93,7 @@ open F, $input_file ||
 
 while (my $line = <F>) {
   next if ($line =~ /^\#.*$/);
-  if ($line =~ /^(\S+)\s+(\d+)\s+(\S+)\s+(\S+)\s+(\d+)\s+(\S+)$/) {
+  if ($line =~ /^(\S+)\s+(\d+)\s+(\S+)\s+(\S+)\s+(\d+)\s+(\S+)\s*$/) {
     my ($src_srv,$src_port,$src_db,$dest_srv,$dest_port,$dest_db) = ($1,$2,$3,$4,$5,$6);
     unless ($dest_srv =~ /^$generic_working_host.*$/) {
       my $generic_destination_server = $dest_srv;
