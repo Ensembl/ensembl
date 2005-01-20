d4744073c2ea0404239814531a66d416b903dea5
Glenn Proctor <gp1@sanger.ac.uk>
2005-01-20 10:08:33 +0000

    Updated. Final (?) version.

--- sql/patch_27_28.sql
+++ sql/patch_27_28.sql
@@ -5,9 +5,7 @@ UPDATE external_db SET db_name='RefSeq_dna' WHERE db_name='RefSeq';
 INSERT INTO external_db VALUES (1810, 'RefSeq_peptide', 1, 'KNOWN');
 
 # Modify column definition of xref.dbprimary_acc to remove binary property
-# and make display_label binary
 ALTER TABLE xref MODIFY dbprimary_acc VARCHAR(40) NOT NULL;
-ALTER TABLE xref MODIFY display_label VARCHAR(40) binary NOT NULL;
 
 # Add analysis_description table
 CREATE TABLE analysis_description (
