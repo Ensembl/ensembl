592cd2d64e4b9fe361ba7cac9e288de29b7e3225
Glenn Proctor <gp1@sanger.ac.uk>
2005-02-08 13:35:11 +0000

    Added automatic CVS commit.

--- docs/schema_description/process-saxon.sh
+++ docs/schema_description/process-saxon.sh
@@ -5,3 +5,7 @@ java -jar "/nfs/acari/gp1/saxon/saxon8.jar" tables.xml xml2html.xsl > schema_des
 java -jar "/nfs/acari/gp1/saxon/saxon8.jar" tables.xml xml2wiki.xsl > schema_description.txt
 
 cp schema_description.html ../../../private-web/htdocs/Docs/
+
+cd ../../../private-web/htdocs/Docs/
+
+cvs commit schema_description.html
