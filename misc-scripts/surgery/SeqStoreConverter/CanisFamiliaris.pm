9784abb67247fa99a2d2017681afe721934d0f18
Steve Trevanion <st3@sanger.ac.uk>
2005-02-18 16:54:30 +0000

    for consistent inheritance structure

---
+++ misc-scripts/surgery/SeqStoreConverter/CanisFamiliaris.pm
@@ -0,0 +1,12 @@
+use strict;
+use warnings;
+
+use SeqStoreConverter::BasicConverter;
+
+package SeqStoreConverter::CanisFamiliaris;
+
+use vars qw(@ISA);
+
+@ISA = qw(SeqStoreConverter::BasicConverter);
+
+1;
