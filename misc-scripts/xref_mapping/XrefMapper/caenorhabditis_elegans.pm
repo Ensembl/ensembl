3ab999392aba6f66e29414519a84c764972bdfb4
Glenn Proctor <gp1@sanger.ac.uk>
2005-04-01 13:32:51 +0000

    Added elegans-specific gene & transcript display_xref setting.

--- misc-scripts/xref_mapping/XrefMapper/caenorhabditis_elegans.pm
+++ misc-scripts/xref_mapping/XrefMapper/caenorhabditis_elegans.pm
@@ -22,4 +22,43 @@ sub gene_description_filter_regexps {
 
 }
 
+# Elegans is imported from WormBase. The gene and transcript stable IDs
+# are the WormBase identifiers. The display_xref_ids for genes and
+# transcripts are calculated directly rather than via the more complex
+# priority-based method in BasicMapper.pm
+
+sub build_display_xrefs {
+
+  my ($self, $type, $external_db) = @_;
+
+  print "Setting $type display_xrefs from $type stable IDs\n";
+  my $dir = $self->dir();
+
+  my $sql = "UPDATE $type t, ${type}_stable_id s, xref x, external_db e SET t.display_xref_id=x.xref_id WHERE t.${type}_id=s.${type}_id AND s.stable_id=x.dbprimary_acc AND e.external_db_id=x.external_db_id AND e.db_name=\'${external_db}\'\n";
+
+  open (SQL, ">$dir/${type}_display_xref.sql");
+
+  print SQL $sql;
+
+  close(SQL);
+
+}
+
+
+sub build_transcript_display_xrefs {
+
+  my ($self) = @_;
+
+  $self->build_display_xrefs("transcript", "wormbase_transcript");
+
+}
+
+sub build_gene_display_xrefs {
+
+  my ($self) = @_;
+
+  $self->build_display_xrefs("gene", "wormbase_gene");
+
+}
+
 1;
