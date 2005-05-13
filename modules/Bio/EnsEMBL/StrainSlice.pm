7c5e6d9617b2a2939e0a8e72e17265e662c536af
Daniel Rios <dr2@sanger.ac.uk>
2005-05-13 13:36:19 +0000

    replaced VariationFeature by AlleleFeature and implemented get_Transcript and get_Exon functions

--- modules/Bio/EnsEMBL/StrainSlice.pm
+++ modules/Bio/EnsEMBL/StrainSlice.pm
@@ -10,7 +10,8 @@
 
 =head1 NAME
 
-Bio::EnsEMBL::StrainSlice - SubClass of the Slice. Represents the slice of the genome for a certain strain (applying the variations)
+Bio::EnsEMBL::StrainSlice - SubClass of the Slice. Represents the slice of the genome 
+for a certain strain (applying the alleles for a this strain)
 
 =head1 SYNOPSIS
 
@@ -30,9 +31,9 @@ Bio::EnsEMBL::StrainSlice - SubClass of the Slice. Represents the slice of the g
    #compare two different strains in the same Slice
    my $sliceStrain2 = $slice->get_by_Strain($strain_name2);
    my $differences = $sliceStrain->get_all_differences_StrainSlice($strain_name2);
-   foreach my $vf (@{$differences}){
-      print "There is a difference between $strain_name and $strain_name2 at ", $vf->start,"-",$vf->end,
-            " with allele ", $vf->allele_string(),"\n";
+   foreach my $af (@{$differences}){
+      print "There is a difference between $strain_name and $strain_name2 at ", $af->start,"-",$af->end,
+            " with allele ", $af->allele_string(),"\n";
    }
 
 =head1 DESCRIPTION
@@ -76,7 +77,7 @@ use Data::Dumper;
                   int    STRAND, (optional, defaults to 1)
                   Bio::EnsEMBL::DBSQL::SliceAdaptor ADAPTOR (optional)
     Arg[N+1]      : string $strain_name
-    Example     : $strainSlice = Bio::EnsEMBL::StrainSlice->new(--coord_system => $cs,
+    Example     : $strainSlice = Bio::EnsEMBL::StrainSlice->new(-coord_system => $cs,
 								-start => 1,
 								-end => 10000,
 								-strand => 1,
@@ -115,10 +116,10 @@ sub new{
 		"retrieve variation information" );
 	return '';
     }
-    #get the VariationFeatures in the Strain
-    my $vf_adaptor = $variation_db->get_VariationFeatureAdaptor;
+    #get the AlleleFeatures in the Strain
+    my $af_adaptor = $variation_db->get_AlleleFeatureAdaptor;
     
-    if( $vf_adaptor ) {
+    if( $af_adaptor ) {
 	#get the Population for the given strain
 	my $pop_adaptor = $variation_db->get_PopulationAdaptor;
 
@@ -127,8 +128,8 @@ sub new{
 	    #check that the population returned is a strain
 	    if ((defined $population) && ($population->is_strain)){
 		#get all the VariationFeatures in the $population and the Slice given
-		my $variation_features = $vf_adaptor->fetch_all_by_Slice_Population($self,$population);
-		$self->{'variationFeatures'} = $variation_features;
+		my $allele_features = $af_adaptor->fetch_all_by_Slice_Population($self,$population);
+		$self->{'alleleFeatures'} = $allele_features;
 		return $self;
 	    }
 	    else{ 
@@ -141,7 +142,7 @@ sub new{
 	    return '';
 	}
     } else {
-	warning("Not possible to retrieve VariationFeatureAdaptor from variation database");
+	warning("Not possible to retrieve AlleleFeatureAdaptor from variation database");
 	return '';
     }
 }
@@ -173,13 +174,13 @@ sub seq {
 
     # sort edits in reverse order to remove complication of
     # adjusting downstream edits
-    my @variation_features_ordered = sort {$b->start() <=> $a->start()} @{$self->{'variationFeatures'}} if (defined $self->{'variationFeatures'});
+    my @allele_features_ordered = sort {$b->start() <=> $a->start()} @{$self->{'alleleFeatures'}} if (defined $self->{'alleleFeatures'});
 
-    foreach my $vf (@variation_features_ordered){
-	$vf->apply_edit($reference_sequence); #change, in the reference sequence, the vf
+    foreach my $af (@allele_features_ordered){
+	$af->apply_edit($reference_sequence); #change, in the reference sequence, the af
     }
 #    return substr(${$reference_sequence},0,1) if ($self->length == 1); 
-    return ${$reference_sequence}; #returns the reference sequence, applying the variationFeatures
+    return ${$reference_sequence}; #returns the reference sequence, applying the alleleFeatures
   }
 
   # no attached sequence, and no db, so just return Ns
@@ -191,7 +192,7 @@ sub seq {
     Args        : nonre
     Example     : my $differences = $strainSlice->get_all_differences_Slice()
     Description : Gets all differences between the StrainSlice object and the Slice is defined
-    ReturnType  : listref of Bio::EnsEMBL::Variation::VariationFeature
+    ReturnType  : listref of Bio::EnsEMBL::Variation::AlleleFeature
     Exceptions  : none
     Caller      : general
 
@@ -200,7 +201,7 @@ sub seq {
 sub get_all_differences_Slice{
     my $self = shift;
 
-    return $self->{'variationFeatures'};
+    return $self->{'alleleFeatures'};
 }
 
 =head2 get_all_differences_StrainSlice
@@ -208,7 +209,7 @@ sub get_all_differences_Slice{
     Arg[1]      : Bio::EnsEMBL::StrainSlice $ss
     Example     : my $differences = $strainSlice->get_all_differences_StrainSlice($strainslice)
     Description : Gets differences between 2 StrainSlice objects
-    ReturnType  : listref of Bio::EnsEMBL::Variation::VariationFeature
+    ReturnType  : listref of Bio::EnsEMBL::Variation::AlleleFeature
     Exceptions  : thrown on bad argument
     Caller      : general
 
@@ -221,43 +222,43 @@ sub get_all_differences_StrainSlice{
     if (!ref($strainSlice) || !$strainSlice->isa('Bio::EnsEMBL::StrainSlice')){
 	throw('Bio::EnsEMBL::StrainSlice arg expected');
     }
-    if ( @{$self->{'variationFeatures'}} == 0 && @{$strainSlice->{'variationFeatures'}} == 0){
+    if ( @{$self->{'alleleFeatures'}} == 0 && @{$strainSlice->{'alleleFeatures'}} == 0){
 	return undef; #there are no differences in any of the Strains
 	
     }
     my $differences; #differences between strains
-    if (@{$strainSlice->{'variationFeatures'}} == 0){
-	#need to create a copy of VariationFeature for the first Strain
-	foreach my $difference (@{$self->{'variationFeatures'}}){
+    if (@{$strainSlice->{'alleleFeatures'}} == 0){
+	#need to create a copy of alleleFeature for the first Strain
+	foreach my $difference (@{$self->{'alleleFeatures'}}){
 	    my %vf = %$difference;
 	    push @{$differences},bless \%vf,ref($difference);
 	}
     }
-    elsif (@{$self->{'variationFeatures'}} == 0){
-	#need to create a copy of VariationFeature, but changing the allele by the allele in the reference sequence
-	foreach my $difference (@{$strainSlice->{'variationFeatures'}}){
+    elsif (@{$self->{'alleleFeatures'}} == 0){
+	#need to create a copy of AlleleFeature, but changing the allele by the allele in the reference sequence
+	foreach my $difference (@{$strainSlice->{'alleleFeatures'}}){
 	    push @{$differences}, $strainSlice->_convert_difference($difference);
 	}
     }
     else{
 	#both strains have differences
 	#create a hash with the differences in the first slice
-	my %variation_features_self = map {$_->start => $_} @{$self->{'variationFeatures'}};
-	foreach my $difference (@{$strainSlice->{'variationFeatures'}}){
+	my %allele_features_self = map {$_->start => $_} @{$self->{'alleleFeatures'}};
+	foreach my $difference (@{$strainSlice->{'alleleFeatures'}}){
 	    #there is no difference in the other strain slice, convert the allele
-	    if (!defined $variation_features_self{$difference->start}){
+	    if (!defined $allele_features_self{$difference->start}){
 		push @{$differences},$strainSlice->_convert_difference($difference);
 	    }		
 	    else{
 		#if it is defined and have the same allele, delete from the hash since it is not a difference
 		#between the strains
-		if ($variation_features_self{$difference->start}->allele_string eq $difference->allele_string){
-		    delete $variation_features_self{$difference->start};
+		if ($allele_features_self{$difference->start}->allele_string eq $difference->allele_string){
+		    delete $allele_features_self{$difference->start};
 		}
 	    }
 	}	
 	#and finally, make a shallow copy of the differences in the first strain
-	foreach my $difference (values %variation_features_self){
+	foreach my $difference (values %allele_features_self){
 	    my %vf = %$difference;
 	    push @{$differences},bless \%vf,ref($difference);
 	}
@@ -295,15 +296,15 @@ sub get_all_differences_StrainSlice{
     return $differences;
 }
 
-#for a given VariationFeature, converts the allele into the reference allele and returns
-#the converted VariationFeature
+#for a given AlleleFeature, converts the allele into the reference allele and returns
+#the converted AlleleFeature
 sub _convert_difference{
     my $self = shift;
     my $difference = shift;
-    my %new_vf = %$difference; #make a copy of the variationFeature
+    my %new_af = %$difference; #make a copy of the alleleFeature
     #and change the allele with the one from the reference Slice
-    $new_vf{'allele_string'} = $self->SUPER::subseq($difference->start,$difference->end,$difference->strand);
-    return bless \%new_vf,ref($difference);
+    $new_af{'allele_string'} = $self->SUPER::subseq($difference->start,$difference->end,$difference->strand);
+    return bless \%new_af,ref($difference);
 }
 
 =head2 sub_Slice
@@ -346,19 +347,19 @@ sub sub_Slice {
   my $subSlice = $self->SUPER::sub_Slice($new_start,$new_end,$new_strand);
   $subSlice->{'strain_name'} = $self->{'strain_name'};
 
-  my $new_variations; #reference to an array that will contain the variationFeatures in the new subSlice
+  my $new_alleles; #reference to an array that will contain the variationFeatures in the new subSlice
   #update the VariationFeatures in the sub_Slice of the Strain
-   my @results;
-  my %vf;
-  my $new_variation_feature;
-  foreach my $variationFeature (@{$self->{'variationFeatures'}}){      
-      $new_variation_feature = $variationFeature->transfer($subSlice);
+  my @results;
+  my %af;
+  my $new_allele_feature;
+  foreach my $alleleFeature (@{$self->{'alleleFeatures'}}){      
+      $new_allele_feature = $alleleFeature->transfer($subSlice);
       #only transfer the coordinates to the SubSlice that are within the boundaries
-      if ($new_variation_feature->start >= 1 && $new_variation_feature->end <= $subSlice->length){
-	  push @{$new_variations}, $new_variation_feature;
+      if ($new_allele_feature->start >= 1 && $new_allele_feature->end <= $subSlice->length){
+	  push @{$new_alleles}, $new_allele_feature;
       }
   }
-  $subSlice->{'variationFeatures'} = $new_variations;
+  $subSlice->{'alleleFeatures'} = $new_alleles;
   return $subSlice;
 
 }
@@ -443,7 +444,7 @@ sub mapper{
 	my $mapper = Bio::EnsEMBL::Mapper->new('Slice','StrainSlice');
 	#align with Slice
 	#get all the VariationFeatures in the strain Slice, from start to end in the Slice
-	my @variation_features_ordered = sort {$a->start() <=> $b->start()} @{$self->{'variationFeatures'}} if (defined $self->{'variationFeatures'});
+	my @allele_features_ordered = sort {$a->start() <=> $b->start()} @{$self->{'alleleFeatures'}} if (defined $self->{'alleleFeatures'});
 	
 	my $start_slice = 1;
 	my $end_slice;
@@ -451,27 +452,27 @@ sub mapper{
 	my $end_strain;
 	my $length_allele;
 	#we will walk from left to right in the slice object, updating the start and end strain every time
-	#there is a new variationFeature in the Strain
-	foreach my $variation_feature (@variation_features_ordered){
+	#there is a new alleleFeature in the Strain
+	foreach my $allele_feature (@allele_features_ordered){
 	    #we have a insertion/deletion: marks the beginning of new slice move coordinates	    
-	    if ($variation_feature->length_diff != 0){
-		$length_allele = $variation_feature->length + $variation_feature->length_diff(); #length of the allele in the Strain		    
-		$end_slice = $variation_feature->start() - 1; #set the end of the slice before the variationFeature
+	    if ($allele_feature->length_diff != 0){
+		$length_allele = $allele_feature->length + $allele_feature->length_diff(); #length of the allele in the Strain		    
+		$end_slice = $allele_feature->start() - 1; #set the end of the slice before the alleleFeature
 		if ($end_slice >= $start_slice){
 		    #normal cases (not with gaps)
 		    $end_strain = $end_slice - $start_slice + $start_strain; #set the end of the strain from the beginning plus the offset
 		    #add the sequence that maps
 		    $mapper->add_map_coordinates('Slice',$start_slice,$end_slice,1,'StrainSlice',$start_strain,$end_strain);
 		    #and add the indel
-		    $mapper->add_indel_coordinates('Slice',$end_slice+1,$end_slice + $variation_feature->length,1,'StrainSlice',$end_strain+1,$end_strain + $length_allele);
+		    $mapper->add_indel_coordinates('Slice',$end_slice+1,$end_slice + $allele_feature->length,1,'StrainSlice',$end_strain+1,$end_strain + $length_allele);
 		    $start_strain = $end_strain + $length_allele + 1; #set the beginning of the strain after the allele
 		}
 		else{
 		    #add the indel
-		    $mapper->add_indel_coordinates('Slice',$end_slice+1,$end_slice + $variation_feature->length,1,'StrainSlice',$end_strain+1,$end_strain + $length_allele);
+		    $mapper->add_indel_coordinates('Slice',$end_slice+1,$end_slice + $allele_feature->length,1,'StrainSlice',$end_strain+1,$end_strain + $length_allele);
 		    $start_strain += $length_allele;
 		}
-		$start_slice = $end_slice + $variation_feature->length+ 1; #set the beginning of the slice after the variation feature
+		$start_slice = $end_slice + $allele_feature->length+ 1; #set the beginning of the slice after the variation feature
 	    }
 	}
 	if ($start_slice <= $self->length){
@@ -481,6 +482,95 @@ sub mapper{
 	$self->{'mapper'} = $mapper;
     }
     return $self->{'mapper'};
+}
+
+
+=head2 get_all_Transcripts
+
+  Args       : None
+  Example    : @transcripts = @{$slice->get_all_Transcripts)_};
+  Description: Gets all transcripts which overlap this Strain Slice.  If you want to
+               specify a particular analysis or type, then you are better off
+               using get_all_Genes or get_all_Genes_by_type and iterating
+               through the transcripts of each gene.
+  Returntype : reference to a list of Bio::EnsEMBL::Transcripts
+  Exceptions : none
+  Caller     : general
+
+=cut
+
+sub get_all_Transcripts {
+  my $self = shift;
+
+  if(!$self->adaptor()) {
+    warning('Cannot get Transcripts without attached adaptor');
+    return [];
+  }
+
+  my $ta = $self->adaptor()->db()->get_TranscriptAdaptor();
+  #get first all Transcript in the Slice
+  my $transcripts = $ta->fetch_all_by_Slice($self, 1);
+  #get the mapper between the Slice and the StrainSlice
+  my $mapper = $self->mapper();
+  my (@results, @results_ordered, $new_start, $new_end, $new_strand);
+  #foreach of the transcripts, map them to the StrainSlice and replace the Slice with the StrainSlice
+  foreach my $transcript (@{$transcripts}){
+      $transcript->{'slice'} = $self; #add the StrainSlice as the Slice for this Transcript
+      #map from the Slice to the Strain Slice
+      my @results = $mapper->map_coordinates('Slice',$transcript->start,$transcript->end,$transcript->strand,'Slice');
+      #from the results, order them but filter out those that are not coordinates
+      @results_ordered = sort {$a->start <=> $b->start} grep {ref($_) eq 'Bio::EnsEMBL::Mapper::Coordinate'} @results;
+      $new_start = $results_ordered[0]->start();
+      $new_strand = $results_ordered[0]->strand();
+      $new_end = $results_ordered[-1]->end();  #get last element of the array, the end of the slice
+      $transcript->start($new_start);  #update the new coordinates for the transcript
+      $transcript->end($new_end);
+      $transcript->strand($new_strand);
+      
+  }
+  return $transcripts;
+}
+
+
+=head2 get_all_Exons
+
+  Arg [1]    : none
+  Example    : @exons = @{$slice->get_all_Exons};
+  Description: Gets all exons which overlap this StrainSlice.  Note that these exons
+               will not be associated with any transcripts, so this may not
+               be terribly useful.
+  Returntype : reference to a list of Bio::EnsEMBL::Exons
+  Exceptions : none
+  Caller     : general
+
+=cut
+
+sub get_all_Exons {
+  my $self = shift;
+
+  if(!$self->adaptor()) {
+    warning('Cannot get Exons without attached adaptor');
+    return [];
+  }
+  my $exons = $self->adaptor->db->get_ExonAdaptor->fetch_all_by_Slice($self);
+  #get the mapper between the Slice and the StrainSlice
+  my $mapper = $self->mapper();
+  my (@results, @results_ordered, $new_start, $new_end, $new_strand);
+  #foreach of the transcripts, map them to the StrainSlice and replace the Slice with the StrainSlice
+  foreach my $exon (@{$exons}){
+      $exon->{'slice'} = $self; #add the StrainSlice as the Slice for this Exon
+      #map from the Slice to the Strain Slice
+      my @results = $mapper->map_coordinates('Slice',$exon->start,$exon->end,$exon->strand,'Slice');
+      #from the results, order them but filter out those that are not coordinates
+      @results_ordered = sort {$a->start <=> $b->start} grep {ref($_) eq 'Bio::EnsEMBL::Mapper::Coordinate'} @results;
+      $new_start = $results_ordered[0]->start();
+      $new_strand = $results_ordered[0]->strand();
+      $new_end = $results_ordered[-1]->end();  #get last element of the array, the end of the slice
+      $exon->start($new_start);  #update new coordinates for the Exon
+      $exon->end($new_end);
+      $exon->strand($new_strand);
+  }
+  return $exons;
 }
 
 1;
