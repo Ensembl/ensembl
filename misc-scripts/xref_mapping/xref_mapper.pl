8fce084947820d4267436d5570fbbb9aa54d72e8
Ian Longden <ianl@sanger.ac.uk>
2005-05-18 13:47:07 +0000

    delete existing changed

--- misc-scripts/xref_mapping/xref_mapper.pl
+++ misc-scripts/xref_mapping/xref_mapper.pl
@@ -40,7 +40,7 @@ usage() if($help);
 if(defined($dumpcheck) && defined($maxdump)){
   die "both dumpcheck and maxdump cannot both be specified\n";
 }
- 
+
 open(FILE, $file) or die("Could not open input file '$file'");
  
 my  @all_species;
@@ -170,6 +170,9 @@ if(defined($species_hash{'species'})){
   if(defined($maxdump)){
     $mapper->maxdump($maxdump);
   }
+  if(defined($deleteexisting)){
+    $mapper->delete_existing("yes");
+  }
   if(defined($use_existing_mappings)){
     $mapper->use_existing_mappings("yes");
   }
@@ -186,8 +189,6 @@ $mapper->xref($xref); # attach xref object to mapper object
 print "\nDumping xref & Ensembl sequences\n";
 $mapper->dump_seqs($location);
 
-print "\nDeleting old data\n" if ($deleteexisting);
-$mapper->delete_existing() if ($deleteexisting);
 
 print "\nChecking external_db table\n" if ($upload);
 $mapper->upload_external_db() if ($upload);
