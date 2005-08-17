0d6dec38df0c2998292952ed0692527a9810742d
Glenn Proctor <gp1@sanger.ac.uk>
2005-08-17 14:51:59 +0000

    New version, with modular parsers

--- misc-scripts/regulatory_regions/load_regulatory.pl
+++ misc-scripts/regulatory_regions/load_regulatory.pl
@@ -3,214 +3,85 @@
 use strict;
 
 use DBI;
+use RegulatoryFeatureParser::BaseParser;
 use Bio::EnsEMBL::DBSQL::DBAdaptor;
-use Bio::EnsEMBL::DBSQL::SliceAdaptor;
-use Bio::EnsEMBL::Slice;
-use Bio::EnsEMBL::Analysis;
 use Getopt::Long;
 
-my ($host, $user, $pass, $port, $dbname, $file, $del, $motif);
+my ($host, $user, $pass, $port, $dbname, $file, $del, $type);
 
 GetOptions( "host=s",   \$host,
 	    "user=s",   \$user,
 	    "pass=s",   \$pass,
 	    "port=i",   \$port,
 	    "dbname=s", \$dbname,
+	    "type=s",   \$type,
 	    "file=s",   \$file,
 	    "delete",   \$del,
 	    "help",     \&usage
 	  );
 
-my $dbi = DBI->connect("dbi:mysql:host=$host;port=$port;database=$dbname",
-		       $user,
-		       $pass,
-		       {'RaiseError' => 1}) || die "Can't connect to database";
+my $db_adaptor = new Bio::EnsEMBL::DBSQL::DBAdaptor('-host' => $host,
+						    '-port' => $port,
+						    '-user' => $user,
+						    '-pass' => $pass,
+						    '-dbname' => $dbname);
 
-delete_existing($dbi) if ($del);
+usage() if (!$host || !$user || !$dbname || !$type);
 
-my $rf_sth = $dbi->prepare("INSERT INTO regulatory_feature (name, seq_region_id, seq_region_start, seq_region_end, seq_region_strand, analysis_id, regulatory_factor_id) VALUES(?,?,?,?,?,?,?)");
-my $rm_select_sth = $dbi->prepare("SELECT regulatory_factor_id FROM regulatory_factor WHERE name=? AND type=?");
-my $rm_insert_sth = $dbi->prepare("INSERT INTO regulatory_factor (name, type) VALUES(?,?)");
-my $rr_obj_sth = $dbi->prepare("INSERT INTO regulatory_feature_object VALUES(?,?,?,?,?)");
+# validate type
+exit(1) if (!RegulatoryFeatureParser::BaseParser::validate_type($db_adaptor, $type));
 
-my $db_adaptor = new Bio::EnsEMBL::DBSQL::DBAdaptor(-host => $host,
-						    -user => $user,
-						    -port => $port,
-						    -pass => $pass,
-						    -dbname => $dbname);
+RegulatoryFeatureParser::BaseParser::delete_existing($db_adaptor, $type) if ($del);
 
-my $analysis_adaptor    = $db_adaptor->get_AnalysisAdaptor;
-my $slice_adaptor       = $db_adaptor->get_SliceAdaptor();
-my $transcript_adaptor  = $db_adaptor->get_TranscriptAdaptor();
-my $translation_adaptor = $db_adaptor->get_TranslationAdaptor();
-my $gene_adaptor        = $db_adaptor->get_GeneAdaptor();
 
-my $count = 0;
-
-my %anal;
-
-open (FILE, "<$file") || die "Can't open $file";
-
-while (<FILE>) {
-
-  next if ($_ =~ /^\s*\#/ || $_ =~ /^\s*$/);
-
-  my ($group, $seq, $method, $feature, $chr, $start, $end, $str, $phase, $score, $type, $id_ignore, $id) = split;
-
-  my $strand = ($str =~ /\+/ ? 1 : -1);
-
-  # $seq is the name of a factor - if it's already there, find its ID, otherwise add it
-  $rm_select_sth->execute($seq, $feature);
-  my $factor_id = ($rm_select_sth->fetchrow_array())[0];
-  if (!$factor_id) {
-    $rm_insert_sth->execute($seq, $feature);
-    $factor_id = $rm_insert_sth->{'mysql_insertid'};
-  }
-
-  # get analysis ID for $method, and cache
-  my $analysis = $anal{$method};
-  if (!$analysis) {
-    $analysis = $analysis_adaptor->fetch_by_logic_name($method);
-    $anal{$method} = $analysis;
-  }
-
-  if (!$analysis) {
-    print STDERR "Can't get analysis for $method, skipping\n";
-    next;
-  }
-
-  # get seq_region id for $chr
-  my $chr_slice = $slice_adaptor->fetch_by_region(undef, $chr, $start, $end, $strand);
-
-  if (!$chr_slice) {
-    print STDERR "Can't get slice for $chr:$start:$end:$strand\n";
-    next;
-  }
-
-  # get internal ID from $type and $id
-  my ($ensembl_type) = $type =~ /(gene|transcript|translation)/;
-  if (!$ensembl_type) {
-    print STDERR "Can't get ensembl type from $type, skipping\n";
-    next;
-  }
-  $ensembl_type = ucfirst(lc($ensembl_type));
-  $id =~ s/[\"\']//g;  # strip quotes
-
-  my $ensembl_object;
-
-  if ($ensembl_type eq "Gene") {
-
-    $ensembl_object = $gene_adaptor->fetch_by_stable_id($id);
-
-  } elsif ($ensembl_type eq "Transcript") {
-
-    $ensembl_object = $transcript_adaptor->fetch_by_stable_id($id);
-
-  } elsif ($ensembl_type eq "Translation") {
-
-    $ensembl_object = $translation_adaptor->fetch_by_stable_id($id);
-
-  } else {
-
-    print STDERR "Don't know how to deal with type $ensembl_type, skipping\n";
-    next;
-
-  }
-
-  if (!$ensembl_object) {
-    print STDERR "Can't find ensembl $ensembl_type corresponding to $id\n";
-    next;
-  }
-
-  # for miRNA_target (and possibly others) where individual features don't
-  # have a unique name, create a composite one. Also set influence for each type.
-  my $feature_name;
-  my $influence;
-  if ($method =~ /miRanda/i) {
-    $feature_name = $id .":" . $seq;
-    $influence = "negative";
-  } elsif ($method =~ /cisRed/i) {
-    $feature_name = $id .":" . $seq;
-    $influence = "unknown";
-  }
-
-  print STDERR "No feature name for $seq of type $feature on $id.\n" if (!$feature_name);
-
-  my $seq_region_id = $slice_adaptor->get_seq_region_id($chr_slice);
-
-  if ($seq_region_id) {
-
-    # add the feature
-    $rf_sth->execute($feature_name,
-		     $seq_region_id,
-		     $chr_slice->start(),
-		     $chr_slice->end(),
-		     $chr_slice->strand(),
-		     $analysis->dbID(),
-		     $factor_id);
-
-    my $rr_id = $rf_sth->{'mysql_insertid'};
-
-    $rr_obj_sth->execute($rr_id,
-			 $ensembl_type,
-			 $ensembl_object->dbID(),
-			 $influence,
-			 "");                     # no evidence yet
-
-    $count++;
-
-  } else {
-
-    print STDERR "Can't get seq_region_id for chromosome $chr_slice\n";
-
-  }
+# load module corresponding to type
+my $module;
+eval "require RegulatoryFeatureParser::$type";
+if($@) {
+  warn("Did not find a parser module corresponding to $type, exiting.\n");
+  exit(1);
+} else{
+  $module = $type;
 }
 
-close FILE;
+my $parser = "RegulatoryFeatureParser::$type"->new();
 
-print "Uploaded $count regulatory features to database\n";
+if ($file) {
 
+  die "Can't find $file\n" if (!-e $file);
 
+  my $objects = $parser->parse($db_adaptor, $file);
 
-sub delete_existing {
-
-  my $dbi = shift;
-
-  $dbi->do("DELETE FROM regulatory_feature");
-  $dbi->do("DELETE FROM regulatory_factor");
-  $dbi->do("DELETE FROM regulatory_feature_object");
-  $dbi->do("DELETE FROM regulatory_factor_transcript");
-
-  print "Deleted existing data in regulatory region tables\n";
+  $parser->upload_features_and_factors($db_adaptor, $objects);
 
 }
+
+
 
 
 sub usage {
+  print <<EOF
 
-  print <<EOF;
-Usage: perl load_regulatory.pl
-         -host   : database host
-         -port   : database port
-         -user   : user name
-         -pass   : password
-         -dbname : database name
-	 -file   : file to load from
-	 -delete : delete existing contents of tables first
+Usage: perl $0 <options>
 
-File format expected:
+  -host    Database host to connect to
 
-GROUP SEQ METHOD FEATURE CHR START END STRAND PHASE SCORE TYPE ID
+  -port    Database port to connect to
 
-e.g.
+  -user    Database username
 
-Similarity      hsa-miR-108     miRanda miRNA_target    3	49546906        49546927        +       .       107     transcript id   "ENST00000308775"
-Similarity      hsa-miR-153     miRanda miRNA_target    3	49546683        49546702        +       .       106     transcript id   "ENST00000308775"
+  -pass    Password for user
 
-Lines beginning with # are ignored.
+  -type    Type of regulatory features to upload; must be present in analysis table and
+           have a correspoinding parser
+
+  -file    File to load data from; must be parseable by parser corresponding to type
+
+  -delete  Delete all data related to regulatory features of specified type before loading
+
+  -help    This message
 
 EOF
 
-  exit(0);
-
 }
+
