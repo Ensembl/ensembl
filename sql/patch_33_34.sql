8f0c3dbbac042ab47105d126d7460e84a55d1c4a
Glenn Proctor <gp1@sanger.ac.uk>
2005-09-07 15:24:11 +0000

    Fixed SQL

--- sql/patch_33_34.sql
+++ sql/patch_33_34.sql
@@ -1,6 +1,6 @@
 # SQL to patch a release 33 Ensembl database schema to release 34
 
-ALTER table object_xref CHANGE COLUMN enembl_object_type ENUM( 'RawContig', 'Transcript', 'Gene', 'Translation', 'regulatory_factor', 'regulatory_feature' ) not null;
+ALTER table object_xref MODIFY ensembl_object_type ENUM( 'RawContig', 'Transcript', 'Gene', 'Translation', 'regulatory_factor', 'regulatory_feature' ) not null;
 
 DROP TABLE regulatory_factor_transcript;
 
