0af3e239ea8061baafbb7c1f3efddd89c753083f
Glenn Proctor <gp1@sanger.ac.uk>
2005-09-21 13:47:33 +0000

    Modify gene description priority list so we get Anopheles_symbol descriptions in preference to anything else if they're there.

--- misc-scripts/xref_mapping/XrefMapper/anopheles_gambiae.pm
+++ misc-scripts/xref_mapping/XrefMapper/anopheles_gambiae.pm
@@ -12,8 +12,21 @@ sub get_set_lists {
 
 }
 
-# transcript, gene display_xrefs and descriptions can use defaults
+# transcript, gene display_xrefs can use defaults
 # since anopheles_symbol is "before" Uniprot
+
+# If there is an Anopheles_symbol xref, use its description
+sub gene_description_sources {
+
+  return ("RefSeq_dna_predicted",
+	  "RefSeq_peptide_predicted",
+	  "Uniprot/SPTREMBL",
+	  "RefSeq_dna",
+	  "RefSeq_peptide",
+	  "Uniprot/SWISSPROT",
+	  "Anopheles_symbol");
+
+}
 
 # regexps to match any descriptons we want to filter out
 sub gene_description_filter_regexps {
