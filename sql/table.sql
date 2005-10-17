6eb79f33bd9291aa8d20317be1dc5f6cd6706cb4
Arne Stabenau <stabenau@sanger.ac.uk>
2005-10-17 11:39:21 +0000

    changed confidence to status (again?) I thought I did that already ...

--- sql/table.sql
+++ sql/table.sql
@@ -351,7 +351,7 @@ CREATE TABLE gene (
   seq_region_strand           tinyint(2) NOT NULL,       
   display_xref_id             int unsigned,
   source                      VARCHAR(20) NOT NULL,
-  confidence                  enum( 'KNOWN', 'NOVEL', 'PUTATIVE', 'PREDICTED' ),
+  status                      enum( 'KNOWN', 'NOVEL', 'PUTATIVE', 'PREDICTED' ),
   description                 text,
 
   PRIMARY KEY (gene_id),
@@ -425,7 +425,7 @@ CREATE TABLE transcript (
   seq_region_strand           tinyint(2) NOT NULL, 
   display_xref_id             int unsigned,
   biotype                     VARCHAR(40) NOT NULL,
-  confidence                  enum( 'KNOWN', 'NOVEL', 'PUTATIVE', 'PREDICTED' ),
+  enum                        status( 'KNOWN', 'NOVEL', 'PUTATIVE', 'PREDICTED' ),
   description                 text,
 
   PRIMARY KEY (transcript_id),
