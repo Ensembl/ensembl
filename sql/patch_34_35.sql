b219a5f72dae349ac410ed6ea6fbece75b50f2de
Glenn Proctor <gp1@sanger.ac.uk>
2005-11-17 13:03:04 +0000

    Set meta schema_version to 35 (not CVS keyword replacement version any more)

--- sql/patch_34_35.sql
+++ sql/patch_34_35.sql
@@ -1,4 +1,4 @@
 # There were no schema changes on the core databases between release 34 and 35
-# :-)
-# Just updating the schema_version in the meta table
-UPDATE meta set meta_value="Revision: 1.234" where meta_key="schema_version";
+# 
+# Just update the schema_version in the meta table
+UPDATE meta set meta_value="35" where meta_key="schema_version";
