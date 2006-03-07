e2edc70292629f65e37777087213b2c1da6df63b
Will Spooner <whs@sanger.ac.uk>
2006-03-07 18:08:32 +0000

    A simple, generic parser for FASTA sequences. Uses Bio::SeqIO

---
+++ misc-scripts/xref_mapping/XrefParser/FastaParser.pm
@@ -0,0 +1,67 @@
+package XrefParser::FastaParser;
+
+use strict;
+use Bio::SeqIO;
+use File::Basename;
+
+use XrefParser::BaseParser;
+
+use vars qw(@ISA);
+@ISA = qw(XrefParser::BaseParser);
+
+# Fasta file format, e.g.
+# >foo peptide sequence for the foo gene
+# MTEYKLVVVGAGGVGKSALTIQLIQNHFVDEYDPTIEDSYRKQVVIDGETCLLDILDTAG
+# PTRTVDTKQAHELAKSYGIPFIETSAKTRQGVEDAFYTLVREIRQYRMKKLNSSDDGTQG
+# CMGLPCVVM
+
+sub run {
+
+  my ($self, $file, $source_id, $species_id) = @_;
+  
+  my $sio = Bio::SeqIO->new(-format=>'fasta' , -file=>$file );
+  my $species_tax_id = $self->get_taxonomy_from_species_id($species_id);
+  
+  my @xrefs;
+  while( my $seq = $sio->next_seq ) {
+
+    # Test species if available
+    if( my $sp = $seq->species ){
+      if( my $tax_id = $sp->ncbi_taxid ){
+        next if $tax_id != $species_tax_id;
+      }
+    }
+
+    # build the xref object and store it
+    my $xref;
+    $xref->{ACCESSION}     = $seq->display_name;
+    $xref->{LABEL}         = $seq->display_name;
+    $xref->{DESCRIPTION}   = $seq->description;
+    $xref->{SEQUENCE}      = $seq->seq;
+    $xref->{SOURCE_ID}     = $source_id;
+    $xref->{SPECIES_ID}    = $species_id;
+    $xref->{SEQUENCE_TYPE} = 'peptide';
+    $xref->{STATUS}        = 'experimental';
+    if( my $v = $seq->version ){ $xref->{VERSION} = $v };
+    push @xrefs, $xref;
+
+  }
+
+  print scalar(@xrefs) . " Fasta xrefs succesfully parsed\n";
+
+  XrefParser::BaseParser->upload_xref_object_graphs(\@xrefs);
+
+  print "Done\n";
+  return 0; #successful
+}
+
+
+sub new {
+
+  my $self = {};
+  bless $self, "XrefParser::FastaParser";
+  return $self;
+
+}
+
+1;
