d94d6ad41d37fe5ea74bc5632e9260f51aab0543
Glenn Proctor <gp1@sanger.ac.uk>
2006-03-17 14:28:05 +0000

    Now use RefSeq first (if available), then Swissprot.

--- misc-scripts/xref_mapping/XrefParser/HUGOParser.pm
+++ misc-scripts/xref_mapping/XrefParser/HUGOParser.pm
@@ -42,60 +42,82 @@ sub run {
 
 
   my (%swiss)  =  %{XrefParser::BaseParser->get_valid_codes("uniprot",$species_id)};
-#  my (%refseq) =  %{XrefParser::BaseParser->get_valid_codes("refseq",$species_id)};
+  my (%refseq) =  %{XrefParser::BaseParser->get_valid_codes("refseq",$species_id)};
 
-  my $count = 0;
+  my $swiss_count = 0;
+  my $refseq_count = 0;
   my $mismatch = 0;
+
   if(!open (HUGO, "<$file")){
-    print  "ERROR: Can't open hugo file $file\n";
+    print  "ERROR: Can't open HUGO file $file\n";
     return 1;
   }
 
   <HUGO>;
-#23	ABAT	4-aminobutyrate aminotransferase		P80404
-#29	ABCA1	ATP-binding cassette, sub-family A (ABC1), member 1	ABC1, HDLDT1	O95477
+
+  #23	ABAT	4-aminobutyrate aminotransferase		P80404
+  #29	ABCA1	ATP-binding cassette, sub-family A (ABC1), member 1	ABC1, HDLDT1	O95477
+  #40	ABCB1	ATP-binding cassette, sub-family B (MDR/TAP), member 1	PGY1, MDR1, CLCS	P-gp, CD243, GP170, ABC20	P08183	NM_000927
+
   while (<HUGO>) {
+
     chomp;
-# 0 HGNC ID	       # primary accession
-# 1 Approved Symbol    # label
-# 2 Approved Name      # description
-# 3 Previous Symbols   # synonyms
-# 4 Aliases            # aliases
-# 5 UniProt ID         # uniprot accession
+
+    # 0 HGNC ID	           # primary accession
+    # 1 Approved Symbol    # label
+    # 2 Approved Name      # description
+    # 3 Previous Symbols   # synonyms
+    # 4 Aliases            # aliases
+    # 5 UniProt ID         # uniprot accession
+    # 6 RefSeq ID
+
     my @array = split(/\t/,$_);
 
-    if ($array[5]) {  #swissprot
-      my $master = $swiss{$array[5]};
-      if(!defined($master)){
-	$mismatch++;
-      }
-      else{
-	XrefParser::BaseParser->add_to_xrefs($master,$array[0],'',$array[1],$array[2],"",$source_id,$species_id);
-	$count++;
-	if(defined($array[3])){ #dead name add to synonym
-	  my @array2 = split(',\s*',$array[3]);
-	  foreach my $arr (@array2){
-#	    print "adding synonym ".$arr." for ".$hugo{$hgnc}." ($hgnc)\n";
-	    XrefParser::BaseParser->add_to_syn($array[0], $source_id, $arr);
-	  }
-	}
-	if(defined($array[4])){ #alias add to synonym
-	  my @array2 = split(',\s*',$array[4]);
-	  foreach my $arr (@array2){
-#	    print "adding synonym ".$arr." for ".$hugo{$hgnc}." ($hgnc)\n";
-	    XrefParser::BaseParser->add_to_syn($array[0], $source_id, $arr);
-	  }
-	}
-	
-      }
-      #	print "$array[1]\tSPTR\t$hgnc\tHUGO\t$hugo_id{$hgnc}\t$hugo_syn{$hgnc}\tXREF\n";
+    # Use the RefSeq if available as this is manually curated
+    # If no RefSeq, use the Swissprot instead
+
+    my $master;
+    if ($array[6]) {               # RefSeq
+      $master = $refseq{$array[6]};
+      $refseq_count++;
+    } elsif ($array[5]) {        # Uniprot
+      $master = $swiss{$array[5]};
+      $swiss_count++;
     }
 
-  }
+    if (!$master) {
+      $mismatch++;
+      next;
+    }
+
+    #print $array[5] ." " . $array[6] . " " . $master . " " . $swiss_count . " " . $refseq_count . " " . $mismatch . "\n";
+
+    XrefParser::BaseParser->add_to_xrefs($master, $array[0], '', $array[1], $array[2], "", $source_id, $species_id);
+
+    if (defined($array[3])) {     # dead name, add to synonym
+      my @array2 = split(',\s*', $array[3]);
+      foreach my $arr (@array2){
+	XrefParser::BaseParser->add_to_syn($array[0], $source_id, $arr);
+      }
+    }
+
+    if (defined($array[4])) {     # alias, add to synonym
+      my @array2 = split(',\s*', $array[4]);
+      foreach my $arr (@array2){
+	XrefParser::BaseParser->add_to_syn($array[0], $source_id, $arr);
+      }
+    }
+
+  } # while HUGO
+
   close (HUGO);
-  print "\t$count xrefs succesfully loaded\n";
-  print "\t$mismatch xrefs ignored\n";
+
+  print "Loaded a total of " . ($swiss_count + $refseq_count) . " HUGO xrefs, $refseq_count from RefSeq curated mappings and $swiss_count from Uniprot (mapped)\n";
+
+  print "$mismatch xrefs could not be associated via RefSeq or Uniprot\n";
+
   return 0; # successful
+
 }
 
 sub rename_url_file{
