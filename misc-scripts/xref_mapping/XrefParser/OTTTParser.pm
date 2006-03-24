186b33d35f3bed42a544378cbfed52e415d00c07
Glenn Proctor <gp1@sanger.ac.uk>
2006-03-24 08:49:48 +0000

    Simple parser for Ensembl-Havana transcript mappings.

---
+++ misc-scripts/xref_mapping/XrefParser/OTTTParser.pm
@@ -0,0 +1,66 @@
+package XrefParser::OTTTParser;
+
+use strict;
+
+use DBI;
+
+use XrefParser::BaseParser;
+
+use vars qw(@ISA);
+@ISA = qw(XrefParser::BaseParser);
+
+# Parse file of Ensembl - Vega OTTT transcript mappings
+# ENST00000373795:	OTTHUMT00000010392
+# ENST00000374603:	OTTHUMT00000057024
+# ENST00000372604:	OTTHUMT00000057746
+# ENST00000329151:	OTTHUMT00000011475
+
+sub run {
+
+  my ($self, $file, $source_id, $species_id) = @_;
+
+  if(!open(OTTT,"<".$file)){
+    print "Could not open $file\n";
+    return 1;
+  }
+  my $line_count = 0;
+  my $xref_count = 0;
+
+  my $xref_sth = $self->dbi()->prepare("SELECT xref_id FROM xref WHERE accession=? AND source_id=$source_id AND species_id=$species_id");
+
+  while (<OTTT>) {
+
+    my ($ens, $ottt) = split;
+
+    $ens =~ s/://g;
+
+    $line_count++;
+
+    # check if an xref already exists
+    $xref_sth->execute($ottt);
+    my $xref_id = ($xref_sth->fetchrow_array())[0];
+    if (!$xref_id) {
+      $xref_id = $self->add_xref($ottt, '', $ottt, "", $source_id, $species_id);
+      $xref_count++;
+    }
+
+    $self->add_direct_xref($xref_id, $ens, "transcript", "");
+
+  }
+
+  print "Parsed $line_count OTTT identifiers from $file, added $xref_count xrefs and $line_count direct_xrefs\n";
+
+  close(OTTT);
+  return 0;
+}
+
+
+sub new {
+
+  my $self = {};
+  bless $self, "XrefParser::OTTTParser";
+  return $self;
+
+}
+
+1;
