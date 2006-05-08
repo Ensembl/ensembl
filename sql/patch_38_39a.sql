537c7e9926ab714b1eacb0725945025f82a261a3
Patrick Meidl <pm2@sanger.ac.uk>
2006-05-08 16:59:59 +0000

    renamed to patch_38_39_a.sql (for easier parsing by scripts)

--- sql/patch_38_39a.sql
+++
@@ -1,3 +0,0 @@
-# Add a new status to the enumeration in gene & transcript
-ALTER TABLE gene CHANGE COLUMN status status enum('KNOWN','NOVEL','PUTATIVE','PREDICTED','KNOWN_BY_PROJECTION');
-ALTER TABLE transcript CHANGE COLUMN status status enum('KNOWN','NOVEL','PUTATIVE','PREDICTED','KNOWN_BY_PROJECTION');
