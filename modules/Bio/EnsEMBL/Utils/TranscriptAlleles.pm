037581c496e51ca458f24a57c03292b92a967166
Web Admin <w3adm@sanger.ac.uk>
2006-05-30 15:05:45 +0000

    stuff needed for text/html dump from tsv page

--- modules/Bio/EnsEMBL/Utils/TranscriptAlleles.pm
+++ modules/Bio/EnsEMBL/Utils/TranscriptAlleles.pm
@@ -93,15 +93,22 @@ sub get_all_ConsequenceType {
     ### The rest don't work anyway until we have a AlignStrainSlice
     ### MUST BE SORTED....
     my $string = $allele->allele_string;
+    my $opposite_strand = 0; #to indicate wether transcript and allele and in different strands
     if( $transcript->strand != $allele->strand ) {
       $string =~tr/ACGT/TGCA/;
+      $opposite_strand = 1;
     }
 
     my $consequence_type = Bio::EnsEMBL::Variation::ConsequenceType->new($transcript->dbID(),'',$allele->start, $allele->end, $transcript->strand, [$string]);
-    if ($allele->start == 16165){
-	print "hello";
+
+    #calculate the consequence type of the Allele if different from the reference Allele
+    if (($opposite_strand && $allele->ref_allele_string eq $allele->allele_string) || (!$opposite_strand && $allele->ref_allele_string eq $string)){      	#same allele as reference, there is no consequence, called SARA
+      	#same allele as reference, there is no consequence, called SARA
+	$consequence_type->type('SARA');
+	push @out, $consequence_type;
+	next;
     }
-    #calculate the consequence type of the Allele
+	
     my $ref_consequences = type_variation($transcript,"",$consequence_type);
     if ($allele->start != $allele->end){
 	#do not calculate for indels effects of 2 or more in same codon
