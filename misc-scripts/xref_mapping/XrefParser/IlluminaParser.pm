bb538a34606f0687b121dd776b515cd51039493d
Glenn Proctor <gp1@sanger.ac.uk>
2006-06-23 16:21:19 +0000

    Cope with file format of Illumina Gene Expression data files.

--- misc-scripts/xref_mapping/XrefParser/IlluminaParser.pm
+++ misc-scripts/xref_mapping/XrefParser/IlluminaParser.pm
@@ -8,23 +8,21 @@ use vars qw(@ISA);
 @ISA = qw(XrefParser::BaseParser);
 
 # Parser for Illumina refs
-#Index,Target,Transcript,Accession,Symbol,Type,Definition,Ontology,Synony
-#1,GI_23097300-A,GI_23097300,NM_021936.1,PLAC3,A,"Homo sapiens placenta-specific 3 (PLAC3), transcript variant 2, mRNA.",,PAPPE;PAPP-A
-#2,GI_21070955-A,GI_21070955,NM_015386.1,COG4,A,"Homo sapiens component of oligomeric golgi complex 4 (COG4), mRNA.",,COD1;DKFZp586E1519
+
+#Gid,Accession,Symbol,Probe_Sequence,Definition
+#GI_4505876,NM_000445.1,PLEC1,AACACTAACCTGACCGTGGGCGGGGCCTTGCGGTATCCGCCCCCAATAAA,"Homo sapiens plectin 1, intermediate filament binding protein 500kDa (PLEC1), transcript variant 1, mRNA."
+#GI_18860893,NM_130393.1,PTPRD,CTACAGGCCCTTCAATATCCATGGAGTCTCTTCTGAGCCATACAGGGCAC,"Homo sapiens protein tyrosine phosphatase, receptor type, D (PTPRD), transcript variant 4, mRNA."
 
 sub run {
 
   my ($self, $file, $source_id, $species_id) = @_;
 
-  my $count = 0;
-  my $missed = 0;
+  my @xrefs;
 
   if(!open(FILE,"<".$file)){
     print "Could not open $file\n";
     return 1;
   }
-
-  my (%refseq) = %{XrefParser::BaseParser->get_valid_codes("refseq", $species_id)};
 
   while (<FILE>) {
 
@@ -35,24 +33,29 @@ sub run {
     # strip ^M at end of line
     $_ =~ s/\015//g;
 
-    my ($idx, $target, $transcript, $accession, $symbol, $type, $definition, $ontology, $synonym) = split(/,/);
-    next if ($idx == "Index" && $target == "Target"); # skip header
+    my ($accession, $refseq, $symbol, $sequence, $description) = split(/,/);
+    next if ($accession == "Gid" && $refseq == "Accession"); # skip header
 
-    my ($refseq) = $accession =~ /([^.]+)(\..*)?$/;
+    # build the xref object and store it
+    $xref->{ACCESSION}     = $accession;
+    $xref->{LABEL}         = $accession;
+    $xref->{SEQUENCE}      = $sequence;
+    $xref->{SOURCE_ID}     = $source_id;
+    $xref->{SPECIES_ID}    = $species_id;
+    $xref->{DESCRIPTION}     = $description;
+    $xref->{SEQUENCE_TYPE} = 'dna';
+    $xref->{STATUS}        = 'experimental';
 
-    if ($refseq{$refseq}) {
-      XrefParser::BaseParser->add_to_xrefs($refseq{$refseq}, $target, '', $target, $definition, '', $source_id, $species_id);
-      $count++;
-    } else {
-      $missed++;
-    }
-
+      push @xrefs, $xref;
 
   }
 
   close(FILE);
 
-  print "$count Illumina xrefs succesfully parsed. Skipped $missed due to non-matching RefSeq IDs.\n";
+  print scalar(@xrefs) . " Illumina xrefs succesfully parsed\n";
+
+  XrefParser::BaseParser->upload_xref_object_graphs(\@xrefs);
+
 
   return 0;
 }
