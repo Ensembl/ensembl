4a519c2006ddee2fcf84c4087b34d0030f2b4956
Glenn Proctor <gp1@sanger.ac.uk>
2006-06-27 08:56:44 +0000

    Store intron number as attribute value instead of intron length.
    Also delete existing frameshift attributes by default.

--- misc-scripts/frameshift_transcript_attribs.pl
+++ misc-scripts/frameshift_transcript_attribs.pl
@@ -1,16 +1,16 @@
 use strict;
 use warnings;
 
-# Finds all potential frameshifts (exons 1, 2 4 or 5 bp apart)
+# Finds all potential frameshifts (exons 1, 2, 4 or 5 bp apart)
 # in a database and adds transcript attributes for them.
-# Attribute value is intron length.
+# Attribute value is intron number (first intron is 1, second 2 etc).
 
 use Bio::EnsEMBL::DBSQL::DBAdaptor;
 use Bio::EnsEMBL::Attribute;
 
 use Getopt::Long;
 
-my ($host, $port, $user, $pass, $dbpattern, $nostore, $delete, $locations);
+my ($host, $port, $user, $pass, $dbpattern, $nostore, $nodelete, $locations);
 
 GetOptions('host=s'      => \$host,
            'user=s'      => \$user,
@@ -18,7 +18,7 @@ GetOptions('host=s'      => \$host,
            'pass=s'      => \$pass,
            'dbpattern=s' => \$dbpattern,
 	   'nostore'     => \$nostore,
-	   'delete'      => \$delete,
+	   'nodelete'    => \$nodelete,
 	   'locations'   => \$locations,
            'help'        => sub { usage(); exit(0); });
 
@@ -48,7 +48,7 @@ for my $dbname ( @dbnames ) {
   my $attribute_adaptor = $db_adaptor->get_AttributeAdaptor();
   my $transcript_adaptor = $db_adaptor->get_TranscriptAdaptor();
 
-  if ($delete) {
+  if (!$nodelete) {
 
     print STDERR "Deleting existing 'Frameshift' transcript attributes\n";
     my $dsth = $db_adaptor->dbc()->prepare("DELETE ta, at FROM transcript_attrib ta, attrib_type at WHERE at.attrib_type_id=ta.attrib_type_id AND at.code='Frameshift'");
@@ -78,19 +78,28 @@ for my $dbname ( @dbnames ) {
 	AND t.transcript_id = ts.transcript_id
         AND s.seq_region_id=g.seq_region_id
 	GROUP BY t.transcript_id
-	HAVING intron_length IN (1,2,4,5)});
+	HAVING intron_length IN (1,2,4,5) ORDER BY t.transcript_id, t.seq_region_start, t.seq_region_end} );
 
   $sth->execute();
 
   my ($transcript_id, $biotype, $intron_length, $stable_id, $start, $end, $strand, $count, $seq_region_name);
   $sth->bind_columns(\$transcript_id, \$biotype, \$intron_length, \$stable_id, \$start, \$end, \$strand, \$seq_region_name);
 
+  my $last_transcript_id;
+  my $intron_number;
+
   while ($sth->fetch()) {
+
+    if ($transcript_id == $last_transcript_id) {
+      $intron_number++;
+    } else {
+      $intron_number = 1;
+    }
 
     my $attribute = Bio::EnsEMBL::Attribute->new(-CODE => 'Frameshift',
 						 -NAME => 'Frameshift',
 						 -DESCRIPTION => 'Frameshift modelled as intron',
-						 -VALUE => $intron_length);
+						 -VALUE => $intron_number);
 
     my @attribs = ($attribute);
 
@@ -98,10 +107,11 @@ for my $dbname ( @dbnames ) {
 
     $attribute_adaptor->store_on_Transcript($transcript->dbID, \@attribs) if (!$nostore);
 
-    print join("\t", $stable_id, $start, $end, $strand, $intron_length, $seq_region_name, "\n") if ($locations);
+    print join("\t", $stable_id, $start, $end, $strand, $intron_number, $seq_region_name, "\n") if ($locations);
 
     $biotypes{$biotype}++;
     $count++;
+    $last_transcript_id = $transcript_id;
 
   }
 
@@ -150,7 +160,7 @@ sub usage {
 
   [--nostore]   Don't store the attributes, just print results.
 
-  [--delete]    Delete any existing "Frameshift" attributes before creating new ones.
+  [--nodelete]  Don't delete any existing "Frameshift" attributes before creating new ones.
 
   [--locations] Print the start, end and strand of the introns.
 
