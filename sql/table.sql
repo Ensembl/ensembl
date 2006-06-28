5248893512eea2b6613ad9c0f750b3ff20d22386
Glenn Proctor <gp1@sanger.ac.uk>
2006-06-28 10:49:38 +0000

    Add schema version patch.

--- sql/table.sql
+++ sql/table.sql
@@ -868,6 +868,7 @@ INSERT INTO meta (meta_key, meta_value) VALUES ('patch', 'patch_39_40_e.sql|is_c
 INSERT INTO meta (meta_key, meta_value) VALUES ('patch', 'patch_39_40_f.sql|remove_all_latest');
 INSERT INTO meta (meta_key, meta_value) VALUES ('patch', 'patch_39_40_g.sql|add_ditag_tables');
 INSERT INTO meta (meta_key, meta_value) VALUES ('patch', 'patch_39_40_h.sql|oligo_feature_analysis_id_type');
+INSERT INTO meta (meta_key, meta_value) VALUES ('patch', 'patch_39_40_i.sql|schema_version');
 
 ################################################################################
 #
