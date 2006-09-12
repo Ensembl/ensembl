c3d22365ce1ad24b31d404c39aed65ca1c9adce6
Glenn Proctor <gp1@sanger.ac.uk>
2006-09-12 13:48:50 +0000

    New version

--- misc-scripts/regulatory_regions/load_regulatory.pl
+++ misc-scripts/regulatory_regions/load_regulatory.pl
@@ -7,19 +7,17 @@ use RegulatoryFeatureParser::BaseParser;
 use Bio::EnsEMBL::DBSQL::DBAdaptor;
 use Getopt::Long;
 
-my ($host, $user, $pass, $port, $dbname, $file, $del, $type, $old_assembly, $new_assembly);
+my ($host, $user, $pass, $port, $dbname, $file, $del, $type);
 
-GetOptions( "host=s",         \$host,
-	    "user=s",         \$user,
-	    "pass=s",         \$pass,
-	    "port=i",         \$port,
-	    "dbname=s",       \$dbname,
-	    "type=s",         \$type,
-	    "file=s",         \$file,
-	    "delete",         \$del,
-	    "help",           \&usage,
-	    "old_assembly=s", \$old_assembly,
-	    "new_assembly=s", \$new_assembly
+GetOptions( "host=s",   \$host,
+	    "user=s",   \$user,
+	    "pass=s",   \$pass,
+	    "port=i",   \$port,
+	    "dbname=s", \$dbname,
+	    "type=s",   \$type,
+	    "file=s",   \$file,
+	    "delete",   \$del,
+	    "help",     \&usage
 	  );
 
 my $db_adaptor = new Bio::EnsEMBL::DBSQL::DBAdaptor('-host' => $host,
@@ -52,7 +50,7 @@ if ($file) {
 
   die "Can't find $file\n" if (!-e $file);
 
-  my $objects = $parser->parse($db_adaptor, $file, $old_assembly, $new_assembly);
+  my $objects = $parser->parse($db_adaptor, $file);
 
   $parser->upload_features_and_factors($db_adaptor, $objects);
 
@@ -70,8 +68,6 @@ Usage: perl $0 <options>
 
   -port    Database port to connect to
 
-  -dbname  Database name to use
-
   -user    Database username
 
   -pass    Password for user
@@ -82,11 +78,6 @@ Usage: perl $0 <options>
   -file    File to load data from; must be parseable by parser corresponding to type
 
   -delete  Delete all data related to regulatory features of specified type before loading
-
-  -old_assembly, -new_assembly If *both* these options are specified, co-ordinates will be
-           projected from the old assembly to the new assembly before being stored. This
-           relies on a suitable mapping between the two assemblies having already been 
-           carried out.
 
   -help    This message
 
