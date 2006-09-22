89462f4995118f016d0377e3c64da6a4760abd8a
Glenn Proctor <gp1@sanger.ac.uk>
2006-09-22 12:12:06 +0000

    Added fetch_all_synonyms to allow lazy loading of synonyms in DBEntry.

--- modules/Bio/EnsEMBL/DBSQL/DBEntryAdaptor.pm
+++ modules/Bio/EnsEMBL/DBSQL/DBEntryAdaptor.pm
@@ -1143,6 +1143,43 @@ sub fetch_all_by_description {
   return \@results;
 }
 
+
+=head2 fetch_all_synonyms
+
+  Arg [1]    : dbID of DBEntry to fetch synonyms for. Used in lazy loading of synonyms.
+
+  Example    : @canc_refs = @{$db_entry_adaptor->fetch_all_synonyms(1234)};
+  Description: Fetches the synonyms for a particular DBEntry.
+  Returntype : listref of synonyms. List referred to may be empty if there are no synonyms.
+  Exceptions : None.
+  Caller     : General
+  Status     : At Risk
+
+=cut
+
+
+sub fetch_all_synonyms {
+
+  my ($self, $dbID) = @_;
+
+  my @synonyms;
+
+  my $sth = $self->prepare("SELECT synonym FROM external_synonym WHERE xref_id = ?");
+
+  $sth->bind_param(1, $dbID, SQL_INTEGER);
+  $sth->execute();
+  while ( my $arrayref = $sth->fetchrow_arrayref()){
+    my ($synonym) = @$arrayref;
+    push (@synonyms, $synonym);
+  }
+
+  @synonyms = [] if (!@synonyms);
+
+  return \@synonyms;
+
+}
+
+
 =head2 geneids_by_extids
 
   Description: DEPRECATED use list_gene_ids_by_extids instead
