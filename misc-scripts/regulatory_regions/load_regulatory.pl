7054b07da7fa3ab85e695d4022b360db67ba544e
Glenn Proctor <gp1@sanger.ac.uk>
2006-09-25 10:22:03 +0000

    Reverted to v1.11

--- misc-scripts/regulatory_regions/load_regulatory.pl
+++ misc-scripts/regulatory_regions/load_regulatory.pl
@@ -7,17 +7,19 @@ use RegulatoryFeatureParser::BaseParser;
 use Bio::EnsEMBL::DBSQL::DBAdaptor;
 use Getopt::Long;
 
-my ($host, $user, $pass, $port, $dbname, $file, $del, $type);
+my ($host, $user, $pass, $port, $dbname, $file, $del, $type, $old_assembly, $new_assembly);
 
-GetOptions( "host=s",   \$host,
-	    "user=s",   \$user,
-	    "pass=s",   \$pass,
-	    "port=i",   \$port,
-	    "dbname=s", \$dbname,
-	    "type=s",   \$type,
-	    "file=s",   \$file,
-	    "delete",   \$del,
-	    "help",     \&usage
+GetOptions( "host=s",         \$host,
+	    "user=s",         \$user,
+	    "pass=s",         \$pass,
+	    "port=i",         \$port,
+	    "dbname=s",       \$dbname,
+	    "type=s",         \$type,
+	    "file=s",         \$file,
+	    "delete",         \$del,
+	    "help",           \&usage,
+	    "old_assembly=s", \$old_assembly,
+	    "new_assembly=s", \$new_assembly
 	  );
 
 my $db_adaptor = new Bio::EnsEMBL::DBSQL::DBAdaptor('-host' => $host,
@@ -50,7 +52,7 @@ if ($file) {
 
   die "Can't find $file\n" if (!-e $file);
 
-  my $objects = $parser->parse($db_adaptor, $file);
+  my $objects = $parser->parse($db_adaptor, $file, $old_assembly, $new_assembly);
 
   $parser->upload_features_and_factors($db_adaptor, $objects);
 
@@ -60,13 +62,15 @@ if ($file) {
 
 
 sub usage {
-  print <<EOF
+  print <<EOF; exit(0);
 
 Usage: perl $0 <options>
 
   -host    Database host to connect to
 
   -port    Database port to connect to
+
+  -dbname  Database name to use
 
   -user    Database username
 
@@ -78,6 +82,11 @@ Usage: perl $0 <options>
   -file    File to load data from; must be parseable by parser corresponding to type
 
   -delete  Delete all data related to regulatory features of specified type before loading
+
+  -old_assembly, -new_assembly If *both* these options are specified, co-ordinates will be
+           projected from the old assembly to the new assembly before being stored. This
+           relies on a suitable mapping between the two assemblies having already been 
+           carried out.
 
   -help    This message
 
