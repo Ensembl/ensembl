7ead34cebd14111943af4e5e5bac45acf3df273f
Glenn Proctor <gp1@sanger.ac.uk>
2006-10-12 14:46:19 +0000

    Added UnconventionalTranscriptAssociation to list of adaptors.

--- modules/Bio/EnsEMBL/DBSQL/DBAdaptor.pm
+++ modules/Bio/EnsEMBL/DBSQL/DBAdaptor.pm
@@ -269,6 +269,7 @@ sub get_available_adaptors{
       RepeatFeature            Sequence             SimpleFeature
       Slice                    SupportingFeature    Transcript
       TranscriptSupportingFeature Translation       UnmappedObject
+      UnconventionalTranscriptAssociation
     ) ),
    # Those whose adaptors are in Map::DBSQL
     map( { $_ => "Bio::EnsEMBL::Map::DBSQL::${_}Adaptor" } qw(
