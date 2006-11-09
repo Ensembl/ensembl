f71f564721c70f2a5595489b460ab1f5cf0a2723
Seth Redmond <snr@sanger.ac.uk>
2006-11-09 12:50:21 +0000

    updated VB array names for Mart consistency

--- misc-scripts/xref_mapping/sql/populate_metadata.sql
+++ misc-scripts/xref_mapping/sql/populate_metadata.sql
@@ -128,8 +128,8 @@ INSERT INTO source VALUES (2611, 'cint_aniseed_jgi_v2', 1, 'Y', 50, 1, "");
 # predicted versions of Uniprot/EMBL - created as dependent in appropriate parse
 INSERT INTO source VALUES (2857, "ARRAY_EMBL_MMC1_20k_v1", 1, 'Y', 40,1, "" );
 INSERT INTO source VALUES (2858, "ARRAY_EMBL_MMC2_12k_v1", 1, 'Y', 40, 1, "");
-INSERT INTO source VALUES (2859, "ARRAY_JHU_GAM3.0_21k_v1", 1, 'Y', 40, 1, "");
-INSERT INTO source VALUES (2860, "ARRAY_LIV_GAMDETOX_0.25k_v1", 1, 'Y', 40, 1, "");
+INSERT INTO source VALUES (2859, "ARRAY_JHU_GAM3_0_21k_v1", 1, 'Y', 40, 1, "");
+INSERT INTO source VALUES (2860, "ARRAY_LIV_GAMDETOX_0_25k_v1", 1, 'Y', 40, 1, "");
 INSERT INTO source VALUES (2801, "Uniprot/SWISSPROT_predicted", 1, 'N',20, 1, "");
 INSERT INTO source VALUES (2802, "Uniprot/SPTREMBL_predicted", 1, 'N',20, 1, "");
 INSERT INTO source VALUES (2810, "EMBL_predicted", 1, 'N',20, 1, "");
@@ -142,9 +142,9 @@ INSERT INTO source VALUES (2856, "Anopheles_symbol", 1, 'Y', 40, 1, "");
 # aedes aegypti
 INSERT INTO source VALUES (7159, 'AedesGenBank', '1','Y','30', 1, "") ;
 INSERT INTO source VALUES (2861, "ARRAY_JHU_AEG1.0_20k_v1", 1, 'Y', 40, 1, "");
-INSERT INTO source VALUES (2862, "ARRAY_LIV_AEGDETOX_0.25k_v1", 1, 'Y', 40, 1, "");
-INSERT INTO source VALUES (2863, "ARRAY_ND_TIGRTC_9.6K_v1", 1, 'Y', 40, 1, "");
-INSERT INTO source VALUES (2864, "ARRAY_UCR_GillMgMT_0.2K_v2", 1, 'Y', 40, 1, "");
+INSERT INTO source VALUES (2862, "ARRAY_LIV_AEGDETOX_0_25k_v1", 1, 'Y', 40, 1, "");
+INSERT INTO source VALUES (2863, "ARRAY_ND_TIGRTC_9_6K_v1", 1, 'Y', 40, 1, "");
+INSERT INTO source VALUES (2864, "ARRAY_UCR_GillMgMT_0_2K_v2", 1, 'Y', 40, 1, "");
 
 # Uniprot alternative splice
 INSERT INTO source VALUES (3000, "Uniprot/Varsplic", 1, 'Y',20, 1, "");
