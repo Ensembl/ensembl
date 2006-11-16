0c8b950b0e3ae8242f73fe219f4876d1b4b038a6
Glenn Proctor <gp1@sanger.ac.uk>
2006-11-16 11:23:04 +0000

    Simple database for holding information about xref projections.

---
+++ misc-scripts/xref_projection/projection_db.sql
@@ -0,0 +1,14 @@
+# Table structure for projection info database
+
+CREATE TABLE projections (
+
+  release					INT NOT null,
+  timestamp					DATETIME,
+  from_db					VARCHAR(255),
+  from_species_latin				VARCHAR(255),
+  from_species_common				VARCHAR(255),
+  to_db					        VARCHAR(255),
+  to_species_latin				VARCHAR(255),
+  to_species_common				VARCHAR(255)
+  
+);
