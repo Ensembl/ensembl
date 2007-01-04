f311328b5dcf29f196f6ba1045159af4171fd6d4
Glenn Proctor <gp1@sanger.ac.uk>
2007-01-04 13:52:08 +0000

    Add probe2transcript to type enum in unmapped_object.

---
+++ sql/patch_42_43_b.sql
@@ -0,0 +1,12 @@
+# patch_42_43_b
+#
+# title: Add probe2transcript to type enum in unmapped_object
+#
+# description:
+# Add probe2transcript to type enum in unmapped_object
+
+ALTER TABLE unmapped_object CHANGE COLUMN type type ENUM('xref','cDNA','Marker', 'probe2transcript');
+
+
+# patch identifier
+INSERT INTO meta (meta_key, meta_value) VALUES ('patch', 'patch_42_43_b.sql|unmapped_object_probe2transcript');
