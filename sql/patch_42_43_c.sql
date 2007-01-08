ee32f3ad26c9a1bfe7cf17c4335e0c5d7b047edf
Glenn Proctor <gp1@sanger.ac.uk>
2007-01-08 10:25:55 +0000

    Added UNMAPPED type as well

--- sql/patch_42_43_c.sql
+++ sql/patch_42_43_c.sql
@@ -1,12 +1,12 @@
 # patch_42_43_c
 #
-# title: Probe info type
+# title: Probe, unmapped info type
 #
 # description:
-# Add PROBE to info_type enum
+# Add PROBE and UNMAPPED types to info_type enum
 
-ALTER TABLE xref CHANGE COLUMN info_type info_type ENUM('PROJECTION', 'MISC', 'DEPENDENT', 'DIRECT', 'SEQUENCE_MATCH', 'INFERRED_PAIR', 'PROBE');
+ALTER TABLE xref CHANGE COLUMN info_type info_type ENUM('PROJECTION', 'MISC', 'DEPENDENT', 'DIRECT', 'SEQUENCE_MATCH', 'INFERRED_PAIR', 'PROBE', 'UNMAPPED');
 
 
 # patch identifier
-INSERT INTO meta (meta_key, meta_value) VALUES ('patch', 'patch_42_43_c.sql|info_type_probe');
+INSERT INTO meta (meta_key, meta_value) VALUES ('patch', 'patch_42_43_c.sql|info_type_probe_unmapped');
