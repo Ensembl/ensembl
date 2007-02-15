4ecfe4abc6c8fefd61d8446903af21b796026cd8
Glenn Proctor <gp1@sanger.ac.uk>
2007-02-15 13:18:23 +0000

    Various tweaks; add job name, plus drosophila to anopheles GO projection

--- misc-scripts/xref_projection/submit_projections.pl
+++ misc-scripts/xref_projection/submit_projections.pl
@@ -8,9 +8,9 @@ my $release = 43;
 
 my $base_dir = "/lustre/work1/ensembl/gp1/projections/";
 
-my $conf = "release_43.conf"; # registry config file, relative to cwd
+my $conf = "release_43.conf"; # registry config file
 
-my $compara = "compara_43"; # name in registry file
+my $compara = "ensembl_compara_43"; # name in registry file
 
 # -------------------------- end of config ----------------------------
 
@@ -30,7 +30,7 @@ if (! -e $dir) {
 # common options
 my $opts = "-conf $conf -compara $compara -release $release";
 
-my ($o, $e);
+my ($o, $e, $n);
 
 # ----------------------------------------
 # Display names
@@ -39,14 +39,17 @@ my ($o, $e);
 foreach my $to ("chimp", "opossum", "dog", "cow", "macaque", "chicken", "xenopus") {
   $o = "$dir/names_human_$to.out";
   $e = "$dir/names_human_$to.err";
-  system "bsub -o $o -e $e perl project_display_xrefs.pl $opts -from human -to $to -names -delete_names";
+  $n = substr("nam_hum_$to", 0, 10); # job name display limited to 10 chars
+  system "bsub -o $o -e $e -J $n perl project_display_xrefs.pl $opts -from human -to $to -names -delete_names";
 }
+exit(0);
 
 # mouse to rat
 foreach my $to ("rat") { # don't need the loop but may add more species later
   $o = "$dir/names_mouse_$to.out";
   $e = "$dir/names_mouse_$to.err";
-  system "bsub -o $o -e $e perl project_display_xrefs.pl $opts -from human -to $to -names -delete_names";
+  $n = substr("nam_mou_$to", 0, 10);
+  system "bsub -o $o -e $e -J $n perl project_display_xrefs.pl $opts -from human -to $to -names -delete_names";
 }
 
 # ----------------------------------------
@@ -58,6 +61,7 @@ $opts .= " -nobackup";
 foreach my $to ("mouse", "rat", "dog", "chicken", "cow") {
   $o = "$dir/go_human_$to.out";
   $e = "$dir/go_human_$to.err";
+  $n = substr("go_hum_to", 0, 10);
   system "bsub -o $o -e $e perl project_display_xrefs.pl $opts -from human -to $to -go_terms -delete_go_terms";
 }
 
@@ -66,5 +70,12 @@ foreach my $to ("mouse", "rat", "dog", "chicken", "cow") {
 foreach my $to ("human", "rat", "dog", "chicken", "cow") {
   $o = "$dir/go_mouse_$to.out";
   $e = "$dir/go_mouse_$to.err";
+  $n = substr("go_mou_$to", 0, 10);
   system "bsub -o $o -e $e perl project_display_xrefs.pl $opts -from mouse -to $to -go_terms";
 }
+
+# drosophila to anopheles
+$o = "$dir/go_drosophila_anopheles.out";
+$e = "$dir/go_drosophila_anopheles.err";
+$n = "go_dro_ano";
+system "bsub -o $o -e $e perl project_display_xrefs.pl $opts -from drosophila -to anopheles -go_terms -delete_go_terms";
