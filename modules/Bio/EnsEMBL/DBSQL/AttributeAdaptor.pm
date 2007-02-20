93d46c96e6fd2133e7b07a4d2c9a388d764648ec
Leo Gordon <lg4@ebi.ac.uk>
2007-02-20 14:54:28 +0000

    no need in this line, really

--- modules/Bio/EnsEMBL/DBSQL/AttributeAdaptor.pm
+++ modules/Bio/EnsEMBL/DBSQL/AttributeAdaptor.pm
@@ -49,8 +49,6 @@ use Bio::EnsEMBL::Attribute;
 
 use Bio::EnsEMBL::Utils::Exception qw(throw warning);
 
-use DBI qw(:sql_types);
-
 use vars qw(@ISA);
 
 @ISA = qw(Bio::EnsEMBL::DBSQL::BaseAdaptor);
