ffea043172f3064c0c90d734707e0a016ffe0c53
Glenn Proctor <gp1@sanger.ac.uk>
2007-03-21 13:05:05 +0000

    Simplified query

--- misc-scripts/web_stats.sh
+++ misc-scripts/web_stats.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-mysql -u ensro -h ecs3 -P 3307 -e 'select week_beginning,hits,size,pi from summary where YEAR(week_beginning) >= 2003 AND vhost_id = 3 ORDER BY week_beginning' ensembl_summary_stats
+mysql -u ensro -h ecs3 -P 3307 -e 'SELECT week_beginning,hits,size,pi FROM summary WHERE YEAR(week_beginning) >= 2003 AND vhost_id = 3 ORDER BY week_beginning' ensembl_summary_stats
