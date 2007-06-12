de75d0ba31fd76c10eb7d06183e6fbef0ad254e1
Will Spooner <whs@sanger.ac.uk>
2007-06-12 16:50:37 +0000

    Added support for parsing InterPro-to-SuperFamily links

--- misc-scripts/xref_mapping/XrefParser/InterproParser.pm
+++ misc-scripts/xref_mapping/XrefParser/InterproParser.pm
@@ -109,10 +109,11 @@ sub run {
         my ($members) = $_ =~ /<member_list>(.+)<\/member_list>/s;
 
         while ( $members =~
-/db="(PROSITE|PFAM|PRINTS|PREFILE|PROFILE|TIGRFAMs|PIRSF|SMART)"\s+dbkey="(\S+)"/cgm
+/db="(PROSITE|PFAM|PRINTS|PREFILE|PROFILE|TIGRFAMs|PIRSF|SMART|SSF)"\s+dbkey="(\S+)"/cgm
           )
         {
             my ( $db_type, $id ) = ( $1, $2 );
+            if( $db_type eq 'SSF' ){ $id =~ s/^SSF// } # Strip SSF prefix
 
             if ( !get_xref( $get_interpro_sth, $interpro, $id ) ) {
                 $add_interpro_sth->execute( $interpro, $id );
