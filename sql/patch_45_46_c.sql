df72c2369806c1662155aac5129b2a5afc4f1d37
Glenn Proctor <gp1@sanger.ac.uk>
2007-07-13 14:27:59 +0000

    Was acting on the wrong table!

--- sql/patch_45_46_c.sql
+++ sql/patch_45_46_c.sql
@@ -5,7 +5,7 @@
 # description:
 # Make external_db column in unmapped_object be SMALLINT UNSIGNED explicitly.
 
-ALTER TABLE external_db CHANGE COLUMN external_db_id external_db_id SMALLINT UNSIGNED;
+ALTER TABLE unmapped_object CHANGE COLUMN external_db_id external_db_id SMALLINT UNSIGNED;
 
 # patch identifier
 INSERT INTO meta (meta_key, meta_value) VALUES ('patch', 'patch_45_46_c.sql|unmapped_object.external_db_id');
