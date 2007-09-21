9cf44613ff8361bac97a1fe19b68515177cc81d9
Glenn Proctor <gp1@sanger.ac.uk>
2007-09-21 11:40:20 +0000

    Added NOT NULL constraint to db_name.

--- sql/patch_46_47_c.sql
+++ sql/patch_46_47_c.sql
@@ -6,7 +6,7 @@
 # Make db_release column of external_db VARCHAR(255)
 
 ALTER TABLE external_db CHANGE COLUMN db_release db_release VARCHAR(255);
-ALTER TABLE external_db CHANGE COLUMN db_name db_name VARCHAR(28);
+ALTER TABLE external_db CHANGE COLUMN db_name db_name VARCHAR(28) NOT NULL;
 
 # patch identifier
 INSERT INTO meta (meta_key, meta_value) VALUES ('patch', 'patch_46_47_c.sql|extend_db_release');
