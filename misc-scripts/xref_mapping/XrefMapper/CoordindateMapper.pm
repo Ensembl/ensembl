6c8f2e4ba22b21103ba97e1acf0489bb0b0e19d5
Andreas Kusalananda Kähäri <ak4@sanger.ac.uk>
2007-11-06 11:01:47 +0000

    Code for the BasicMapper to do Xref mapping based on coordinate overlaps. Only contains stubs at the moment.

---
+++ misc-scripts/xref_mapping/XrefMapper/CoordindateMapper.pm
@@ -0,0 +1,17 @@
+# $Id$
+
+# This is a set of subroutines used for creating Xrefs based on
+# coordinate overlaps.
+
+package XrefMapper::CoordinateMapper;
+
+use strict;
+use warnings;
+
+sub rub_coordinatemapping {
+  my $self = shift;
+
+  # Just a stub for now.
+}
+
+1;
