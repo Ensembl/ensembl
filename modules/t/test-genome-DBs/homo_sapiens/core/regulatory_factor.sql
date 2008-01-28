10113ced99dc4c111e7240d334229e34cd133bd8
Ian Longden <ianl@sanger.ac.uk>
2008-01-28 16:38:26 +0000

    removing old test

--- modules/t/test-genome-DBs/homo_sapiens/core/regulatory_factor.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE `regulatory_factor` (
-  `regulatory_factor_id` int(11) NOT NULL auto_increment,
-  `name` varchar(255) NOT NULL default '',
-  `type` enum('miRNA_target','transcription_factor','transcription_factor_complex') default NULL,
-  PRIMARY KEY  (`regulatory_factor_id`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
-
--- modules/t/test-genome-DBs/homo_sapiens/core/regulatory_factor_coding.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE `regulatory_factor_coding` (
-  `regulatory_factor_id` int(11) NOT NULL default '0',
-  `transcript_id` int(11) default NULL,
-  `gene_id` int(11) default NULL,
-  KEY `transcript_idx` (`transcript_id`),
-  KEY `gene_idx` (`gene_id`),
-  KEY `regulatory_factor_idx` (`regulatory_factor_id`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
-
