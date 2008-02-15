97134e11763d9e33dee3d4c28f9850f432c8265f
Andreas Kusalananda Kähäri <ak4@sanger.ac.uk>
2008-02-15 10:17:53 +0000

    New feature collection code with new class structure.  Using RepeatFeature as the test case for now.  Won't do anything useful just yet.

---
+++ modules/Bio/EnsEMBL/Collection.pm
@@ -0,0 +1,33 @@
+package Bio::EnsEMBL::Collection;
+
+use strict;
+use warnings;
+
+use Bio::EnsEMBL::Utils::Argument  ('rearrange');
+use Bio::EnsEMBL::Utils::Exception ('throw');
+
+sub _remap {
+  my ( $features, $mapper, $slice ) = @_;
+
+  return $features;
+}
+
+sub _create_feature {
+  my ( $this, $feature_type, $args ) = @_;
+
+  my ( $dbid, $start, $end, $strand, $slice ) =
+    rearrange( [ 'DBID', 'START', 'END', 'STRAND', 'SLICE' ],
+               @{$args} );
+
+  return [ $dbid, $slice->get_seq_region_id(), $start, $end, $strand ];
+}
+
+sub _create_feature_fast {
+  my ( $this, $feature_type, $args ) = @_;
+
+  return [ $args->{'dbID'},  $args->{'slice'}->get_seq_region_id(),
+           $args->{'start'}, $args->{'end'},
+           $args->{'strand'} ];
+}
+
+1;
---
+++ modules/Bio/EnsEMBL/Collection/RepeatFeature.pm
@@ -0,0 +1,48 @@
+# $Id$
+
+package Bio::EnsEMBL::Collection::RepeatFeature;
+
+use strict;
+use warnings;
+
+use Bio::EnsEMBL::Utils::Argument  ('rearrange');
+use Bio::EnsEMBL::Utils::Exception ('throw');
+
+use base( 'Bio::EnsEMBL::Collection',
+          'Bio::EnsEMBL::DBSQL::RepeatFeatureAdaptor' );
+
+sub _create_feature {
+  my ( $this, $feature_type, $args ) = @_;
+
+  my @feature =
+    @{ $this->SUPER::_create_feature( $feature_type, $args ) };
+
+  my ( $hstart, $hend, $score, $repeat_consensus, $analysis ) =
+    rearrange( [ 'HSTART', 'HEND',
+                 'SCORE',  'REPEAT_CONSENSUS',
+                 'ANALYSIS'
+               ],
+               @{$args} );
+
+  push( @feature,
+        $hstart, $hend, $score, $repeat_consensus->dbID(),
+        $analysis->dbID() );
+
+  return \@feature;
+}
+
+sub _create_feature_fast {
+  my ( $this, $feature_type, $args ) = @_;
+
+  my @feature =
+    @{ $this->SUPER::_create_feature_fast( $feature_type, $args ) };
+
+  push( @feature,
+        $args->{'hstart'}, $args->{'hend'}, $args->{'score'},
+        $args->{'repeat_consensus'}->dbID(),
+        $args->{'analysis'}->dbID() );
+
+  return \@feature;
+}
+
+1;
