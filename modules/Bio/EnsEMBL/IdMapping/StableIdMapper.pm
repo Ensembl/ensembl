0858b9cfd0e757aac01ce763eb73942ca4273c12
Andreas Kusalananda Kähäri <ak4@sanger.ac.uk>
2008-04-04 14:52:07 +0000

    Avoid division by zero in generate_mapping_stats().

--- modules/Bio/EnsEMBL/IdMapping/StableIdMapper.pm
+++ modules/Bio/EnsEMBL/IdMapping/StableIdMapper.pm
@@ -691,13 +691,35 @@ sub generate_mapping_stats {
   my $novel_total = $stats->{'mapped_novel'} + $stats->{'lost_novel'};
 
   # no split into known and novel for exons
-  unless ($type eq 'exon') {
-    $result .= sprintf($fmt2, 'known', $stats->{'mapped_known'},
-      $stats->{'lost_known'}, $stats->{'mapped_known'}/$known_total*100);
-    $result .= sprintf($fmt2, 'novel', $stats->{'mapped_novel'},
-      $stats->{'lost_novel'}, $stats->{'mapped_novel'}/$novel_total*100);
-  }
-  
+  unless ( $type eq 'exon' ) {
+    if ( $known_total > 0 ) {
+      $result .= sprintf( $fmt2,
+                          'known',
+                          $stats->{'mapped_known'},
+                          $stats->{'lost_known'},
+                          $stats->{'mapped_known'}/$known_total*100 );
+    } else {    # Avoid division by zero
+      $result .= sprintf( $fmt2,
+                          'known',
+                          $stats->{'mapped_known'},
+                          $stats->{'lost_known'}, 0 );
+    }
+
+    if ( $novel_total > 0 ) {
+      $result .= sprintf( $fmt2,
+                          'novel',
+                          $stats->{'mapped_novel'},
+                          $stats->{'lost_novel'},
+                          $stats->{'mapped_novel'}/$novel_total*100 );
+    } else {    # Avoid division by zero
+      $result .= sprintf( $fmt2,
+                          'novel',
+                          $stats->{'mapped_novel'},
+                          $stats->{'lost_novel'}, 0 );
+    }
+
+  } ## end unless ( $type eq 'exon' )
+
   $result .= sprintf($fmt2, 'total', $mapped_total, $lost_total,
     $mapped_total/($known_total + $novel_total)*100);
 
