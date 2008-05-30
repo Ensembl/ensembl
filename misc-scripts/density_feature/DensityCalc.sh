8c1b450c21459c0dd1e54e41f0b092254e4f80d2
Bronwen Aken <ba1@sanger.ac.uk>
2008-05-30 14:21:55 +0000

    Allows user to enter output dir. (If -outdir is not specified, or if the dir does not exist, then output from bjobs is written to pwd)

--- misc-scripts/density_feature/DensityCalc.sh
+++ misc-scripts/density_feature/DensityCalc.sh
@@ -3,6 +3,8 @@
 # This script submits jobs to the farm to calculate the various density
 # features for a particular core database.
 
+# If an output dir is not specified, output from bjobs will be written to PWD.
+
 # Default values for command line switches:
 
 host='ens-staging'  # -h
@@ -10,14 +12,17 @@ port='3306'         # -P
 user='ensadmin'     # -u
 pass=               # -p
 dbname=             # -d
+outdir=             # -o
 
-while getopts 'h:P:d:u:p:' opt; do
+
+while getopts 'h:P:d:u:p:o:' opt; do
   case ${opt} in
     h)  host=${OPTARG} ;;
     P)  port=${OPTARG} ;;
     d)  dbname=${OPTARG} ;;
     u)  user=${OPTARG} ;;
     p)  pass=${OPTARG} ;;
+    o)  outdir=${OPTARG} ;;
   esac
 done
 
@@ -26,6 +31,14 @@ then
   print -u2 "Usage:\n\t$0 -h host -P port -d database -u user -p password"
   exit 1
 fi
+
+# Enter an output dir so results don't get written in the same place as your script
+if [[ -z ${outdir} || ! -d ${outdir} ]]
+then
+  outdir=$PWD
+fi
+
+echo "\nOutput dir is '${outdir}'\n"
 
 # Make sure this is a core database.
 if [[ -n ${dbname##*_core_*} ]]; then
@@ -36,7 +49,7 @@ fi
 print "Submitting percent GC calculation to queue 'normal'"
 print "\tThe output from this job goes to the file"
 print "\t'${dbname}_gc.out'"
-bsub -q normal -J gc_calc -oo ${dbname}_gc.out \
+bsub -q normal -J gc_calc -oo ${outdir}/${dbname}_gc.out \
   perl ./percent_gc_calc.pl \
   -host ${host} \
   -port ${port} \
@@ -47,7 +60,7 @@ bsub -q normal -J gc_calc -oo ${dbname}_gc.out \
 print "Submitting gene density calculation to queue 'normal'"
 print "\tThe output from this job goes to the file"
 print "\t'${dbname}_gene.out'"
-bsub -q normal -J gene_density -oo ${dbname}_gene.out \
+bsub -q normal -J gene_density -oo ${outdir}/${dbname}_gene.out \
   perl ./gene_density_calc.pl \
   -host ${host} \
   -port ${port} \
@@ -58,7 +71,7 @@ bsub -q normal -J gene_density -oo ${dbname}_gene.out \
 print "Submitting repeat coverage calculation to queue 'long'"
 print "\tThe output from this job goes to the file"
 print "\t'${dbname}_repeat.out'"
-bsub -q long -J repeat_cov -oo ${dbname}_repeat.out \
+bsub -q long -J repeat_cov -oo ${outdir}/${dbname}_repeat.out \
   perl ./repeat_coverage_calc.pl \
   -host ${host} \
   -port ${port} \
@@ -69,7 +82,7 @@ bsub -q long -J repeat_cov -oo ${dbname}_repeat.out \
 print "Submitting variation density calculation to queue 'normal'"
 print "\tThe output from this job goes to the file"
 print "\t'${dbname}_var.out'"
-bsub -q normal -J var_density -oo ${dbname}_var.out \
+bsub -q normal -J var_density -oo ${outdir}/${dbname}_var.out \
   perl ./variation_density.pl \
   -host ${host} \
   -port ${port} \
@@ -80,7 +93,7 @@ bsub -q normal -J var_density -oo ${dbname}_var.out \
 print "Submitting seq region stats calculation to queue 'normal'"
 print "\tThe output from this job goes to the file"
 print "\t'${dbname}_seqreg.out'"
-bsub -q normal -J seqreg_stats -oo ${dbname}_seqreg.out \
+bsub -q normal -J seqreg_stats -oo ${outdir}/${dbname}_seqreg.out \
   perl ./seq_region_stats.pl \
   -host ${host} \
   -port ${port} \
