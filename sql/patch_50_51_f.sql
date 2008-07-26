9fe37c56a887d8b18217efb116e9401dc3981c1f
ens-stephenf <stephenf@ebi.ac.uk>
2008-07-26 19:50:56 +0000

    corrected error in sql syntax.

--- sql/patch_50_51_f.sql
+++ sql/patch_50_51_f.sql
@@ -5,7 +5,7 @@
 # description:
 # Set the species_id of non-species-specific rows in the meta table to be NULL.
 
-UPDATE TABLE meta SET species_id = NULL WHERE meta_key IN ('patch', 'schema_version');
+UPDATE  meta SET species_id = NULL WHERE meta_key IN ('patch', 'schema_version');
 
 # patch identifier
 INSERT INTO meta (species_id, meta_key, meta_value) VALUES (NULL, 'patch', 'patch_50_51_f.sql|meta_species_id_values');
