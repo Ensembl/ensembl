0ad40ad32ec94c04b4199fa28d36987e7cd395f3
Andreas Kusalananda Kähäri <ak4@sanger.ac.uk>
2008-08-07 13:44:38 +0000

    Only join with seq_region and coord_system in generic_fetch() if this is a multi-species database.
    API passes unit tests.

--- modules/Bio/EnsEMBL/DBSQL/BaseAdaptor.pm
+++ modules/Bio/EnsEMBL/DBSQL/BaseAdaptor.pm
@@ -374,7 +374,8 @@ sub generic_fetch {
 
   # Hack for feature types that needs to be restricted to species_id (in
   # coord_system).
-  if ( $self->isa('Bio::EnsEMBL::DBSQL::BaseFeatureAdaptor')
+  if (    $self->is_multispecies()
+       && $self->isa('Bio::EnsEMBL::DBSQL::BaseFeatureAdaptor')
        && !$self->isa('Bio::EnsEMBL::DBSQL::UnmappedObjectAdaptor') )
   {
     push @tabs, [ 'seq_region', 'sr' ], [ 'coord_system', 'cs' ];
