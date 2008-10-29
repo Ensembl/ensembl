3ab90c131cfc4675a55229c2ecf3fb3ad6fb2bd1
Andreas Kusalananda Kähäri <ak4@sanger.ac.uk>
2008-10-29 15:24:46 +0000

    Module description less apologetic. Formatting of method documentation.

--- modules/Bio/EnsEMBL/DBSQL/BaseAlignFeatureAdaptor.pm
+++ modules/Bio/EnsEMBL/DBSQL/BaseAlignFeatureAdaptor.pm
@@ -14,18 +14,18 @@ AlignFeatureAdaptors
 
 =head1 SYNOPSIS
 
-Abstract class - should not be instantiated.  Implementation of
-abstract methods must be performed by subclasses.
+Abstract class, should not be instantiated.  Implementation of abstract
+methods must be performed by subclasses.
 
 =head1 DESCRIPTION
 
-This is a base adaptor for align adaptors.  Since DnaAlignFeatureAdaptor and
-PepAlignFeatureAdaptor had almost the same functionality it made sense to 
-streamline by creating this superclass.
+This is a base adaptor for the align feature adaptors
+DnaAlignFeatureAdaptor and PepAlignFeatureAdaptor.
 
 =head1 CONTACT
 
-Post questions/comments to the ensembl developer list: ensembl-dev@ebi.ac.uk
+Post questions/comments to the ensembl developer list:
+ensembl-dev@ebi.ac.uk
 
 =head1 METHODS
 
@@ -46,15 +46,18 @@ use Bio::EnsEMBL::DBSQL::BaseFeatureAdaptor;
   Arg [1]    : Bio::EnsEMBL::Slice $slice
                The slice from which to obtain align features.
   Arg [2]    : (optional) float $hcoverage 
-               a lower bound for the hcoverage of feats to obtain
+               A lower bound for the hcoverage of feats to obtain.
   Arg [3]    : (optional) string $logic_name
-               the logic name of the type of features to obtain
-  Example    : @feats = $adaptor->fetch_all_by_Slice_and_hcoverage($slice, 50.0);
-  Description: Returns a listref of features created from the database which
-               are on the Slice $slice and with a hcoverage
-               greater than $hcoverage.  If logic name is defined, only features
-               with an analysis of type $logic_name will be returned.
-  Returntype : listref of Bio::EnsEMBL::BaseAlignFeatures in Slice coordinates
+               The logic name of the type of features to obtain.
+  Example    : @feats =
+                $adaptor->fetch_all_by_Slice_and_hcoverage( $slice, 50.0 );
+  Description: Returns a listref of features created from the
+               database which are on the Slice $slice and with a
+               hcoverage greater than $hcoverage.  If logic name
+               is defined, only features with an analysis of type
+               $logic_name will be returned.
+  Returntype : listref of Bio::EnsEMBL::BaseAlignFeatures
+               in Slice coordinates
   Exceptions : thrown if pid is not defined
   Caller     : general
   Status     : At Risk
@@ -80,15 +83,18 @@ sub fetch_all_by_Slice_and_hcoverage {
   Arg [1]    : Bio::EnsEMBL::Slice $slice
                The slice from which to obtain align features.
   Arg [2]    : (optional) float $pid 
-               a lower bound for the percentage identity of feats to obtain
+               A lower bound for the percentage identity of features
+               to obtain.
   Arg [3]    : (optional) string $logic_name
-               the logic name of the type of features to obtain
-  Example    : @feats = $adaptor->fetch_all_by_Slice_and_pid($slice, 50.0);
-  Description: Returns a listref of features created from the database which
-               are on the Slice $slice and with a percentage identity
-               greater than $pid.  If logic name is defined, only features
-               with an analysis of type $logic_name will be returned.
-  Returntype : listref of Bio::EnsEMBL::BaseAlignFeatures in Slice coordinates
+               The logic name of the type of features to obtain.
+  Example    : @feats = $adaptor->fetch_all_by_Slice_and_pid( $slice, 50.0 );
+  Description: Returns a listref of features created from the
+               database which are on the Slice $slice and with a
+               percentage identity greater than $pid.  If logic name
+               is defined, only features with an analysis of type
+               $logic_name will be returned.
+  Returntype : listref of Bio::EnsEMBL::BaseAlignFeatures
+               in Slice coordinates
   Exceptions : thrown if pid is not defined
   Caller     : general
   Status     : Stable
@@ -120,14 +126,16 @@ sub fetch_all_by_Slice_and_pid {
 =head2 fetch_all_by_hit_name
 
   Arg [1]    : string $hit_name
-               the hit_name of the features to obtain
+               The hit_name of the features to obtain
   Arg [2]    : (optional) string $logic_name
-               the analysis logic name of the type of features to obtain
-  Example    : @feats = $adaptor->fetch_all_by_hit_name($name, $logic_name);
-  Description: Returns a listref of features created from the database
-               which correspond to the given hit_name. If logic name
-               is defined, only features with an analysis of type
-               $logic_name will be returned.
+               The analysis logic name of the type of features to
+               obtain.
+  Example    : @feats =
+                $adaptor->fetch_all_by_hit_name( $name, $logic_name );
+  Description: Returns a listref of features created from the
+               database which correspond to the given hit_name.  If
+               logic name is defined, only features with an analysis
+               of type $logic_name will be returned.
   Returntype : listref of Bio::EnsEMBL::BaseAlignFeatures
   Exceptions : thrown if hit_name is not defined
   Caller     : general
@@ -155,15 +163,18 @@ sub fetch_all_by_hit_name{
 =head2 fetch_all_by_hit_name_unversioned
 
   Arg [1]    : string $hit_name
-               the beginning of the hit_name of the features to obtain
-               e.g. AA768786 would retrieve AA768786.1, AA768786.2 etc
+               The beginning of the hit_name of the features to
+               obtain, e.g. AA768786 would retrieve AA768786.1,
+               AA768786.2 etc.
   Arg [2]    : (optional) string $logic_name
-               the analysis logic name of the type of features to obtain
-  Example    : @feats = $adaptor->fetch_all_by_hit_name($name, $logic_name);
-  Description: Returns a listref of features created from the database
-               which start with the  given hit_name. If logic name
-               is defined, only features with an analysis of type
-               $logic_name will be returned.
+               The analysis logic name of the type of features to
+               obtain.
+  Example    : @feats =
+                $adaptor->fetch_all_by_hit_name( $name, $logic_name );
+  Description: Returns a listref of features created from the
+               database which start with the given hit_name.  If
+               logic name is defined, only features with an analysis
+               of type $logic_name will be returned.
   Returntype : listref of Bio::EnsEMBL::BaseAlignFeatures
   Exceptions : thrown if hit_name is not defined
   Caller     : general
