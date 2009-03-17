7ac8de25fc7e428ec25b2e725a787f5d7c0660a4
Andreas Kusalananda Kähäri <ak4@sanger.ac.uk>
2009-03-17 12:44:03 +0000

    Readable.

--- modules/Bio/EnsEMBL/Utils/Argument.pm
+++ modules/Bio/EnsEMBL/Utils/Argument.pm
@@ -128,23 +128,31 @@ use vars qw(@ISA @EXPORT);
 
 sub rearrange {
   my $order = shift;
-  $order = shift if($order eq "Bio::EnsEMBL::Utils::Argument"); #skip object if one provided
+
+  if ( $order eq "Bio::EnsEMBL::Utils::Argument" ) {
+    # skip object if one provided
+    $order = shift;
+  }
 
   # If we've got parameters, we need to check to see whether
   # they are named or simply listed. If they are listed, we
   # can just return them.
-  return @_ unless (@_ && $_[0] && substr($_[0], 0,1) eq '-');
+  unless ( @_ && $_[0] && substr( $_[0], 0, 1 ) eq '-' ) {
+    return @_;
+  }
 
   # Convert all of the parameter names to uppercase, and create a
   # hash with parameter names as keys, and parameter values as values
   my $i = 0;
-  my (%param) = map {if($i) { $i--; $_; } else { $i++; uc($_); }} @_;
-
+  my (%param) = map {
+    if   ($i) { $i--; $_; }
+    else      { $i++; uc($_); }
+  } @_;
 
   # What we intend to do is loop through the @{$order} variable,
   # and for each value, we use that as a key into our associative
   # array, pushing the value at that key onto our return array.
-  return map {$param{uc("-$_")}} @$order;
+  return map { $param{ uc("-$_") } } @$order;
 }
 
 1;
