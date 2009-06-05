622bc0af835d5269881e18d63702d67784ec709a
Karyn Megy <kmegy@sanger.ac.uk>
2009-06-05 11:03:06 +0000

    Update Anoph_Symbol to VB_Community_Symbol

--- misc-scripts/xref_mapping/XrefMapper/anopheles_gambiae.pm
+++ misc-scripts/xref_mapping/XrefMapper/anopheles_gambiae.pm
@@ -1,4 +1,5 @@
 
+
 package XrefMapper::anopheles_gambiae;
 
 use  XrefMapper::BasicMapper;
@@ -18,13 +19,17 @@ sub get_set_lists {
 # since anopheles_symbol is "before" Uniprot
 
 # If there is an Anopheles_symbol xref, use its description
+
+# mh4 says Anopheles_symbol doesn't get chosen over UniP
+# (but they do get chosen in other cases)
+
 sub gene_description_sources {
 
-  return ("Anopheles_symbol",
+  return ("VB_Community_Symbol",
 	  "Uniprot/SWISSPROT",
-	  "RefSeq_peptide",
-	  "RefSeq_dna",
-	  "Uniprot/SPTREMBL",
+	  #"RefSeq_peptide",
+	  #"RefSeq_dna",
+	  #"Uniprot/SPTREMBL",
 	  #"RefSeq_peptide_predicted",
 	  #"RefSeq_dna_predicted",
 	  #"EntrezGene");
@@ -35,10 +40,11 @@ sub transcript_display_xref_sources {
 
   my @list = qw(RFAM
 		miRBase
-		Anopheles_symbol
+		VB_Community_Symbol
 		Uniprot/SWISSPROT
-		Uniprot/Varsplic
-		Uniprot/SPTREMBL);
+		#Uniprot/Varsplic
+		#Uniprot/SPTREMBL);
+   );
 
   my %ignore;
   return [\@list,\%ignore];
