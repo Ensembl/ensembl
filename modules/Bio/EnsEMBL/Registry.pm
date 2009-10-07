50f4a77d61353be7ce082ace81518cdd7f4ad4f5
Glenn Proctor <gp1@sanger.ac.uk>
2009-10-07 07:06:56 +0000

    Updated to version 57.

--- modules/Bio/EnsEMBL/Registry.pm
+++ modules/Bio/EnsEMBL/Registry.pm
@@ -129,7 +129,7 @@ use DBI;
 
 use vars qw(%registry_register);
 
-my $API_VERSION = 56;
+my $API_VERSION = 57;
 
 # This is a map from group names to Ensembl DB adaptors.  Used by
 # load_all() and reset_DBAdaptor().
