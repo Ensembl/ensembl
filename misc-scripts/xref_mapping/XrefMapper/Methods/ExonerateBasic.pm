2587b586aad4cf7c4615894e42c8d97310a75389
Ian Longden <ianl@sanger.ac.uk>
2009-12-11 10:04:02 +0000

    removed old test case of incorrect exonerate path

--- misc-scripts/xref_mapping/XrefMapper/Methods/ExonerateBasic.pm
+++ misc-scripts/xref_mapping/XrefMapper/Methods/ExonerateBasic.pm
@@ -9,8 +9,7 @@ use File::Basename;
 use IPC::Open3;
 
 # Path to exonerate executable
-#my $exonerate_path = "/usr/local/ensembl/bin/exonerate-0.9.0";
-my $exonerate_path = "/rubbish/software/ensembl/bin/exonerate-1.4.0"; #  remove rubbish after wards as just to test
+my $exonerate_path = "/usr/local/ensembl/bin/exonerate-0.9.0";
 
 sub new {
 
