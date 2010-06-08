d8b6a94d7338901ac66b6394523fa8a4bab9ba2b
Andreas Kusalananda Kähäri <ak4@sanger.ac.uk>
2010-06-08 14:29:55 +0000

    Don't just drop the old column but use it to update the new column with data from the attrib_type table.

--- sql/patch_58_59_c.sql
+++ sql/patch_58_59_c.sql
@@ -7,11 +7,20 @@
 #   The 'type' enumeration in the splicing_event table is too terse.
 #   Replace it with a reference to a proper attrib_type.
 
-# Modify the splicing_event table.
+# Add the new column.
 ALTER TABLE splicing_event
-  DROP COLUMN `type`,
   ADD COLUMN attrib_type_id SMALLINT(5) UNSIGNED NOT NULL DEFAULT 0
     AFTER seq_region_strand;
+
+# Update the new column using info from the old column and the
+# attrib_type table.
+UPDATE  splicing_event se
+  JOIN  attribute_type at USING (se.`type` = at.code)
+SET     se.attrib_type_id = at.attribut_type_id
+WHERE   at.attribut_type_id BETWEEN 300 AND 311;
+
+# Drop the old column.
+ALTER TABLE splicing_event DROP COLUMN `type`;
 
 # Insert patch identifier.
 INSERT INTO meta (species_id, meta_key, meta_value)
