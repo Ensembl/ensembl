18ad6df9d0d3019b151d44d7d607ef6809c1c5ab
Andreas Kusalananda Kähäri <ak4@sanger.ac.uk>
2010-06-10 13:13:17 +0000

    Fix SQL bugs.

--- sql/patch_58_59_c.sql
+++ sql/patch_58_59_c.sql
@@ -15,9 +15,9 @@ ALTER TABLE splicing_event
 # Update the new column using info from the old column and the
 # attrib_type table.
 UPDATE  splicing_event se
-  JOIN  attribute_type at ON (se.`type` = at.code)
-SET     se.attrib_type_id = at.attribut_type_id
-WHERE   at.attribut_type_id BETWEEN 300 AND 311;
+  JOIN  attrib_type at ON (se.`type` = at.code)
+SET     se.attrib_type_id = at.attrib_type_id
+WHERE   at.attrib_type_id BETWEEN 300 AND 311;
 
 # Drop the old column.
 ALTER TABLE splicing_event DROP COLUMN `type`;
