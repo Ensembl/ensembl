eb77d6b7770836e6df18fe5b5bfb41a836dc58b7
Ian Longden <ianl@sanger.ac.uk>
2010-06-28 14:31:43 +0000

    uncomment code needed to chekc for HAP REF

--- modules/Bio/EnsEMBL/Feature.pm
+++ modules/Bio/EnsEMBL/Feature.pm
@@ -1097,10 +1097,12 @@ sub get_all_alt_locations {
     } elsif(uc($axf->type()) =~ 'PAR') {
       push @alt, $axf;
     } elsif( $axf->type() eq "PATCH_FIX"){
-#    } elsif( $axf->type() eq "HAP REF" ) {
-#      push @haps, $axf if $return_all > 0 ;
-#      # do nothing when you are on REF
+      push @haps, $axf;
+    } elsif( $axf->type() eq "HAP REF" ) { 
+      push @haps, $axf if $return_all > 0 ;
+      # do nothing when you are on REF
     } elsif( $axf->type() eq "PATCH_NOVEL"){
+      push @haps, $axf;
     } else {
       warning("Unknown exception feature type ". $axf->type()."- ignoring.");
     }
