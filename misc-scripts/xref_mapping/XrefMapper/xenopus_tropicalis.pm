af19e37cf37e22e522a06288d7f1581f84f3451e
Ian Longden <ianl@sanger.ac.uk>
2010-08-13 07:44:47 +0000

    Added new direct Jamboree mappings

--- misc-scripts/xref_mapping/XrefMapper/xenopus_tropicalis.pm
+++ misc-scripts/xref_mapping/XrefMapper/xenopus_tropicalis.pm
@@ -12,7 +12,24 @@ sub get_set_lists{
 
 }
 
+sub gene_description_sources {
+
+  return ("Xenopus_Jamboree",
+	  "MGI",
+	  "RFAM",
+	  "flybase_symbol",
+	  "Anopheles_symbol",
+	  "Genoscope_annotated_gene",
+	  "Uniprot/SWISSPROT",
+	  "RefSeq_peptide",
+	  "RefSeq_dna",
+	  "Uniprot/SPTREMBL",
+	  "EntrezGene");
+}
+
 sub transcript_display_xref_sources {
+  my $self     = shift;
+  my $fullmode = shift;
 
   my @list = qw(Xenopus_Jamboree
 		MGI
@@ -27,8 +44,12 @@ sub transcript_display_xref_sources {
 
 
   my %ignore;
-  $ignore{"EntrezGene"}= 'FROM:RefSeq_[pd][en][pa].*_predicted';
-  
+  if(!$fullmode){
+    $ignore{"EntrezGene"}= 'FROM:RefSeq_[pd][en][pa].*_predicted';
+  }
+  else{
+    $ignore{"EntrezGene"} = 'select ox.object_xref_id from object_xref ox, dependent_xref dx, source s1, xref x1, source s2, xref x2 where ox.object_xref_id = dx.object_xref_id and dx.dependent_xref_id = x1.xref_id and x1.source_id = s1.source_id and s1.name = "EntrezGene" and x2.xref_id = dx.master_xref_id and x2.source_id = s2.source_id and (s2.name like "Refseq_dna_predicted" or s2.name like "RefSeq_peptide_predicted") and ox.ox_status = "DUMP_OUT"';
+  }
   return [\@list,\%ignore];
 }
 
--- misc-scripts/xref_mapping/XrefParser/XenopusJamboreeParser.pm
+++ misc-scripts/xref_mapping/XrefParser/XenopusJamboreeParser.pm
@@ -7,10 +7,6 @@ use File::Basename;
 
 use base qw( XrefParser::BaseParser );
 
-# Xenopus Jamboree peptides file format: fasta, e.g.
-
-# >anxa2 BC061610
-# MSLIHEILGKLSLEGNQSSTRQSTLGSVKASSNFDAERDAAAIETAIKTKGVDELTII
 
 
 sub run {
@@ -25,50 +21,25 @@ sub run {
 
   my $file = @{$files}[0];
 
-  my @xrefs;
-
-  local $/ = "\n>";
-
-  my $file_io = $self->getline($file);
+  my $file_io = $self->get_filehandle($file);
 
   if ( !defined $file_io ) {
     print STDERR "ERROR: Could not open $file\n";
     return 1;    # 1 error
   }
 
+  my $count = 0;
   while ( $_ = $file_io->getline() ) {
-    my $xref;
+    chomp;
+    my ($acc, $label, $desc, $stable_id) = split /\t/;
 
-    my ($header, $sequence) = $_ =~ /^>?(.+?)\n([^>]*)/s or warn("Can't parse FASTA entry: $_\n");
-
-    # deconstruct header
-    my ($accession, @other) = split /\s+/, $header;
-    my $other = join(" ", @other);
-
-    # make sequence into one long string
-    $sequence =~ s/\n//g;
-
-    # build the xref object and store it
-    $xref->{ACCESSION}     = $accession;
-    $xref->{LABEL}         = $accession;
-    $xref->{DESCRIPTION}   = '';
-    $xref->{SEQUENCE}      = $sequence;
-    $xref->{SOURCE_ID}     = $source_id;
-    $xref->{SPECIES_ID}    = $species_id;
-    $xref->{SEQUENCE_TYPE} = 'peptide';
-    $xref->{STATUS}        = 'experimental';
-
-    push @xrefs, $xref;
-
+    XrefParser::BaseParser->add_to_direct_xrefs($stable_id,'gene', $acc, '', $label, $desc, "", $source_id, $species_id);
+    $count++;
   }
 
   $file_io->close();
 
-  print scalar(@xrefs) . " XenopusJamboreeParser xrefs succesfully parsed\n" if($verbose);
-
-  if(!defined(XrefParser::BaseParser->upload_xref_object_graphs(\@xrefs))){
-    return 1; #1 error
-  }
+  print $count . " XenopusJamboreeParser xrefs succesfully parsed\n" if($verbose);
 
   return 0;
 }
