654b13de49906bdcc9af33899bc50dc0b1b647ef
Andreas Kusalananda Kähäri <ak4@sanger.ac.uk>
2010-09-20 13:24:17 +0000

    Commit forgottern change in variable names from last week.

--- misc-scripts/production_database/scripts/update_database_list.pl
+++ misc-scripts/production_database/scripts/update_database_list.pl
@@ -54,10 +54,10 @@ my @servers = ('ens-staging1', 'ens-staging2');
 my $master = 'ens-staging1';
 
 my $dbport = '3306';
-my $dbuser = 'ensadmin';
+my $dbwuser = 'ensadmin';
+my $dbwpass;
+my $dbuser = 'ensro';
 my $dbpass;
-my $dbrouser = 'ensro';
-my $dbropass;
 
 my $opt_help  = 0;
 my $opt_about = 0;
