a36d283295cf820c82c16d62a6f8b90c8a9bcc58
Glenn Proctor <gp1@sanger.ac.uk>
2010-09-24 12:40:11 +0000

    Many improvements to reduce memory usage. Now uses registry, so don't need to specify databases explicitly, just use e.g. -species human

--- misc-scripts/density_feature/variation_density.pl
+++ misc-scripts/density_feature/variation_density.pl
@@ -1,11 +1,7 @@
 #!/usr/local/ensembl/bin/perl -w
 
-#
-# calculates the variation density from given core database
-# It finds Variation database by itself using naming convention
-# s/core/variation/
-#
-#
+# Calculates the variation density from given core database
+
 use strict;
 
 use Bio::EnsEMBL::DBSQL::DBAdaptor;
@@ -17,105 +13,73 @@ use Getopt::Long;
 use Data::Dumper;
 $Data::Dumper::Maxdepth = 2;
 
-my $bin_count  = 150;
 my $max_slices = 100;
+my $bin_count  = 150;
 
-my ( $host, $user, $pass, $port, $dbname  );
+my ($host, $user, $pass, $port, $species);
 
-my ( $block_count, $genome_size, $block_size );
+my ($block_count, $genome_size, $block_size );
 
-GetOptions( "host=s", \$host,
-	    "user=s", \$user,
-	    "pass=s", \$pass,
-	    "port=i", \$port,
-	    "dbname=s", \$dbname
-	  );
+GetOptions( "host=s",     \$host,
+	    "user=s",     \$user,
+	    "pass=s",     \$pass,
+	    "port=i",     \$port,
+	    "species=s",  \$species );
 
+Bio::EnsEMBL::Registry->load_registry_from_db(-host => $host, -user => $user, -pass => $pass, -port => $port);
 
-my $db = new Bio::EnsEMBL::DBSQL::DBAdaptor(-host => $host,
-					    -user => $user,
-					    -port => $port,
-					    -pass => $pass,
-					    -dbname => $dbname,
-					    -group => 'core',
-					    -species => 'DEFAULT'
-					   );
+my $density_feature_adaptor   = Bio::EnsEMBL::Registry->get_adaptor($species, "core", "DensityFeature")        || die "Can't create density feature adaptor";
+my $density_type_adaptor      = Bio::EnsEMBL::Registry->get_adaptor($species, "core", "DensityType")           || die "Can't create density type adaptor";
+my $analysis_adaptor          = Bio::EnsEMBL::Registry->get_adaptor($species, "core", "analysis")              || die "Can't create analysis adaptor";
+my $slice_adaptor             = Bio::EnsEMBL::Registry->get_adaptor($species, "core", "slice")                 || die "Can't create slice adaptor";
 
-if( ! variation_attach( $db )) {
-  die( "Couldnt attach variation to $dbname" );
-}
+my $variation_feature_adaptor = Bio::EnsEMBL::Registry->get_adaptor($species, "variation", "VariationFeature") || die "Can't create variation feature adaptor";
 
+# TODO - variation from registry
 
-#
-# Clean up old features first. Also remove analysis and density type entry as these are recreated.
-#
-
-my $sth = $db->dbc->prepare("DELETE df, dt, a, ad FROM analysis_description ad, density_feature df, density_type dt, analysis a WHERE ad.analysis_id = a.analysis_id AND a.analysis_id=dt.analysis_id AND dt.density_type_id=df.density_type_id AND a.logic_name='snpDensity'");
+# Clean up old features first. Also remove analysis and density type entry as these are recreated
+my $sth = $slice_adaptor->dbc->prepare("DELETE df, dt, a, ad FROM analysis_description ad, density_feature df, density_type dt, analysis a WHERE ad.analysis_id = a.analysis_id AND a.analysis_id=dt.analysis_id AND dt.density_type_id=df.density_type_id AND a.logic_name='snpDensity'");
 $sth->execute();
 
-# $sth = $db->dbc()->prepare(
-#   qq(
-#   DELETE ad
-#   FROM analysis_description ad
-#   WHERE ad.display_label = 'snpDensity') );
-# $sth->execute();
-
-#
-# Get the adaptors needed;
-#
-
-my $dfa = $db->get_DensityFeatureAdaptor();
-my $dta = $db->get_DensityTypeAdaptor();
-my $aa  = $db->get_AnalysisAdaptor();
-my $slice_adaptor = $db->get_SliceAdaptor();
-
-
-# Sort slices by coordinate system rank, then by length.
+# Sort slices by coordinate system rank, then by length
 my @sorted_slices =
   sort( {      $a->coord_system()->rank() <=> $b->coord_system()->rank()
-            || $b->seq_region_length() <=> $a->seq_region_length()
-  } @{ $slice_adaptor->fetch_all('toplevel') } );
+	       || $b->seq_region_length() <=> $a->seq_region_length()
+	} @{ $slice_adaptor->fetch_all('toplevel') } );
 
-my $analysis =
-  new Bio::EnsEMBL::Analysis(
-              -program     => "variation_density.pl",
-              -database    => "ensembl",
-              -gff_source  => "variation_density.pl",
-              -gff_feature => "density",
-              -logic_name  => "snpDensity",
-              -description => 'Density of SNP features on the sequence',
-              -display_label => 'SNP Density',
-              -displayable   => 1 );
+my $analysis = new Bio::EnsEMBL::Analysis(-program       => "variation_density.pl",
+					  -database      => "ensembl",
+					  -gff_source    => "variation_density.pl",
+					  -gff_feature   => "density",
+					  -logic_name    => "snpDensity",
+					  -description   => 'Density of SNP features on the sequence',
+					  -display_label => 'SNP Density',
+					  -displayable   => 1 );
 
-$aa->store($analysis);
-$aa->update($analysis);
+$analysis_adaptor->store($analysis);
+$analysis_adaptor->update($analysis);
 
+# Create and store new density type
 
-#
-# Create new density type.
-#
-my $dt = Bio::EnsEMBL::DensityType->new(-analysis   => $analysis,
+my $dt = Bio::EnsEMBL::DensityType->new(-analysis        => $analysis,
 					-region_features => $bin_count,
-					-value_type => 'sum');
-$dta->store($dt);
+					-value_type      => 'sum');
+$density_type_adaptor->store($dt);
 
-
-#
 # Now the actual feature calculation loop
-#
 
 my $slice_count = 0;
-my ( $current, $current_start, $current_end  );
 
+my ($current, $current_start, $current_end);
 
+# prepare statement outside of loop
+$sth = $variation_feature_adaptor->prepare("SELECT COUNT(*) FROM variation_feature WHERE seq_region_id = ? AND seq_region_start < ? AND seq_region_end > ?");
 
 foreach my $slice (@sorted_slices){
 
   $block_size = $slice->length() / $bin_count;
 
-
-  print "SNP densities for ".$slice->seq_region_name().
-    " with block size $block_size\n";
+  print "Calculating SNP densities for ". $slice->seq_region_name() . " with block size $block_size\n";
 
   $current_end = 0;
   $current = 0;
@@ -126,113 +90,28 @@ foreach my $slice (@sorted_slices){
     $current_start = $current_end+1;
     $current_end = int( $current + 1 );
 
-    if( $current_end < $current_start ) { 
+    if ($current_end < $current_start) {
       $current_end = $current_start;
     }
 
-    if( $current_end > $slice->end() ) {
+    if ($current_end > $slice->end()) {
       $current_end = $slice->end();
     }
 
+    # Get count of SNPs in this region
+    $sth->execute($slice->get_seq_region_id(), $current_end, $current_start);
+    my $count = ($sth->fetchrow_array())[0];
 
-    my $sub_slice = $slice->sub_Slice( $current_start, $current_end );
+    my $df = Bio::EnsEMBL::DensityFeature->new(-seq_region    => $slice,
+					       -start         => $current_start,
+        				       -end           => $current_end,
+        				       -density_type  => $dt,
+        				       -density_value => $count);
+    $density_feature_adaptor->store($df);
 
-    my $count =0;
-
-    #
-    #  How many snps fall into this subslice
-    #
-    foreach my $varf (@{$sub_slice->get_all_VariationFeatures()}){
-      if( $varf->start >= 1 ) {
-	$count++
-      }
-    }
-
-    my $df = Bio::EnsEMBL::DensityFeature->new
-      (-seq_region    => $slice,
-       -start         => $current_start,
-       -end           => $current_end,
-       -density_type  => $dt,
-       -density_value => $count);
-
-    $dfa->store($df);
   }
 
   last if ( $slice_count++ > $max_slices );
 
 }
 
-
-#
-# tries to attach variation database.
-#
-
-sub variation_attach {
-  my $db = shift;
-
-  my $core_db_name;
-  $core_db_name = $db->dbc->dbname();
-  if( $core_db_name !~ /_core_/ ) {
-    return 0;
-  }
-  #
-  # get a list of all databases on that server
-  #
-  my $sth = $db->dbc->prepare( "show databases" );
-  $sth->execute();
-  my $all_db_names = $sth->fetchall_arrayref();
-  my %all_db_names = map {( $_->[0] , 1)} @$all_db_names;
-  my $snp_db_name = $core_db_name;
-  $snp_db_name =~ s/_core_/_variation_/;
- if( ! exists $all_db_names{ $snp_db_name } ) {
-   return 0;
- }
-
- # this should register the dbadaptor with the Registry
- my $snp_db = Bio::EnsEMBL::Variation::DBSQL::DBAdaptor->new
-   ( -host => $db->host(),
-     -user => $db->username(),
-     -pass => $db->password(),
-     -port => $db->port(),
-     -dbname => $snp_db_name,
-     -group => "variation",
-     -species => "DEFAULT"
-   );
-
-  return 1;
-}
-
-  
-
-#
-# helper to draw an ascii representation of the density features
-#
-sub print_features {
-  my $features = shift;
-
-  return if(!@$features);
-
-  my $sum = 0;
-  my $length = 0;
-#  my $type = $features->[0]->{'density_type'}->value_type();
-
-  print("\n");
-  my $max=0;
-  foreach my $f (@$features) {
-    if($f->density_value() > $max){
-      $max=$f->density_value();
-    }
-  }
-  if( !$max ) { $max = 1 };
-
-  foreach my $f (@$features) {
-    my $i=1;
-    for(; $i< ($f->density_value()/$max)*40; $i++){
-      print "*";
-    }
-    for(my $j=$i;$j<40;$j++){
-      print " ";
-    }
-    print "  ".$f->density_value()."\t".$f->start()."\n";
-  }
-}
