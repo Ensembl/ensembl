6a31c4b1930daee01f2a4ee83a672ec948935635
Ian Longden <ianl@sanger.ac.uk>
2010-12-10 09:45:13 +0000

    revert to normal xrefs, no special official naming methids used

--- misc-scripts/xref_mapping/XrefMapper/danio_rerio.pm
+++ misc-scripts/xref_mapping/XrefMapper/danio_rerio.pm
@@ -6,57 +6,57 @@ use vars '@ISA';
 
 @ISA = qw{ XrefMapper::BasicMapper };
 
-sub set_display_xrefs{
-  my $self = shift;
-  my $display = XrefMapper::DisplayXrefs->new($self);
-  $display->set_display_xrefs_from_stable_table();
+#sub set_display_xrefs{
+#  my $self = shift;
+#  my $display = XrefMapper::DisplayXrefs->new($self);
+#  $display->set_display_xrefs_from_stable_table();
 
-}
+#}
 
-sub set_gene_descriptions(){
-  my $self = shift;
-  my $display = XrefMapper::DisplayXrefs->new($self);
-  $display->set_gene_descriptions_from_display_xref()
-}
+#sub set_gene_descriptions(){
+#  my $self = shift;
+#  my $display = XrefMapper::DisplayXrefs->new($self);
+#  $display->set_gene_descriptions_from_display_xref()
+#}
 
-sub get_official_name{
-   return "ZFIN_ID";
-}
+#sub get_official_name{
+#   return "ZFIN_ID";
+#}
+#
+#sub get_canonical_name{
+#   return "ZFIN_ID";
+#}
 
-sub get_canonical_name{
-   return "ZFIN_ID";
-}
+#sub species_specific_pre_attributes_set{
+#  my $self  = shift;
+#  $self->official_naming();
+#}
 
-sub species_specific_pre_attributes_set{
-  my $self  = shift;
-  $self->official_naming();
-}
+#sub species_specific_cleanup{
+#  my $self = shift;
+#  my $dbname = $self->get_canonical_name;
 
-sub species_specific_cleanup{
-  my $self = shift;
-  my $dbname = $self->get_canonical_name;
+#  print "Removing all $dbname from object_xref not on a Gene\n";
+#  my $remove_old_ones = (<<JSQL);
+#delete ox
+#  from object_xref ox, xref x, external_db e
+#    where e.db_name like "$dbname" and
+#          ox.ensembl_object_type != "Gene" and
+#          ox.xref_id = x.xref_id and
+#          x.external_db_id = e.external_db_id;
+#JSQL
 
-  print "Removing all $dbname from object_xref not on a Gene\n";
-  my $remove_old_ones = (<<JSQL);
-delete ox
-  from object_xref ox, xref x, external_db e
-    where e.db_name like "$dbname" and
-          ox.ensembl_object_type != "Gene" and
-          ox.xref_id = x.xref_id and
-          x.external_db_id = e.external_db_id;
-JSQL
+#  #
+#  # First Delete all the hgnc object_xrefs not on a gene. (i.e these are copys).
+#  #
 
-  #
-  # First Delete all the hgnc object_xrefs not on a gene. (i.e these are copys).
-  #
+#  my $sth = $self->core->dbc->prepare($remove_old_ones);
 
-  my $sth = $self->core->dbc->prepare($remove_old_ones);
+#  $sth->execute() || die "Could not execute: \n$remove_old_ones \n";
 
-  $sth->execute() || die "Could not execute: \n$remove_old_ones \n";
+#  $sth->finish;
 
-  $sth->finish;
-
-}
+#}
 
 sub get_set_lists {
 
