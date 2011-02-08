9537ddc83106467d174ca1afe55ca05f37a0dc99
Monika Komorowska <mk8@sanger.ac.uk>
2011-02-08 15:37:05 +0000

    Patch file patch_61_62_b.sql, extends field synonym in external_synonym table to 100 chars.

---
+++ sql/patch_61_62_b.sql
@@ -0,0 +1,14 @@
+# patch_61_62_b.sql
+#
+# Title: 'Synonym' field extension.
+#
+# Description:
+#   Extend field 'synonym' in 'external_synonym' table to 100 chars
+#   to support some very long synonyms in D. melanogaster.
+
+ALTER table external_synonym MODIFY VARCHAR(100) NOT NULL;
+
+
+# Patch identifier
+INSERT INTO meta (species_id, meta_key, meta_value)
+  VALUES (NULL, 'patch', 'patch_61_62_b.sql|synonym_field_extension');
