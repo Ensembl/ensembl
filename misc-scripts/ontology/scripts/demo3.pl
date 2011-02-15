8d080c6ad9be04c8a1b0ebaec094c46351a6fef5
Andreas Kusalananda Kähäri <ak4@sanger.ac.uk>
2011-02-15 11:40:25 +0000

    #----------------------------------------------------------------------- # Demo program for the Ensembl ontology database and API. # # This program fetches a set of ontology terms by name and displays them # on the console. #-----------------------------------------------------------------------

---
+++ misc-scripts/ontology/scripts/demo3.pl
@@ -0,0 +1,34 @@
+#!/usr/bin/env perl
+
+#-----------------------------------------------------------------------
+# Demo program for the Ensembl ontology database and API.
+#
+# This program fetches a set of ontology terms by name and displays them
+# on the console.
+#-----------------------------------------------------------------------
+
+use strict;
+use warnings;
+
+use Bio::EnsEMBL::Registry;
+
+my $registry = 'Bio::EnsEMBL::Registry';
+
+$registry->load_registry_from_db( '-host' => 'ensembldb.ensembl.org',
+                                  '-user' => 'anonymous' );
+
+my $pattern = '%splice_site%';
+
+# Get an ontology term adaptor and a gene adaptor (for human).
+my $adaptor =
+  $registry->get_adaptor( 'Multi', 'Ontology', 'OntologyTerm' );
+
+# Fetch the terms by its accession.
+my @terms = @{ $adaptor->fetch_all_by_name($pattern) };
+
+foreach my $term (@terms) {
+  printf( "Accession = %s\tName = %s\n",
+          $term->accession(), $term->name() );
+}
+
+# $Id$
