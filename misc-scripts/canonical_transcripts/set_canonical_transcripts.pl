e18523c337c52e459f5335ab33c0fbb0e828f59a
Amy Tang <at6@sanger.ac.uk>
2011-02-17 23:33:18 +0000

    The script now compares Ensembl transcripts against CCDS models by fetching CCDS models from a DB instead of checking CCDS xrefs.
    Also updated print statements

--- misc-scripts/canonical_transcripts/set_canonical_transcripts.pl
+++ misc-scripts/canonical_transcripts/set_canonical_transcripts.pl
@@ -12,7 +12,7 @@
 
 =head1 OPTIONS
 
-=head2 Required arguments
+=head2 Required DB connection arguments
 
   -dbname       Databsse name
 
@@ -24,7 +24,22 @@
 
   -dbpass       Database password
 
-=head2  Optional arguments
+=head2  Optional DB connection arguments
+
+  -dnadbname    DNA Databsse name
+
+  -dnadbhost    DNA Database host
+
+  -dnadbuser    DNA Database user
+
+  -ccds_dbname  CCDS database name
+
+  -ccds_host    CCDS database host
+
+  -ccds_user    CCDS database user
+
+
+=head2 Other optional arguments
 
   -coord_system_name    Coordinate system to use
 
@@ -60,14 +75,10 @@ use Getopt::Long;
 use Bio::EnsEMBL::DBSQL::DBAdaptor;
 use Bio::EnsEMBL::Utils::Exception qw(throw);
 
-my $host;
-my $port;
-my $dbname;
-my $user;
-my $pass;
-my $dnahost;
-my $dnadbname;
-my $dnauser;
+my ($host, $port, $dbname, $user,$pass);
+my ($dnahost, $dnadbname, $dnauser);
+my ($ccds_host, $ccds_dbname, $ccds_user);
+
 my $coord_system;
 my $seq_region_name;
 my $write = 0;
@@ -79,9 +90,12 @@ GetOptions( 'dbhost:s'            => \$host,
             'dbname:s'            => \$dbname,
             'dbuser:s'            => \$user,
             'dbpass:s'            => \$pass,
-            'dnahost:s'         => \$dnahost,
+            'dnahost:s'           => \$dnahost,
             'dnadbname:s'         => \$dnadbname,
             'dnauser:s'           => \$dnauser,
+            'ccds_host:s'         => \$ccds_host,
+            'ccds_dbname:s'       => \$ccds_dbname,
+            'ccds_user:s'         => \$ccds_user,
             'coord_system_name:s' => \$coord_system,
             'seq_region_name:s'   => \$seq_region_name,
             'write!'              => \$write,
@@ -99,9 +113,21 @@ my $db =
                                       -port   => $port,
                                       -dbname => $dbname,
                                       -pass   => $pass );
+my $ccds_db;
 
+if ($ccds_dbname) {
+  if (!$ccds_user || !$ccds_host) {
+    throw ("You must provide user, host and dbname details to connect to CCDS DB!");
+  }
+  $ccds_db = 
+  new Bio::EnsEMBL::DBSQL::DBAdaptor( -host   => $ccds_host,
+                                      -user   => $ccds_user,
+                                      -port   => $port,
+                                      -dbname => $ccds_dbname );
+}
 
 my $geneDB_has_DNA = check_if_DB_contains_DNA($db);
+
 my $dnadb;
 
 if ($geneDB_has_DNA == 0 && !$dnadbname) {
@@ -134,19 +160,25 @@ my $gene_update_sql = "update gene set canonical_transcript_id = ? where gene_id
 my $gene_sth = $db->dbc->prepare($gene_update_sql);
 
 SLICE: foreach my $slice (@$slices) {
-  print "Getting genes for " . $slice->name . "\n" if ($verbose);
+  print "\nGetting genes for " . $slice->name . "\n" if ($verbose);
   my $genes = $slice->get_all_Genes( undef, undef, 1 );
   my %canonical;
 
-GENE: foreach my $gene (@$genes) {
-    print "Looking at gene: (dbID " . $gene->dbID . ")";
+  GENE: foreach my $gene (@$genes) {
+    print "\nLooking at gene: (dbID " . $gene->dbID . ")";
     if ($gene->stable_id) {
       print " :: " . $gene->stable_id ;
     }
     print "\n";
     my $transcripts = $gene->get_all_Transcripts;
     if ( @$transcripts == 1 ) {
+      if ($ccds_db) {
+        check_Ens_trans_against_CCDS($transcripts->[0], $ccds_db);
+      }
       $canonical{ $gene->dbID } = $transcripts->[0]->dbID;
+      print "Transcript ". $transcripts->[0]->display_id . " of biotype " . $transcripts->[0]->biotype .
+            " is chosen as the canonical transcript for gene ". $gene->display_id .
+            " of biotype ". $gene->biotype. "\n";
       next GENE;
     }
 
@@ -162,7 +194,13 @@ GENE: foreach my $gene (@$genes) {
       if ( ( $key eq 'protein_coding' ) && ( @{ $trans{$key} } == 1 ) ) {
         my $trans_id = $trans{$key}->[0]->dbID;
         if ( $trans{$key}->[0]->translation->seq !~ /\*/ ) {
+          if ($ccds_db) {
+            check_Ens_trans_against_CCDS($trans{$key}->[0], $ccds_db);
+          }
           $canonical{ $gene->dbID } = $trans_id;
+          print "Transcript ". $trans{$key}->[0]->display_id . " of biotype " . $trans{$key}->[0]->biotype .
+            " is chosen as the canonical transcript for gene ". $gene->display_id . 
+            " of biotype ". $gene->biotype. "\n";
           next GENE;
         } else {
           carp(   "Transcript $trans_id has internal stop(s) "
@@ -180,8 +218,13 @@ GENE: foreach my $gene (@$genes) {
           print "The NMD transcript $trans_id will become "
               . "the canonical transcript because there are no "
               . "protein coding transcripts.\n";
-
+          if ($ccds_db) {
+            check_Ens_trans_against_CCDS($trans{$key}->[0], $ccds_db);
+          }
           $canonical{ $gene->dbID } = $trans_id;
+          print "Transcript ". $trans{$key}->[0]->display_id . " of biotype nonsense_mediated_decay" .
+            " is chosen as the canonical transcript for gene ". $gene->display_id .
+            " of biotype ". $gene->biotype. "\n";
           next GENE;
         } else {
           carp(   "Transcript $trans_id is an NMD with either "
@@ -212,17 +255,26 @@ GENE: foreach my $gene (@$genes) {
     }
 
     my @sorted;
+
     if (@with_translation) {
-      my ( @ccds_prot_coding_len_and_trans, @merge_prot_coding_len_and_trans, @prot_coding_len_and_trans, @merge_len_and_trans, @len_and_trans );
+
+      my ( @ccds_prot_coding_len_and_trans, @merge_prot_coding_len_and_trans, @prot_coding_len_and_trans);
+      my ( @merge_translateable_len_and_trans, @translateable_len_and_trans );
+      my ( @merge_len_and_trans, @len_and_trans );
+
       foreach my $trans (@with_translation) {
 
-        if ($verbose) {
-          print $trans->dbID . " :: " . $trans->biotype . " :: " .  $trans->translate->length . "\n";
-        }
+        print "Looking at Ens trans dbID " . $trans->dbID . " :: biotype " . $trans->biotype . " :: Transl length " .  
+              $trans->translate->length . "\n" if ($verbose);
+
+        my $h = { trans => $trans, len => $trans->translate->length };
+        my $ensembl_trans_found_in_CCDS = 0;
 
         if ( $trans->biotype eq 'protein_coding') {
-          my $h = { trans => $trans, len => $trans->translate->length };
-          if (scalar(@{$dea->fetch_all_by_Transcript($trans, 'CCDS')})) {
+          if ($ccds_db) {
+            $ensembl_trans_found_in_CCDS = check_Ens_trans_against_CCDS($trans, $ccds_db);
+          }
+          if ($ensembl_trans_found_in_CCDS) {
             push @ccds_prot_coding_len_and_trans, $h;
           } elsif ($trans->analysis->logic_name eq 'ensembl_havana_transcript') {
             push @merge_prot_coding_len_and_trans, $h;
@@ -230,38 +282,35 @@ GENE: foreach my $gene (@$genes) {
             push @prot_coding_len_and_trans, $h;
           }
           next;
-        } else {
-          my $h = { trans => $trans, len => $trans->translate->length };
+        } else { # transcript biotype is not protein_coding but the transcript has translation
           if ($trans->analysis->logic_name eq 'ensembl_havana_transcript') {
-            push @merge_len_and_trans, $h;
+            push @merge_translateable_len_and_trans, $h;
           } else {
-            push @len_and_trans, $h;
+            push @translateable_len_and_trans, $h;
           }
         }
       }
 
       my @tmp_sorted;
       if (@ccds_prot_coding_len_and_trans) {
-        @tmp_sorted = sort { $b->{len} <=> $a->{len} } @ccds_prot_coding_len_and_trans;
+        @tmp_sorted = sort { $b->{len} <=> $a->{len} } @ccds_prot_coding_len_and_trans;  # sort all the Ens transcripts which matched CCDS by coding length
       } elsif (@merge_prot_coding_len_and_trans) {
         @tmp_sorted = sort { $b->{len} <=> $a->{len} } @merge_prot_coding_len_and_trans;
       } elsif (@prot_coding_len_and_trans) {
         @tmp_sorted = sort { $b->{len} <=> $a->{len} } @prot_coding_len_and_trans;
       } elsif (@merge_len_and_trans) {
-        @tmp_sorted = sort { $b->{len} <=> $a->{len} } @merge_len_and_trans;
+        print "There are no 'protein_coding' labelled transcripts, ".
+              "will take the longest of merged tranlslateable transcripts\n" if ($verbose);
+        @tmp_sorted = sort { $b->{len} <=> $a->{len} } @merge_translateable_len_and_trans;
       } else {
-        if ($verbose) {
-          print "There are no 'protein_coding' labelled transcripts, "
-              . "will take the longest of the other coding transcripts\n";
-        }
-        @tmp_sorted = sort { $b->{len} <=> $a->{len} } @len_and_trans;
+        print "There are neither 'protein_coding' labelled transcripts nor merged ".
+              "translateable transcripts. Will take the longest of any unmerged ".
+              "translateable transcript.\n" if ($verbose);
+        @tmp_sorted = sort { $b->{len} <=> $a->{len} } @translateable_len_and_trans;
       }
 
       foreach my $h (@tmp_sorted) {
-
-        if ($verbose) {
-          print "Adding to sorted " . $h->{trans}->dbID . " :: " . $h->{trans}->biotype . "\n";
-        }
+        print "Adding to sorted " . $h->{trans}->dbID . " :: " . $h->{trans}->biotype . "\n" if ($verbose);
         push @sorted, $h->{trans};
       }
     } else {
@@ -284,6 +333,9 @@ GENE: foreach my $gene (@$genes) {
     # # #
     # set canonical transcirpt
     # # #
+    print "Transcript ". $sorted[0]->display_id . " of biotype " . $sorted[0]->biotype .
+          " is chosen as the canonical transcript for gene ". $gene->display_id . 
+          " of biotype ". $gene->biotype. "\n";
     $canonical{ $gene->dbID } = $sorted[0]->dbID;
 
   } ## end foreach my $gene (@$genes)
@@ -313,4 +365,33 @@ sub check_if_DB_contains_DNA {
     return 0;
   }
 }
+
+sub check_Ens_trans_against_CCDS {
+  my ($ensembl_trans, $ccds_db) = @_;
+  my @ensembl_translateable_exons = @{ $ensembl_trans->get_all_translateable_Exons };
+
+  my $ext_slice = $ccds_db->get_SliceAdaptor->fetch_by_region( 'toplevel',
+                     $ensembl_trans->slice->seq_region_name,
+                     $ensembl_trans->seq_region_start, $ensembl_trans->seq_region_end );
+
+  EXT_GENE: foreach my $ext_gene ( @{ $ext_slice->get_all_Genes } ) {
+    EXT_TRANS: foreach my $ext_trans ( @{ $ext_gene->get_all_Transcripts } ) {
+      my @ext_exons = @{ $ext_trans->get_all_Exons };
+
+      if ( scalar(@ensembl_translateable_exons) == scalar(@ext_exons) ) {
+        for ( my $i = 0 ; $i < scalar(@ensembl_translateable_exons) ; $i++ ) {
+          if ( $ensembl_translateable_exons[$i]->coding_region_start($ensembl_trans) != $ext_exons[$i]->seq_region_start
+             || $ensembl_translateable_exons[$i]->strand != $ext_exons[$i]->strand
+             || $ensembl_translateable_exons[$i]->coding_region_end($ensembl_trans) != $ext_exons[$i]->seq_region_end )
+          {
+             next EXT_TRANS;
+          }
+        }
+        print "Ensembl transcript " . $ensembl_trans->display_id . " found match " . 
+          $ext_gene->display_id . " in CCDS DB.\n";
+        return 1;
+      }
+    } # end foreach EXT_TRANS
+  } # end foreach EXT_GENE 
+} ## end sub
 
