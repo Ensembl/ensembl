dafc62ab8b5b64d2e58fe8cd2e4ed430da208892
Andreas Kusalananda Kähäri <ak4@sanger.ac.uk>
2011-02-17 11:47:57 +0000

    Fix syntax error.

--- sql/patch_61_62_b.sql
+++ sql/patch_61_62_b.sql
@@ -6,8 +6,7 @@
 #   Extend field 'synonym' in 'external_synonym' table to 100 chars
 #   to support some very long synonyms in D. melanogaster.
 
-ALTER table external_synonym MODIFY VARCHAR(100) NOT NULL;
-
+ALTER table external_synonym MODIFY synonym VARCHAR(100) NOT NULL;
 
 # Patch identifier
 INSERT INTO meta (species_id, meta_key, meta_value)
