e81d5578ac4fcf94309c6488fedeb3b58ea80924
Monika Komorowska <mk8@sanger.ac.uk>
2011-02-28 16:17:45 +0000

    Added db_release field to the unique index.

--- sql/patch_61_62_c.sql
+++ sql/patch_61_62_c.sql
@@ -1,12 +1,12 @@
 # patch_61_62_c.sql
 #
-# Title: Index for db_name.
+# Title: Index for db_name and db_release.
 #
 # Description:
-# Add unique index to db_name field in external_db table.
+# Add unique index to db_name and db_release fields in external_db table.
 
-CREATE UNIQUE INDEX db_name_idx ON external_db(db_name);
+CREATE UNIQUE INDEX db_name_db_release_idx ON external_db(db_name,db_release);
 
 # Patch identifier
 INSERT INTO meta (species_id, meta_key, meta_value)
-  VALUES (NULL, 'patch', 'patch_61_62_c.sql|db_name_idx');
+  VALUES (NULL, 'patch', 'patch_61_62_c.sql|db_name_db_release_idx');
