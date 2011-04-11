229d2618d7f3f9accd8882755cecd2306045c8dc
Monika Komorowska <mk8@sanger.ac.uk>
2011-04-11 08:47:43 +0000

    ndexing changes for stable ids.

---
+++ sql/patch_62_63_b.sql
@@ -0,0 +1,68 @@
+# patch_62_63_b.sql
+#
+# Title: Indexing changes for core database.
+#
+# Description:
+
+#change stable Id version to not null, default 1
+
+ALTER TABLE exon_stable_id MODIFY version NOT NULL DEFAULT 1; 
+
+DROP INDEX stable_id_idx ON exon_stable_id;
+
+CREATE UNIQUE INDEX stable_id_idx ON exon_stable_id(stable_id, version);
+
+ALTER TABLE gene_stable_id MODIFY version NOT NULL DEFAULT 1; 
+
+DROP INDEX stable_id_idx ON gene_stable_id;
+
+CREATE UNIQUE INDEX stable_id_idx ON gene_stable_id(stable_id, version);
+
+ALTER TABLE transcript_stable_id MODIFY version NOT NULL DEFAULT 1;  
+
+DROP INDEX stable_id_idx ON transcript_stable_id;
+
+CREATE UNIQUE INDEX stable_id_idx ON transcript_stable_id(stable_id, version);
+
+ALTER TABLE translation_stable_id MODIFY version NOT NULL DEFAULT 1;  
+
+DROP INDEX stable_id_idx ON translation_stable_id;
+
+CREATE UNIQUE INDEX stable_id_idx ON translation_stable_id(stable_id, version);
+
+ALTER TABLE gene_archive MODIFY gene_version DEFAULT 1, MODIFY transcript_version DEFAULT 1, MODIFY translation_version DEFAULT 1;
+
+DROP INDEX  gene_idx ON gene_archive;
+
+CREATE UNIQUE INDEX  gene_idx ON gene_archive(gene_stable_id, gene_version);
+
+DROP INDEX  transcript_idx ON gene_archive;
+
+CREATE UNIQUE INDEX  transcript_idx ON gene_archive(transcript_stable_id, transcript_version);
+
+DROP INDEX  translation_idx ON gene_archive;
+ 
+CREATE UNIQUE INDEX  translation_idx ON gene_archive(translation_stable_id, translation_version);
+
+
+#umapped_object new unique index
+
+DROP INDEX anal_idx ON unmapped_object(analysis_id);
+
+CREATE UNIQUE INDEX unique_unmapped_obj_idx ON unmapped_object(identifier, ensembl_id, parent, unmapped_reason_id, ensembl_object_type, external_db_id);
+
+DROP INDEX id_idx ON unmapped_object;
+
+#reduce identifier index to 50 characters – most ids are under 30, index will be faster
+
+CREATE INDEX id_idx ON unmapped_object(identifier(50));
+
+#add index for queries using external_db_id in the where clause
+
+CREATE INDEX ext_db_identifier_idx ON unmapped_object(external_db_id, identifier);
+
+
+
+
+
+
