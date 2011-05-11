90acdec1163ee97f40e789c7776ecf1dd661177d
Monika Komorowska <mk8@sanger.ac.uk>
2011-05-11 11:25:26 +0000

    Corrected syntax

--- sql/rnaseq_update_version.sql
+++ sql/rnaseq_update_version.sql
@@ -1,14 +1,11 @@
 CREATE PROCEDURE rnaseq_version_update()
-BEGIN
+
+BLOCK1: BEGIN
  
 DECLARE v_count INT;
 DECLARE v_stable_id varChar(128);
 DECLARE v_version INT;
-DECLARE v2_gene_id INT;
-DECLARE v2_version INT;
 DECLARE done INT DEFAULT 0;
-DECLARE done2 INT DEFAULT 0;
-DECLARE counter INT DEFAULT 0;
 
 DECLARE cur1 CURSOR FOR select count(1) , stable_id, version from gene_stable_id group by stable_id, version having count(1) > 1 ;
 DECLARE CONTINUE HANDLER FOR NOT FOUND SET done = TRUE;
@@ -17,38 +14,51 @@ DECLARE CONTINUE HANDLER FOR NOT FOUND SET done = TRUE;
 OPEN cur1;
 
 
-main_loop: LOOP
+LOOP1: loop
 
 	SET done = FALSE;
 	FETCH cur1 INTO v_count, v_stable_id, v_version;
 
 	IF done THEN
-   		LEAVE main_loop;
+   		LEAVE LOOP1;
 	END IF;
 
+	BLOCK2: BEGIN
+
+	DECLARE v2_gene_id INT;
+	DECLARE v2_version INT;
+	DECLARE done2 INT DEFAULT 0;
+	DECLARE counter INT DEFAULT 0;
+	DECLARE initial_version INT; 
+
 	DECLARE cur2 CURSOR FOR	select g.gene_id, gs.version  from seq_region s, gene_stable_id gs, gene g where g.gene_id=gs.gene_id and s.seq_region_id =g.seq_region_id and stable_id = v_stable_id;
-	DECLARE CONTINUE HANDLER FOR NOT FOUND SET done = TRUE;
+	DECLARE CONTINUE HANDLER FOR NOT FOUND SET done2 = TRUE;
 	OPEN cur2;
-	counter = 0;
-	loop2: LOOP2
-	       SET done = FALSE;
+	SET counter = 0;
+	LOOP2: loop
+	       SET done2 = FALSE;
 	       FETCH cur2 INTO v2_gene_id, v2_version;
 
 	       IF done2 THEN
-   		  LEAVE loop2;
-		  END IF;
-		  SET counter = counter + 1;
-
+   		  LEAVE LOOP2;
+	       END IF;
+	      
+	       SET counter = counter + 1;
+	       IF (counter = 1) THEN
+		SET initial_version = v2_version;
+	       END IF;
 	       IF (counter > 1) THEN
-	       	  UPDATE gene_stable_id gs set version = (v2_version + counter - 1)  where gene_id = v2_gene_id;
+	       	  UPDATE gene_stable_id set version = (initial_version + counter - 1)  where gene_id = v2_gene_id;
 	       END IF; 
 		  
-	END LOOP2;
-	       CLOSE cur2;
+	END loop LOOP2;
+	CLOSE cur2;
+	
+	end BLOCK2;
 
-END LOOP;
+END loop LOOP1;
 
 CLOSE cur1;
 
-END
+END BLOCK1;
 
