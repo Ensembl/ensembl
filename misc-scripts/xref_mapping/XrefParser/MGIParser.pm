95304162b0cfdbb4b74a94b8c8496a5ada8ec440
Ian Longden <ianl@sanger.ac.uk>
2011-05-24 08:49:55 +0000

    Add synonyms

--- misc-scripts/xref_mapping/XrefParser/MGIParser.pm
+++ misc-scripts/xref_mapping/XrefParser/MGIParser.pm
@@ -51,6 +51,7 @@ sub run {
   my %description;
   my %accession;
 
+
   my $dbi = $self->dbi();  
 
   my $sql = 'select source_id, priority_description from source where name like "MGI"';
@@ -80,7 +81,15 @@ sub run {
     }
   }
   $sth->finish;
+
+  #synonyms
+  $sql = "insert into synonym (xref_id, synonym) values (?, ?)";
+  my $add_syn_sth = $dbi->prepare($sql);    
+
+  my $syn_hash = $self->get_ext_synonyms("MGI");
+
   my $count = 0;
+  my $syn_count = 0;
   while ( my $line = $file_io->getline() ) {
 #MGI:1915941	1110028C15Rik	RIKEN cDNA 1110028C15 gene	33.61	1	ENSMUSG00000026004	ENSMUST00000042389 ENSMUST00000068168 ENSMUST00000113987 ENSMUST00000129190 ENSMUST00000132960	ENSMUSP00000036975 ENSMUSP00000063843 ENSMUSP00000109620 ENSMUSP00000118603
 
@@ -89,6 +98,11 @@ sub run {
       my $ensid = $2;
       my $xref_id = XrefParser::BaseParser->add_xref($acc, $version{$acc}, $label{$acc}, $description{$acc}, $source_id, $species_id, "DIRECT");
       XrefParser::BaseParser->add_direct_xref( $xref_id, $ensid, "Gene", '');
+      if(defined($syn_hash->{$acc})){
+	foreach my $syn (@{$syn_hash->{$acc}}){
+	  $add_syn_sth->execute($xref_id, $syn);
+	}
+      }
       $count++;
     }	
     else{
