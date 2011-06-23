a60cc9ea42a6ce6304dc9c0af853dd92ed90291e
Monika Komorowska <mk8@sanger.ac.uk>
2011-06-23 09:56:43 +0000

    Changed parameter names in density feature scripts

--- misc-scripts/density_feature/DensityCalc.sh
+++ misc-scripts/density_feature/DensityCalc.sh
@@ -12,6 +12,7 @@ port='3306'         # -P
 user='ensadmin'     # -u
 pass=               # -p
 dbname=             # -d
+species=            # -s
 outdir=             # -o
 
 
@@ -20,15 +21,16 @@ while getopts 'h:P:d:u:p:o:' opt; do
     h)  host=${OPTARG} ;;
     P)  port=${OPTARG} ;;
     d)  dbname=${OPTARG} ;;
+    s)  species=${OPTARG};;
     u)  user=${OPTARG} ;;
     p)  pass=${OPTARG} ;;
     o)  outdir=${OPTARG} ;;
   esac
 done
 
-if [[ -z ${host} || -z ${port} || -z ${dbname} || -z ${user} || -z ${pass} ]]
+if [[ -z ${host} || -z ${port} || -z ${dbname} || -z ${species} || -z ${user} || -z ${pass} ]]
 then
-  print -u2 "Usage:\n\t$0 -h host -P port -d database -u user -p password"
+  print -u2 "Usage:\n\t$0 -h host -P port -d database -s species -u user -p password"
   exit 1
 fi
 
@@ -53,11 +55,11 @@ bsub -q normal -J gc_calc \
   -oo ${outdir}/${dbname}_gc.out \
   -eo ${outdir}/${dbname}_gc.err \
   perl ./percent_gc_calc.pl \
-  -host ${host} \
+  -h ${host} \
   -port ${port} \
-  -user ${user} \
-  -pass ${pass} \
-  -dbname ${dbname}
+  -u ${user} \
+  -p ${pass} \
+  -d ${dbname}
 
 print "Submitting gene density calculation to queue 'normal'"
 print "\tThe output from this job goes to the file"
@@ -66,11 +68,11 @@ bsub -q normal -J gene_density \
   -oo ${outdir}/${dbname}_gene.out \
   -eo ${outdir}/${dbname}_gene.err \
   perl ./gene_density_calc.pl \
-  -host ${host} \
+  -h ${host} \
   -port ${port} \
-  -user ${user} \
-  -pass ${pass} \
-  -dbname ${dbname}
+  -u ${user} \
+  -p ${pass} \
+  -d ${dbname}
 
 print "Submitting repeat coverage calculation to queue 'long'"
 print "\tThe output from this job goes to the file"
@@ -79,11 +81,11 @@ bsub -q long -J repeat_cov \
   -oo ${outdir}/${dbname}_repeat.out \
   -eo ${outdir}/${dbname}_repeat.err \
   perl ./repeat_coverage_calc.pl \
-  -host ${host} \
+  -h ${host} \
   -port ${port} \
-  -user ${user} \
-  -pass ${pass} \
-  -dbname ${dbname}
+  -u ${user} \
+  -p ${pass} \
+  -d ${dbname}
 
 print "Submitting variation density calculation to queue 'normal'"
 print "\tThe output from this job goes to the file"
@@ -92,24 +94,39 @@ bsub -q normal -J var_density \
   -oo ${outdir}/${dbname}_var.out \
   -eo ${outdir}/${dbname}_var.err \
   perl ./variation_density.pl \
-  -host ${host} \
+  -h ${host} \
   -port ${port} \
-  -user ${user} \
-  -pass ${pass} \
-  -dbname ${dbname}
+  -u ${user} \
+  -p ${pass} \
+  -s ${species}
 
-print "Submitting seq region stats calculation to queue 'normal'"
+print "Submitting seq region gene stats calculation to queue 'normal'"
 print "\tThe output from this job goes to the file"
 print "\t'${dbname}_seqreg.out'"
-bsub -q normal -J seqreg_stats \
-  -oo ${outdir}/${dbname}_seqreg.out \
-  -eo ${outdir}/${dbname}_seqreg.err \
+bsub -q normal -J seqreg_stats_gene \
+  -oo ${outdir}/${dbname}_seqreg_gene.out \
+  -eo ${outdir}/${dbname}_seqreg_gene.err \
   perl ./seq_region_stats.pl \
-  -host ${host} \
+  -h ${host} \
   -port ${port} \
-  -user ${user} \
-  -pass ${pass} \
-  -dbname ${dbname}
+  -u ${user} \
+  -p ${pass} \
+  -d ${dbname} \
+  -s gene
+
+print "Submitting seq region snp stats calculation to queue 'normal'"
+print "\tThe output from this job goes to the file"
+print "\t'${dbname}_seqreg.out'"
+bsub -q normal -J seqreg_stats_snp \
+  -oo ${outdir}/${dbname}_seqreg_snp.out \
+  -eo ${outdir}/${dbname}_seqreg_snp.err \
+  perl ./seq_region_stats.pl \
+  -h ${host} \
+  -port ${port} \
+  -u ${user} \
+  -p ${pass} \
+  -d ${dbname} \
+  -s snp
 
 print "Submitting gene_gc content calculation to queue 'normal'"
 print "\tThe output from this job goes to the file"
@@ -118,10 +135,10 @@ bsub -q normal -J genegc_stats \
   -oo ${outdir}/${dbname}_genegc.out \
   -eo ${outdir}/${dbname}_genegc.err \
   perl ../gene_gc.pl \
-  -host ${host} \
+  -h ${host} \
   -port ${port} \
-  -user ${user} \
-  -pass ${pass} \
+  -u ${user} \
+  -p ${pass} \
   -pattern ${dbname}
 
 print "All jobs submitted."
