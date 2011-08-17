662ca1852d0087767dfde28dd4e852ec7057e538
Andrew Yates <ayates@ebi.ac.uk>
2011-08-17 09:25:24 +0000

    Due to a bug in MySQL we need to drop the unique index, change the field to varchar then reaply the index otherwise latin1 encodings can cause a constraint error to occur. This only affects the conversion of enum to varchar

--- sql/patch_63_64_d.sql
+++ sql/patch_63_64_d.sql
@@ -7,9 +7,13 @@
 #    Change the column definition to VARCHAR(3) instead of providing
 #    a list of allowable values. Values will be tested in healthchecks.
 
+ALTER TABLE ontology_xref DROP INDEX object_source_type_idx;
 
 ALTER TABLE ontology_xref
   MODIFY linkage_type VARCHAR(3) DEFAULT NULL;
+
+ALTER TABLE ontology_xref 
+  ADD UNIQUE INDEX object_source_type_idx (object_xref_id, source_xref_id, linkage_type);
 
 # Insert patch identifier.
 INSERT INTO meta (species_id, meta_key, meta_value)
