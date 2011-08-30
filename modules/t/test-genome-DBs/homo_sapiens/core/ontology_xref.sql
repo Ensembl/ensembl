642c4ef819dece85e7420d6a686d3fe9cefe5891
Monika Komorowska <mk8@sanger.ac.uk>
2011-08-30 14:26:19 +0000

    Changed definition for column linkage_type

--- modules/t/test-genome-DBs/homo_sapiens/core/ontology_xref.sql
+++ modules/t/test-genome-DBs/homo_sapiens/core/ontology_xref.sql
@@ -1,6 +1,6 @@
 CREATE TABLE `ontology_xref` (
   `object_xref_id` int(10) unsigned NOT NULL default '0',
-  `linkage_type` enum('IC','IDA','IEA','IEP','IGI','IMP','IPI','ISS','NAS','ND','TAS','NR','RCA') NOT NULL,
+  `linkage_type` varchar(3) DEFAULT NULL,
   `source_xref_id` int(10) unsigned default NULL,
   UNIQUE KEY `object_xref_id_2` (`object_xref_id`,`source_xref_id`,`linkage_type`),
   KEY `object_xref_id` (`object_xref_id`),
