a6de3cdba8556d0e80995269ec1e996f83b08f85
Andreas Kusalananda Kähäri <ak4@sanger.ac.uk>
2011-09-01 12:23:15 +0000

    Removed.

--- sql/update_indexes.sql
+++
@@ -1,8 +0,0 @@
-DROP INDEX stable_id_idx on exon_stable_id;
-CREATE INDEX  stable_id_idx on exon_stable_id(stable_id, version);
-DROP INDEX stable_id_idx on gene_stable_id;
-CREATE INDEX  stable_id_idx on gene_stable_id(stable_id, version);
-DROP INDEX stable_id_idx on transcript_stable_id;
-CREATE INDEX  stable_id_idx on transcript_stable_id(stable_id, version);
-DROP INDEX stable_id_idx on translation_stable_id;
-CREATE INDEX  stable_id_idx on translation_stable_id(stable_id, version);
