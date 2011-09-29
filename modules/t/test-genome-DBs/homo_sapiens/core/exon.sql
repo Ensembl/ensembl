3b9aea5021dea76afcdd8ba2241d1822cda90791
Monika Komorowska <mk8@sanger.ac.uk>
2011-09-29 14:33:06 +0000

    stable_id table merged with main object table

--- modules/t/test-genome-DBs/homo_sapiens/core/exon.sql
+++ modules/t/test-genome-DBs/homo_sapiens/core/exon.sql
@@ -1,14 +1,24 @@
-CREATE TABLE `exon` (
-  `exon_id` int(10) unsigned NOT NULL auto_increment,
-  `seq_region_id` int(10) unsigned NOT NULL default '0',
-  `seq_region_start` int(10) unsigned NOT NULL default '0',
-  `seq_region_end` int(10) unsigned NOT NULL default '0',
-  `seq_region_strand` tinyint(2) NOT NULL default '0',
-  `phase` tinyint(2) NOT NULL default '0',
-  `end_phase` tinyint(2) NOT NULL default '0',
-  `is_current` BOOLEAN DEFAULT 1,
-  `is_constitutive` BOOLEAN DEFAULT 0,
-  PRIMARY KEY  (`exon_id`),
-  KEY `seq_region_idx` (`seq_region_id`,`seq_region_start`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1 COLLATE=latin1_bin;
+CREATE TABLE exon (
 
+  exon_id                     INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+  seq_region_id               INT(10) UNSIGNED NOT NULL,
+  seq_region_start            INT(10) UNSIGNED NOT NULL,
+  seq_region_end              INT(10) UNSIGNED NOT NULL,
+  seq_region_strand           TINYINT(2) NOT NULL,
+
+  phase                       TINYINT(2) NOT NULL,
+  end_phase                   TINYINT(2) NOT NULL,
+
+  is_current                  BOOLEAN NOT NULL DEFAULT 1,
+  is_constitutive             BOOLEAN NOT NULL DEFAULT 0,
+
+  stable_id                   VARCHAR(128) DEFAULT NULL,
+  version                     SMALLINT UNSIGNED NOT NULL DEFAULT 1,
+  created_date                DATETIME NOT NULL DEFAULT '0000-00-00 00:00:00',
+  modified_date               DATETIME NOT NULL DEFAULT '0000-00-00 00:00:00',
+
+  PRIMARY KEY (exon_id),
+  KEY seq_region_idx (seq_region_id, seq_region_start),
+  KEY stable_id_idx (stable_id, version)
+
+) COLLATE=latin1_swedish_ci ENGINE=MyISAM;
