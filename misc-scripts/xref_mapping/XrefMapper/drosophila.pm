1e5a3806134eedde1d95d90357d3a1fdd8dfca62
Monika Komorowska <mk8@sanger.ac.uk>
2011-10-06 13:53:29 +0000

    update ignore hash

--- misc-scripts/xref_mapping/XrefMapper/drosophila.pm
+++ misc-scripts/xref_mapping/XrefMapper/drosophila.pm
@@ -61,26 +61,13 @@ sub gene_display_xref_sources {
 		my $self     = shift;
 		my $fullmode = shift;
 
-  my @list = qw(
-								FlyBaseName_gene
+  my @list = qw(FlyBaseName_gene
                 FlyBaseCGID_gene
-                flybase_gene_id
-                );
+                flybase_gene_id);
 
   my %ignore;
   
 		print "Get FlyBase Gene display_xrefs_sources with fullmode=$fullmode\n" if ($self->verbose);
-
-  # Both methods
-
-  if(!$fullmode){
-    $ignore{"EntrezGene"}= 'FROM:RefSeq_[pd][en][pa].*_predicted';
-  }
-  else{
-    $ignore{"EntrezGene"} = 'select ox.object_xref_id from object_xref ox, dependent_xref dx, source s1, xref x1, source s2, xref x2 where ox.object_xref_id = dx.object_xref_id and dx.dependent_xref_id = x1.xref_id and x1.source_id = s1.source_id and s1.name = "EntrezGene" and x2.xref_id = dx.master_xref_id and x2.source_id = s2.source_id and (s2.name like "Refseq_dna_predicted" or s2.name like "RefSeq_peptide_predicted") and ox.ox_status = "DUMP_OUT"';
-
-  }
-
 
   return [\@list,\%ignore];
 
