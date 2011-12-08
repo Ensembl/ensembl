647769491e047792f12f0b45c15539ba8d1c8c9d
Monika Komorowska <mk8@sanger.ac.uk>
2011-12-08 16:31:03 +0000

    schema version update

--- sql/patch_65_66_a.sql
+++ sql/patch_65_66_a.sql
@@ -1,12 +1,12 @@
-# patch_64_65_a.sql
+# patch_65_66_a.sql
 #
 # Title: Update schema version.
 #
 # Description:
-#   Update schema_version in meta table to 65.
+#   Update schema_version in meta table to 66.
 
-UPDATE meta SET meta_value='65' WHERE meta_key='schema_version';
+UPDATE meta SET meta_value='66' WHERE meta_key='schema_version';
 
 # Patch identifier
 INSERT INTO meta (species_id, meta_key, meta_value)
-  VALUES (NULL, 'patch', 'patch_64_65_a.sql|schema_version');
+  VALUES (NULL, 'patch', 'patch_65_66_a.sql|schema_version');
