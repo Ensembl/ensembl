34a1fda1070c2a93b0d19413a381cae22b893464
Monika Komorowska <mk8@sanger.ac.uk>
2012-03-19 15:19:57 +0000

    use alt_allele data from vega to populate core as gene names may not be up to date in core when alt_alleles.pl is run

--- misc-scripts/alt_alleles/alt_alleles.pl
+++ misc-scripts/alt_alleles/alt_alleles.pl
@@ -6,81 +6,113 @@ use Getopt::Long qw(:config pass_through);
 
 # (make sure api version is correct
 # Usage:
-# perl alt_alleles.pl -pass XXXX > & human_release_63_alt_alleles
+# perl alt_alleles.pl -cpass XXXX > & human_release_63_alt_alleles
 #
 #
 # long way
-# perl alt_alleles.pl -dbname homo_sapiens_core_63_37 -host ens-staging1 -pass XXXX > & human_release_63_alt_alleles
+# perl alt_alleles.pl -vhost ens-staging1 -vport 3306 -vdbname homo_sapiens_vega_63_37 -cdbname homo_sapiens_core_63_37 -chost ens-staging1 -cpass XXXX > & human_release_63_alt_alleles
 #
 
-my ($host, $pass, $port, $dbname, $user);
+my ($vhost, $vpass, $vport, $vdbname, $vuser, $chost, $cpass, $cport, $cdbname, $cuser);
 
 GetOptions(
-    'user=s'  => \$user,
-    'pass=s'  => \$pass,
-    'host=s'  => \$host,
-    'port=i'  => \$port,
-    'dbname=s'       => \$dbname);
-
-
+    'vuser=s'  => \$vuser,
+    'vpass=s'  => \$vpass,
+    'vhost=s'  => \$vhost,
+    'vport=i'  => \$vport,
+    'vdbname=s'       => \$vdbname,
+    'cuser=s'  => \$cuser,
+    'cpass=s'  => \$cpass,
+    'chost=s'  => \$chost,
+    'cport=i'  => \$cport,
+    'cdbname=s'       => \$cdbname);
+#
+# Connect to the vgea databse to get the alt allele data.
+#
 
 my $api_version = Bio::EnsEMBL::ApiVersion->software_version();
 
-if(!defined($dbname)){
-  $dbname = "homo_sapiens_core_".$api_version."_37";
+if(!defined($vdbname)){
+  $vdbname = "homo_sapiens_vega_".$api_version."_37";
 }
 
+if(!defined($cdbname)){
+  $cdbname = "homo_sapiens_core_".$api_version."_37";
+}
+
+#
 # Connect to the core database 
+#
 
-my $core_dba = Bio::EnsEMBL::DBSQL::DBAdaptor->new(-host => $host||'ens-staging1',
-					  -port => $port || '3306',
-                                          -user => $user||'ensadmin',
-					  -pass => $pass,
-                                          -dbname => $dbname);
+my $core_dba = Bio::EnsEMBL::DBSQL::DBAdaptor->new(-host => $chost||'ens-staging1',
+                                          -user => $cuser||'ensadmin',
+					  -pass => $cpass,
+                                          -species => "test",
+                                          -dbname => $cdbname||"homo_sapiens_core_63_37");
 
-my $sa = $core_dba->get_adaptor("slice");
-my $ga = $core_dba->get_adaptor("gene");
 
-my @top_slices = @{$sa->fetch_all("toplevel",undef,1)}; #returns all slices including non-reference ("toplevel",undef,1)
-print "Looping over chromosomes...\n";
 
+#
+# get ensembl gene ids and vega stable ids from the core database
+# 
+
+
+my %vega_to_ens_id;
+my ($vega_stable_id, $gene_id);
+
+my $sth =  $core_dba->dbc->prepare("select ensembl_id, display_label from object_xref join xref using(xref_id) join external_db using(external_db_id) where db_name = 'OTTG' and ensembl_object_type = 'Gene'");
+$sth->execute;
+
+$sth->bind_columns(\$gene_id, \$vega_stable_id);
+
+while ($sth->fetch){
+  $vega_to_ens_id{$vega_stable_id} = $gene_id;
+}
+$sth->finish;
+
+
+my $vega_dba = Bio::EnsEMBL::DBSQL::DBAdaptor->new(-host => $vhost||'ens-staging1',
+                                          -user => $vuser||'ensro',
+                                          -port => $vport||3306,
+                                          -dbname => $vdbname||"homo_sapiens_vega_63_37");
+
+#
+# SQL to get alt_allele data from vega
+#
+
+my $sql =(<<EOS);
+select aa.alt_allele_id, g.stable_id
+ from alt_allele aa, gene g 
+ where aa.gene_id = g.gene_id
+EOS
+
+
+#
+# Store data in a hash where the key is the alt_id and the ensembl gene ids 
+# stored in an anonymous array (value of the hash).
+#
+
+my $sth = $vega_dba->dbc->prepare($sql);
+$sth->execute;
+my ($alt_id, $vega_stable_id);
 my %alt_alleles;
-my %reference_genes;
+$sth->bind_columns(\$alt_id, \$vega_stable_id);
 
-my %logic_names = ("havana" => 1,
-		   "ensembl_havana_gene" => 1,
-		   "ensembl_havana_lincrna" => 1,);
 
-my %skip_sources = ("Clone_based_vega_gene" => 1,
-		    "Clone_based_ensembl_gene" => 1);
+my %no_gene_id;
 
-foreach my $slice (@top_slices) {
-  my $slice_name = $slice->seq_region_name;
- GENE:
-  foreach my $gene (@{$ga->fetch_all_by_Slice($slice)}) {
-     
-    next GENE if (exists($skip_sources{$gene->external_db}) );
-
-    next GENE unless (exists($logic_names{$gene->analysis->logic_name}) );
-
-    my $gene_id = $gene->dbID;
-
-    #get name and synonyms
-    my ($syns,$display_id);
-    eval { $display_id = $gene->display_xref->display_id }; 
-    if ($@) {
-      print "Can't get name for gene $gene_id on slice $slice_name\n";
-    } else {
-	push @{$alt_alleles{$display_id}}, $gene_id ;
-	if ($slice->is_reference) {
-	    $reference_genes{$gene_id} = 1;
-	}
-
-    }
-
+while($sth->fetch()){
+    my $gene_id = $vega_to_ens_id{$vega_stable_id};
+  if ( defined($gene_id) ) {
+      push @{$alt_alleles{$alt_id}}, $gene_id ;
+  } else {
+      push @{$no_gene_id{$alt_id}}, $vega_stable_id;
+      print STDERR "no ensembl gene_id found for vega stable id $vega_stable_id in core\n";
   }
 
-}   
+}
+$sth->finish;
+
 
 #
 # Delete the old data
@@ -91,49 +123,24 @@ $sth->execute;
 
 
 #
-# Check that all alt_alleles are valid.
-# NOTE: if an alt allele has only one gene_id delete it from the hash 
+# Store alt_alleles.
 #
+
+my $alt_allele_count=0;
+my $gene_count = 0;
+
+my $ga = $core_dba->get_adaptor("gene");
 
 foreach my $key (keys %alt_alleles){
-  my @arr = @{$alt_alleles{$key}};
-  if(scalar(@arr) <= 1){
-      delete $alt_alleles{$key};
-  }
-}
-
-#
-# Then store the alt_allele and gene_id in the alt_allele table.
-#
-
-my $sql = "insert into alt_allele (alt_allele_id, gene_id, is_ref) values(?, ?, ?)";
-my $insert_sth = $core_dba->dbc->prepare($sql);
-
-my $count=0;
-my $alt_id_count = 0;
-
-
-
-foreach my $key (keys %alt_alleles){
-  my @arr = @{$alt_alleles{$key}};
-  if(scalar(@arr) > 1){
-    my $sanity_check =0; # should be 1 reference gene only if 0 or more than 1 we may have a problem
-    $alt_id_count++;
-    foreach my $gene_id (@arr){
-      my $ref = 0;
-      if(exists($reference_genes{$gene_id})){
-	$ref = 1;
-	$sanity_check++;
-      }
-      $insert_sth->execute($alt_id_count, $gene_id, $ref);
-      $count++;
-    }
-    if($sanity_check !=1){
-      print STDERR "Problem we have $sanity_check genes on the reference sequence for alt_id $alt_id_count gene display_id $key\n";
-    }
-   
+  my @gene_ids = @{$alt_alleles{$key}};
+  my @genes;
+  foreach my $gene_id (@gene_ids) {
+      push @genes, $ga->fetch_by_dbID($gene_id);
   }
 
+  my $alt_allele_id = $ga->store_alt_alleles(\@genes);
+  $alt_allele_count ++ if ($alt_allele_id);
+  $gene_count += scalar(@genes) if ($alt_allele_id);
 }
 
-print "Added $alt_id_count alt_allele ids for $count genes\n";
+print "Added $alt_allele_count alt_allele ids for $gene_count genes\n";
