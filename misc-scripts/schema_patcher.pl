f3166fb8a8366ab001daa028bc9c616e4bf0ecd8
Andrew Yates <ayates@ebi.ac.uk>
2012-05-24 12:47:27 +0000

    Fixes for --fixlast making sure we do not attempt to fix a schema too far in the past

--- misc-scripts/schema_patcher.pl
+++ misc-scripts/schema_patcher.pl
@@ -471,8 +471,9 @@ while ( $sth->fetch() ) {
   #Quick check if fix-last is active. If so we will hard-code some values
   if($opt_fixlast) {
     $opt_fix = 1;
-    $opt_oldest = $schema_version - 1;
-    printf("--fixlast is active. Will apply patches for versions %d and %d\n", $opt_oldest, $schema_version);
+    $opt_oldest = ($schema_version == $latest_release) ? $latest_release : $latest_release - 1;
+    die "Cannot use --fixlast with a schema release too far from the latest release; oldest allowed is $opt_oldest" if $schema_version < ($opt_oldest);
+    printf("--fixlast is active. Will apply patches for version %d and up (if available)\n", $opt_oldest);
   }
   
   if ( $schema_version_ok &&
