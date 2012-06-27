e833c05b8aa00fd6fd08596b7a9e854c7f204d68
Andrew Yates <ayates@ebi.ac.uk>
2012-06-27 22:29:24 +0000

    update to new resource classes

--- modules/Bio/EnsEMBL/Pipeline/PipeConfig/Flatfile_conf.pm
+++ modules/Bio/EnsEMBL/Pipeline/PipeConfig/Flatfile_conf.pm
@@ -81,7 +81,7 @@ sub pipeline_analyses {
         -module     => 'Bio::EnsEMBL::Pipeline::Flatfile::DumpFile',
         -max_retry_count  => 1,
         -hive_capacity    => 10,
-        -rc_id => 1,
+        -rc_name => 'dump',
       },
       
       ####### CHECKSUMMING
@@ -128,8 +128,7 @@ sub beekeeper_extra_cmdline_options {
 sub resource_classes {
     my $self = shift;
     return {
-      0 => { -desc => 'default', 'LSF' => ''},
-      1 => { -desc => 'dump', 'LSF' => '-q normal -M4000000 -R"select[mem>4000] rusage[mem=4000]"'},
+      1 => { -desc => 'dump', 'LSF' => '-q normal -M1000000 -R"select[mem>1000] rusage[mem=1000]"'},
     }
 }
 
