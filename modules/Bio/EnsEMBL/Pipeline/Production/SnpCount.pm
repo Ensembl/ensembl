f18cfb11b2c81c51db99de24734e8f1630232b5c
Magali Ruffier <mr6@ebi.ac.uk>
2012-07-26 08:55:21 +0000

    pipeline run by the release production, to produce stats used for variation features

---
+++ modules/Bio/EnsEMBL/Pipeline/Production/SnpCount.pm
@@ -0,0 +1,40 @@
+package Bio::EnsEMBL::Pipeline::Production::SnpCount;
+
+use strict;
+use warnings;
+
+use base qw/Bio::EnsEMBL::Pipeline::Production::StatsGenerator/;
+
+
+use Bio::EnsEMBL::Attribute;
+
+
+
+sub get_feature_count {
+  my ($self, $slice, $key) = @_;
+  my $variation_adaptor = Bio::EnsEMBL::Registry->get_DBAdaptor($self->param('species'), 'variation');
+  my $helper = $variation_adaptor->dbc()->sql_helper();
+  my $sql = q{
+     SELECT count(*) FROM variation_feature
+     WHERE seq_region_id = ? };
+  my @params = [$slice->get_seq_region_id];
+  my $count = $helper->execute_single_result(-SQL => $sql, -PARAMS => @params);
+  return $count;
+}
+
+
+sub get_attrib_codes {
+  my ($self) = @_;
+  my $prod_dba   = $self->get_production_DBAdaptor();
+  my $prod_helper     = $prod_dba->dbc()->sql_helper();
+  my $sql = q{
+    SELECT name, code
+    FROM attrib_type
+    WHERE name = 'SNP count' };
+  my %attrib_codes = %{ $prod_helper->execute_into_hash(-SQL => $sql) };
+  return %attrib_codes;
+}
+
+
+1;
+
