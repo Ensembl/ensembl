428c93e324a9a507c322e4d9b61dafb34b2e3d46
Magali Ruffier <mr6@ebi.ac.uk>
2012-07-26 08:56:04 +0000

    config file used for the variation stats pipeline

---
+++ modules/Bio/EnsEMBL/Pipeline/PipeConfig/Variation_handover_conf.pm
@@ -0,0 +1,122 @@
+package Bio::EnsEMBL::Pipeline::PipeConfig::Variation_handover_conf;
+
+use strict;
+use warnings;
+
+use base ('Bio::EnsEMBL::Hive::PipeConfig::HiveGeneric_conf');
+
+use Bio::EnsEMBL::ApiVersion qw/software_version/;
+
+sub default_options {
+    my ($self) = @_;
+    
+    return {
+        # inherit other stuff from the base class
+        %{ $self->SUPER::default_options() }, 
+        
+        ### OVERRIDE
+        
+        ### Optional overrides        
+        species => [],
+        
+        release => software_version(),
+
+        bin_count => '150',
+        
+        ### Defaults 
+        
+        pipeline_name => 'variation_handover_update_'.$self->o('release'),
+        
+        email => $self->o('ENV', 'USER').'@sanger.ac.uk',
+    };
+}
+
+sub pipeline_create_commands {
+    my ($self) = @_;
+    return [
+      # inheriting database and hive tables' creation
+      @{$self->SUPER::pipeline_create_commands}, 
+    ];
+}
+
+## See diagram for pipeline structure 
+sub pipeline_analyses {
+    my ($self) = @_;
+    
+    return [
+    
+      {
+        -logic_name => 'ScheduleSpecies',
+        -module     => 'Bio::EnsEMBL::Pipeline::Production::ClassSpeciesFactory',
+        -parameters => {
+          species => $self->o('species'),
+        },
+        -input_ids  => [ {} ],
+        -flow_into  => {
+          1 => 'Notify',
+          4 => ['SnpDensity', 'SnpCount'],
+        },
+      },
+
+      {
+        -logic_name => 'SnpCount',
+        -module     => 'Bio::EnsEMBL::Pipeline::Production::SnpCount',
+        -max_retry_count  => 1,
+        -hive_capacity    => 10,
+        -rc_name          => 'default',
+        -can_be_empty     => 1,
+      },
+
+      {
+        -logic_name => 'SnpDensity',
+        -module     => 'Bio::EnsEMBL::Pipeline::Production::SnpDensity',
+        -parameters => {
+          table => 'gene', logic_name => 'snpdensity', value_type => 'sum',
+          bin_count => $self->o('bin_count'),
+        },
+        -max_retry_count  => 1,
+        -hive_capacity    => 10,
+        -rc_name          => 'default',
+        -can_be_empty     => 1,
+      },
+
+      ####### NOTIFICATION
+      
+      {
+        -logic_name => 'Notify',
+        -module     => 'Bio::EnsEMBL::Pipeline::Production::EmailSummaryVariation',
+        -parameters => {
+          email   => $self->o('email'),
+          subject => $self->o('pipeline_name').' has finished',
+        },
+        -wait_for   => ['SnpDensity', 'SnpCount'],
+      }
+    
+    ];
+}
+
+sub pipeline_wide_parameters {
+    my ($self) = @_;
+    
+    return {
+        %{ $self->SUPER::pipeline_wide_parameters() },  # inherit other stuff from the base class
+        release => $self->o('release'),
+    };
+}
+
+# override the default method, to force an automatic loading of the registry in all workers
+sub beekeeper_extra_cmdline_options {
+    my $self = shift;
+    return "-reg_conf ".$self->o("registry");
+}
+
+sub resource_classes {
+    my $self = shift;
+    return {
+      'default' => { 'LSF' => ''},
+      'normal'  => { 'LSF' => '-q normal -M 400000 -R"select[mem>400] rusage[mem=400]"'},
+      'mem'     => { 'LSF' => '-q normal -M 4000000 -R"select[mem>4000] rusage[mem=4000]"'},
+    }
+}
+
+1;
