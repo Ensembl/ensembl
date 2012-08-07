b64d135626e2e7ab7755089d53b9ed54551db5d0
Monika Komorowska <mk8@sanger.ac.uk>
2012-08-07 10:15:36 +0000

    updated transcript_display_xref_sources and added gene_display_xref_sources

--- misc-scripts/xref_mapping/XrefMapper/xenopus_tropicalis.pm
+++ misc-scripts/xref_mapping/XrefMapper/xenopus_tropicalis.pm
@@ -17,37 +17,31 @@ sub gene_description_filter_regexps {
 
 sub gene_description_sources {
 
-  return ("Xenopus_Jamboree",
-	  "MGI",
+  return ("Xenopus_Jamboree",	  
 	  "RFAM",
-	  "flybase_symbol",
-	  "Anopheles_symbol",
-	  "Genoscope_annotated_gene",
+	  "miRBase",
 	  "Uniprot/SWISSPROT",
 	  "RefSeq_peptide",
 	  "RefSeq_dna",
-	  "Uniprot/SPTREMBL",
-	  "EntrezGene");
+	  "Uniprot/Varsplic",
+	  "Uniprot/SPTREMBL");
 }
 
-sub transcript_display_xref_sources {
+
+sub gene_display_xref_sources {
   my $self     = shift;
-
+	
   my @list = qw(Xenopus_Jamboree
-		MGI
-		flybase_symbol
-		Anopheles_symbol
-		Genoscope_annotated_gene
-		Uniprot/SWISSPROT
-		RefSeq_peptide
-		RefSeq_dna
-		Uniprot/SPTREMBL
-		EntrezGene);
-
+                RFAM
+                miRBase
+                Uniprot_genename
+                EntrezGene);
 
   my %ignore;
 
-  $ignore{"EntrezGene"} =(<<'IEG');
+  #don't use EntrezGene labels dependent on predicted RefSeqs
+
+$ignore{'EntrezGene'} =<<IEG;
 SELECT DISTINCT ox.object_xref_id
   FROM object_xref ox, dependent_xref dx, 
        xref xmas, xref xdep, 
@@ -59,16 +53,34 @@ SELECT DISTINCT ox.object_xref_id
           xdep.source_id = sdep.source_id AND
           smas.name like "Refseq%predicted" AND
           sdep.name like "EntrezGene" AND
-          ox.ox_status = "DUMP_OUT"
+          ox.ox_status = "DUMP_OUT" 	 
 IEG
 
-  $ignore{"Uniprot/SPTREMBL"} =(<<BIGN);
-SELECT object_xref_id
-    FROM object_xref JOIN xref USING(xref_id) JOIN source USING(source_id)
-     WHERE ox_status = 'DUMP_OUT' AND name = 'Uniprot/SPTREMBL' 
-      AND priority_description = 'protein_evidence_gt_2'
-BIGN
+  #don't use labels starting with LOC
 
+$ignore{'LOC_prefix'} =<<LOCP;
+SELECT object_xref_id
+  FROM object_xref JOIN xref USING(xref_id) JOIN source USING(source_id)
+   WHERE ox_status = 'DUMP_OUT' AND label REGEXP '^LOC[[:digit:]]+'
+LOCP
+
+  return [\@list,\%ignore];
+
+}
+
+
+sub transcript_display_xref_sources {
+  my $self     = shift;
+
+  my @list = qw(Xenopus_Jamboree
+                RFAM
+                miRBase
+                Uniprot/SWISSPROT
+                Uniprot/Varsplic	
+  );
+
+
+  my %ignore;
 
   return [\@list,\%ignore];
 }
