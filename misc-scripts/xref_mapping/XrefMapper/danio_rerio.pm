0c1835985271415917afd0a27f42cc4daf9ef651
Monika Komorowska <mk8@sanger.ac.uk>
2012-08-07 09:23:20 +0000

    no need to override method transcript_display_xref_sources - diplay xrefs are done in OfficialNaming.pm for zebrafish

--- misc-scripts/xref_mapping/XrefMapper/danio_rerio.pm
+++ misc-scripts/xref_mapping/XrefMapper/danio_rerio.pm
@@ -71,52 +71,6 @@ sub gene_description_sources {
 	  "Uniprot/SPTREMBL");
 
 }
-sub transcript_display_xref_sources {
 
-  my @list = qw(ZFIN_ID
-		MGI
-		flybase_symbol
-		Anopheles_symbol
-		Genoscope_annotated_gene
-		Uniprot/SWISSPROT
-		RefSeq_peptide
-		RefSeq_dna
-		Uniprot/SPTREMBL
-		EntrezGene);
-
-  my %ignore;
-
-
-  $ignore{"EntrezGene"} =(<<'IEG');
-SELECT DISTINCT ox.object_xref_id
-  FROM object_xref ox, dependent_xref dx, 
-       xref xmas, xref xdep, 
-       source smas, source sdep
-    WHERE ox.xref_id = dx.dependent_xref_id AND
-          dx.dependent_xref_id = xdep.xref_id AND
-          dx.master_xref_id = xmas.xref_id AND
-          xmas.source_id = smas.source_id AND
-          xdep.source_id = sdep.source_id AND
-          smas.name like "Refseq%predicted" AND
-          sdep.name like "EntrezGene" AND
-          ox.ox_status = "DUMP_OUT"
-IEG
-
-    $ignore{"Uniprot/SPTREMBL"} =(<<BIGN);
-SELECT object_xref_id
-    FROM object_xref JOIN xref USING(xref_id) JOIN source USING(source_id)
-     WHERE ox_status = 'DUMP_OUT' AND name = 'Uniprot/SPTREMBL' 
-      AND priority_description = 'protein_evidence_gt_2'
-BIGN
-
-  return [\@list,\%ignore];
-
-}
-
-sub gene_description_filter_regexps {
-
-  return ();
-
-}
 
 1;
