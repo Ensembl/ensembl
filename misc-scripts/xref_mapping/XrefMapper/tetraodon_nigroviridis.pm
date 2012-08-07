35be2fd7b930f2cbe37393e70aff1babd564bd68
Monika Komorowska <mk8@sanger.ac.uk>
2012-08-07 10:09:26 +0000

    updated transcript_display_xref_sources and added gene_display_xref_sources

--- misc-scripts/xref_mapping/XrefMapper/tetraodon_nigroviridis.pm
+++ misc-scripts/xref_mapping/XrefMapper/tetraodon_nigroviridis.pm
@@ -6,34 +6,62 @@ use vars '@ISA';
 
 @ISA = qw{ XrefMapper::BasicMapper };
 
-# Same as in BasicMapper but Genoscope order reversed.
-
-sub transcript_display_xref_sources {
-
-  my @list = qw(HGNC
-		MGI
-		wormbase_transcript
-		flybase_symbol
-		Anopheles_symbol
-		Genoscope_annotated_gene
-		Genoscope_predicted_transcript
-		Uniprot/SWISSPROT
-		RefSeq
-		Uniprot/SPTREMBL
-		LocusLink);
+sub gene_display_xref_sources {
+  my $self     = shift;
+	
+  my @list = qw(Genoscope_annotated_gene
+                RFAM
+                miRBase
+                Uniprot_genename
+                EntrezGene
+                Genoscope_pred_gene
+);
 
   my %ignore;
 
-  $ignore{"Uniprot/SPTREMBL"} =(<<BIGN);
-SELECT object_xref_id
-    FROM object_xref JOIN xref USING(xref_id) JOIN source USING(source_id)
-     WHERE ox_status = 'DUMP_OUT' AND name = 'Uniprot/SPTREMBL' 
-      AND priority_description = 'protein_evidence_gt_2'
-BIGN
+  #don't use EntrezGene labels dependent on predicted RefSeqs
 
+$ignore{'EntrezGene'} =<<IEG;
+SELECT DISTINCT ox.object_xref_id
+  FROM object_xref ox, dependent_xref dx, 
+       xref xmas, xref xdep, 
+       source smas, source sdep
+    WHERE ox.xref_id = dx.dependent_xref_id AND
+          dx.dependent_xref_id = xdep.xref_id AND
+          dx.master_xref_id = xmas.xref_id AND
+          xmas.source_id = smas.source_id AND
+          xdep.source_id = sdep.source_id AND
+          smas.name like "Refseq%predicted" AND
+          sdep.name like "EntrezGene" AND
+          ox.ox_status = "DUMP_OUT" 	 
+IEG
+
+  #don't use labels starting with LOC
+
+$ignore{'LOC_prefix'} =<<LOCP;
+SELECT object_xref_id
+  FROM object_xref JOIN xref USING(xref_id) JOIN source USING(source_id)
+   WHERE ox_status = 'DUMP_OUT' AND label REGEXP '^LOC[[:digit:]]+'
+LOCP
 
   return [\@list,\%ignore];
 
+}
+
+sub transcript_display_xref_sources {
+
+  my @list = qw(	
+	        Genoscope_ann_transcript
+		RFAM
+                miRBase          
+		Uniprot/SWISSPROT
+                Uniprot/Varsplic
+                Genoscope_pred_transcript
+);
+
+  my %ignore;
+
+  return [\@list,\%ignore];
 
 }
 
