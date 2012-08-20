8cc61a1d0d331e453d91e9655b654f64100d332f
Kevin Howe <klh@ebi.ac.uk>
2012-08-20 14:45:57 +0000

    Override set_methods for WormBase species to allow non-exact matching to Uniprot records

--- misc-scripts/xref_mapping/XrefMapper/wormbase.pm
+++ misc-scripts/xref_mapping/XrefMapper/wormbase.pm
@@ -7,8 +7,23 @@ use vars qw(@ISA);
 @ISA = qw(XrefMapper::BasicMapper);
 
 
-# Abstact superclass, not intended to be created directly. Contains
-# common methods for the "tierII" nematodes in WormBase
+# This module is activated by specifying "taxon=wormbase" in the mapping input file
+# It contains some common config for worms maintained by WormBase (i.e. having genes
+# with WBGene ids etc)
+
+sub set_methods{
+ 
+  my $default_method = 'ExonerateGappedBest1';
+  my %override_method_for_source = (  
+           ExonerateGappedBest5 => ['RefSeq_mRNA',
+                                    'RefSeq_mRNA_predicted', 
+                                    'RefSeq_ncRNA', 
+                                    'RefSeq_ncRNA_predicted' ],
+         );
+
+  return $default_method, \%override_method_for_source;
+}
+
 
 
 sub set_display_xrefs {
