f4ce1d991313db76d8db28ebc132860af13b9f42
Magali Ruffier <mr6@ebi.ac.uk>
2012-08-22 12:21:08 +0000

    added a get_total method for snp density when counting features, do not use strict lower/higher

--- modules/Bio/EnsEMBL/Pipeline/Production/SnpDensity.pm
+++ modules/Bio/EnsEMBL/Pipeline/Production/SnpDensity.pm
@@ -15,11 +15,20 @@ sub get_density {
   my $sql = q{
      SELECT count(*) FROM variation_feature
      WHERE seq_region_id = ?
-     AND seq_region_start < ?
-     AND seq_region_end > ? };
+     AND seq_region_start <= ?
+     AND seq_region_end >= ? };
   my @params = [$block->get_seq_region_id, $block->end, $block->start];
   my $count = $helper->execute_single_result(-SQL => $sql, -PARAMS => @params);
   return $count;
+}
+
+sub get_total {
+  my ($self, $option) = @_;
+  my $species = $self->param('species');
+  my $variation_adaptor = Bio::EnsEMBL::Registry->get_DBAdaptor($species, 'variation');
+  my $helper = $variation_adaptor->dbc()->sql_helper();
+  my $sql = "SELECT count(*) FROM variation_feature";
+  return $helper->execute_single_result(-SQL => $sql);
 }
 
 return 1;
