72b5259c5ff9c5a0dc80e1ef7c7b2a782d250af4
Arnaud Kerhornou <arnaud@ebi.ac.uk>
2012-10-04 14:36:14 +0000

    Added a method to update the list of sources for assigning gene names for Ensembl Genomes species

--- misc-scripts/xref_mapping/XrefMapper/eukaryota.pm
+++ misc-scripts/xref_mapping/XrefMapper/eukaryota.pm
@@ -18,10 +18,69 @@ sub set_methods{
   return $default_method, \%override_method_for_source;
 }
 
-sub transcript_display_xref_sources {
+sub gene_display_xref_sources {
     my $self     = shift;
 
     print STDERR "getting the list of external_dbs for assigning gene names from eukaryota.pm\n";
+
+    my @list = qw(
+                 RFAM
+                 RNAMMER
+                 TRNASCAN_SE
+                 Uniprot_genename
+                 ENA_GENE
+                 BROAD_U_maydis
+                 BROAD_F_oxysporum
+                 BROAD_G_zeae
+                 BROAD_G_moniliformis
+                 BROAD_P_infestans
+                 phyra_jgi_v1.1
+                 physo1_jgi_v1.1
+	   	 phatr_jgi_v2
+		 phatr_jgi_v2_bd
+                 PGD_GENE
+                 Mycgr3_jgi_v2.0_gene
+                 BROAD_Magnaporthe_DB
+                 PGSC_GENE
+                 PHYTOZOME_GMAX_GENE
+               );
+    
+    my %ignore;
+
+    
+    #don't use EntrezGene labels dependent on predicted RefSeqs
+
+    $ignore{'EntrezGene'} =<<IEG;
+SELECT DISTINCT ox.object_xref_id
+  FROM object_xref ox, dependent_xref dx, 
+       xref xmas, xref xdep, 
+       source smas, source sdep
+    WHERE ox.xref_id = dx.dependent_xref_id AND
+          dx.dependent_xref_id = xdep.xref_id AND
+          dx.master_xref_id = xmas.xref_id AND
+          xmas.source_id = smas.source_id AND
+          xdep.source_id = sdep.source_id AND
+          smas.name like "Refseq%predicted" AND
+          sdep.name like "EntrezGene" AND
+          ox.ox_status = "DUMP_OUT" 	 
+IEG
+
+    #don't use labels starting with LOC
+
+    $ignore{'LOC_prefix'} =<<LOCP;
+SELECT object_xref_id
+  FROM object_xref JOIN xref USING(xref_id) JOIN source USING(source_id)
+   WHERE ox_status = 'DUMP_OUT' AND label REGEXP '^LOC[[:digit:]]+'
+LOCP
+
+    return [\@list,\%ignore];
+}
+
+
+sub transcript_display_xref_sources {
+    my $self     = shift;
+
+    print STDERR "getting the list of external_dbs for assigning transcript names from eukaryota.pm\n";
 
     my @list = qw(
                  RFAM
