25548a24a3efe62551a5f44e37b4ade461f16628
Magali Ruffier <mr6@ebi.ac.uk>
2012-11-01 14:03:56 +0000

    moved get_total and get_density modules to parent module

--- modules/Bio/EnsEMBL/Pipeline/Production/NonCodingDensity.pm
+++ modules/Bio/EnsEMBL/Pipeline/Production/NonCodingDensity.pm
@@ -13,22 +13,6 @@ sub get_option {
   return \@biotypes;
 }
 
-sub get_density {
-  my ($self, $block, $biotypes) = @_;
-  my $count = 0;
-  foreach my $biotype (@$biotypes) {
-    $count += scalar(@{ $block->get_all_Genes_by_type($biotype) });
-  }
-  return $count;
-}
-
-sub get_total {
-  my ($self, $option) = @_;
-  my $species = $self->param('species');
-  my $ga = Bio::EnsEMBL::Registry->get_adaptor($species, 'core', 'gene');
-  return scalar(@{ $ga->fetch_all_by_biotype($option) });
-}
-
 1;
 
 
