4f5584b83df7dc8c8d2376839211c2e9f3cc6b7b
Derek Wilson <dw8@sanger.ac.uk>
2013-01-04 15:37:08 +0000

    Improved variable names.  Mikkel is doing something kinky with source ids.

--- misc-scripts/xref_mapping/XrefParser/VBCommunitySymbolParser.pm
+++ misc-scripts/xref_mapping/XrefParser/VBCommunitySymbolParser.pm
@@ -17,15 +17,20 @@ sub run {
   my $release_file = $ref_arg->{rel_file};
   my $verbose      = $ref_arg->{verbose};
 
-  if((!defined $source_id) or (!defined $species_id) or (!defined $files)){
+  if ((!defined $source_id) or (!defined $species_id) or (!defined $files)) {
     croak "Need to pass source_id, species_id, files and rel_file as pairs";
   }
   $verbose |=0;
 
   my $file = @{$files}[0];
 
-  print "source_id = $source_id, species= $species_id, file = $file\n" if($verbose);
-
+  print "source_id = $source_id, species= $species_id, file = $file\n" if $verbose;
+  
+  #Hack to get source ID to Synonyms
+  open OUT,">>","source_id_file";
+  print OUT "$species_id\t$source_id\n";
+  close OUT;
+ 
   my $added = 0;
   my $count = 0;
 
@@ -33,33 +38,41 @@ sub run {
 
   if ( !defined $file_io ) {
     print STDERR "ERROR: Could not open file $file\n";
+
     return 1;
   }
 
   while ( my $line = $file_io->getline() ) {
-    chomp $line;
-    my ($acc,$full_description, $gene_id)
-      = split("\t",$line);
+    next unless $line =~ /^\w+/;
 
-    my $xref_id = $self->get_xref($acc,$source_id, $species_id);
-    if(!defined($xref_id)){
-      $xref_id = $self->add_xref({ acc        => $acc,
-				   label      => $acc,
-				   desc       => $full_description,
-				   source_id  => $source_id,
-				   species_id => $species_id,
-				   info_type  =>  "DIRECT"} );
+    chomp $line;
+    my ($cap_id, $stable_id, $type, $gene_symbol, $full_description) = split "\t", $line;
+
+    my $xref_id = $self->get_xref($stable_id, $source_id, $species_id);
+
+    if (!defined($xref_id)) {
+      $full_description = '' unless defined $full_description;
+
+      $xref_id = $self->add_xref({ acc        => $stable_id,
+				   label      => $gene_symbol,
+                                   desc       => $full_description,
+                                   source_id  => $source_id,
+                                   species_id => $species_id,
+                                   info_type  => 'DIRECT'});
       $count++;
+      
     }
-    if(defined($gene_id) and $gene_id ne "-"){
-      $self->add_direct_xref($xref_id, $gene_id, "Gene", "") ;
+
+    if (defined($stable_id) and $stable_id ne '-') {
+      $self->add_direct_xref($xref_id, $stable_id, 'Gene', '');
       $added++;
     }
   }
 
   $file_io->close();
 
-  print "Added $count xrefs and $added Direct xrefs to genes for VBCommunitySymbol\n" if($verbose);
+  print "Added $count xrefs and $added Direct xrefs to genes for VBCommunitySymbol\n" if $verbose;
+
   return 0;
 }
 
