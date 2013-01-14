6fbbb86e6caaa7c8763531c36bb10012523e5bd7
Arnaud Kerhornou <arnaud@ebi.ac.uk>
2013-01-14 10:10:35 +0000

    Foxed Type, sTDERR => STDERR

--- misc-scripts/xref_mapping/XrefParser/CoreXrefParser.pm
+++ misc-scripts/xref_mapping/XrefParser/CoreXrefParser.pm
@@ -145,7 +145,7 @@ sub run_script {
       # print STDERR "processing " . @xrefs . " xrefs\n";
 
       if (@xrefs == 0) {
-	  print sTDERR "No xrefs for gene, " . $object->stable() . "!\n";
+	  print STDERR "No xrefs for gene, " . $object->stable() . "!\n";
       }
 
       foreach my $xref (@xrefs) {
