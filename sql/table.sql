786ae70f5758ca2889e73721f5cf89853b5e79fa
Kieron Taylor <ktaylor@ebi.ac.uk>
2013-01-16 11:46:22 +0000

    Added modifications to alt_allele table definition.

--- sql/table.sql
+++ sql/table.sql
@@ -895,6 +895,7 @@ CREATE TABLE unconventional_transcript_association (
 @column alt_allele_id          Primary key, internal identifier.
 @column gene_id                Foreign key references to the @link gene table.
 @column is_ref		       Indicates a reference allele if set to 1.
+@column type               Type of alt_allele, e.g. manually annotated
 
 */
 
@@ -902,9 +903,11 @@ CREATE TABLE alt_allele (
   alt_allele_id         INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
   gene_id               INT(10) UNSIGNED NOT NULL,
   is_ref                BOOLEAN NOT NULL DEFAULT 0,
+  type                  VARCHAR(40),
 
   UNIQUE KEY gene_idx (gene_id),
-  UNIQUE KEY allele_idx (alt_allele_id, gene_id)
+  UNIQUE KEY allele_idx (alt_allele_id, gene_id),
+  KEY type_idx (type,alt_allele_id)
 
 ) COLLATE=latin1_swedish_ci ENGINE=MyISAM;
 
