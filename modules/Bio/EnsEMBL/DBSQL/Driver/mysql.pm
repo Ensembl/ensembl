a569c7db3d521754735888d9622785ea31afca6d
Michael Gray <mg13@sanger.ac.uk>
2013-02-15 17:52:23 +0000

    Move DBD connection details into Driver::<type> subclasses.

--- modules/Bio/EnsEMBL/DBSQL/Driver/mysql.pm
+++ modules/Bio/EnsEMBL/DBSQL/Driver/mysql.pm
@@ -5,6 +5,32 @@ use strict;
 
 use base 'Bio::EnsEMBL::DBSQL::Driver';
 
+sub connect_params {
+    my ($self, $conn) = @_;
+
+    my $dbname = $conn->dbname();
+    my $dbparam = ($dbname) ? "database=${dbname};" : q{};
+
+    my $dsn = sprintf( "DBI:%s:%shost=%s;port=%s",
+                       $conn->driver(), $dbparam,
+                       $conn->host(),   $conn->port() );
+
+    if ( $conn->{'disconnect_when_inactive'} ) {
+      $conn->{'count'}++;
+      if ( $conn->{'count'} > 1000 ) {
+        sleep 1;
+        $conn->{'count'} = 0;
+      }
+    }
+
+    return {
+        dsn        => $dsn,
+        username   => $conn->username(),
+        password   => $conn->password(),
+        attributes => { 'RaiseError' => 1 },
+    };
+}
+
 sub from_date_to_seconds {
     my ($self, $column) = @_;
     return "UNIX_TIMESTAMP($column)";
