3b14dc320bdbc03413713dcffb7ff0a488039dc3
Michael Gray <mg13@sanger.ac.uk>
2013-02-15 17:34:25 +0000

    AttributeAdaptor.pm: portable SQL.
    This may be sub-optimal for MySQL.

--- modules/Bio/EnsEMBL/DBSQL/AttributeAdaptor.pm
+++ modules/Bio/EnsEMBL/DBSQL/AttributeAdaptor.pm
@@ -247,10 +247,12 @@ sub remove_from_{
 
   my $sth;
   if(defined($code)){
-    $sth = $self->prepare("DELETE a FROM ".$table."_attrib a ,attrib_type at " .
-                         "WHERE a.attrib_type_id = at.attrib_type_id AND ".
-                         "a.".$type."_id = ? AND ".
-			 "at.code like ?");
+    $sth = $self->prepare(qq{DELETE FROM ${table}_attrib
+                              WHERE ${type}_id = ? AND
+                                     attrib_type_id IN
+                             (SELECT attrib_type_id
+                                FROM attrib_type
+                               WHERE code LIKE ? ) });
     $sth->bind_param(1,$object_id,SQL_INTEGER);
     $sth->bind_param(2,$code,SQL_VARCHAR);
   }
