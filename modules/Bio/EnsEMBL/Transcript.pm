7d827de2f906e2c68cac783561d26f31a688a333
Kieron Taylor <ktaylor@ebi.ac.uk>
2013-03-18 13:48:37 +0000

    Removed some defunct POD

--- modules/Bio/EnsEMBL/Transcript.pm
+++ modules/Bio/EnsEMBL/Transcript.pm
@@ -597,7 +597,6 @@ sub is_known {
 =head2 status
 
   Arg [1]    : string $status
-  Example    : none
   Description: get/set for attribute status
   Returntype : string
   Exceptions : none
@@ -615,7 +614,6 @@ sub status {
 =head2 biotype
 
   Arg [1]    : string $biotype
-  Example    : none
   Description: get/set for attribute biotype
   Returntype : string
   Exceptions : none
@@ -820,7 +818,6 @@ sub add_alternative_translation {
 =head2 spliced_seq
 
   Args       : none
-  Example    : none
   Description: Retrieves all Exon sequences and concats them together.
                No phase padding magic is done, even if phases do not align.
   Returntype : Text
@@ -1450,7 +1447,7 @@ sub get_all_Exons {
 
   Arg        :  None
 
-  Examples   :  my @exons = @{ $transcript->get_all_constitutive_Exons() };
+  Example   :  my @exons = @{ $transcript->get_all_constitutive_Exons() };
 
   Description:  Returns an listref of the constitutive exons in this
                 transcript in order, i.e. the first exon in the
@@ -1780,7 +1777,6 @@ sub three_prime_utr_Feature {
 =head2 get_all_translateable_Exons
 
   Args       : none
-  Example    : none
   Description: Returns a list of exons that translate with the
                start and end exons truncated to the CDS regions.
                This function does not take into account any SeqEdits
