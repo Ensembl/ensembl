26447a2c24077e2197182e1a5d658233b4d1dbe4
Magali Ruffier <mr6@ebi.ac.uk>
2013-05-16 16:01:13 +0000

    latest schema for expression tables

--- sql/patch_rnaseq.sql
+++ sql/patch_rnaseq.sql
@@ -4,9 +4,10 @@ CREATE TABLE gene_expression (
   tissue_id                   INT(10) UNSIGNED NOT NULL,
   value                       TEXT NOT NULL,
   analysis_id                 SMALLINT UNSIGNED NOT NULL,
+  value_type                  ENUM('count', 'RPKM') NOT NULL,
 
   PRIMARY KEY (gene_expression_id),
-  UNIQUE KEY gene_expression_idx(gene_id, tissue_id, analysis_id)
+  UNIQUE KEY gene_expression_idx(gene_id, tissue_id, analysis_id, value_type)
 
 
 
@@ -18,9 +19,10 @@ CREATE TABLE transcript_expression (
   tissue_id                   INT(10) UNSIGNED NOT NULL,
   value                       TEXT NOT NULL,
   analysis_id                 SMALLINT UNSIGNED NOT NULL,
+  value_type                  ENUM('count', 'RPKM') NOT NULL,
 
   PRIMARY KEY (transcript_expression_id),
-  UNIQUE KEY transcript_expression_idx(transcript_id, tissue_id, analysis_id)
+  UNIQUE KEY transcript_expression_idx(transcript_id, tissue_id, analysis_id, value_type)
 
 
 
@@ -32,9 +34,10 @@ CREATE TABLE exon_expression (
   tissue_id                   INT(10) UNSIGNED NOT NULL,
   value                       TEXT NOT NULL,
   analysis_id                 SMALLINT UNSIGNED NOT NULL,
+  value_type                  ENUM('count', 'RPKM') NOT NULL,
 
   PRIMARY KEY (exon_expression_id),
-  UNIQUE KEY exon_expression_idx(exon_id, tissue_id, analysis_id)
+  UNIQUE KEY exon_expression_idx(exon_id, tissue_id, analysis_id, value_type)
 
 
 
@@ -44,10 +47,10 @@ CREATE TABLE tissue (
   tissue_id                   INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
   ontology                    VARCHAR(64) NOT NULL,
   name                        VARCHAR(255),
-  is_a                        VARCHAR(255),
   description                 TEXT,
 
   PRIMARY KEY (tissue_id),
   UNIQUE KEY name_idx (name)
 
 ) COLLATE=latin1_swedish_ci ENGINE=MyISAM;
+
