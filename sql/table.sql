3393635c34b90ae6d9e145c886994a2a656a2041
Magali Ruffier <mr6@ebi.ac.uk>
2013-05-16 15:46:27 +0000

    added column value_type

--- sql/table.sql
+++ sql/table.sql
@@ -2662,9 +2662,10 @@ CREATE TABLE gene_expression (
   tissue_id                   INT(10) UNSIGNED NOT NULL,
   value                       TEXT NOT NULL,
   analysis_id                 SMALLINT UNSIGNED NOT NULL,
+  value_type                  ENUM('count', 'RPKM'),
 
   PRIMARY KEY (gene_expression_id),
-  UNIQUE KEY gene_expression_idx(gene_id, tissue_id, analysis_id)
+  UNIQUE KEY gene_expression_idx(gene_id, tissue_id, analysis_id, value_type)
 
 
 
@@ -2685,12 +2686,13 @@ CREATE TABLE gene_expression (
 CREATE TABLE transcript_expression (
   transcript_expression_id          INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
   transcript_id                     INT(10) UNSIGNED NOT NULL,
-  tissue_id                   INT(10) UNSIGNED NOT NULL,
-  value                       TEXT NOT NULL,
-  analysis_id                 SMALLINT UNSIGNED NOT NULL,
+  tissue_id                         INT(10) UNSIGNED NOT NULL,
+  value                             TEXT NOT NULL,
+  analysis_id                       SMALLINT UNSIGNED NOT NULL,
+  value_type                        ENUM('count', 'RPKM'),
 
   PRIMARY KEY (transcript_expression_id),
-  UNIQUE KEY transcript_expression_idx(transcript_id, tissue_id, analysis_id)
+  UNIQUE KEY transcript_expression_idx(transcript_id, tissue_id, analysis_id, value_type)
 
 
 
@@ -2714,9 +2716,10 @@ CREATE TABLE exon_expression (
   tissue_id                   INT(10) UNSIGNED NOT NULL,
   value                       TEXT NOT NULL,
   analysis_id                 SMALLINT UNSIGNED NOT NULL,
+  value_type                  ENUM('count', 'RPKM'),
 
   PRIMARY KEY (exon_expression_id),
-  UNIQUE KEY exon_expression_idx(exon_id, tissue_id, analysis_id)
+  UNIQUE KEY exon_expression_idx(exon_id, tissue_id, analysis_id, value_type)
 
 
 
