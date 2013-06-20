b1a1291f8e6dc4c857b46d714215dc40c89f4d01
Magali Ruffier <mr6@ebi.ac.uk>
2013-06-20 11:23:51 +0000

    ontology schema files stay in ensembl-core

---
+++ misc-scripts/ontology/sql/patch_71_72_c.sql
@@ -0,0 +1,16 @@
+-- patch_71_72_c.sql
+--
+-- Title: Insert schema version.
+--
+-- Description:
+--   Adding schema version to the meta table (set to 72)
+--   so that script schema_patcher.pl would work
+
+INSERT INTO meta (meta_key, meta_value)
+  VALUES ('schema_version', 72);
+
+-- Patch identifier
+INSERT INTO meta (meta_key, meta_value)
+  VALUES ('patch', 'patch_71_72_c.sql|schema_version');
+
+
