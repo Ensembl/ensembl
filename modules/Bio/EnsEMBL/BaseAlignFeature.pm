4a03ee77bf7ca8dfd0bcd272cc0f49b15825d037
Alessandro Vullo <avullo@ebi.ac.uk>
2013-06-27 10:37:35 +0000

    Removed new_fast method already inherited from parent (Feature) module

--- modules/Bio/EnsEMBL/BaseAlignFeature.pm
+++ modules/Bio/EnsEMBL/BaseAlignFeature.pm
@@ -187,33 +187,6 @@ sub new {
 }
 
 
-=head2 new_fast
-
-  Arg [1]    : hashref $hashref
-               A hashref which will be blessed into a PepDnaAlignFeature.
-  Example    : none
-  Description: This allows for very fast object creation when a large number
-               of PepDnaAlignFeatures needs to be created.  This is a bit of
-               a hack but necessary when thousands of features need to be
-               generated within a couple of seconds for web display. It is
-               not recommended that this method be called unless you know what
-               you are doing.  It requires knowledge of the internals of this
-               class and its superclasses.
-  Returntype : Bio::EnsEMBL::BaseAlignFeature
-  Exceptions : none
-  Caller     : none currently
-  Status     : Stable
-
-=cut
-
-sub new_fast {
-  my ($class, $hashref) = @_;
-  my $self = bless $hashref, $class;
-  weaken($self->{adaptor})  if ( ! isweak($self->{adaptor}) );
-  return $self;
-}
-
-
 =head2 cigar_string
 
   Arg [1]    : string $cigar_string
