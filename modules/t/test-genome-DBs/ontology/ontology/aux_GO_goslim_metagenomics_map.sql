7a25e65851e5d7ad8d04cab06ba73c1370520379
Magali Ruffier <mr6@ebi.ac.uk>
2013-08-27 09:32:38 +0000

    added schema_version information to meta table allows for future schema patching added single table.sql file, replaces the need for individual sql files for each table

--- modules/t/test-genome-DBs/ontology/ontology/aux_GO_goslim_metagenomics_map.sql
+++
@@ -1,9 +0,0 @@
-SET @saved_cs_client     = @@character_set_client;
-SET character_set_client = utf8;
-CREATE TABLE `aux_GO_goslim_metagenomics_map` (
-  `term_id` int(10) unsigned NOT NULL,
-  `subset_term_id` int(10) unsigned NOT NULL,
-  `distance` tinyint(3) unsigned NOT NULL,
-  UNIQUE KEY `map_idx` (`term_id`,`subset_term_id`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
-SET character_set_client = @saved_cs_client;
--- modules/t/test-genome-DBs/ontology/ontology/relation_type.sql
+++
@@ -1,9 +0,0 @@
-SET @saved_cs_client     = @@character_set_client;
-SET character_set_client = utf8;
-CREATE TABLE `relation_type` (
-  `relation_type_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `name` varchar(64) NOT NULL,
-  PRIMARY KEY (`relation_type_id`),
-  UNIQUE KEY `name_idx` (`name`)
-) ENGINE=MyISAM AUTO_INCREMENT=85 DEFAULT CHARSET=latin1;
-SET character_set_client = @saved_cs_client;
