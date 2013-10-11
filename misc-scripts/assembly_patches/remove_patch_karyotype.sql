4d235995f625325d963ebb6083a04b35ea671ca2
ens-carlos <cgg@sanger.ac.uk>
2013-10-11 12:37:56 +0000

    The nomadic scripts finally settled down in the right location.

--- misc-scripts/assembly_patches/remove_patch_karyotype.sql
+++
@@ -1,1 +0,0 @@
-delete karyotype from attrib_type, seq_region_attrib, karyotype where attrib_type.code in ('patch_novel','patch_fix') and attrib_type.attrib_type_id = seq_region_attrib.attrib_type_id and seq_region_attrib.seq_region_id = karyotype.seq_region_id;
