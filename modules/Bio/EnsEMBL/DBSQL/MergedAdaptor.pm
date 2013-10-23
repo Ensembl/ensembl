d89a9899c3c1dd114044ce5539bb8039c2f965b5
Andrew Yates <ayates@ebi.ac.uk>
2013-10-23 11:58:19 +0000

    [ENSCORESW-730]. Better implementation of this class.
    The idea is good. Consult multiple adaptors as if they are one logical
    adaptor with the same query. Thing is that the old version was abused. Let's
    stop that now and bring in this nicer new version which does work well

--- modules/Bio/EnsEMBL/DBSQL/MergedAdaptor.pm
+++ modules/Bio/EnsEMBL/DBSQL/MergedAdaptor.pm
@@ -24,15 +24,29 @@ Bio::EnsEMBL::DBSQL::MergedAdaptor
 
 =head1 SYNOPSIS
 
-  $merged_adaptor = new Bio::EnsEMBL::DBSQL::MergedAdaptor(
+  # load all available adaptors of the given type for the species
+  my $merged_adaptor = Bio::EnsEMBL::DBSQL::MergedAdaptor->new(
     -species => "human",
-    -type    => "Population"
+    -type    => "gene"
+  );
+
+  # only load adaptors from the given groups of the given type for the species
+  my $merged_adaptor = Bio::EnsEMBL::DBSQL::MergedAdaptor->new(
+    -species => "human",
+    -type    => "gene",
+    -groups  => ['core','otherfeatures']
   );
 
 =head1 DESCRIPTION
 
 The MergedAdaptor object is merely a list of adaptors. AUTOLOAD is used
-to call a subroutine on each adaptor and merge the results.
+to call a subroutine on each adaptor and merge the results. This object structure
+allows you to treat a set of adaptors as a logical single entity. The end result
+is that disparate database source data sets are accessible through a single
+adaptor call.
+
+This code will convert single object return calls into ArrayRef returning calls
+and so is only safe to use with the C<fetch_all_XXX> or C<get_all_XXX> methods.
 
 =head1 METHODS
 
@@ -41,27 +55,26 @@ to call a subroutine on each adaptor and merge the results.
 
 package Bio::EnsEMBL::DBSQL::MergedAdaptor;
 
-
-use vars qw(@ISA);
 use strict;
+use warnings;
 
-use Bio::EnsEMBL::Utils::Exception qw(throw warning deprecate);
+use Bio::EnsEMBL::Utils::Exception qw(throw warning);
 use Bio::EnsEMBL::Utils::Argument qw(rearrange);
-use Bio::EnsEMBL::Registry;
+use Bio::EnsEMBL::Utils::Scalar qw(wrap_array assert_ref);
+require Bio::EnsEMBL::Registry;
 my $registry = "Bio::EnsEMBL::Registry";
 
 
 =head2 new
 
-  Arg [SPECIES]: (optional) string 
-                  species name to get adaptors for
-  Arg [TYPE]   : (optional) string 
-                  type to get adaptors for
-  Arg [GROUPS] : (optional) ref to list
-  Example      : $MergedAdaptor = new 
-               : Bio::EnsEMBL::DBSQL::MergedAdaptor(-species=> 'human', -type =>'Population', -groups => ['Sanger','Ensembl']);
-
-  Description: Creates a new MergedAdaptor
+  Arg [SPECIES]: String species name to get adaptors for
+  Arg [TYPE]   : String type to get adaptors for
+  Arg [GROUPS] : (optional) ArrayRef of groups to load
+  Example      : my $adaptor = Bio::EnsEMBL::DBSQL::MergedAdaptor->new(
+                    -species=> 'human', -type =>'Population', -groups => ['Sanger','Ensembl']);
+                 my $alL_adaptor = Bio::EnsEMBL::DBSQL::MergedAdaptor->new(
+                    -species=> 'human', -type =>'Population');
+  Description: Creates a new MergedAdaptor.
   Returntype : Bio::EnsEMBL::DBSQL::MergedAdaptor
   Exceptions : throws if species or type not specified
   Caller     : general
@@ -71,118 +84,141 @@ my $registry = "Bio::EnsEMBL::Registry";
 =cut
 
 sub new {
-  my ($class,@args) = @_;
-
-  my $self ={};
-  bless $self,$class;
-
-  my ($species, $type, $groups) =
-    rearrange([qw(SPECIES TYPE GROUPS)], @args);
+  my ($class, @args) = @_;
+  $class = ref($class) || $class;
+  my $self = bless({}, $class);
+  my ($species, $type, $groups) = rearrange([qw(SPECIES TYPE GROUPS)], @args);
 
   if(!defined($species)|| !defined($type)){
-    die "Species and Type must be specified\n";
+    throw "-SPECIES and -TYPE must be specified";
   }
-
-  my @adaps;
-  if (!defined ($groups)){
-      #get all adaptors for that species and type
-      @adaps = @{$registry->get_all_adaptors(-species => $species, -type => $type)};
-  }
-  else{
-      #get only specified adaptors for the particular groups
-      foreach my $group (@{$groups}){
-	  push @adaps, $registry->get_adaptor($species,$group,$type);
-      }
-  }
- 
-  my @list =();
-  push(@list,@adaps);
-  $self->{'list'}= \@list;
-
+  $self->_populate_adaptors($species, $type, $groups);
   return $self;
+}
+
+=head2 _populate_adaptors
+
+  Arg [1]      : String species name to get adaptors for
+  Arg [2]      : String type to get adaptors for
+  Arg [3]      : (optional) ArrayRef of groups to load
+  Description  : Auto-populates the current MergedAdaptor with the
+                 adaptors linked to this species, type and optional set of groups
+  Caller       : general
+  Status       : At Risk
+
+=cut
+
+sub _populate_adaptors {
+  my ($self, $species, $type, $groups) = @_;
+  if($groups) {
+    assert_ref($groups, 'ARRAY', '-GROUPS');
+    my @adaptors = map { $registry->get_adaptor($species, $_, $type) } @{$groups};
+    $self->add_list(@adaptors);
+  }
+  else {
+    my $adaptors = $registry->get_all_adaptors(-SPECIES => $species, -TYPE => $type);
+    $self->add_list(@{$adaptors});
+  }
+  return;
 }
 
 =head2 add_list
 
-  Example    : $MergedAdaptor->add_list(@adaptors);
-  Description: adds a list of adaptors to the Merged adaptor list.
-  Returntype : none
-  Exceptions : none
-  Status     : At Risk
-             : Under development
+  Arg [n]     : Adaptors to add into this instance
+  Description : Adds the given adaptors to the internal adaptor list
 
 =cut
 
-sub add_list{
-  my ($self, @arr) = @_;
-
-  foreach my $adap (@arr){
-    $self->add_adaptor($adap);
-  }
+sub add_list {
+  my ($self, @adaptors) = @_;
+  push(@{$self->{adaptors}}, @adaptors);
+  return;
 }
 
-=head2 add_adaptor
+=head2 add_list
 
-  Example    : $MergedAdaptor->add_adaptor(@adaptors);
-  Description: adds an adaptor to the Merged adaptor list.
-  Returntype : none
-  Exceptions : none
-  Status     : At Risk
-             : Under development
+  Arg [1]     : Adaptor to add into this instance
+  Description : Adds the given adaptor to the internal adaptor list. For
+                multiple adaptor addition use C<add_list()>.
 
 =cut
 
-sub add_adaptor{
-  my ($self,$adaptor)=@_;
+sub add_adaptor {
+  my ($self, $adaptor) = @_;
+  $self->add_list($adaptor);
+  return;
+}
 
-  if(!defined ($self->{'list'})){
-    my @list =();
-    push(@list,$adaptor);
-    $self->{'list'}= \@list;
+=head2 can
+
+  Arg [1]     : String method name to be called
+  Description : Implementation of UNIVERSAL::can(). We loop through the
+                available adaptors and return true if any will respond
+                to the given method name
+  Returntype  : Boolean indicating if any delegating object will respond to this method
+
+=cut
+
+sub can {
+  my ($self, $method) = @_;
+  foreach my $adaptor (@{$self->{adaptors}}) {
+    return 1 if $adaptor->can($method);
   }
-  else{
-    push(@{$self->{'list'}},$adaptor);
-  }
+  return 0;
 }
 
 
-sub printit{
-  my ($self)=@_;
+=head2 isa
 
-  foreach my $adaptor (@{$self->{'list'}}){
-    print "printit $adaptor\t".$adaptor->db->group()."\n";
+  Arg [1]     : String method name to be called
+  Description : Implementation of UNIVERSAL::isa(). We loop through the
+                available adaptors and return true if any inherited from
+                the given class
+  Returntype  : Boolean indicating if any delegating object inherits from the given class
+
+=cut
+
+sub isa {
+  my ($self, $isa) = @_;
+  foreach my $adaptor (@{$self->{adaptors}}) {
+    return 1 if $adaptor->isa($isa);
   }
+  return 0;
 }
-
 
 use vars '$AUTOLOAD';
 
-sub AUTOLOAD {
-  my ($self,@args) = @_;
-  my @array_return=();
-  my $ref_return = undef;
-  $AUTOLOAD =~ /^.*::(\w+)+$/ ;
+=head2 AUTOLOAD
 
+  Description : Internal override of AUTLOAD. The code will detect the requested
+                method, loop through all available adaptors and will 
+  Returntype  : Boolean indicating if any delegating object inherits from the given class
+
+=cut
+
+sub AUTOLOAD {
+  my ($self, @args) = @_;
+  my @return;
+
+  #Detect required method
+  $AUTOLOAD =~ /^.*::(\w+)+$/ ;
   my $sub = $1;
 
-  foreach my $adaptor (@{$self->{'list'}}) {
-    my $ref;
-    if($adaptor->can($sub)){
-      $ref = $adaptor->$sub(@args);
-      if( ref($ref) eq 'ARRAY' ) {
-        push @array_return, @{$ref};
-      } else {
-        push @array_return, $ref;
-      }
+  foreach my $adaptor (@{$self->{adaptors}}) {
+    my @local_return;
+    if(my $method_ref = $adaptor->can($sub)) {
+      my $ref = $method_ref->($adaptor, @args);
+      push(@return, @{wrap_array($ref)});
     }
-    else{ # end of can
-      warn("In Merged Adaptor $adaptor cannot call sub $sub");
+    else{
+      warning("In Merged Adaptor $adaptor cannot call sub $sub");
     }
   }
-  return \@array_return;
+  return \@return;
 }
 
-sub DESTROY{
+sub DESTROY {
+
 }
 
 1;
