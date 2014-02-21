f902a97d53150c29d98a5d3c05adace5e6c90702
Michael Gray <mg13@sanger.ac.uk>
2014-02-21 15:43:24 +0000

    Travis: set up SQLite config; fix matrix.

--- travisci/harness.sh
+++ travisci/harness.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
-export PERL5LIB=$PWD/bioperl-live-bioperl-release-1-2-3:$PWD/ensembl-test/modules:$PWD/modules 
+export PERL5LIB=$PWD/bioperl-live-bioperl-release-1-2-3:$PWD/ensembl-test/modules:$PWD/modules
+
+if [ "$DB" = 'mysql' ]; then
+    (cd modules/t && ln -sf MultiTestDB.conf.mysql MultiTestDB.conf)
+elif [ "$DB" = 'sqlite' ]; then
+    (cd modules/t && ln -sf MultiTestDB.conf.SQLite MultiTestDB.conf)
+else
+    echo "Don't know about DB '$DB'"
+    exit 1;
+fi
 
 echo "Running test suite"
 if [ "$COVERALLS" = 'true' ]; then
