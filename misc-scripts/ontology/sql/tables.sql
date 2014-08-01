ea781cd64a2b40f336cadbb2007f2b5b0e352c64
Magali Ruffier <mr6@ebi.ac.uk>
2014-08-01 15:17:11 +0100

    updated ontology db to schema 77

--- misc-scripts/ontology/sql/tables.sql
+++ misc-scripts/ontology/sql/tables.sql
@@ -30,11 +30,11 @@ CREATE TABLE meta (
 # Add schema type and schema version to the meta table
 INSERT INTO meta (meta_key, meta_value) VALUES 
   ('schema_type', 'ontology'),
-  ('schema_version', '76');
+  ('schema_version', '77');
 
 # Patches included in this schema file
 INSERT INTO meta (meta_key, meta_value)
-  VALUES ('patch', 'patch_75_76_a.sql|schema_version');
+  VALUES ('patch', 'patch_76_77_a.sql|schema_version');
 
 CREATE TABLE ontology (
   ontology_id   INT UNSIGNED NOT NULL AUTO_INCREMENT,
