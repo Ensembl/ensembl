056751a82fb9e05888566c17abcaa95ed2022d75
Magali Ruffier <mr6@ebi.ac.uk>
2014-11-27 15:14:54 +0000

    ENSCORESW-366: CGNC parser updated to working link some sanity checks to make sure the file contains the data we expect

--- misc-scripts/xref_mapping/XrefParser/CGNCParser.pm
+++ misc-scripts/xref_mapping/XrefParser/CGNCParser.pm
@@ -21,42 +21,62 @@ sub run {
   $verbose |=0;
 
   my $file = @{$files}[0];
-
   my $file_io = $self->get_filehandle($file);
-  if ( !defined $file_io ) {
+  if ( !defined $file_io) {
     print STDERR "ERROR: Could not open $file\n";
     return 1;    # 1 is an error
   }
 
+  my $head = $file_io->getline(); # first record are the headers
+  chomp $head;
+  # Sanity check on the headers, see if they are what is expected
+  my @headers = split(/\t/,$head);
+  if ($headers[0] ne 'CGNC id') {
+    croak "First column " . $headers[0] . " does not contain CGNC id";
+  }
+  if ($headers[2] ne 'Ensembl id') {
+    croak "Third column " . $headers[2] . " does not contain Ensembl id";
+  }
+  if ($headers[3] ne 'gene symbol') {
+    croak "Fourth column " . $headers[3] . " does not contain gene symbol";
+  }
+  if ($headers[4] ne 'gene name') {
+    croak "Fifth column " . $headers[4] . " does not contain gene name";
+  }
+  if ($headers[5] ne 'gene synonym') {
+    croak "Sixth column " . $headers[5] . " does not contain gene synonym";
+  }
+
   $source_id = $self->get_source_id_for_source_name("CGNC");
 
-  
+  my $dbi = $self->dbi();
+  my $sql = "insert into synonym (xref_id, synonym) values (?, ?)";
+  my $add_syn_sth = $dbi->prepare($sql);
   my $count = 0;
+
   while ( $_ = $file_io->getline() ) {
-#48941  ENSGALG00000002652      FZD10
-
+## 2     426934  ENSGALG00000000003      PANX2   pannexin 2              Approved        2011-10-24
     chomp;
-    my @array = split /\t/x, $_;
-
-    my $ensid = $array[1];
-    my $acc = $array[2];
-    my $desc = $array[3];
+    my ($cgnc_id, $entrez_id, $ensid, $symbol, $gene_name, $synonym, $status, $edit_date) = split(/\t/, $_);
 
     if($ensid =~ /ENSGAL/){
-      my $xref_id = $self->add_xref({ acc        => $acc,
+      my $xref_id = $self->add_xref({ acc        => $cgnc_id,
 				      version    => 0,
-				      label      => $acc,
-				      desc       => $desc,
+				      label      => $symbol,
+				      desc       => $gene_name,
 				      source_id  => $source_id,
 				      species_id => $species_id,
 				      info_type  => "DIRECT"} );
 
       $self->add_direct_xref( $xref_id, $ensid, "Gene", '');
       $count++;
-    } else{
-      print STDERR "No match for $acc\n";
+
+      if ($synonym) {
+        $add_syn_sth->execute($xref_id, $synonym);
+      }
     }
   }
+
   print "$count direct CGNC xrefs added\n";
   return 0;
 
