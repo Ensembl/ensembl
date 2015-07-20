b5d77ff6ce85ed9ed66fe32ea9c94e514fdf3ce6
Dan Staines <dstaines@ebi.ac.uk>
2015-07-20 16:05:26 +0100

    increased xref dbprimary_acc/display_label field width to support very long identifier strings

---
+++ sql/patch_81_82_b.sql
@@ -0,0 +1,27 @@
+-- Copyright [1999-2015] Wellcome Trust Sanger Institute and the EMBL-European Bioinformatics Institute
+-- 
+-- Licensed under the Apache License, Version 2.0 (the "License");
+-- you may not use this file except in compliance with the License.
+-- You may obtain a copy of the License at
+-- 
+--      http://www.apache.org/licenses/LICENSE-2.0
+-- 
+-- Unless required by applicable law or agreed to in writing, software
+-- distributed under the License is distributed on an "AS IS" BASIS,
+-- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+-- See the License for the specific language governing permissions and
+-- limitations under the License.
+
+# patch_81_82_a.sql
+#
+# Title: Update xref column width
+#
+# Description:
+#   Update xref dbprimary_acc and display_label to support very long identifiers
+
+ALTER TABLE xref MODIFY dbprimary_acc VARCHAR(512) NOT NULL;
+ALTER TABLE xref MODIFY display_label VARCHAR(512) NOT NULL;
+
+# Patch identifier
+INSERT INTO meta (species_id, meta_key, meta_value)
+  VALUES (NULL, 'patch', 'patch_81_82_b.sql|xref_width');
