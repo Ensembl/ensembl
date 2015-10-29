8a331d095e841af4787e24a88cdc9b7dbefbf94b
Anja Thormann <anja@ebi.ac.uk>
2015-10-29 14:09:20 +0000

    update perl5lib

--- travisci/harness.sh
+++ travisci/harness.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export PERL5LIB=$PWD/bioperl-live-bioperl-release-1-2-3:$PWD/ensembl-test/modules:$PWD/modules
+export PERL5LIB=$PWD/bioperl-live-bioperl-release-1-2-3:$PWD/ensembl-test/modules:$PWD/modules:$PWD/ensembl-variation/modules
 
 if [ "$DB" = 'mysql' ]; then
     (cd modules/t && ln -sf MultiTestDB.conf.mysql MultiTestDB.conf)
