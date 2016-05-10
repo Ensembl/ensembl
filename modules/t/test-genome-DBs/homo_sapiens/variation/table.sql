e0490f7bb1091b4bd66a9bf4efda634d805c601d
Anja Thormann <anja@ebi.ac.uk>
2016-05-10 15:22:16 +0100

    Remove variation_feature_bak

--- modules/t/test-genome-DBs/homo_sapiens/variation/table.sql
+++ modules/t/test-genome-DBs/homo_sapiens/variation/table.sql
@@ -611,30 +611,6 @@ CREATE TABLE `variation_feature` (
   KEY `source_idx` (`source_id`)
 ) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
 
-CREATE TABLE `variation_feature_bak` (
-  `variation_feature_id` int(10) unsigned NOT NULL DEFAULT '0',
-  `seq_region_id` int(10) unsigned NOT NULL,
-  `seq_region_start` int(11) NOT NULL,
-  `seq_region_end` int(11) NOT NULL,
-  `seq_region_strand` tinyint(4) NOT NULL,
-  `variation_id` int(10) unsigned NOT NULL,
-  `allele_string` varchar(50000) DEFAULT NULL,
-  `variation_name` varchar(255) DEFAULT NULL,
-  `map_weight` int(11) NOT NULL,
-  `flags` set('genotyped') DEFAULT NULL,
-  `source_id` int(10) unsigned NOT NULL,
-  `validation_status` set('cluster','freq','submitter','doublehit','hapmap','1000Genome','precious') DEFAULT NULL,
-  `consequence_types` set('intergenic_variant','splice_acceptor_variant','splice_donor_variant','stop_lost','coding_sequence_variant','missense_variant','stop_gained','synonymous_variant','frameshift_variant','nc_transcript_variant','non_coding_exon_variant','mature_miRNA_variant','NMD_transcript_variant','5_prime_UTR_variant','3_prime_UTR_variant','incomplete_terminal_codon_variant','intron_variant','splice_region_variant','downstream_gene_variant','upstream_gene_variant','initiator_codon_variant','stop_retained_variant','inframe_insertion','inframe_deletion','transcript_ablation','transcript_fusion','transcript_amplification','transcript_translocation','TFBS_ablation','TFBS_fusion','TFBS_amplification','TFBS_translocation','regulatory_region_ablation','regulatory_region_fusion','regulatory_region_amplification','regulatory_region_translocation','feature_elongation','feature_truncation','regulatory_region_variant','TF_binding_site_variant') NOT NULL DEFAULT 'intergenic_variant',
-  `variation_set_id` set('1','2','3','4','5','6','7','8','9','10','11','12','13','14','15','16','17','18','19','20','21','22','23','24','25','26','27','28','29','30','31','32','33','34','35','36','37','38','39','40','41','42','43','44','45','46','47','48','49','50','51','52','53','54','55','56','57','58','59','60','61','62','63','64') NOT NULL DEFAULT '',
-  `class_attrib_id` int(10) unsigned DEFAULT '0',
-  `somatic` tinyint(1) NOT NULL DEFAULT '0',
-  `minor_allele` varchar(50) DEFAULT NULL,
-  `minor_allele_freq` float DEFAULT NULL,
-  `minor_allele_count` int(10) unsigned DEFAULT NULL,
-  `alignment_quality` double DEFAULT NULL,
-  `evidence` set('Multiple_observations','Frequency','HapMap','1000Genomes','Cited','ESP') DEFAULT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
-
 CREATE TABLE `variation_genename` (
   `variation_id` int(10) unsigned NOT NULL,
   `gene_name` varchar(255) NOT NULL,
