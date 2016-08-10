39faaf5f82aaa7823d1008682dc8cda22056b140
Alessandro Vullo <avullo@ebi.ac.uk>
2016-08-10 16:48:22 +0100

    [ENSCORESW-1846]. Reset to default queue if not known.

--- misc-scripts/xref_mapping/XrefMapper/Methods/ExonerateBasic.pm
+++ misc-scripts/xref_mapping/XrefMapper/Methods/ExonerateBasic.pm
@@ -284,10 +284,11 @@ EON
 
   my $output = $self->get_class_name() . "_" . $ensembl_type . "_" . "\$LSB_JOBINDEX.map";
 
-  my $queue = $self->mapper->farm_queue || 'normal';
+  my $queue = $self->mapper->farm_queue;
 
 
-  my $usage = "-q $queue ".'-M 1500 -R"select[mem>1500] rusage[tmp='.$disk_space_needed.', mem=1500]" '.'-J "'.$unique_name.'[1-'.$num_jobs.']%200" -o '.$prefix.'.%J-%I.out -e  '.$prefix.'.%J-%I.err';
+  my $usage = '-M 1500 -R"select[mem>1500] rusage[tmp='.$disk_space_needed.', mem=1500]" '.'-J "'.$unique_name.'[1-'.$num_jobs.']%200" -o '.$prefix.'.%J-%I.out -e  '.$prefix.'.%J-%I.err';
+  $queue and $usage = "-q $queue " . $usage;
 
 
   my $command = $exe." ".$query." ".$target.' --querychunkid $LSB_JOBINDEX --querychunktotal '.$num_jobs.' --showvulgar false --showalignment FALSE --ryo "xref:%qi:%ti:%ei:%ql:%tl:%qab:%qae:%tab:%tae:%C:%s\n" '.$options_str;
