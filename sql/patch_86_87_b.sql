077b4c91817c2eaa2fd60b63acee7b68465c1ec2
premanand17 <prem.apa@gmail.com>
2016-09-21 11:35:06 +0100

    Update to add NOT NULL constraint to meta_value column in meta table

---
+++ sql/patch_86_87_b.sql
@@ -0,0 +1,27 @@
+-- Copyright [1999-2015] Wellcome Trust Sanger Institute and the EMBL-European Bioinformatics Institute
+-- Copyright [2016] EMBL-European Bioinformatics Institute
+-- 
+-- Licensed under the Apache License, Version 2.0 (the "License");
+-- you may not use this file except in compliance with the License.
+-- You may obtain a copy of the License at
+-- 
+--      http://www.apache.org/licenses/LICENSE-2.0
+-- 
+-- Unless required by applicable law or agreed to in writing, software
+-- distributed under the License is distributed on an "AS IS" BASIS,
+-- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+-- See the License for the specific language governing permissions and
+-- limitations under the License.
+
+# patch_86_87_a.sql
+#
+# Title: Meta case insensitive
+#
+# Description:
+# Enfored meta_value in meta table to be NOT NULL
+
+ALTER TABLE meta MODIFY COLUMN meta_value VARCHAR(255) NOT NULL;
+
+# Patch identifier
+INSERT INTO meta (species_id, meta_key, meta_value)
+  VALUES (NULL, 'patch', 'patch_86_87_b.sql|meta_value_NOT_NULL');
