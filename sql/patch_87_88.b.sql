c2894241012258ce57f6480dbd92273ea1eb79a7
Guy Naamati <gnaamati@ebi.ac.uk>
2017-01-05 16:10:45 +0000

    patch for seq_region

---
+++ sql/patch_87_88.b.sql
@@ -0,0 +1,27 @@
+-- Copyright [1999-2015] Wellcome Trust Sanger Institute and the EMBL-European Bioinformatics Institute
+-- Copyright [2016] EMBL-European Bioinformatics Institute
+-- 
+-- Licensed under the Apache License, Version 2.0 (the "License");
+-- you may not use this file except in compliance with the License.
+-- You may obtain a copy of the License at
+-- 
+--      http://www.apache.org/licenses/LICENSE-2.0
+-- 
+-- Unless required by applicable law or agreed to in writing, software
+-- distributed under the License is distributed on an "AS IS" BASIS,
+-- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+-- See the License for the specific language governing permissions and
+-- limitations under the License.
+
+# patch_87_88_b.sql
+#
+# Title: Allow seq_region name to be longer
+#
+# Description:
+# Name in seq_region table will be enlarged to VARCHAR(255)
+
+ALTER TABLE seq_region MODIFY COLUMN name VARCHAR(255) NOT NULL;
+
+# Patch identifier
+INSERT INTO meta (species_id, meta_key, meta_value)
+  VALUES (NULL, 'patch', 'patch_87_88_b.sql|seq_region_name_255');
