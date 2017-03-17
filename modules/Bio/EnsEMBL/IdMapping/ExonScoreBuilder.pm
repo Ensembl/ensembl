7f111d8d9063961f7231eff8608342f43cf77789
premanand17 <prem.apa@gmail.com>
2017-03-17 10:06:29 +0000

    Updates to be able to run stable id mapping in EBI-farm

--- modules/Bio/EnsEMBL/IdMapping/ExonScoreBuilder.pm
+++ modules/Bio/EnsEMBL/IdMapping/ExonScoreBuilder.pm
@@ -520,8 +520,8 @@ sub run_exonerate {
   $self->logger->info("Waiting for exonerate jobs to finish...\n", 0, 'stamped');
 
   my $dependent_job =
-    qq{bsub -K -w "ended($lsf_name)" -q small } .
-    qq{-M 100 -R 'select[mem>100]' -R 'rusage[mem=100]' } .
+    qq{bsub -K -w "ended($lsf_name)" -q production-rh7 } .
+    qq{-M 1000 -R 'select[mem>1000]' -R 'rusage[mem=1000]' } .
     qq{-o $logpath/exonerate_depend.out /bin/true};
 
   system($dependent_job) == 0 or
--- modules/Bio/EnsEMBL/IdMapping/SyntenyFramework.pm
+++ modules/Bio/EnsEMBL/IdMapping/SyntenyFramework.pm
@@ -412,8 +412,8 @@ sub rescore_gene_matrix_lsf {
   $self->logger->info("Waiting for jobs to finish...\n", 0, 'stamped');
 
   my $dependent_job =
-    qq{bsub -K -w "ended($lsf_name)" -q small } .
-    qq{-M 100 -R 'select[mem>100]' -R 'rusage[mem=100]' } .
+    qq{bsub -K -w "ended($lsf_name)" -q production-rh7 } .
+    qq{-M 1000 -R 'select[mem>1000]' -R 'rusage[mem=1000]' } .
     qq{-o $logpath/synteny_rescore_depend.out /bin/true};
 
   system($dependent_job) == 0 or
