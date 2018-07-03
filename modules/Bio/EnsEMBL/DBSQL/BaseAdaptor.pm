3f4b2d6b83dca2d004b15095f133343af91d71d0
Alessandro Vullo <avullo@ebi.ac.uk>
2018-07-03 11:13:02 +0100

    Restrict protein feature to species following translation-transcript link to species specific seq region.

--- modules/Bio/EnsEMBL/DBSQL/BaseAdaptor.pm
+++ modules/Bio/EnsEMBL/DBSQL/BaseAdaptor.pm
@@ -543,11 +543,12 @@ sub _generate_sql {
     }
 
     $extra_default_where = sprintf(
-                      '%s.seq_region_id = %s.seq_region_id '
-                        . 'AND %s.coord_system_id = %s.coord_system_id '
-                        . 'AND %s.species_id = ?',
-                      $tabs[0]->[1], $sr_alias, $sr_alias,
-                      $cs_alias,     $cs_alias );
+				   'transcript.seq_region_id = %s.seq_region_id '
+				   . 'AND transcript.transcript_id = translation.transcript_id'
+				   . 'AND translation.translation_id = %s.translation_id'
+				   . 'AND %s.coord_system_id = %s.coord_system_id '
+				   . 'AND %s.species_id = ?',
+				   $sr_alias, $tabs[0]->[1], $sr_alias, $cs_alias, $cs_alias );
 
     $self->bind_param_generic_fetch( $self->species_id(), SQL_INTEGER );
   } ## end if ( $self->is_multispecies...)
