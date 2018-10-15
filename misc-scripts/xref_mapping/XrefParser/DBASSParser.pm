d72f93ccde1d94f318b661869edf98904dc806c1
Marek Szuba <marek.szuba@ebi.ac.uk>
2018-10-15 09:51:05 +0100

    DBASSParser: apply perltidy
    Fixes inconsistent indentation depth at different level, the mixing of
    tabs and spaces, trailing whitespace, and many others.

--- misc-scripts/xref_mapping/XrefParser/DBASSParser.pm
+++ misc-scripts/xref_mapping/XrefParser/DBASSParser.pm
@@ -34,111 +34,111 @@ use parent qw( XrefParser::BaseParser );
 # 2)    DBASS Gene Name
 # 3)    Ensembl Gene ID
 
-
 my $dbi;
 my $xref_id;
 my $source_id;
 
-
 sub run {
-  my ($self, $ref_arg) = @_;
-  my $source_id    = $ref_arg->{source_id};
-  my $species_id   = $ref_arg->{species_id};
-  my $files        = $ref_arg->{files};
-  my $verbose      = $ref_arg->{verbose};
-  my $dbi          = $ref_arg->{dbi};
+  my ( $self, $ref_arg ) = @_;
+  my $source_id  = $ref_arg->{source_id};
+  my $species_id = $ref_arg->{species_id};
+  my $files      = $ref_arg->{files};
+  my $verbose    = $ref_arg->{verbose};
+  my $dbi        = $ref_arg->{dbi};
   $dbi = $self->dbi unless defined $dbi;
 
-  if((!defined $source_id) or (!defined $species_id) or (!defined $files) ){
+  if ( ( !defined $source_id ) or
+       ( !defined $species_id ) or
+       ( !defined $files ) )
+  {
     croak "Need to pass source_id, species_id and files as pairs";
   }
-  $verbose |=0;
+  $verbose |= 0;
 
   my $filename = @{$files}[0];
 
+  my $file_io = $self->get_filehandle($filename);
+  if ( !defined($file_io) ) {
+    return 1;
+  }
 
-    my $file_io = $self->get_filehandle($filename);
-    if ( !defined($file_io) ) {
-        return 1;
+  my $parsed_count = 0;
+
+  $file_io->getline();
+
+  while ( defined( my $line = $file_io->getline() ) ) {
+
+    $line =~ s/\s*$//;
+    # csv format can come with quoted columns, remove them
+    $line =~ s/"//g;
+
+    my ( $dbass_gene_id, $dbass_gene_name, $ensembl_id ) =
+      split( /,/, $line );
+
+    if ( !defined($dbass_gene_id) || !defined($ensembl_id) ) {
+      printf STDERR ( "Line %d contains  has less than two columns.\n",
+                      1 + $parsed_count );
+      print STDERR ("The parsing failed\n");
+      return 1;
     }
 
-     my $parsed_count = 0;
+    my $first_gene_name = $dbass_gene_name;
+    my $second_gene_name;
 
-     $file_io->getline();
-    
-    while ( defined( my $line = $file_io->getline() ) ) {
- 
-	$line =~ s/\s*$//;
-        # csv format can come with quoted columns, remove them
-        $line =~ s/"//g;
+    if ( $dbass_gene_name =~ /.\/./ ) {
+      ( $first_gene_name, $second_gene_name ) =
+        split( /\//, $dbass_gene_name );
+    }
 
-        my ( $dbass_gene_id, $dbass_gene_name, $ensembl_id) = split( /,/, $line );
+    if ( $dbass_gene_name =~ /(.*)\((.*)\)/ ) {
+      $first_gene_name  = $1;
+      $second_gene_name = $2;
+#      print $first_gene_name, "\n", $second_gene_name, "\n" if($verbose);
+    }
 
-        if ( !defined($dbass_gene_id) || !defined($ensembl_id) ) {
-            printf STDERR ( "Line %d contains  has less than two columns.\n",
-                    1 + $parsed_count );
-            print STDERR ("The parsing failed\n");
-            return 1;
-        }
-	
-	
-	my $first_gene_name = $dbass_gene_name;
-	my $second_gene_name;
-	
-	
-	if ($dbass_gene_name =~ /.\/./){
-		($first_gene_name, $second_gene_name) = split( /\//, $dbass_gene_name );
-	}
-	
-	if ($dbass_gene_name =~ /(.*)\((.*)\)/){
-		$first_gene_name = $1;
-	 	$second_gene_name = $2;
-#		print $first_gene_name, "\n", $second_gene_name, "\n" if($verbose);
-	}
-       
-       
-        my $label       = $first_gene_name;
-	my $type        = 'gene';
-        my $description = '';
-        my $version     = '1';
-	my $synonym	= $second_gene_name;
+    my $label       = $first_gene_name;
+    my $type        = 'gene';
+    my $description = '';
+    my $version     = '1';
+    my $synonym     = $second_gene_name;
 
-        ++$parsed_count;
+    ++$parsed_count;
 
-        my $xref_id = $self->get_xref( $dbass_gene_id, $source_id, $species_id, $dbi );
+    my $xref_id =
+      $self->get_xref( $dbass_gene_id, $source_id, $species_id, $dbi );
 
-        if ( !defined($xref_id) || $xref_id eq '' ) {
-            $xref_id = $self->add_xref({ acc        => $dbass_gene_id,
-					 version    => $version,
-					 label      => $label,
-					 desc       => $description,
-					 source_id  => $source_id,
-                                         dbi        => $dbi,
-					 species_id => $species_id,
-					 info_type => "DIRECT"} );
-        }
-	
-	$self->add_direct_xref( $xref_id, $ensembl_id, $type, '', $dbi);
-	
-	if (defined ($synonym)) {
-		$self->add_synonym($xref_id, $synonym, $dbi);
-	}
-	elsif ($synonym =~ /^\s/){
-		print "There is white space! \n" if($verbose);	
-	}
-	else {
-		next;
-	}
-	
-	
-    } ## end while ( defined( my $line...
+    if ( !defined($xref_id) || $xref_id eq '' ) {
+      $xref_id = $self->add_xref(
+                                  { acc        => $dbass_gene_id,
+                                    version    => $version,
+                                    label      => $label,
+                                    desc       => $description,
+                                    source_id  => $source_id,
+                                    dbi        => $dbi,
+                                    species_id => $species_id,
+                                    info_type  => "DIRECT" } );
+    }
 
-    printf( "%d direct xrefs succesfully parsed\n", $parsed_count ) if($verbose);
+    $self->add_direct_xref( $xref_id, $ensembl_id, $type, '', $dbi );
 
-    $file_io->close();
+    if ( defined($synonym) ) {
+      $self->add_synonym( $xref_id, $synonym, $dbi );
+    }
+    elsif ( $synonym =~ /^\s/ ) {
+      print "There is white space! \n" if ($verbose);
+    }
+    else {
+      next;
+    }
 
+  } ## end while ( defined( my $line...))
 
-    return 0;
+  printf( "%d direct xrefs succesfully parsed\n", $parsed_count )
+    if ($verbose);
+
+  $file_io->close();
+
+  return 0;
 } ## end sub run
 
 
