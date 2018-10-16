e41d128c5cb31eab1c86c90f291387aa8bd2fb0a
Alessandro Vullo <avullo@ebi.ac.uk>
2018-10-16 11:27:18 +0100

    [ENSCORESW-2879]. Refactoring parser. Same output as current version on human 94.
    - changed stylistic conventions
    - uniformly croak on errors

--- misc-scripts/xref_mapping/XrefParser/EntrezGeneParser.pm
+++ misc-scripts/xref_mapping/XrefParser/EntrezGeneParser.pm
@@ -36,9 +36,9 @@ sub run {
   my $dbi          = $ref_arg->{dbi};
   $dbi = $self->dbi unless defined $dbi;
 
-  if((!defined $source_id) or (!defined $species_id) or (!defined $files) ){
-    croak "Need to pass source_id, species_id, files and rel_file as pairs";
-  }
+  croak "Need to pass source_id, species_id, files and rel_file as pairs"
+    unless defined $source_id and defined $species_id and defined $files;
+
   $verbose |=0;
 
   my $file = @{$files}[0];
@@ -50,40 +50,31 @@ sub run {
   
 
   my $eg_io = $self->get_filehandle($file);
-  if ( !defined $eg_io ) {
-    print STDERR "ERROR: Could not open $file\n";
-    return 1;    # 1 is an error
-  }
-
-  my %seen;
-
+  croak "ERROR: Could not open $file\n" unless defined $eg_io;
 
   my $head = $eg_io->getline(); # first record are the headers
   chomp $head;
-  my (@arr) = split(/\s+/,$head);
-  # process this to the correct indexes to use. (incase they change);
+
+  # process header to figure out correct indexes to use, in case they change
+  my (@arr) = split(/\s+/, $head);
 
   my $gene_id_index = -2;
   my $gene_symbol_index = -2;
   my $gene_desc_index = -2;
   my $gene_tax_id_index = -2;
   my $gene_synonyms_index = -2;
+
   foreach (my $i=0; $i<= $#arr; $i++){
-    # Format change by Entrez, first header
-    # element is #tax_id
-    if($arr[$i] eq "#tax_id"){
+    # Format change by Entrez, first header element is #tax_id
+    if($arr[$i] eq "#tax_id") {
       $gene_tax_id_index = $i;
-    }
-    elsif($arr[$i] eq "GeneID"){
+    } elsif($arr[$i] eq "GeneID") {
       $gene_id_index = $i;
-    }
-    elsif($arr[$i] eq "Symbol"){
+    } elsif($arr[$i] eq "Symbol") {
       $gene_symbol_index = $i;
-    }
-    elsif($arr[$i] eq "description"){
+    } elsif($arr[$i] eq "description") {
       $gene_desc_index = $i;
-    }
-    elsif($arr[$i] eq "Synonyms"){
+    } elsif($arr[$i] eq "Synonyms") {
       $gene_synonyms_index = $i;
     }
   }
@@ -91,31 +82,28 @@ sub run {
       $gene_symbol_index   == -2 ||
       $gene_desc_index     == -2 ||
       $gene_synonyms_index == -2 ||
-      $gene_tax_id_index == -2){
-    print "HEADER\n$head\n\n";
-    print "Unable to get all the indexes needed\n";
-    print "gene_id = $gene_id_index\n";
-    print "tax_id = $gene_tax_id_index\n";
-    print "symbol = $gene_symbol_index\n";
-    print "desc = $gene_desc_index\n";
-    print "synonyms = $gene_synonyms_index\n";
-    return 0; # this is an error
+      $gene_tax_id_index == -2) {
+    croak "HEADER\n$head\n\n" .
+	"Unable to get all the indexes needed\n" .
+	"gene_id = $gene_id_index\n" .
+	"tax_id = $gene_tax_id_index\n" .
+	"symbol = $gene_symbol_index\n" .
+	"desc = $gene_desc_index\n" .
+	"synonyms = $gene_synonyms_index";
   }
+
   my $xref_count = 0;
   my $syn_count  = 0;
+  my %seen; # record already processed xrefs
+
   while ( $_ = $eg_io->getline() ) {
     chomp;
-    my (@arr) = split(/\t/,$_);
-    if(!defined($species_tax_id{$arr[$gene_tax_id_index]})){
-      next;
-    }
-    my $acc    = $arr[$gene_id_index];
-    if($seen{$acc}){
-      next;
-    }
-    else{
-      $seen{$acc} = 1;
-    }
+    my (@arr) = split(/\t/, $_);
+    next unless defined $species_tax_id{$arr[$gene_tax_id_index]};
+
+    my $acc = $arr[$gene_id_index];
+    next if $seen{$acc};
+
     my $symbol = $arr[$gene_symbol_index];
     my $desc   = $arr[$gene_desc_index];
 
@@ -143,11 +131,14 @@ sub run {
 	$syn_count++;
       }
     }
+
+    $seen{$acc} = 1;
   }
 
   $eg_io->close();
 
-  print $xref_count." EntrezGene Xrefs added with $syn_count synonyms\n" if($verbose);
+  print $xref_count." EntrezGene Xrefs added with $syn_count synonyms\n" if $verbose;
+
   return 0; #successful
 }
 
