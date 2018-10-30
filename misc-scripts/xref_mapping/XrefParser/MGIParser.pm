6db6132fbcaf31d0fd635c1920c17be5f6133d28
Premanand Achuthan <prem.apa@gmail.com>
2018-10-30 11:41:49 +0000

    Replaced custom sql insert with BaseParser method call, other post-review updates and applied perltidy

--- misc-scripts/xref_mapping/XrefParser/MGIParser.pm
+++ misc-scripts/xref_mapping/XrefParser/MGIParser.pm
@@ -1,3 +1,4 @@
+
 =head1 LICENSE
 
 Copyright [1999-2015] Wellcome Trust Sanger Institute and the EMBL-European Bioinformatics Institute
@@ -29,14 +30,17 @@ use parent qw(XrefParser::BaseParser);
 
 sub run {
 
-  my ($self, $ref_arg) = @_;
-  my $source_id    = $ref_arg->{source_id};
-  my $species_id   = $ref_arg->{species_id};
-  my $files        = $ref_arg->{files};
-  my $verbose      = $ref_arg->{verbose} || 0;
-  my $dbi          = $ref_arg->{dbi} || $self->dbi;
+  my ( $self, $ref_arg ) = @_;
+  my $source_id  = $ref_arg->{source_id};
+  my $species_id = $ref_arg->{species_id};
+  my $files      = $ref_arg->{files};
+  my $verbose    = $ref_arg->{verbose} // 0;
+  my $dbi        = $ref_arg->{dbi} // $self->dbi;
 
-  if((!defined $source_id) or (!defined $species_id) or (!defined $files) ){
+  if ( ( !defined $source_id )
+    or ( !defined $species_id )
+    or ( !defined $files ) )
+  {
     croak "Need to pass source_id, species_id and files as pairs";
   }
 
@@ -44,54 +48,62 @@ sub run {
 
   my $file_io = $self->get_filehandle($file);
   if ( !defined $file_io ) {
-    print STDERR "ERROR: Could not open $file\n";
-    return 1;    # 1 is an error
+    croak "Could not open $file\n";
   }
 
   #synonyms
-  my $sql = "insert ignore into synonym (xref_id, synonym) values (?, ?)";
-  my $add_syn_sth = $dbi->prepare($sql);    
-  my $syn_hash = $self->get_ext_synonyms("MGI", $dbi);
+  my $syn_hash = $self->get_ext_synonyms( "MGI", $dbi );
 
   #Init input file
-  my $input_file = Text::CSV->new({
-                                    sep_char           => "\t",
-				    empty_is_undef     => 1,
-                                    strict => 1,
-                                    allow_loose_quotes => 1,
-				   }) or croak "Cannot use file $file: ".Text::CSV->error_diag ();
+  my $input_file = Text::CSV->new(
+    {
+      sep_char           => "\t",
+      empty_is_undef     => 1,
+      strict             => 1,
+      allow_loose_quotes => 1,
+    }
+  ) or croak "Cannot use file $file: " . Text::CSV->error_diag();
 
-  # init headers
-  # MGI:1915941	1110028C15Rik	RIKEN cDNA 1110028C15 gene	33.61	1	ENSMUSG00000026004	ENSMUST00000042389 ENSMUST00000068168 ENSMUST00000113987 ENSMUST00000129190 ENSMUST00000132960	ENSMUSP00000036975 ENSMUSP00000063843 ENSMUSP00000109620 ENSMUSP00000118603
-  $input_file->column_names([qw(accession symbol name position chrom ens_gene_stableid)] ); #ignore last two columns EnsemblTranscriptIDs and EnsemblProteinIDs
-  my $count = 0;
+# init headers
+# MGI:1915941	1110028C15Rik	RIKEN cDNA 1110028C15 gene	33.61	1	ENSMUSG00000026004	ENSMUST00000042389 ENSMUST00000068168 ENSMUST00000113987 ENSMUST00000129190 ENSMUST00000132960	ENSMUSP00000036975 ENSMUSP00000063843 ENSMUSP00000109620 ENSMUSP00000118603
+  $input_file->column_names( [qw(accession symbol name position chrom ens_gene_stableid)] );    #ignore last two columns EnsemblTranscriptIDs and EnsemblProteinIDs
+  my $count     = 0;
   my $syn_count = 0;
-  while ( my $data = $input_file->getline_hr( $file_io ) ){
+  while ( my $data = $input_file->getline_hr($file_io) ) {
 
-      my $acc = $data->{'accession'};
-      my $ensid = $data->{'ens_gene_stableid'};
-      my $xref_id = $self->add_xref({ acc        => $acc,
-				      version    => 0,
-				      label      => $data->{'symbol'},
-				      desc       => $data->{'name'},
-				      source_id  => $source_id,
-                                      dbi        => $dbi,
-				      species_id => $species_id,
-				      info_type  => "DIRECT"} );
-
-      $self->add_direct_xref( $xref_id, $ensid, "Gene", '', $dbi);
-      if(defined($syn_hash->{$acc})){
-	foreach my $syn (@{$syn_hash->{$acc}}){
-	  $add_syn_sth->execute($xref_id, $syn);
-	}
+    my $acc     = $data->{'accession'};
+    my $ensid   = $data->{'ens_gene_stableid'};
+    my $xref_id = $self->add_xref(
+      {
+        acc        => $acc,
+        version    => 0,
+        label      => $data->{'symbol'},
+        desc       => $data->{'name'},
+        source_id  => $source_id,
+        dbi        => $dbi,
+        species_id => $species_id,
+        info_type  => "DIRECT"
       }
-      $count++;
+    );
+
+    $self->add_direct_xref( $xref_id, $ensid, "Gene", undef, $dbi );
+    if ( defined( $syn_hash->{$acc} ) ) {
+      foreach my $syn ( @{ $syn_hash->{$acc} } ) {
+        $self->add_to_syn( $acc, $source_id, $syn, $species_id, $dbi );
+        $syn_count++;
+      }
+    }
+    $count++;
 
   }
-  $input_file->eof or croak "Error parsing file $file: " . $input_file->error_diag();
+  $input_file->eof
+    or croak "Error parsing file $file: " . $input_file->error_diag();
   $file_io->close();
 
-  print "$count direct MGI xrefs added\n";
+  if ($verbose) {
+    print "$count direct MGI xrefs added\n";
+    print $syn_count. " synonyms added\n";
+  }
   return 0;
 
 }
