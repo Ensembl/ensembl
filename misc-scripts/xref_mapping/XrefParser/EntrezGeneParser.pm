cd3d3794c21feb17e651afcd0c8fb233ca62cf5e
Alessandro Vullo <avullo@ebi.ac.uk>
2018-11-07 11:29:04 +0000

    [ENSCORESW-2876]. Addressing comments in PR.

--- misc-scripts/xref_mapping/XrefParser/EntrezGeneParser.pm
+++ misc-scripts/xref_mapping/XrefParser/EntrezGeneParser.pm
@@ -1,3 +1,4 @@
+
 =head1 LICENSE
 
 Copyright [1999-2015] Wellcome Trust Sanger Institute and the EMBL-European Bioinformatics Institute
@@ -20,51 +21,55 @@ limitations under the License.
 package XrefParser::EntrezGeneParser;
 
 use strict;
+use warnings;
 
 use Carp;
-use POSIX qw(strftime);
-use File::Basename;
 use Text::CSV;
 
 use parent qw( XrefParser::BaseParser );
 
 sub run {
 
-  my ($self, $ref_arg) = @_;
+  my ( $self, $ref_arg ) = @_;
   my $source_id    = $ref_arg->{source_id};
   my $species_id   = $ref_arg->{species_id};
   my $species_name = $ref_arg->{species};
   my $files        = $ref_arg->{files};
-  my $verbose      = $ref_arg->{verbose} || 0;
-  my $dbi          = $ref_arg->{dbi} || $self->dbi;
+  my $verbose      = $ref_arg->{verbose} // 0;
+  my $dbi          = $ref_arg->{dbi} // $self->dbi;
 
   croak "Need to pass source_id, species_id, files and rel_file as pairs"
-    unless defined $source_id and defined $species_id and defined $files;
+    unless defined $source_id and
+    defined $species_id and
+    defined $files;
 
   my $file = @{$files}[0];
 
-  my $wiki_source_id = $self->get_source_id_for_source_name("WikiGene", undef, $dbi);
+  my $wiki_source_id =
+    $self->get_source_id_for_source_name( "WikiGene", undef, $dbi );
 
-  my %species_tax_id = %{$self->get_taxonomy_from_species_id($species_id, $dbi)};
+  my %species_tax_id =
+    %{ $self->get_taxonomy_from_species_id( $species_id, $dbi ) };
   $species_tax_id{$species_id} = $species_id if defined $species_name;
-  
 
   my $eg_io = $self->get_filehandle($file);
   croak "Could not open $file\n" unless defined $eg_io;
 
-  my $input_file = Text::CSV->new({ sep_char => "\t", empty_is_undef => 1, allow_loose_quotes => 1 }) 
-    or croak "Cannot use file $file: " . Text::CSV->error_diag ();
+  my $input_file = Text::CSV->new(
+    { sep_char => "\t", empty_is_undef => 1, allow_loose_quotes => 1 } )
+    or
+    croak "Cannot use file $file: " . Text::CSV->error_diag();
 
   # process header
-  $input_file->column_names( @{ $input_file->getline( $eg_io ) } );
+  $input_file->column_names( @{ $input_file->getline($eg_io) } );
 
   # read data and load xrefs
   my $xref_count = 0;
   my $syn_count  = 0;
-  my %seen; # record already processed xrefs
+  my %seen;    # record already processed xrefs
 
-  while ( my $data = $input_file->getline_hr( $eg_io ) ) {
-    next unless exists $species_tax_id{$data->{'#tax_id'}};
+  while ( my $data = $input_file->getline_hr($eg_io) ) {
+    next unless exists $species_tax_id{ $data->{'#tax_id'} };
 
     my $acc = $data->{'GeneID'};
     next if $seen{$acc};
@@ -72,41 +77,47 @@ sub run {
     my $symbol = $data->{'Symbol'};
     my $desc   = $data->{'description'};
 
-    $self->add_xref({ acc        => $acc,
-		      label      => $symbol,
-		      desc       => $desc,
-		      source_id  => $source_id,
-		      species_id => $species_id,
-                      dbi        => $dbi,
-		      info_type  =>"DEPENDENT"} );
+    $self->add_xref(
+                     { acc        => $acc,
+                       label      => $symbol,
+                       desc       => $desc,
+                       source_id  => $source_id,
+                       species_id => $species_id,
+                       dbi        => $dbi,
+                       info_type  => "DEPENDENT" } );
 
-    $self->add_xref({ acc        => $acc,
-		      label      => $symbol,
-		      desc       => $desc,
-		      source_id  => $wiki_source_id,
-		      species_id => $species_id,
-                      dbi        => $dbi,
-		      info_type  => "DEPENDENT" } ); #,"From EntrezGene $acc");
+    $self->add_xref(
+                     { acc        => $acc,
+                       label      => $symbol,
+                       desc       => $desc,
+                       source_id  => $wiki_source_id,
+                       species_id => $species_id,
+                       dbi        => $dbi,
+                       info_type  => "DEPENDENT" }
+    );    #,"From EntrezGene $acc");
     $xref_count++;
 
-    my (@syn) = split(/\|/, $data->{'Synonyms'});
-    foreach my $synonym (@syn){
-      if($synonym ne "-"){
-	$self->add_to_syn($acc, $source_id, $synonym, $species_id, $dbi);
-	$syn_count++;
+    my (@syn) = split( /\|/, $data->{'Synonyms'} );
+    foreach my $synonym (@syn) {
+      if ( $synonym ne "-" ) {
+        $self->add_to_syn( $acc, $source_id, $synonym, $species_id,
+                           $dbi );
+        $syn_count++;
       }
     }
 
     $seen{$acc} = 1;
-  }
+  } ## end while ( my $data = $input_file...)
 
-  $input_file->eof or croak "Error parsing file $file: " . $input_file->error_diag();
+  $input_file->eof or
+    croak "Error parsing file $file: " . $input_file->error_diag();
   $eg_io->close();
 
-  print $xref_count." EntrezGene Xrefs added with $syn_count synonyms\n" if $verbose;
+  print $xref_count.
+    " EntrezGene Xrefs added with $syn_count synonyms\n"
+    if $verbose;
 
-  return 0; # success
-}
+  return 0;    # success
+} ## end sub run
 
- 
 1;
