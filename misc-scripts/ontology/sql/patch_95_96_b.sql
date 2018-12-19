b7afb98c2124c201905f504422387154033416e0
Marc Chakiachvili <mchakiachvili@ebi.ac.uk>
2018-12-19 13:10:07 +0000

    Updated typo in patch

--- misc-scripts/ontology/sql/patch_95_96_b.sql
+++ misc-scripts/ontology/sql/patch_95_96_b.sql
@@ -13,7 +13,7 @@
 -- See the License for the specific language governing permissions and
 -- limitations under the License.
 
--- patch_95_96_a.sql
+-- patch_95_96_b.sql
 --
 -- Title: Update schema version.
 --
