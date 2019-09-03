ff0ea1e1a28c89fb6750bef0dfa204469f253e3e
Marek Szuba <marek.szuba@ebi.ac.uk>
2019-09-03 14:40:36 +0100

    HPAParser: Backport further changes from ensembl-xref
    Summary:
     - new "please see the NOTICE file" copyright note
     - use confess() instead of croak()
     - add POD blocks
     - confirm all input rows contain the expected number of columns: the
       header explictly, the data via the use of Text::CSV strict mode
     - streamlined record loop
     - some reformatting
    
    Issue: ENSCORESW-3213

--- misc-scripts/xref_mapping/XrefParser/HPAParser.pm
+++ misc-scripts/xref_mapping/XrefParser/HPAParser.pm
@@ -1,9 +1,8 @@
 
 =head1 LICENSE
 
-Copyright [1999-2015] Wellcome Trust Sanger Institute and the EMBL-European Bioinformatics Institute
-Copyright [2016-2019] EMBL-European Bioinformatics Institute
-
+See the NOTICE file distributed with this work for additional information
+regarding copyright ownership.
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
@@ -18,6 +17,39 @@ limitations under the License.
 
 =cut
 
+=head1 NAME
+
+XrefParser::HPAParser
+
+=head1 DESCRIPTION
+
+This parser will read and creates direct xrefs from a simple comma-delimited file downloaded from the Human Protein Atlas (HPA) database.
+The database contains two types of antibody, their own HPA antibodies and Collaborator antibody (CAB) commercial antibodies.
+
+ data_uri        = http://www.proteinatlas.org/download/xref.php
+
+The columns of the file should be the following:
+
+ 1)  Antibody
+ 2)  Antibody ID
+ 3)  Ensembl Peptide ID
+ 4)  Link (URL)
+
+ Antibody,antibody_id,ensembl_peptide_id,link
+ CAB000001,1,ENSP00000363822,http://www.proteinatlas.org/ENSG00000169083-AR
+ CAB000001,1,ENSP00000379358,http://www.proteinatlas.org/ENSG00000169083-AR
+
+=head1 SYNOPSIS
+
+  my $parser = XrefParser::HPAParser->new($db->dbh);
+  $parser->run({
+    source_id  => 11,
+    species_id => 9606,
+    files      => ["hpa.txt"],
+  });
+
+=cut
+
 package XrefParser::HPAParser;
 
 use strict;
@@ -28,14 +60,16 @@ use Text::CSV;
 
 use parent qw( XrefParser::BaseParser);
 
-# This parser will read direct xrefs from a simple comma-delimited file downloaded from the Human Protein Atlas (HPA) database.
-# The database contains two types of antibody, their own HPA antibodies and Collaborator antibody (CAB) commercial antibodies.
-# The columns of the file should be the following:
-#
-# 1)    Antibody
-# 2)    Antibody ID
-# 3)    Ensembl Peptide ID
-# 4)	Link (URL)
+
+=head2
+The run method does the actual parsing and creation of direct xrefs.
+Parser gets initialized as noted above and run is called from
+Bio::EnsEMBL::Production::Pipeline::Xrefs::ParseSource
+
+my $parser = XrefParser::HPAParser->new($db->dbh);
+$parser->run(...);
+
+=cut
 
 sub run {
   my ( $self, $ref_arg ) = @_;
@@ -45,71 +79,60 @@ sub run {
   my $verbose    = $ref_arg->{verbose} // 0;
   my $dbi        = $ref_arg->{dbi} // $self->dbi;
 
-  croak
-    "Need to pass source_id, species_id, files and rel_file as pairs"
-    unless defined $source_id and
-    defined $species_id and
-    defined $files;
+  if ( ( !defined $source_id ) or
+       ( !defined $species_id ) or
+       ( !defined $files ) ) {
+    confess 'Need to pass source_id, species_id, and files';
+  }
 
   my $file = @{$files}[0];
 
   my $file_io = $self->get_filehandle($file);
-  croak "Could not open $file\n" unless defined $file_io;
+  if ( !defined $file_io ) {
+    confess "Could not open $file\n";
+  }
 
-  my $input_file =
-    Text::CSV->new( { sep_char => ",", empty_is_undef => 1 } ) or
-    croak "Cannot use file $file: " . Text::CSV->error_diag();
+  my $input_file = Text::CSV->new({
+    sep_char       => q{,},
+    empty_is_undef => 1,
+    strict         => 1,
+  }) or confess "Cannot use file $file: " . Text::CSV->error_diag();
 
-  $file_io->getline()
-    ;    # skip header # Antibody,antibody_id,ensembl_peptide_id,link
+  my @expected_columns = qw( Antibody antibody_id ensembl_peptide_id link );
+  my $header           = $input_file->getline($file_io);
 
+  if ( scalar @{$header} != scalar @expected_columns ) {
+    confess "input file $file has an incorrect number of columns";
+  }
+
+  $input_file->column_names(@expected_columns);
   my $parsed_count = 0;
-  while ( my $row = $input_file->getline($file_io) ) {
-    my ( $antibody, $antibody_id, $ensembl_peptide_id, $link );
-    ( $antibody           = $row->[0] ) =~ s/\s*$//;
-    ( $antibody_id        = $row->[1] ) =~ s/\s*$//;
-    ( $ensembl_peptide_id = $row->[2] ) =~ s/\s*$//;
-    ( $link               = $row->[3] ) =~ s/\s*$//;
+  while ( my $data = $input_file->getline_hr($file_io) ) {
 
-    croak
-      sprintf(
-          "Line %d contains has less than two columns.\nParsing failed",
-          1 + $parsed_count )
-      unless defined $antibody and
-      defined $ensembl_peptide_id;
-
-    my $label   = $antibody;
-    my $type    = 'translation';
-    my $version = '1';
-
-    my $xref_id =
-      $self->get_xref( $antibody_id, $source_id, $species_id, $dbi );
-
-    if ( !defined($xref_id) || $xref_id eq q{} ) {
-      $xref_id = $self->add_xref(
-                                  { acc        => $antibody_id,
-                                    version    => $version,
-                                    label      => $label,
-                                    source_id  => $source_id,
-                                    species_id => $species_id,
-                                    dbi        => $dbi,
-                                    info_type  => "DIRECT" } );
-    }
-
-    $self->add_direct_xref( $xref_id, $ensembl_peptide_id, $type,
-                            undef, $dbi );
+    $self->add_to_direct_xrefs({
+      acc        => $data->{'antibody_id'},
+      version    => '1',
+      label      => $data->{'Antibody'},
+      stable_id  => $data->{'ensembl_peptide_id'},
+      type       => 'translation',
+      source_id  => $source_id,
+      species_id => $species_id,
+      info_type  => 'DIRECT'
+    });
 
     ++$parsed_count;
-  } ## end while ( my $row = $input_file...)
+  } ## end while
 
   $input_file->eof or
-    croak "Error parsing file $file: " . $input_file->error_diag();
+    confess "Error parsing file $file: " . $input_file->error_diag();
   $file_io->close();
 
-  printf( "%d direct xrefs succesfully parsed\n", $parsed_count )
-    if $verbose;
+  if ($verbose) {
+    printf( "%d direct xrefs succesfully parsed\n", $parsed_count );
+  }
 
-  return 0;    # success
+  return 0;
 } ## end sub run
+
 
 1;
