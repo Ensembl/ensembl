f0138b313d66696e8f0b71fd721a0ad382280a40
Tiago Grego <tgrego@ebi.ac.uk>
2019-09-12 14:30:06 +0100

    confess for error handling

--- misc-scripts/xref_mapping/XrefParser/VGNCParser.pm
+++ misc-scripts/xref_mapping/XrefParser/VGNCParser.pm
@@ -37,7 +37,7 @@ sub run {
 
 
   if ( (!defined $source_id) or (!defined $species_id) or (!defined $files) ) {
-    croak "Need to pass source_id, species_id and files as pairs";
+    confess "Need to pass source_id, species_id and files as pairs";
   }
 
   my $file = shift @{$files};
@@ -47,7 +47,7 @@ sub run {
   my $file_io = $self->get_filehandle($file);
 
   if ( !defined $file_io ) {
-    croak "Can't open VGNC file $file\n";
+    confess "Can't open VGNC file $file\n";
   }
 
   my $source_name = $self->get_source_name_for_source_id($source_id, $dbi);
@@ -60,7 +60,7 @@ sub run {
   my $input_file = Text::CSV->new({
     sep_char       => "\t",
     empty_is_undef => 1
-  }) or croak "Cannot use file $file: ".Text::CSV->error_diag ();
+  }) or confess "Cannot use file $file: ".Text::CSV->error_diag ();
 
   # header must contain these columns
   my @required_columns = qw(
@@ -79,7 +79,7 @@ sub run {
   # die if some required_column is not in columns
   foreach my $colname (@required_columns) {
     if ( !grep { /$colname/xms } @columns ) {
-      croak "Can't find required column $colname in VGNC file $file\n";
+      confess "Can't find required column $colname in VGNC file $file\n";
     }
   }
 
@@ -116,7 +116,7 @@ sub run {
 
   }
 
-  $input_file->eof or croak "Error parsing file $file: " . $input_file->error_diag();
+  $input_file->eof or confess "Error parsing file $file: " . $input_file->error_diag();
   $file_io->close();
 
   if($verbose){
