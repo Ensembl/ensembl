9acfc4c8afd4bed2da07ac26b713e531d5b2924f
Marek Szuba <marek.szuba@ebi.ac.uk>
2019-10-09 14:34:59 +0100

    JGI_ProteinParser: Backport further changes from ensembl-xref
    Summary:
     - new "please see the NOTICE file" copyright note
     - add POD blocks for all functions
     - enable warnings
    
    Issue: ENSCORESW-3214

--- misc-scripts/xref_mapping/XrefParser/JGI_ProteinParser.pm
+++ misc-scripts/xref_mapping/XrefParser/JGI_ProteinParser.pm
@@ -1,8 +1,7 @@
 =head1 LICENSE
 
-Copyright [1999-2015] Wellcome Trust Sanger Institute and the EMBL-European Bioinformatics Institute
-Copyright [2016-2019] EMBL-European Bioinformatics Institute
-
+See the NOTICE file distributed with this work for additional information
+regarding copyright ownership.
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
@@ -17,16 +16,46 @@ limitations under the License.
 
 =cut
 
+=head1 NAME
+
+XrefParser::JGI_ProteinParser
+
+=head1 DESCRIPTION
+
+Parser for JGI protein files with gene description, FASTA format.
+This inherits from JGI_Parser to provide the sequence type
+
+=head1 SYNOPSIS
+
+  my $parser = XrefParser::JGI_ProteinParser->new($db->dbh);
+  $parser->run({
+    source_id  => 70,
+    species_id => 7719,
+    files      => [ "ciona.prot.fasta.gz" ],
+  });
+
+=cut
 
 package XrefParser::JGI_ProteinParser;
 
 use strict;
+use warnings;
 
 use parent qw( XrefParser::JGI_Parser );
 
-# See JGI_Parser for details
+
+=head2 get_sequence_type
+
+  Arg []     : None
+  Description: Return the type of sequences handled by this specialised parser
+  Return type: Scalar; a string representing the sequence type
+  Caller     : Parent class run method
+
+=cut
 
 sub get_sequence_type {
+  my ( $self ) = @_;
+
   return 'peptide';
 }
 
