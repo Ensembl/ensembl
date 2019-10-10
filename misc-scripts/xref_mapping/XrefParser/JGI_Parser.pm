7e1cf2e7b196d2fa7a381406ba3abf036c2cc939
Marek Szuba <marek.szuba@ebi.ac.uk>
2019-10-10 13:08:15 +0100

    JGI_Parser: Backport further changes from ensembl-xref
    Summary:
     - new "please see the NOTICE file" copyright note
     - use confess() instead of croak()
     - add POD blocks for all functions
     - drop support for JGI 2.0 format, which we do not use in Ensembl
     - restructured the code responsible for extraction of data from input
       records
     - more reformatting and clean-up
    
    Issue: ENSCORESW-3214
    
    foo

--- misc-scripts/xref_mapping/XrefParser/JGI_Parser.pm
+++ misc-scripts/xref_mapping/XrefParser/JGI_Parser.pm
@@ -1,8 +1,7 @@
 =head1 LICENSE
 
-Copyright [1999-2015] Wellcome Trust Sanger Institute and the EMBL-European Bioinformatics Institute
-Copyright [2016-2019] EMBL-European Bioinformatics Institute
-
+See the NOTICE file distributed with this work for additional information
+regarding copyright ownership.
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
@@ -17,122 +16,137 @@ limitations under the License.
 
 =cut
 
+=head1 NAME
+
+XrefParser::JGI_Parser
+
+=head1 DESCRIPTION
+
+Parser for JGI protein files with gene description, FASTA format.
+This is the base class the provides most functionality, subclasses
+just implement the method to set sequence type
+
+=head1 SYNOPSIS
+
+  my $parser = XrefParser::JGI_Parser->new($db->dbh);
+  $parser->run({
+    source_id  => 70,
+    species_id => 7719,
+    files      => [ "ciona.prot.fasta.gz" ],
+  });
+
+=cut
+
 package XrefParser::JGI_Parser;
 
 use strict;
 use warnings;
+
 use Carp;
-use File::Basename;
 
 use parent qw( XrefParser::BaseParser );
 
-# JGI protein file with gene predictons  - FASTA FORMAT
-#
-# This is the parser that provides most functionality, subclasses
-# (JGI_ProteinParser) just set sequence type)
+
+=head2 run
+
+  Arg []     : None
+  Description: Parse input file, extract xrefs and add them to the xref DB
+  Return type: Int; 0 upon success
+  Caller     : An analysis step in the Xref pipeline
+
+=cut
 
 sub run {
+  my ( $self, $ref_arg ) = @_;
 
-  my ($self, $ref_arg) = @_;
-  my $source_id    = $ref_arg->{source_id};
-  my $species_id   = $ref_arg->{species_id};
-  my $files        = $ref_arg->{files};
-  my $verbose      = $ref_arg->{verbose};
-  my $dbi          = $ref_arg->{dbi};
-  $dbi = $self->dbi unless defined $dbi;
+  my $source_id  = $ref_arg->{source_id};
+  my $species_id = $ref_arg->{species_id};
+  my $files      = $ref_arg->{files};
+  my $verbose    = $ref_arg->{verbose} // 0;
+  my $dbi        = $ref_arg->{dbi} // $self->dbi;
 
-  if((!defined $source_id) or (!defined $species_id) or (!defined $files) ){
-    croak "Need to pass source_id, species_id and files as pairs";
+  if ( ( !defined $source_id ) or
+       ( !defined $species_id ) or
+       ( !defined $files ) )
+  {
+    confess 'Need to pass source_id, species_id and files as pairs';
   }
-  $verbose |=0;
 
   my $file = @{$files}[0];
 
-  
-  my $source_name = $self->get_source_name_for_source_id ($source_id, $dbi) ;
-  # the source name defines how to parse the header 
-
-  # different formats for different sources (all have entries in external_db.txt and populate_metadata.sql )
-  # 
-  #
-  # SOURCES  :  cint_jgi_v1  AND cint_aniseed_jgi_v1 
-  # -------------------------------------------------
-  #
-  #  filename :  ciona.prot.fasta 
-  #  >ci0100130001
-  #  MLPIVDFKQCRPSVEASDKEINETAKLLVDALSTVGFAYLKNCGIKKNCRRSQKHRG*MGGVRYLYYPPI
-  #  RVNIPDDEVKRNSIRRSIGYFVFPDDDVVINQPLQFKGDADVPDPVKDPITALKYIQQKLSHTCQNT*
-  # 
-  #
-  #
-  # SOURCES :  cint_jgi_v2  && cint_aniseed_jgi_v2  
-  # ------------------------------------------------
-  #
-  # filename : FM1.aa.fasta
-  #   >jgi|Cioin2|201001|fgenesh3_pm.C_chr_01p000019
-  #  MQQQQQDDLVVKLVLVGDGGVGKTTFVKRHLTGEFEKKYVATLGVEVHPIVFQTQRGRIRFNVWDTAGQE
-  #  DEDDDL*
-  #
-
-  my @xrefs;
+  # the source name defines how to parse the header
+  my $source_name =
+    $self->get_source_name_for_source_id($source_id, $dbi);
 
   my $file_io = $self->get_filehandle($file);
-
   if ( !defined $file_io ) {
     confess "Could not open $file\n";
   }
 
-  IO::Handle->input_record_separator( "\n>");
-  
-  while ( my $record = $file_io->getline() ) {
+  IO::Handle->input_record_separator("\n>");
 
-    next if ($record =~ /^File:/);   # skip header
+  my @xrefs;
 
-    my $xref;
+ RECORD:
+  while ( my $input_data = $file_io->getline() ) {
 
-    my ($header, $sequence) = $record =~ /^>?(.+?)\n([^>]*)/s or croak("Can't parse FASTA entry: $record\n");
-    my $accession = $header;
+    next RECORD if ( $input_data =~ /^File:/x );  # skip header
 
-    # split header in different ways according to source name : 
+    my ( $accession, $sequence ) = ( $input_data =~ /^>?(.+?)\n([^>]*)/xs ) ||
+      confess "Can't parse FASTA entry: $input_data";
 
-    if ($source_name=~m/cint_jgi_v1/) {
+    # split header in different ways according to source name
+    if ( $source_name =~ m/cint_aniseed_.*v1|cint_jgi_v1/x ) {
       # header format is  >ci0100146277
       # JGI 1.0
       # we want accession 146277 from above
-      ($accession = $header) =~s/\w{6}//;
+      $accession =~ s/\w{6}//x;
 
-    } elsif ($source_name=~m/cint_aniseed_.*v1/) {
-      # header format is  >ci0100146277, we want this 
-      # JGI 1.0
-      ($accession = $header) =~s/\w{6}//;
     } else {
-      confess "The source-name specified ($source_name) in the populate_metatable.sql file is\n" .
-              "not matching the different cases specified in JGI_Parser.pm - please\n".
-              "edit the parser \n" ;
+      confess
+        "The source-name specified ($source_name) is not matching\n" .
+        "the different cases specified in JGI_Parser.pm - please\n" .
+        "edit the parser \n";
     }
 
     # make sequence into one long string
-    $sequence =~ s/\n//g;
+    $sequence =~ s/\n//xg;
 
     # build the xref object and store it
-    $xref->{ACCESSION}     = $accession;
-    $xref->{LABEL}         = $accession;
-    $xref->{SEQUENCE}      = $sequence;
-    $xref->{SOURCE_ID}     = $source_id;
-    $xref->{SPECIES_ID}    = $species_id;
-    $xref->{SEQUENCE_TYPE} = $self->get_sequence_type();
+    push @xrefs,
+      { ACCESSION     => $accession,
+        SEQUENCE      => $sequence,
+        SOURCE_ID     => $source_id,
+        SPECIES_ID    => $species_id,
+        SEQUENCE_TYPE => $self->get_sequence_type() };
 
-    push @xrefs, $xref;
-
-  }
+  } ## end while ( my $input_data = $file_io...)
 
   $file_io->close();
-  IO::Handle->input_record_separator( "\n");
-  print scalar(@xrefs) . " JGI_ xrefs succesfully parsed\n" if($verbose);
+  IO::Handle->input_record_separator("\n");
 
-  $self->upload_xref_object_graphs(\@xrefs, $dbi);
+  $self->upload_xref_object_graphs( \@xrefs, $dbi );
 
-  return 0; # successful
-}
+  if ( $verbose ) {
+    print scalar(@xrefs) . " JGI_ xrefs succesfully parsed\n";
+  }
+
+  return 0;
+} ## end sub run
+
+
+=head2 get_sequence_type
+
+  Arg []     : None
+  Description: Abstract method to guard against calling it in the base class
+
+=cut
+
+sub get_sequence_type {
+  my ( $self ) = @_;
+
+  confess
+'Abstract method, please instantiate derived class providing an implementation';
+} ## end sub get_sequence_type
 
 1;
