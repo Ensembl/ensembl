5f8ce91f2b9d4bfdf038a7c5e013aa27b019af42
ens-carlos <ens-carlos@users.noreply.github.com>
2019-12-17 11:12:26 +0000

    Database schema patch to increase the length of the supported assembly names in the mapping_session table.

---
+++ sql/patch_99_100_c.sql
@@ -0,0 +1,28 @@
+-- Copyright [1999-2015] Wellcome Trust Sanger Institute and the EMBL-European Bioinformatics Institute
+-- Copyright [2016-2019] EMBL-European Bioinformatics Institute
+--
+-- Licensed under the Apache License, Version 2.0 (the "License");
+-- you may not use this file except in compliance with the License.
+-- You may obtain a copy of the License at
+--
+--      http://www.apache.org/licenses/LICENSE-2.0
+--
+-- Unless required by applicable law or agreed to in writing, software
+-- distributed under the License is distributed on an "AS IS" BASIS,
+-- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+-- See the License for the specific language governing permissions and
+-- limitations under the License.
+
+# patch_99_100_c.sql
+#
+# Title: Update mapping_session.old_assembly and mapping_session.new_assembly columns to VARCHAR(80).
+#
+# Description:
+#   Title: Update mapping_session.old_assembly and mapping_session.new_assembly columns to VARCHAR(80).
+
+ALTER TABLE mapping_session MODIFY COLUMN old_assembly VARCHAR(80) NOT NULL DEFAULT '';
+ALTER TABLE mapping_session MODIFY COLUMN new_assembly VARCHAR(80) NOT NULL DEFAULT '';
+
+# Patch identifier
+INSERT INTO meta (species_id, meta_key, meta_value)
+  VALUES (NULL, 'patch', 'patch_99_100_c.sql|alter_mapping_session_assembly_length');
