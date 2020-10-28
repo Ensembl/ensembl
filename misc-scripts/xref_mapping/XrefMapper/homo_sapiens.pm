fc3f7f4e358b113ac50a2ee7eea7234f9e49d261
Magali Ruffier <mr6@ebi.ac.uk>
2020-10-28 16:28:04 +0000

    ENSCORESW-3586: single list of filters for all vertebrate species

--- misc-scripts/xref_mapping/XrefMapper/homo_sapiens.pm
+++ misc-scripts/xref_mapping/XrefMapper/homo_sapiens.pm
@@ -27,50 +27,6 @@ use vars '@ISA';
 @ISA = qw{ XrefMapper::BasicMapper };
 
 
-sub gene_description_filter_regexps {
-
-  return ('^BA\S+\s+\(NOVEL PROTEIN\)\.?',
-	  '^DJ\S+\s+\(NOVEL PROTEIN\)\.?',
-	  '^LOC\d+\s*(PROTEIN)?\.?',
-	  '^Putative uncharacterized protein.*',
-	  '^ORF.*',
-	  '^PROTEIN C\d+ORF\d+\.*',
-	  '\(CLONE \S+\)\s+',
-	  '^BC\d+\_\d+\.?',
-	  '^CGI\-\d+ PROTEIN\.?\;?',
-	  '[0-9A-Z]{10}RIK PROTEIN[ \.]',
-	  'R\d{5}_\d[ \.,].*',
-	  'PROTEIN KIAA\d+[ \.].*',
-	  'RIKEN CDNA [0-9A-Z]{10}[ \.]',
-	  '^\(*HYPOTHETICAL\s+.*',
-	  '^UNKNOWN\s+.*',
-	  '^DKFZP[A-Z0-9]+\s+PROTEIN[\.;]?.*',
-	  '^CHROMOSOME\s+\d+\s+OPEN\s+READING\s+FRAME\s+\d+\.?.*',
-	  '^FKSG\d+\.?.*',
-	  '^HSPC\d+\s+PROTEIN\.?.*',
-	  '^KIAA\d+\s+PROTEIN\.?.*',
-	  '^KIAA\d+\s+GENE\s+PRODUCT\.?.*',
-	  '^HSPC\d+.*',
-	  '^PRO\d+\s+PROTEIN\.?.*',
-	  '^PRO\d+\.?.*',
-	  '^FLJ\d+\s+PROTEIN.*',
-	  '^PRED\d+\s+PROTEIN.*',
-	  '^WUGSC:.*\s+PROTEIN\.?.*',
-	  '^SIMILAR TO GENE.*',
-	  '^SIMILAR TO PUTATIVE[ \.]',
-	  '^SIMILAR TO HYPOTHETICAL.*',
-	  '^SIMILAR TO (KIAA|LOC).*',
-	  '^SIMILAR TO\s+$',
-          '^WUGSC:H_.*',
-          '^\s*\(?PROTEIN\)?\.?\s*$',
-	  '^\s*\(?FRAGMENT\)?\.?\s*$',
-          '^\s*\(?GENE\)?\.?\s*$',
-	  '^\s*\(\s*\)\s*$',
-          '^\s*\(\d*\)\s*[ \.]$');
-
-}
-
-
 sub get_official_name{
    return "HGNC";
 }
--- misc-scripts/xref_mapping/XrefMapper/rattus_norvegicus.pm
+++ misc-scripts/xref_mapping/XrefMapper/rattus_norvegicus.pm
@@ -52,39 +52,4 @@ sub set_gene_descriptions(){
   $display->set_gene_descriptions_from_display_xref()
 }
 
-sub gene_description_filter_regexps {
-
-  return ('^\(CLONE REM\d+\) ORF \(FRAGMENT\)\.*',
-	  '^ORF\s*\d+\s+PROTEIN\.*',
-	  '\(?[0-9A-Z]{10}RIK PROTEIN\)?[ \.]',
-	  'RIKEN CDNA [0-9A-Z]{10}[ \.;]',
-	  '.*RIKEN FULL-LENGTH ENRICHED LIBRARY.*PRODUCT:',
-	  '.*RIKEN FULL-LENGTH ENRICHED LIBRARY.*',
-	  '\(*HYPOTHETICAL\s+.*',
-	  '^UNKNOWN\s+.*',
-	  'CDNA SEQUENCE\s?,? [A-Z]+\d+[ \.;]',
-	  'CLONE MGC:\d+[ \.;]',
-	  'MGC:\s*\d+[ \.;]',
-	  'HYPOTHETICAL PROTEIN,',
-	  'HYPOTHETICAL PROTEIN \S+[\.;]',
-	  'DNA SEGMENT, CHR.*',
-	  'PROTEIN \S+ HOMOLOG\.?',
-	  '^SIMILAR TO GENE.*',
-	  'SIMILAR TO PUTATIVE[ \.]',
-	  '^SIMILAR TO HYPOTHETICAL.*',
-	  'SIMILAR TO (KIAA|LOC|RIKEN).*',
-	  'SIMILAR TO GENBANK ACCESSION NUMBER\s+\S+',
-	  'SIMILAR TO\s+$',
-          'EXPRESSED SEQUENCE [A-Z]+\d+[ \.;]',
-          'EST [A-Z]+\d+[ \.;]',
-          '^\s*\(FRAGMENT\)\.?\s*$',
-	  '^\s*\(?GENE\)?\.?;?\s*$',
-          '\s*\(?GENE\)?\.?;?',
-          '\s*\(?PRECURSOR\)?\.?;?',
-          '^\s*\(\s*\)\s*$',
-	  '^\s*\(\d*\)\s*[ \.]$',
-          '^\s+\(?\s*$');
-
-}
-
 1;
