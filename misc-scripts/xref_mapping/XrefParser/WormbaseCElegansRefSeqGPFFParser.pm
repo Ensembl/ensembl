279f54964ca46d2a08d28a6f408b615d6ba23f2c
Tamara El Naboulsi <ten@codon-login-01.ebi.ac.uk>
2022-01-05 10:43:27 +0000

    Yearly copyright update - 2022

--- misc-scripts/xref_mapping/XrefParser/WormbaseCElegansRefSeqGPFFParser.pm
+++ misc-scripts/xref_mapping/XrefParser/WormbaseCElegansRefSeqGPFFParser.pm
@@ -1,6 +1,6 @@
 =head1 LICENSE
 
-Copyright [2018-2021] EMBL-European Bioinformatics Institute
+Copyright [2018-2022] EMBL-European Bioinformatics Institute
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
