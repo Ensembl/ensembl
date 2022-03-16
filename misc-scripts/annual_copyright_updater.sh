229bc669fd331f843e99e3a7f1dfb5b80983dea0
Alex Cockburn <ahc@ebi.ac.uk>
2022-03-16 11:47:39 +0000

    Add warnings to annual_copyright_updater script
    annual_copyright_updater script misses some lines that are likely
    to need updating.
    This change adds a check against a looser regex to warn the user
    that there are probably copyright lines that the script was unable
    to replace.

--- misc-scripts/annual_copyright_updater.sh
+++ misc-scripts/annual_copyright_updater.sh
@@ -37,15 +37,32 @@ for var in $dirs; do
   search="^\(.*\)\\[\([0-9]*\)\(-*[0-9]*\)\\] EMBL-European Bioinformatics Institute"
   replacement="\1[\2-$year] EMBL-European Bioinformatics Institute"
 
+  likely_copyright_line=".*[0-9].* EMBL-European Bioinformatics Institute"
+  exception_line="Copyright \\[1999-2015\\] Wellcome Trust Sanger Institute and the EMBL-European Bioinformatics Institute"
+
   echo "About to scan $(pwd) for files to replace '$search' with '$replacement'"
 
-  for file in $(grep -r --files-with-matches "$search" --exclude-dir=.git .); do
-    echo "Replacing date in $file"
+  for file in $(grep -r --files-with-matches "$likely_copyright_line" --exclude-dir=.git .); do
+
+    if [[ $(grep "$search" $file) ]]; then
+      echo "Replacing date in $file"
+    fi
+
     if [ "$(uname)" = "Darwin" ]; then
       LC_CTYPE=C LANG=C sed -i '' -e "s/$search/$replacement/g" $file
     else
       sed --in-place -e "s/$search/$replacement/g" $file
     fi
+
+    # If the line matches the more general $likely_copyright_line, but not the more specific $search regex that we
+    # know how to update, and isn't the $exception_line, then we warn the user that the line is likely a copyright
+    # line that we don't know how to update.
+    grep "$likely_copyright_line" $file | while read -r line ; do
+      if [[ $(echo $line | grep -v "$search") && $(echo $line | grep -v "$exception_line") ]]; then
+        echo "Unable to replace date in $file.  The line is: $line"
+      fi
+    done
+
   done
 
   cd $original_wd
