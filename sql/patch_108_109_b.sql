05a01d6ffac80eabae2cb8538f173a6f18f7da8b
Tamara El Naboulsi <ten@codon-login-01.ebi.ac.uk>
2022-06-09 15:55:58 +0100

    Patch to add new external_db row

---
+++ sql/patch_108_109_b.sql
@@ -0,0 +1,27 @@
+-- Copyright [1999-2015] Wellcome Trust Sanger Institute and the EMBL-European Bioinformatics Institute
+-- Copyright [2016-2022] EMBL-European Bioinformatics Institute
+--
+-- Licensed under the Apache License, Version 2.0 (the "License");
+-- you may not use this file except in compliance with the License.
+-- You may obtain a copy of the License at
+--
+--      http://www.apache.org/licenses/LICENSE-2.0
+--
+-- Unless required by applicable law or agreed to in writing, software
+-- distributed under the License is distributed on an "AS IS" BASIS,
+-- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+-- See the License for the specific language governing permissions and
+-- limitations under the License.
+
+# patch_108_109_b.sql
+#
+# Title: Added external_db
+#
+# Description:
+#   Added xref source GeneCards into external_db
+
+INSERT INTO external_db (db_name, status, priority, db_display_name, type) VALUES ('GeneCards', 'XREF', 50, 'GeneCards', 'MISC');
+
+# patch identifier
+INSERT INTO meta (species_id, meta_key, meta_value)
+  VALUES (NULL, 'patch', 'patch_108_109_b.sql|Added xref source GeneCards into external_db');
