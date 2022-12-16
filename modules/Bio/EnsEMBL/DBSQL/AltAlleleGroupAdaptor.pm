411b6fbf475991de8fe8e79a2b2a50081f6d928b
nwillhoft <70575561+nwillhoft@users.noreply.github.com>
2022-12-16 13:57:12 +0000

    Remove debug print comments

--- modules/Bio/EnsEMBL/DBSQL/AltAlleleGroupAdaptor.pm
+++ modules/Bio/EnsEMBL/DBSQL/AltAlleleGroupAdaptor.pm
@@ -264,19 +264,15 @@ sub fetch_all_by_gene_id {
     $sth->execute();
     $sth->bind_col(1,\$group_id);
     while ( $sth->fetch ) {
-        # print "group id: $group_id\n";
         push( @group_ids, $group_id );
     }
     $sth->finish;
     if (!$@ && $group_id) {
         my @aag;
         foreach my $group (@group_ids) {
-            # print "About to fetch aag object using group id $group\n";
             my $aag = $self->fetch_by_dbID($group);
-            # print "aag object: $aag\n";
             push(@aag, $aag);
         }
-        # print "@aag\n";
         return \@aag;
     }
     return;
