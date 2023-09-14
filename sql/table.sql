ff7b3bc4343fd6539d89684572e7cd5aa7603edb
Natalie Willhoft <nwillhoft@codon-login-02.ebi.ac.uk>
2023-09-14 12:44:18 +0100

    Update meta table schema to default null

--- sql/table.sql
+++ sql/table.sql
@@ -296,7 +296,7 @@ CREATE TABLE IF NOT EXISTS meta (
   meta_id                     INT NOT NULL AUTO_INCREMENT,
   species_id                  INT UNSIGNED DEFAULT 1,
   meta_key                    VARCHAR(40) NOT NULL,
-  meta_value                  VARCHAR(255) NOT NULL,
+  meta_value                  VARCHAR(255) DEFAULT NULL,
 
   PRIMARY   KEY (meta_id),
   UNIQUE    KEY species_key_value_idx (species_id, meta_key, meta_value),
