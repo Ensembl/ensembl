7f2741957adb0108c2809abdc85037952c83b24f
Marc Chakiachvili <mchakiachvili@ebi.ac.uk>
2024-02-07 10:25:44 +0000

    Updated core meta table definition to allow NULL meta_value and extend the meta_key VARCHAR length

--- sql/table.sql
+++ sql/table.sql
@@ -295,8 +295,8 @@ CREATE TABLE IF NOT EXISTS meta (
 
   meta_id                     INT NOT NULL AUTO_INCREMENT,
   species_id                  INT UNSIGNED DEFAULT 1,
-  meta_key                    VARCHAR(40) NOT NULL,
-  meta_value                  VARCHAR(255) NOT NULL,
+  meta_key                    VARCHAR(64) NOT NULL,
+  meta_value                  VARCHAR(255) NULL,
 
   PRIMARY   KEY (meta_id),
   UNIQUE    KEY species_key_value_idx (species_id, meta_key, meta_value),
