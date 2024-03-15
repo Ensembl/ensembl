b4ee97183425f6793d31896e98fa2bfd128e722e
nwillhoft <70575561+nwillhoft@users.noreply.github.com>
2024-03-15 10:34:01 +0000

    Update meta_key length in table SQL file to reflect recent changes

--- sql/table.sql
+++ sql/table.sql
@@ -295,7 +295,7 @@ CREATE TABLE IF NOT EXISTS meta (
 
   meta_id                     INT NOT NULL AUTO_INCREMENT,
   species_id                  INT UNSIGNED DEFAULT 1,
-  meta_key                    VARCHAR(40) NOT NULL,
+  meta_key                    VARCHAR(64) NOT NULL,
   meta_value                  VARCHAR(255) NOT NULL,
 
   PRIMARY   KEY (meta_id),
