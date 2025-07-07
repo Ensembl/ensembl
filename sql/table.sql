673b68233e5bcc23c4184e81fb0d4cef997ee0e0
nwillhoft <nwillhoft@ebi.ac.uk>
2025-07-07 16:20:21 +0100

    Add SQL patch for 115 to 116

--- sql/table.sql
+++ sql/table.sql
@@ -308,13 +308,13 @@ CREATE TABLE IF NOT EXISTS meta (
 # Add schema type and schema version to the meta table.
 INSERT INTO meta (species_id, meta_key, meta_value) VALUES
   (NULL, 'schema_type', 'core'),
-  (NULL, 'schema_version', '115');
+  (NULL, 'schema_version', '116');
 
 # Patches included in this schema file:
 # NOTE: At start of release cycle, remove patch entries from last release.
 # NOTE: Avoid line-breaks in values.
 INSERT INTO meta (species_id, meta_key, meta_value)
-  VALUES (NULL, 'patch', 'patch_114_115_a.sql|schema_version');
+  VALUES (NULL, 'patch', 'patch_115_116_a.sql|schema_version');
 
 
 /**
